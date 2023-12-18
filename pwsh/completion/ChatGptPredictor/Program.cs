c4106605219ab8a05180a36c89d1e9e3fd721eab
Wes Higbee <wes.mcclure@gmail.com>
2023-12-18 12:55:01 -0600

    remove => going diff route w/ python

--- pwsh/completion/ChatGptPredictor/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// https://learn.microsoft.com/en-us/dotnet/api/system.management.automation.subsystem.prediction.icommandpredictor
--- pwsh/completion/ChatGptPredictor/README.md
+++
@@ -1,3 +0,0 @@
-# ChatGPT Predictor
-
-Quick example of using OpenAI's API to generate predictions to complete powershell commands, with multiple possible completions per invocation
