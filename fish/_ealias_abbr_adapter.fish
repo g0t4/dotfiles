07582623a9e788c357001582bf4d014232d9fba2
Wes Higbee <wes.mcclure@gmail.com>
2023-12-24 11:32:51 -0600

    quick hack to load zsh ealiases into abbreviations

---
+++ fish/_ealias_abbr_adapter.fish
@@ -0,0 +1,34 @@
+
+# ealias is a big part of what I use in my dotfiles in terms of zsh customization.... a adapater alone for this would make fish much more usable to test drive it...
+function ealias --description "map ealias to fish abbr(eviation)"
+    # todo if argv has 2 args then no split on =
+    if test (count $argv) -eq 0 || test (count $argv) -gt 2
+        echo "invalid alias definition:"
+        echo "  ealias <aliasname>=<alias_value>"
+        echo "  ealias <aliasname> <alias_value>"
+        return
+    end
+
+    if test (count $argv) -eq 2
+        # ealias foo bar
+        set aliasname $argv[1]
+        set alias_value $argv[2]
+        echo "name: $aliasname"
+        echo "value: $alias_value"
+        abbr -a $aliasname $alias_value
+        return
+    end
+
+    # ealias foo=bar
+    set aliasdef $argv[1]
+    # split aliasdef on first = into two parts:
+    # 1. the alias name
+    # 2. the alias definition
+    set aliasname (string split -m 1 = $aliasdef)[1]
+    set alias_value (string split -m 1 = $aliasdef)[2]
+
+    echo "name: $aliasname"
+    echo "value: $alias_value"
+
+    abbr -a $aliasname $alias_value
+end
