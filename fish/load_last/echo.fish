a42231d488e420d6644338638cf08ce6d5ff0faa
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 14:45:24 -0600

    log helpers => WIP

---
+++ fish/load_last/echo.fish
@@ -0,0 +1,19 @@
+function log_info
+    echo (set_color blue) "[INFO] $argv" (set_color normal)
+end
+
+function log_warn
+    echo (set_color cyan) "[WARN] $argv" (set_color normal)
+end
+
+function log_error
+    echo (set_color red) "[ERROR] $argv" (set_color normal)
+end
+
+function log_md
+    echo $argv | bat --language Markdown --paging never --plain
+end
+
+function log_blankline
+    echo ""
+end
