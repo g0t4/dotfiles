accf1a8180790071674c8aafddac559fcee736b4
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 00:37:21 -0600

    copy over initial config.fish

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -2,3 +2,7 @@
 # fish startup:
 # - https://fishshell.com/docs/current/language.html#configuration
 # - https://fishshell.com/docs/current/tutorial.html#tut-config
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
