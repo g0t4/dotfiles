16907a56c1ead15505b82d555be68f4b41ab69d1
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 03:43:41 -0600

    start splitting out zsh specific

--- zsh/compat_fish/git-aliases.zsh
+++ zsh/compat_fish/git-aliases.zsh
@@ -1,3 +1,5 @@
+# !!!! LOADS WITH ealias adapter to abbr in fish!
+
 # Note: alias collisions to avoid:
 # `go*` for `go` commands
 # `grep` for default grep params
@@ -66,9 +68,6 @@ ealias gmark="git commit -a -m 'mark'"
 ealias gc='git commit -v' # FYI gc=Get-Content in powershell (I am very tempted to overwrite it!) ... I always want a gc command and struggle to find it
 ealias gcv='git commit -v' # keeping around b/c I will need this on pwsh
 ealias gca='git commit -v -a'
-# - w/ message
-ealias gcmsg='git commit -m "' -NoSpaceAfter
-ealias gcam='git commit -a -m "' -NoSpaceAfter
 # - amend
 ealias gc!='git commit -v --amend'
 ealias gcn!='git commit -v --no-edit --amend'
@@ -98,39 +97,6 @@ ealias gcl='git clone --recurse-submodules'
 ealias gf='git fetch'
 ealias gfa='git fetch --all --prune --jobs=10' # * favorite
 
-# log
-## https://git-scm.com/docs/git-log
-#    ! https://git-scm.com/docs/gitrevisions (for how to express revision ranges)
-#       @ = HEAD
-#       {push} = where push to (for branch specified)
-#         HEAD@{push}~1..HEAD       # long form / explicit
-#         @{push}~1..               # short form (force myself to learn syntax)
-#           # cannot use "origin" unless origin/HEAD specified
-#       {upstream} = IIUC where pull from
-#       term: triangular workflow (pull from remote A and push to remote B)
-#       if, not in a triangular workflow (push/pull to same remote) then {push} = {upstream} = {u}
-#         HEAD@{upstream}~1..HEAD   # long form
-#         @{u}~1..                  # shortest form
-#
-# *** USE git config pretty.format to set default style!!!
-ealias glf='git log'
-for i in {1..10}; do ealias gl$i="git log -$i"; done # last N commits # !FISHISSUE => use abbr + regex + func to expand this to any number and not need a loop! # split zsh specific loop out and keep in zsh files only
-# ! fish like: (super fast hack to test it)
-# # gl11 => git log -11 # YEAH!
-# abbr --regex 'gl\d+' --function glX  gl
-# function glX
-#   string replace --regex '^gl' 'git log -' $argv
-# end
-# ! end fish like
-#
-local _unpushed_commits="HEAD@{push}~1..HEAD"
-ealias glo="git log ${_unpushed_commits}"
-ealias glp="git log --patch ${_unpushed_commits}" # include patch (diff)
-for i in {1..10}; do ealias glp$i="git log --patch -$i"; done # last N commits # !FISHISSUE abbr+regex!
-ealias gls="git log --stat ${_unpushed_commits}" # summary of file changes
-for i in {1..10}; do ealias gls$i="git log --stat -$i"; done # last N commits # !FISHISSUE abbr+regex!
-ealias glg="git log --graph ${_unpushed_commits}" # graph of changes
-
 # push
 ealias gp='git push'
 # FYI gpr is a command so be careful # * what about submodules?
@@ -142,12 +108,6 @@ ealias gpf='git push --force'
 ealias gl='git pull'
 ealias glr='git pull --recurse-submodules' # keep separate alias for now as its time consuming to pull multiple submodules
 #
-# tracked branch
-function git_current_branch() {
-  # zsh has smth for this?
-  git rev-parse --abbrev-ref HEAD
-}
-ealias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
 #
 # remotes
 ealias gr='git remote'
@@ -223,23 +183,6 @@ ealias gd="git diff --word-diff=color"
 ealias gdc="git diff --cached --word-diff=color"
 ealias kgdc="git diff --cached" # common typo alias (cmd+k clear => kgst)
 # last commit diff:
-ealias gdlc="git log --patch HEAD~1..HEAD"
-ealias gdlc1="gdlc"
-for i in {2..10}; do ealias "gdlc$i"="git log --patch HEAD~$i..HEAD~$(($i - 1))"; done # !FISHISSUE abbr+regex!
 ealias gdlf='git diff-tree -r HEAD~1 HEAD'
 #
 ealias dsf='diff-so-fancy'
-
-# VCS in general:
-ealias rr='_repo_root'
-# prd = print repo directoy ;) (like pwd)
-alias prd='echo $(rr)' # don't expand this alias! it just returns a path
-function _repo_root() {
-  if git rev-parse --is-inside-work-tree 2>/dev/null  1>/dev/null; then
-    git rev-parse --show-toplevel 2>/dev/null
-  elif hg root 2>/dev/null  1>/dev/null; then
-    hg root 2>/dev/null
-  else
-    pwd
-  fi
-}
---
+++ zsh/universals/3-last/git-aliases-zsh-specific.zsh
@@ -0,0 +1,62 @@
+# - w/ message
+ealias gcmsg='git commit -m "' -NoSpaceAfter
+ealias gcam='git commit -a -m "' -NoSpaceAfter
+
+# log
+## https://git-scm.com/docs/git-log
+#    ! https://git-scm.com/docs/gitrevisions (for how to express revision ranges)
+#       @ = HEAD
+#       {push} = where push to (for branch specified)
+#         HEAD@{push}~1..HEAD       # long form / explicit
+#         @{push}~1..               # short form (force myself to learn syntax)
+#           # cannot use "origin" unless origin/HEAD specified
+#       {upstream} = IIUC where pull from
+#       term: triangular workflow (pull from remote A and push to remote B)
+#       if, not in a triangular workflow (push/pull to same remote) then {push} = {upstream} = {u}
+#         HEAD@{upstream}~1..HEAD   # long form
+#         @{u}~1..                  # shortest form
+#
+# *** USE git config pretty.format to set default style!!!
+ealias glf='git log'
+for i in {1..10}; do ealias gl$i="git log -$i"; done # last N commits # !FISHISSUE => use abbr + regex + func to expand this to any number and not need a loop! # split zsh specific loop out and keep in zsh files only
+# ! fish like: (super fast hack to test it)
+# # gl11 => git log -11 # YEAH!
+# abbr --regex 'gl\d+' --function glX  gl
+# function glX
+#   string replace --regex '^gl' 'git log -' $argv
+# end
+# ! end fish like
+#
+local _unpushed_commits="HEAD@{push}~1..HEAD"
+ealias glo="git log ${_unpushed_commits}"
+ealias glp="git log --patch ${_unpushed_commits}" # include patch (diff)
+for i in {1..10}; do ealias glp$i="git log --patch -$i"; done # last N commits # !FISHISSUE abbr+regex!
+ealias gls="git log --stat ${_unpushed_commits}" # summary of file changes
+for i in {1..10}; do ealias gls$i="git log --stat -$i"; done # last N commits # !FISHISSUE abbr+regex!
+ealias glg="git log --graph ${_unpushed_commits}" # graph of changes
+
+# tracked branch
+function git_current_branch() {
+  # ! can this be rewritten to be interop w/ fish and zsh?
+  git rev-parse --abbrev-ref HEAD
+}
+ealias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
+
+# diff
+ealias gdlc="git log --patch HEAD~1..HEAD"
+ealias gdlc1="gdlc"
+for i in {2..10}; do ealias "gdlc$i"="git log --patch HEAD~$i..HEAD~$(($i - 1))"; done # !FISHISSUE abbr+regex!
+
+# VCS in general:
+ealias rr='_repo_root'
+# prd = print repo directoy ;) (like pwd)
+alias prd='echo $(rr)' # don't expand this alias! it just returns a path
+function _repo_root() {
+  if git rev-parse --is-inside-work-tree 2>/dev/null  1>/dev/null; then
+    git rev-parse --show-toplevel 2>/dev/null
+  elif hg root 2>/dev/null  1>/dev/null; then
+    hg root 2>/dev/null
+  else
+    pwd
+  fi
+}
