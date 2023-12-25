219905e8b4d60c5cba3e8c0cf45531c584ebb9c5
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 01:06:05 -0600

    fish

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -29,6 +29,24 @@ end
 # - man pages for builtins are direct (don't have to use man zshbuiltins)
 # - abbreviations (expand) => equiv of my zsh ealias
 #   - AND aliases (don't expand)
+#   - cursor position in aliases, function to build expansion (dynamic), regex matching expansions
 # - status builtin
 #     status basename/dirname => of current script, AMEN
 # CONS:
+
+
+## TO READ UP ON / USEs
+# - event handlers: https://fishshell.com/docs/current/language.html#event-handlers
+
+## TO HABITUATE
+# - type foo
+
+
+## TMP to get me by while I migrate zsh ealiases:
+abbr ta 'type -a'
+abbr --set-cursor='!' gcmsg 'git commit -m "!"'
+abbr gst 'git status'
+abbr gaa 'git add --all'
+abbr gdc 'git diff --cached'
+abbr gd 'git diff'
+abbr gap 'git add --patch'
