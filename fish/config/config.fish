7e363c5f468adf4a0eca87af7b4c85ec49494c54
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 00:42:25 -0600

    equiv of touch hushlogin for fish

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -3,6 +3,8 @@
 # - https://fishshell.com/docs/current/language.html#configuration
 # - https://fishshell.com/docs/current/tutorial.html#tut-config
 
+set fish_greeting ""
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
