d973e51a3a962a7e54b8c6cd4b7a014230289cd1
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 13:53:47 -0600

    modify ealias fish adapter to parse -g => --position=anywhere ... wow I like arg parsing in fish

--- fish/load_first/_ealias_abbr_adapter.fish
+++ fish/load_first/_ealias_abbr_adapter.fish
@@ -4,6 +4,10 @@ function ealias --description "map ealias to fish abbr(eviation)"
 
     # wow I already like fish scripting way better than zsh/bash!
 
+    set -l options (fish_opt --short=g) # explicit arg specs! ==> same as 'g' but this is clear
+    argparse $options -- $argv # removes matching specs from $argv
+    # PRN warn on -NoSpaceAfter? don't impl this just warn so I know to custom rewrite it for fish
+
     if test (count $argv) -eq 0 || test (count $argv) -gt 2
         echo "invalid alias definition:"
         echo "  ealias <aliasname>=<alias_value>"
@@ -11,26 +15,26 @@ function ealias --description "map ealias to fish abbr(eviation)"
         return
     end
 
+    # PRN use argparse on positionals?
     if test (count $argv) -eq 2
         # ealias foo bar
         set aliasname $argv[1]
         set alias_value $argv[2]
-        # echo "name: $aliasname"
-        # echo "value: $alias_value"
         abbr -a $aliasname $alias_value
-        return
+    else
+        # ealias foo=bar
+        set aliasdef $argv[1]
+        # split aliasdef on first '=':
+        set aliasname (string split -m 1 = $aliasdef)[1]
+        set alias_value (string split -m 1 = $aliasdef)[2]
     end
-
-    # ealias foo=bar
-    set aliasdef $argv[1]
-    # split aliasdef on first = into two parts:
-    # 1. the alias name
-    # 2. the alias definition
-    set aliasname (string split -m 1 = $aliasdef)[1]
-    set alias_value (string split -m 1 = $aliasdef)[2]
 
     # echo "name: $aliasname"
     # echo "value: $alias_value"
+    set -l position command
+    if test $_flag_g
+        set position anywhere
+    end
 
-    abbr -a $aliasname $alias_value
+    abbr --position $position --add $aliasname $alias_value
 end
