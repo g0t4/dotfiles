6c39fbaa59c31a2335bc752096159d69e6ec1073
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 04:53:45 -0600

    switch out rr

--- zsh/compat_fish/files.zsh
+++ zsh/compat_fish/files.zsh
@@ -1,19 +1,19 @@
 ## general cd
-ealias cdr='cd "$(rr)"' # * favorite
+ealias cdr='cd "$(_repo_root)"' # * favorite
 
 ## open
-ealias or='open "$(rr)"'
+ealias or='open "$(_repo_root)"'
 ealias oh='open .'
 
 ####### vscode aliases:
 # no path
-ealias c='code "$(rr)"'
+ealias c='code "$(_repo_root)"'
 ealias cg='code --goto'
 # path relative:
 ealias ch='code .' # * favorite
 ealias chp='code ..'
 # repo relative:
-ealias cr='code "$(rr)"' # * favorite
-ealias crg='code "$(rr)" --goto'
-ealias crg-use='code "$(rr)" --goto FILE:LINE:COL'
+ealias cr='code "$(_repo_root)"' # * favorite
+ealias crg='code "$(_repo_root)" --goto'
+ealias crg-use='code "$(_repo_root)" --goto FILE:LINE:COL'
 # open aliases (similar idea)
