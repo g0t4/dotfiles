a4cf0faf99df42c9a5b259043438e5d6cd92cf28
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 17:18:39 -0600

    reimpl fish

--- fish/load_last/TODO-ansibles-specific.fish
+++ fish/load_last/TODO-ansibles-specific.fish
@@ -1,12 +1,11 @@
-# # pass an option (key) filter => 0 or more matching option names and details of each!
-# ealias aclsn="_ansible-config_options_name_contains"
-# _ansible-config_options_name_contains() {
-#   filter=$@
-#   \ansible-config list | yq "with_entries(select(.key | test(\"(?i).*${filter}.*\")))"
-# }
-# ealias aclsd="_ansible-config_option_details_contains"
-# _ansible-config_option_details_contains() {
-#   key=$1
-#   \ansible-config list | yq "with_entries(select(.. | has(\"$key\")))"
-# }
-
+# pass an option (key) filter => 0 or more matching option names and details of each!
+ealias aclsn="_ansible-config_options_name_contains"
+function _ansible-config_options_name_contains
+    set -l filter $argv
+    command ansible-config list | yq "with_entries(select(.key | test(\"(?i).*$filter.*\")))"
+end
+ealias aclsd="_ansible-config_option_details_contains"
+function _ansible-config_option_details_contains
+    set -l key $1
+    command ansible-config list | yq "with_entries(select(.. | has(\"$key\")))"
+end
