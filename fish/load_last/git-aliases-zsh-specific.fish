1418b271f925364adcb3b834da0fae951a79e778
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 04:40:41 -0600

    impl gdlc loop - cool

--- fish/load_last/git-aliases-zsh-specific.fish
+++ fish/load_last/git-aliases-zsh-specific.fish
@@ -35,10 +35,16 @@ function git_current_branch
 end
 ealias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
 
-# # # diff
-# # ealias gdlc="git log --patch HEAD~1..HEAD"
-# # ealias gdlc1="gdlc"
-# # for i in {2..10}; do ealias "gdlc$i"="git log --patch HEAD~$i..HEAD~$(($i - 1))"; done # !FISHISSUE abbr+regex!
+# diff
+ealias gdlc="git log --patch HEAD~1..HEAD"
+abbr --regex 'gdlc\d+' --function gdlcX _gdlcX
+# ealias gdlc1="gdlc"
+# for i in {2..10}; do ealias "gdlc$i"="git log --patch HEAD~$i..HEAD~$(($i - 1))"; done # !FISHISSUE abbr+regex!
+function gdlcX
+    set -l num (string replace --regex '^gdlc' '' $argv)
+    set -l prev (math $num - 1)
+    echo "git log --patch HEAD~$num..HEAD~$prev"
+end
 
 # # # VCS in general:
 # # ealias rr='_repo_root'
