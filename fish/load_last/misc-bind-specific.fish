7ec07691127b57fa4716a4f572457c5b2f25fad2
Wes Higbee <wes.mcclure@gmail.com>
2023-12-26 13:29:07 -0600

    notes

--- fish/load_last/misc-bind-specific.fish
+++ fish/load_last/misc-bind-specific.fish
@@ -1,7 +1,6 @@
 
 # modify delay to consider if esc key a seq or standalone
-set fish_escape_delay_ms 200 # 30ms is default and way too fast
-
+set fish_escape_delay_ms 200 # 30ms is default and way too fast (ie esc+k is almost impossible to trigger)
 
 function kill_whole_line_and_copy
     # is there a better way to get last entry from kill ring instead of reading buffer (and trim newline) before kill?
@@ -11,4 +10,3 @@ function kill_whole_line_and_copy
 end
 
 bind \ek kill_whole_line_and_copy
-# ! TODO port / review mybindings.plugin.zsh
