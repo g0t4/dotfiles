2e58acce7eec9ba540adbd3c21196a49756b657f
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 21:57:29 -0600

    fixes

--- zsh/universals/3-last/variable-specific.zsh
+++ zsh/universals/3-last/variable-specific.zsh
@@ -1,15 +1,14 @@
 # for quickly finding aliases by their value
-ealias als="alias | bat --language sh -p --color"
+ealias als="alias | bat --language sh -p"
 ealias agr="alias | grep -i" # name or value contain
 # TODO ISSUE - alias command pipes out '' around special chars - so searching on start would need with and without ' single quote - so I am missing aliases that have special chars when matching as is
 ealias agrs="alias | grep -i ^" # name starts with
-ealias agre="alias | grep -i '='" # name ends with # ! FISH set-cursor position!
 
-ealias els="env | bat --language dotenv -p --color"
+ealias els="env | bat --language dotenv -p"
 ealias egr="env | grep -i "
 
 # shell variables names and values
-ealias vls="set | bat --language ini -p --color"
+ealias vls="set | bat --language ini -p"
 ealias vgr="set | grep -i "
-ealias ols="set +o | bat --language sh -p --color"
+ealias ols="set +o | bat --language sh -p"
 ealias ogr="set +o | grep -i "
