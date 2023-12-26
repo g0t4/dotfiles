c6c21ba6e2aedef13d17d063edbe8368e8ed8ed6
Wes Higbee <wes.mcclure@gmail.com>
2023-12-25 20:31:01 -0600

    move

---
+++ zsh/compat_fish/gh.zsh
@@ -0,0 +1,45 @@
+# awesome forking:
+# gh repo fork --remote docker/hub-tool
+#  --clone optional to clone it
+
+# PRN prune unnecessary aliases => I clearly added the kitchen sink
+
+# live docs!
+ealias ghr='gh reference'
+ealias ghh='gh reference'
+
+# gh issue
+ealias ghi='gh issue' # CWD
+ealias ghi='gh issue list' # CWD
+ealias ghir='gh issue list --repo o/r' # CWD
+ealias ghils='gh issue list' # CWD
+ealias ghilsj='gh issue list --json' # CWD
+ealias ghis='gh issue list --search Q' # CWD
+ealias ghisw='gh issue list --web --search Q ' # CWD
+ealias ghiw='gh issue list --web' # CWD
+
+# gh tags
+
+# gs search
+ealias ghs='gh search'
+ealias ghsr='gh search repos'
+ealias ghsro='gh search repos --owner='
+ealias ghsi='gh search issues'
+ealias ghsio='gh search issues --owner='
+ealias ghsir='gh search issues --repo='
+ealias ghsp='gh search prs'
+ealias ghspo='gh search prs --owner='
+ealias ghspr='gh search prs --repo='
+ealias ghsc='gh search code'
+ealias ghsco='gh search code --owner='
+ealias ghscr='gh search code --repo='
+ealias ghscof='gh search code --filename --owner'
+ealias ghscm='gh search commits'
+ealias ghscmo='gh search commits --owner='
+ealias ghscmr='gh search commits --repo='
+
+# gh view
+ealias ghw='gh repo view --web'
+ealias ghb='gh repo view --branch'
+ealias ghwb='gh repo view --web --branch'
+ealias ghv='gh repo view'
