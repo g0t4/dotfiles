47837fd643a9bcebf515defe93c230ba5adf813e
Wes Higbee <wes.mcclure@gmail.com>
2023-12-26 13:21:34 -0600

    copy and kill

--- fish/load_last/misc-bind.fish
+++ fish/load_last/misc-bind.fish
@@ -1,7 +1,14 @@
 
 # modify delay to consider if esc key a seq or standalone
-set fish_escape_delay_ms 100 # 100 ms, 30ms is default and way too fast
+set fish_escape_delay_ms 200 # 30ms is default and way too fast
 
-bind \ek kill-whole-line
-# PRN setup Esc+k to also copy to clipboard? IIRC I do that in zsh
+
+function kill_whole_line_and_copy
+    # is there a better way to get last entry from kill ring instead of reading buffer (and trim newline) before kill?
+    commandline -b | tr -d '\n' | fish_clipboard_copy
+    commandline -f kill-whole-line
+    # without copy to clipboard, have to use yank to paste removed line
+end
+
+bind \ek kill_whole_line_and_copy
 # ! TODO port / review mybindings.plugin.zsh
