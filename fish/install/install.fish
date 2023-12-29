5c9a0892e614a120dc219a074c0daae550ef9bc5
Wes Higbee <wes.mcclure@gmail.com>
2023-12-29 17:08:47 -0600

    fisher install script

---
+++ fish/install/install.fish
@@ -0,0 +1,10 @@
+# install fisher
+# https://github.com/jorgebucaran/fisher
+# PRN version fish_plugins (which fisher mentions as a way to share plugins, but it shows that as if fisher is already installed locally so lets do that w/o fish_plugins)
+# if fisher function not avail then install fisher
+if not functions -q fisher
+  echo "installing fisher"
+  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+end
+
+# install z
