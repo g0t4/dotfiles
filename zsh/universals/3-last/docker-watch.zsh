9d70391dbac53df310c06b5587fdd4ca0792c36e
Wes Higbee <wes.mcclure@gmail.com>
2023-12-30 17:56:20 -0600

    test w/ new desktop-linux default context

--- zsh/universals/3-last/docker-watch.zsh
+++ zsh/universals/3-last/docker-watch.zsh
@@ -3,42 +3,42 @@
 
 ealias diw="__watch_docker_images"
 function __watch_docker_images() {
-    local _context="${1:-default}"
+    local _context="${1:-desktop-linux}"
     watch --no-title -n 0.5 -d docker -c $_context image ls
 }
 
 ealias dcw="__watch_docker_containers"
 function __watch_docker_containers() {
-    local _context="${1:-default}"
+    local _context="${1:-desktop-linux}"
     watch --no-title -n 0.5 -d docker -c $_context container ls
 }
 
 ealias dnew="__watch_docker_networks"
 function __watch_docker_networks() {
-    local _context="${1:-default}"
+    local _context="${1:-desktop-linux}"
     watch --no-title -n 0.5 -d docker -c $_context network ls
 }
 
 ealias dvw="__watch_docker_volumes"
 function __watch_docker_volumes() {
-    local _context="${1:-default}"
+    local _context="${1:-desktop-linux}"
     watch --no-title -n 0.5 -d docker -c $_context volume ls
 }
 
 ealias dstw="__watch_docker_stacks"
 function __watch_docker_stacks() {
-    local _context="${1:-default}"
+    local _context="${1:-desktop-linux}"
     watch --no-title -n 0.5 -d docker -c $_context stack ls
 }
 
 ealias dsvw="__watch_docker_services"
 function __watch_docker_services() {
-    local _context="${1:-default}"
+    local _context="${1:-desktop-linux}"
     watch --no-title -n 0.5 -d docker -c $_context services ls
 }
 
 ealias dnow="__watch_docker_nodes"
 function __watch_docker_nodes() {
-    local _context="${1:-default}"
+    local _context="${1:-desktop-linux}"
     watch --no-title -n 0.5 -d docker -c $_context node ls
 }
