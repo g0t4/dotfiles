e0225e0b83c4bd235568f37d49479a2a4ea46e2b
Wes Higbee <wes.mcclure@gmail.com>
2023-12-30 16:11:20 -0600

    other path mods from my zsh config

---
+++ fish/load_last/paths.fish
@@ -0,0 +1,31 @@
+
+# if not status is-login
+#     return
+# end
+
+# PRN if other scripts need HOMEBREW_PREFIX then move this earlier in startup scripts
+# if homebrew is present, add env vars/PATH
+if test -f /opt/homebrew/bin/brew
+    # apple silicon macs
+    eval $(/opt/homebrew/bin/brew shellenv)
+else if test -f /usr/local/bin/brew
+    # intel macs
+    eval $(/usr/local/bin/brew shellenv)
+end
+
+# export PATH="$HOME/bin:$PATH"
+# export PATH="$HOME/go/bin:$PATH"
+# export PATH="$HOME/.local/bin:$PATH"
+
+# ~/bin
+if test -d "$HOME/bin"
+    export PATH="$HOME/bin:$PATH"
+end
+# ~/go/bin
+if test -d "$HOME/go/bin"
+    export PATH="$HOME/go/bin:$PATH"
+end
+# ~/.local/bin
+if test -d "$HOME/.local/bin"
+    export PATH="$HOME/.local/bin:$PATH"
+end
