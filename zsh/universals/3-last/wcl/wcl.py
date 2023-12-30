491283a935c8be28e3f2324724d65452c6ac49e2
Wes Higbee <wes.mcclure@gmail.com>
2023-12-30 02:31:49 -0600

    copy over my new wcl python impl

---
+++ zsh/universals/3-last/wcl/wcl.py
@@ -0,0 +1,99 @@
+from sys import argv
+import os
+import re
+import subprocess
+
+# https://pypi.org/project/giturlparse/
+#   pip3 install giturlparse
+#   (careful there are several other similar named packages)
+import giturlparse
+
+
+def wcl(url: str):
+
+    parsed = parse_repo(url)
+    repos_dir = os.path.join(os.environ["HOME"], "repos")
+    repo_dir = os.path.join(repos_dir, relative_repo_dir(parsed))
+    org_dir = os.path.dirname(repo_dir)
+
+    # ensure org dir exists (including parents)
+    os.makedirs(org_dir, exist_ok=True)
+
+    ### add dir to z ahead of cloning so I can CD to it while cloning
+    #   or if dir already exists, then add to the stats count for it
+    #
+    # add to zsh's z:
+    z_add_zsh = f"z --add '{repo_dir}'"
+    # zsh -i => interactive which is where I load z command
+    subprocess.run(['zsh', '-il', '-c', z_add_zsh])
+    #
+    # add to fish's z:
+    # fish has __z_add which uses $PWD hence set cwd
+    # fish doesn't need interactive for z to be loaded (installed in functions dir)
+    subprocess.run(['fish', '-c', "__z_add"], cwd=repo_dir)
+    # FYI I had issues w/ auto-venv (calling deactivate) in fish but not zsh, so I am not using interactive for fish and I disabled auto-venv for non-interactive fish shells
+
+    if os.path.isdir(repo_dir):
+        print("repo_dir found, pulling latest")
+        pull = ["git", "-C", repo_dir, "pull"]
+        subprocess.run(pull)
+    else:
+        clone_from = clone_url(parsed)
+        print(f"cloning {clone_from}...")
+        clone = ["git", "clone", "--recurse-submodules", clone_from, repo_dir]
+        subprocess.run(clone)
+
+
+def clone_url(parsed) -> str:
+    # prefer ssh for git repos (simple, standard, supports ssh auth), plus I've been using this forever now and it's been great.
+    return f"git@{parsed.domain}:{parsed.owner}/{parsed.repo}"
+
+
+def relative_repo_dir(parsed) -> str:
+    return parsed.platform + "/" + parsed.owner + "/" + parsed.repo
+
+
+def parse_repo(url: str):
+
+    url = url.strip()
+
+    p = giturlparse.parse(url, check_domain=True)
+    if p.valid:
+        return p
+
+    # see if non-url passed and use defaults for github:
+    if not re.search(r"\/", url):
+        # repo name only (no slashes)
+        #     "g0t4" => "github.com:g0t4/{url}"
+        url = f"git@github.com:g0t4/{url}.git"
+    elif re.search(r"\/", url):
+        # org/repo name only (one slash)
+        #     "g0t4/dotfiles" => "github.com:g0t4/dotfiles"
+        url = f"git@github.com:{url}.git"
+
+    p = giturlparse.parse(url)
+    # PRN raise on `not p.valid`
+    return p
+
+    # p.url = 'git@github.com:g0t4/dotfiles'
+    # p.domain = 'github.com'
+    #   p.platform = 'github'
+    # p.owner = 'g0t4'
+    #   p.repo = 'dotfiles'
+    #   p.pathname = 'g0t4/dotfiles'
+    # p._user = 'git'
+    #   p.protocol = 'ssh'
+    # dump p obj nicely formatted:
+    # pprint.pprint(vars(p))
+    # docs lists attrs nicely:
+    #   https://pypi.org/project/giturlparse/
+    #   https://github.com/nephila/giturlparse
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print("missing repository url")
+        print(" usage: wcl <url>")
+        exit(1)
+    url = argv[1]
+    wcl(url)
---
+++ zsh/universals/3-last/wcl/wcl.tests.py
@@ -0,0 +1,157 @@
+import unittest
+from wcl import parse_repo, relative_repo_dir, clone_url
+
+
+class TestParseGeneral(unittest.TestCase):
+
+    def test_https_dotgit_suffix(self):
+        # .git suffix shown in the various "clone" popups (confirmed: github,bitbucket,gitlab)
+        parsed = parse_repo('https://gitlab.com/g0t4/dotfiles.git')
+        self.assertEqual(parsed.domain, 'gitlab.com')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+    def test_space_padding_ignores(self):
+        parsed = parse_repo('  https://gitlab.com/g0t4/dotfiles.git  ')
+        self.assertEqual(parsed.domain, 'gitlab.com')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+
+class TestMapToRepoDir(unittest.TestCase):
+
+    def test_github(self):
+        repo_dir = relative_repo_dir(
+            parse_repo('git@github.com:g0t4/dotfiles.git'))
+        self.assertEqual(repo_dir, 'github/g0t4/dotfiles')
+
+    def test_bitbucket(self):
+        repo_dir = relative_repo_dir(
+            parse_repo('git@bitbucket.org:g0t4/dotfiles.git'))
+        self.assertEqual(repo_dir, 'bitbucket/g0t4/dotfiles')
+
+    def test_gitlab(self):
+        repo_dir = relative_repo_dir(
+            parse_repo('git@gitlab.com:g0t4/dotfiles.git'))
+        self.assertEqual(repo_dir, 'gitlab/g0t4/dotfiles')
+
+    def test_repo_only(self):
+        repo_dir = relative_repo_dir(parse_repo('dotfiles'))
+        self.assertEqual(repo_dir, 'github/g0t4/dotfiles')
+
+    def test_org_repo_only(self):
+        repo_dir = relative_repo_dir(parse_repo('g0t4/dotfiles'))
+        self.assertEqual(repo_dir, 'github/g0t4/dotfiles')
+
+
+class TestNormalizedCloneUrl(unittest.TestCase):
+
+    def test_git_uses_git(self):
+        url = clone_url(parse_repo('git@github.com:g0t4/dotfiles.git'))
+        self.assertEqual(url, 'git@github.com:g0t4/dotfiles')  # drop .git
+
+    def test_https_uses_git(self):
+        url = clone_url(parse_repo('https://github.com/g0t4/dotfiles.git'))
+        self.assertEqual(url, 'git@github.com:g0t4/dotfiles')  # drop .git
+
+
+class TestParseGitHub(unittest.TestCase):
+
+    def test_github_urls(self):
+        parsed = parse_repo('git@github.com:g0t4/dotfiles.git')
+        self.assertEqual(parsed.domain, 'github.com')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+    def test_github_https(self):
+        parsed = parse_repo('https://github.com/g0t4/dotfiles')
+        self.assertEqual(parsed.domain, 'github.com')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+    def test_https_includes_branch_and_path(self):
+        # this use case is NICE-TO-HAVE and not mission critical
+        # - I rarely use a file link to trigger a clone
+        # - if it gets too complicated, remove it
+
+        parsed = parse_repo(
+            'https://github.com/g0t4/dotfiles/blob/master/git/linux.gitconfig')
+        self.assertEqual(parsed.domain, 'github.com')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+    # PRN map githubusercontent.com => github
+    # def test_map_githubusercontent_to_github(self):
+    #     parsed = parse_repo(
+    #         'https://raw.githubusercontent.com/g0t4/dotfiles/master/git/linux.gitconfig')
+    #     self.assertEqual(parsed.domain, 'github.com')
+    #     self.assertEqual(parsed.owner, 'g0t4')
+    #     self.assertEqual(parsed.repo, 'dotfiles')
+
+
+class TestParseBitbucket(unittest.TestCase):
+
+    def test_bitbucket_https(self):
+        parsed = parse_repo('https://bitbucket.org/g0t4/dotfiles')
+        self.assertEqual(parsed.domain, 'bitbucket.org')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+    def test_bitbucket_ssh(self):
+        parsed = parse_repo('git@bitbucket.org:g0t4/dotfiles.git')
+        self.assertEqual(parsed.domain, 'bitbucket.org')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+
+class TestParseGitLab(unittest.TestCase):
+
+    def test_gitlab_https(self):
+        parsed = parse_repo('https://gitlab.com/g0t4/dotfiles')
+        self.assertEqual(parsed.domain, 'gitlab.com')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+    def test_gitlab_ssh(self):
+        parsed = parse_repo('git@gitlab.com:g0t4/dotfiles.git')
+        self.assertEqual(parsed.domain, 'gitlab.com')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+
+class TestDefaultToGitHub(unittest.TestCase):
+
+    def test_repoOnly_assumes_github_g0t4(self):
+        parsed = parse_repo('dotfiles')
+        self.assertEqual(parsed.domain, 'github.com')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+    def test_orgRepoOnly_assumes_github(self):
+        parsed = parse_repo('g0t4/dotfiles')
+        self.assertEqual(parsed.domain, 'github.com')
+        self.assertEqual(parsed.owner, 'g0t4')
+        self.assertEqual(parsed.repo, 'dotfiles')
+
+
+class TestParseHuggingFace(unittest.TestCase):
+
+    # IIRC hf.co / huggingface.co are interchangeable (though huggingface.co website shows https+huggingface.co and git+hf.co only)
+    # git@hf.co:microsoft/speecht5_tts
+    # https://huggingface.co/microsoft/speecht5_tts
+    #   PRN https+hf.co?
+    #   PRN git@huggingface.co?
+
+    def test_huggingface(self):
+        parsed = parse_repo('https://huggingface.co/microsoft/speecht5_tts')
+        self.assertEqual(parsed.domain, 'huggingface.co')
+        self.assertEqual(parsed.owner, 'microsoft')
+        self.assertEqual(parsed.repo, 'speecht5_tts')
+
+
+# TODO support hg (mercurial)?
+#  hg.nginx.org/nginx
+#  hg.nginx.org/nginx.org
+
+if __name__ == '__main__':
+    unittest.main()
