e76c70195bde515acb0a2f5af526727abb0fb293
Wes Higbee <wes.mcclure@gmail.com>
2023-12-31 03:32:45 -0600

    split out gi.fish to keep completions bundled together and avoid any eager loading at shell startup

--- fish/load_last/gitignore-wrappers.fish
+++ fish/load_last/gitignore-wrappers.fish
@@ -10,14 +10,3 @@ end
 function gia --wraps gi
     zsh -ic "gia $argv"
 end
-
-complete -c gi --no-files
-
-function _gitignoreio_get_command_list
-    # uncomment to see this is run at completion time:
-    #   echo querying gitignore.io commands
-    zsh -ic "_gitignoreio_get_command_list $argv"
-end
-# _gitignore_git_command_list won't be run until completion time
-complete -c gi -a '(_gitignoreio_get_command_list)'
-# complete -c foo # shows completions for foo
