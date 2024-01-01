3022b6d77a9c2c2626fe752bdb6da35ad59c290c
Wes Higbee <wes.mcclure@gmail.com>
2023-12-31 19:22:23 -0600

    notes

--- fish/load_last/WIP-diff-specific.fish
+++ fish/load_last/WIP-diff-specific.fish
@@ -3,9 +3,14 @@ ealias ic="icdiff -H"
 ealias icr="icdiff --recursive" # diff all files in two dirs
 ealias icg="git-icdiff"
 # configure git-icdiff via git config: git config --global icdiff.options '--line-numbers'
-# thanks to expanding alises + zsh param expansion => it's clear what will happen (ie commands will be re-run)... essentially saves copy/paste previous two commands into icdiff <(cmd1) <(cmd2) format"
-# - i.e. `echo foo` then `echo bar` results in: `icdiff <(echo foo) <(echo foobar)`
+
+# For example:
+# > echo foo\nbar
+# > echo bar
+# > diff_last_two_commands<SPACE> # expands to:
+# > icdiff -L "echo foo\nbar" (echo foo\nbar | psub) -L "echo bar" (echo bar | psub)
 function expand_diff_last_two_commands
+    # TODO validate this works for more complex commands
     set last_two_commands (history | head -n 2)
     set -l command_a $last_two_commands[2]
     set -l command_b $last_two_commands[1]
@@ -15,6 +20,7 @@ function expand_diff_last_two_commands
     # https://fishshell.com/docs/current/cmds/psub.html
 end
 abbr -a diff_last_two_commands --function expand_diff_last_two_commands
+
 # FYI if want these then add back (maybe new file)... I never really have used these in zsh so I don't think they're pivotal here though I could do icdiff (!-2) (!-2) typed out and it would expand out to work in that one off case :)... still more work than other ways
 # # expand !!
 # # based on https://fishshell.com/docs/current/relnotes.html#fish-3-6-0-released-january-7-2023
