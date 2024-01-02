20a2f45fe5ba4f75a3973e87a77a3c52b292cdcf
Wes Higbee <wes.mcclure@gmail.com>
2024-01-01 18:24:40 -0600

    force fish help to show fishshell.com

--- fish/load_last/misc-specific.fish
+++ fish/load_last/misc-specific.fish
@@ -17,3 +17,8 @@ bind \cc 'commandline -r ""'
 # PRN add a binding to clear screen + reset status of last run command
 #    OR modify prompt (type fish_prompt) as it already distinguishes (with bold) if status was carried over from previous command so perhaps I could find a way to hijack that ? 
 #    OR hide status in the prompt (perhaps like zsh I could show non-zero exit code on last line before new prompt?)
+
+
+### FISH HELP ###
+set __fish_help_dir "" # overwrite fish help dir thus forcing the use of https://fishshell.com instead of local files (which I prefer b/c I have highlighting of fishshell.com pages) # ... try it with: `help help` => opens https://fishshell.com/docs/3.6/interactive.html#help
+# see `type help` to find the part of the help command that decides what to open 
