aa5aa414635ded4687c1faf0f650383afa9572dc
Wes Higbee <wes.mcclure@gmail.com>
2024-01-04 01:14:06 -0600

    clipboard

--- fish/load_last/pbcopy-specific.fish
+++ fish/load_last/pbcopy-specific.fish
@@ -1,4 +1,4 @@
-#
+
 # helpers (idea is always use pbcopy/paste -- see below)
 ealias pwdcp="pwd | pbcopy"
 ealias wdcp="pwd | pbcopy"
@@ -6,6 +6,7 @@ ealias wdcp="pwd | pbcopy"
 # alias pbcopy/paste (muscle memory for me)
 alias pbcopy="fish_clipboard_copy"
 alias pbpaste="fish_clipboard_paste"
-# PRN expand pbcopy/paste? (don't hide shell specific mechanism)
+# NOTE fish alias == function (its not expanding)
+#   PRN expand pbcopy/paste? (don't hide shell specific mechanism)
 # fish_*_copy/paste call system specific backend (just like omz's clipcopy/clippaste)
-
+# PRN - if I have issues using these aliases to fish_*_copy/paste on a mac then I can skip these aliases on macs and go back to directly using pbcopy/paste
--- zsh/universals/3-last/pbcopy-specific.zsh
+++ zsh/universals/3-last/pbcopy-specific.zsh
@@ -1,6 +1,3 @@
-## TODOs
-#    - omz clipcopy/paste (make public)
-
 #
 # helpers (idea is always use pbcopy/paste -- see below)
 ealias pwdcp="pwd | pbcopy"
@@ -9,5 +6,7 @@ ealias wdcp="pwd | pbcopy"
 # alias pbcopy/paste (muscle memory for me)
 alias pbcopy="clipcopy" # from omz => works
 alias pbpaste="clippaste"
-# PRN expand pbcopy/paste? (don't hide shell specific mechanism)
+# NOTE alias => not expanding
+#   PRN expand pbcopy/paste? (don't hide shell specific mechanism)
 # omz clipcopy/paste (calls system specific backend: ie pbcopy/paste on mac)
+# PRN - if I have issues using these aliases to clipcopy/paste on a mac then I can skip these aliases on macs and go back to directly using pbcopy/paste
