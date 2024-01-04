c75d00925236f05e66c35a723e52956bc1eb0121
Wes Higbee <wes.mcclure@gmail.com>
2024-01-04 01:27:33 -0600

    move fish specific binding too

--- fish/load_last/misc-specific.fish
+++ fish/load_last/misc-specific.fish
@@ -2,18 +2,6 @@
 # modify delay to consider if esc key a seq or standalone
 set fish_escape_delay_ms 200 # 30ms is default and way too fast (ie esc+k is almost impossible to trigger)
 
-function kill_whole_line_and_copy
-    # is there a better way to get last entry from kill ring instead of reading buffer (and trim newline) before kill?
-    commandline -b | tr -d '\n' | fish_clipboard_copy
-    commandline -f kill-whole-line
-    # without copy to clipboard, have to use yank to paste removed line
-end
-
-bind \ek kill_whole_line_and_copy
-
-# ctrl+c clear command line instead of cancel-commandline (why pollute terminal history to cancel a command!)
-bind \cc 'commandline -r ""'
-
 # PRN add a binding to clear screen + reset status of last run command
 #    OR modify prompt (type fish_prompt) as it already distinguishes (with bold) if status was carried over from previous command so perhaps I could find a way to hijack that ? 
 #    OR hide status in the prompt (perhaps like zsh I could show non-zero exit code on last line before new prompt?)
--- fish/load_last/pbcopy-specific.fish
+++ fish/load_last/pbcopy-specific.fish
@@ -10,3 +10,16 @@ alias pbpaste="fish_clipboard_paste"
 #   PRN expand pbcopy/paste? (don't hide shell specific mechanism)
 # fish_*_copy/paste call system specific backend (just like omz's clipcopy/clippaste)
 # PRN - if I have issues using these aliases to fish_*_copy/paste on a mac then I can skip these aliases on macs and go back to directly using pbcopy/paste
+
+# *** yank+copy binding:
+function kill_whole_line_and_copy
+    # is there a better way to get last entry from kill ring instead of reading buffer (and trim newline) before kill?
+    commandline -b | tr -d '\n' | fish_clipboard_copy
+    commandline -f kill-whole-line
+    # without copy to clipboard, have to use yank to paste removed line
+end
+
+bind \ek kill_whole_line_and_copy
+
+# ctrl+c clear command line instead of cancel-commandline (why pollute terminal history to cancel a command!)
+bind \cc 'commandline -r ""'
