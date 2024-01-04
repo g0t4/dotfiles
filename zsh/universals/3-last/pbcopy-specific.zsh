e2de169adda6cb70d7a82be615cbfa30704a994a
Wes Higbee <wes.mcclure@gmail.com>
2024-01-04 01:21:16 -0600

    move copy related key bindings

--- zsh/universals/3-last/pbcopy-specific.zsh
+++ zsh/universals/3-last/pbcopy-specific.zsh
@@ -10,3 +10,15 @@ alias pbpaste="clippaste"
 #   PRN expand pbcopy/paste? (don't hide shell specific mechanism)
 # omz clipcopy/paste (calls system specific backend: ie pbcopy/paste on mac)
 # PRN - if I have issues using these aliases to clipcopy/paste on a mac then I can skip these aliases on macs and go back to directly using pbcopy/paste
+
+# *** yank+copy binding: meta/esc+k
+_custom_kill_and_copy_buffer() {
+    # clears line and puts it into clipboard (also yank goes to killring)
+    zle kill-buffer
+    echo -n $CUTBUFFER | pbcopy
+}
+
+zle -N _custom_kill_and_copy_buffer
+
+# use meta/esc+k - since ctrl+k is kill alone
+bindkey '\ek' _custom_kill_and_copy_buffer
