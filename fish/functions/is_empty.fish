89342f61acd2a7b07fc39f05912b1049c1b62dc1
Wes Higbee <wes.mcclure@gmail.com>
2024-01-25 01:18:15 -0600

    is_empty helper for readability

---
+++ fish/functions/is_empty.fish
@@ -0,0 +1,14 @@
+# I hate using `if test -n/z` to check for empty strings vs not-empty strings... so lets add a helper function that reads nicely when used in an `if` statement
+
+function is_empty \
+    --argument what \
+    --description "use instead of test -z/n!"
+    if string length --quiet -- $what
+        return 1 # Not empty, return false in terms of emptiness
+    else
+        return 0 # Empty, return true in terms of emptiness
+    end
+end
+
+# PRN write some tests using fishtape
+#   fisher install jorgebucaran/fishtape
