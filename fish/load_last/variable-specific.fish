7161bd8dfcc4be57f1a6c3c5b1896973e59135cb
Wes Higbee <wes.mcclure@gmail.com>
2024-02-13 14:16:15 -0600

    actually lets use just this subset of abbrs => covers most cases

--- fish/load_last/variable-specific.fish
+++ fish/load_last/variable-specific.fish
@@ -5,8 +5,7 @@
 # in fish I am using abbr mostly (and right now not using alias at all though I may go back to it for ealias) so lets search through abbr instead of alias
 ealias als="abbr | bat --language sh -p"
 ealias agr="abbr | grep -i" # name or value contain
-# temp disable agrs b/c it won't work w/o alias funcs right now as abbr output is not so easily parsed for the start of the name of an abbr
-# abbr --add agrs --set-cursor='!' "alias | grep -i 'alias !'" # name starts with (fish shows `alias ` in front of each alias name so use that instead of zsh's ^ to match)
+abbr --add agrs --set-cursor='!' "abbr | grep -i 'abbr -a -- !'" # this won't match all abbr's but will find most of them (i.e. regex won't match)
 
 ealias els="env | bat --language dotenv -p"
 ealias egr="env | grep -i "
