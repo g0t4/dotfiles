3e78d2260f1fdbac544752ab4f366a659c73d5af
Wes Higbee <wes.mcclure@gmail.com>
2024-02-13 14:12:36 -0600

    temp fix for agr using abbr

--- fish/load_last/variable-specific.fish
+++ fish/load_last/variable-specific.fish
@@ -1,10 +1,12 @@
 #
 # TODO modify for fishisms
 
-ealias als="alias | bat --language sh -p"
-ealias agr="alias | grep -i" # name or value contain
-
-abbr --add agrs --set-cursor='!' "alias | grep -i 'alias !'" # name starts with (fish shows `alias ` in front of each alias name so use that instead of zsh's ^ to match)
+# abbr
+# in fish I am using abbr mostly (and right now not using alias at all though I may go back to it for ealias) so lets search through abbr instead of alias
+ealias als="abbr | bat --language sh -p"
+ealias agr="abbr | grep -i" # name or value contain
+# temp disable agrs b/c it won't work w/o alias funcs right now as abbr output is not so easily parsed for the start of the name of an abbr
+# abbr --add agrs --set-cursor='!' "alias | grep -i 'alias !'" # name starts with (fish shows `alias ` in front of each alias name so use that instead of zsh's ^ to match)
 
 ealias els="env | bat --language dotenv -p"
 ealias egr="env | grep -i "
