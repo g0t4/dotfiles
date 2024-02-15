447fc06617aca184ced3aeeac5a153d6d67f975c
Wes Higbee <wes.mcclure@gmail.com>
2024-02-15 12:02:29 -0600

    get rid of unused aliases that I dont forsee ever using and add some zed aliases to test the hype

--- zsh/compat_fish/files.zsh
+++ zsh/compat_fish/files.zsh
@@ -6,14 +6,9 @@ ealias orr='open "$(_repo_root)"' # can't use `or` in fish :)
 ealias oh='open .'
 
 ####### vscode aliases:
-# no path
-ealias c='code "$(_repo_root)"'
-ealias cg='code --goto'
-# path relative:
 ealias ch='code .' # * favorite
-ealias chp='code ..'
-# repo relative:
 ealias cr='code "$(_repo_root)"' # * favorite
-ealias crg='code "$(_repo_root)" --goto'
-ealias crg-use='code "$(_repo_root)" --goto FILE:LINE:COL'
-# open aliases (similar idea)
+
+#### zed
+ealias zh='zed .'
+ealias zr='zed "$(_repo_root)"'
