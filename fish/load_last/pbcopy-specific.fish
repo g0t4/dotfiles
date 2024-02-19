113317ced5fe12b4fc92120fc8dd4114f6d05cc6
Wes Higbee <wes.mcclure@gmail.com>
2024-02-19 02:05:34 -0600

    w00t first pass of copy over SSH => i.e. to yank command line ... or dump a file

--- fish/load_last/pbcopy-specific.fish
+++ fish/load_last/pbcopy-specific.fish
@@ -21,8 +21,21 @@ function kill_whole_line_and_copy
     commandline -b | tr -d '\n' | fish_clipboard_copy
     commandline -f kill-whole-line
     # without copy to clipboard, have to use yank to paste removed line
-    # CONFIRMED fish_clipboard_copy works here on a mac
-    # TODO TEST fish_clipboard_copy works here in WSL
+end
+
+# if SSH => replace fish_clipboard_copy
+if test -n "$SSH_CLIENT"
+    if command -q osc-copy
+        function fish_clipboard_copy
+            # TODO think through this? is this robust? review fish_clipboard_copy
+            # TODO do I wanna have my own wes_clipboard_copy that I use in special places so I am not trying to cover all other scenarios for using fish_clipboard_copy?
+            osc-copy
+            # osc-copy via => pipx install oscclip
+        end
+    end
+    # else other osc copy commands?
+
+    # NOT modifying fish_clipboard_paste b/c I am happy with paste via iterm2/winterm/etc
 end
 
 bind \ek kill_whole_line_and_copy
