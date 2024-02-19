0f5bf58de3533b56d198b4fb4e29f4418d99c5ad
Wes Higbee <wes.mcclure@gmail.com>
2024-02-19 13:58:38 -0600

    tar ealias

--- zsh/compat_fish/files.zsh
+++ zsh/compat_fish/files.zsh
@@ -15,3 +15,10 @@ ealias zr='zed "$(_repo_root)"'
 
 # z
 ealias zx='z -x'
+
+# tar:
+ealias tarx='tar -xf' # e(x)tract
+ealias tart='tar -tf' # lis(t) / (t)est
+ealias tarc='tar --xz -cf' # create xz (todo use set-position to put cursor in name that already has .txz extension)
+ealias tarcg='tar --gzip -cf' # create gzip (todo use set-position to put cursor in name that already has .tgz extension)
+ealias tarcb='tar --bzip2 -cf' # create bzip2 (todo use set-position to put cursor in name that already has .tbz2 extension)
