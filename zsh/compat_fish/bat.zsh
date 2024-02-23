7b576707bd79398eeecb4cd7a1439b5503e23f40
Wes Higbee <wes.mcclure@gmail.com>
2024-02-22 19:54:28 -0600

    bat alises cleanup => add for showing filename and full since plain is my default

--- zsh/compat_fish/bat.zsh
+++ zsh/compat_fish/bat.zsh
@@ -1,8 +1,4 @@
-ealias cat="bat" # let's see how I feel about this! this now works at any command position b/c I fixed when to trigger ealias expansion (not just first word of command)
+ealias cat='bat' # let's see how I feel about this! this now works at any command position b/c I fixed when to trigger ealias expansion (not just first word of command)
 
-ealias batp="bat --plain" # -p also
-  ealias batpp="bat --plain --paging never" # same as --style=plain --pager=never
-
-## diagnostic
-ealias batd="bat --diagnostic" # print diagnostic report for troubleshooting (think brew doctor)
-ealias batcc="bat cache --clear"
+ealias bath='bat --style=header' # == header-filename (i.e. for multi files show names)
+ealias batf='bat --style=full'
