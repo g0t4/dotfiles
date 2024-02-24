eb9df17da5a0071877b07536e4128666bbd40f21
Wes Higbee <wes.mcclure@gmail.com>
2024-02-24 11:05:05 -0600

    eabbr

--- zsh/compat_fish/python.zsh
+++ zsh/compat_fish/python.zsh
@@ -7,45 +7,45 @@
 #   i.e. pip => pip3, python => python3
 #   global interpreter => command3 is ubiquitous AND recommended (for the default 3.X version)
 #   venv => command3 defined (for all v3 venvs!)
-ealias python='python3'
-ealias py='python3'
-ealias pip='pip3'
-ealias pydoc='pydoc3'
-ealias python-config='python3-config'
+eabbr python 'python3'
+eabbr py 'python3'
+eabbr pip 'pip3'
+eabbr pydoc 'pydoc3'
+eabbr python-config 'python3-config'
 
 ### VENV (--clear allows to recreate venv if already dir exists, --upgrade-deps makes sure pip is latest)
-ealias ve='python3.10 -m venv --clear --upgrade-deps'
-ealias vedir='echo $VIRTUAL_ENV'
-ealias veinit='python3.10 -m venv --clear --upgrade-deps .venv && vea' # PRN follow with pip install -r requirements.txt (if req file exists)
-ealias veinitr='python3.10 -m venv --clear --upgrade-deps .venv && vea && pip3 install -r requirements.txt'
+eabbr ve 'python3.10 -m venv --clear --upgrade-deps'
+eabbr vedir 'echo $VIRTUAL_ENV'
+eabbr veinit 'python3.10 -m venv --clear --upgrade-deps .venv && vea' # PRN follow with pip install -r requirements.txt (if req file exists)
+eabbr veinitr 'python3.10 -m venv --clear --upgrade-deps .venv && vea && pip3 install -r requirements.txt'
 # PRN make install requirements.txt conditional on its presence in current dir => i.e. fish abbreviation + function
-ealias veinitl='python3.10 -m venv --clear --upgrade-deps .venv.local && vea'
+eabbr veinitl 'python3.10 -m venv --clear --upgrade-deps .venv.local && vea'
 
 # manually activate/deactivate a venv, remember I have my autovenv plugin that will activate on cd
 # FYI use activate.fish for fish (override is in python-specific.fish)
 # FYI vea is shell specific
-ealias ved='deactivate'
+eabbr ved 'deactivate'
 
 ### pipx ###
-ealias pipxi='pipx install'
-ealias pipxir='pipx install -r requirements.txt'
-ealias pipxip='pipx install --python'
-ealias pipxls='pipx list'
-ealias pipxrp='pipx runpip' # ie to install pypi deps (of primary command), i.e.:
+eabbr pipxi 'pipx install'
+eabbr pipxir 'pipx install -r requirements.txt'
+eabbr pipxip 'pipx install --python'
+eabbr pipxls 'pipx list'
+eabbr pipxrp 'pipx runpip' # ie to install pypi deps (of primary command), i.e.:
 #  pipx install ansible
 #  pipx runpip ansible install toml  # add toml dependency, i.e. used by ansible-inventory --toml
-ealias pipxr='pipx run' # think `npx foo`
+eabbr pipxr 'pipx run' # think `npx foo`
 
 ### PIP ###
-ealias pipls='pip3 list'
-ealias piplo='pip3 list --outdated'
+eabbr pipls 'pip3 list'
+eabbr piplo 'pip3 list --outdated'
 
-ealias pipir='pip3 install -r requirements.txt'
-ealias pipi='pip3 install'
+eabbr pipir 'pip3 install -r requirements.txt'
+eabbr pipi 'pip3 install'
 
-ealias pipup='pip3 install --upgrade '
-ealias pipupp='pip3 install --upgrade pip'
+eabbr pipup 'pip3 install --upgrade '
+eabbr pipupp 'pip3 install --upgrade pip'
 
 # uninstall all packages:
-ealias pipun='pip3 uninstall -y $(pip3 freeze)'
-ealias pipunu='pip3 uninstall -y $(pip3 freeze --user)' # --user site (if applicable)
+eabbr pipun 'pip3 uninstall -y $(pip3 freeze)'
+eabbr pipunu 'pip3 uninstall -y $(pip3 freeze --user)' # --user site (if applicable)
