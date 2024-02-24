3702d8ab6ccf9889d911286eec203f5bdb642fc9
Wes Higbee <wes.mcclure@gmail.com>
2024-02-24 11:04:28 -0600

    eabbr

--- zsh/compat_fish/media.zsh
+++ zsh/compat_fish/media.zsh
@@ -1,39 +1,39 @@
 ### *** exiftool
 
-ealias exiftool="grc exiftool"
+eabbr exiftool "grc exiftool"
 # TODO if I add secondary expansions then need to inline this in them or find a way to recursively expand aliases (does fish do that? with abbr?)
-ealias exifc="grc exiftool -common -duration -AudioFormat -CompressorName" # limit to common metadata that I look for
-ealias exifs="grc exiftool -s" # show tag names instead of descriptions
+eabbr exifc "grc exiftool -common -duration -AudioFormat -CompressorName" # limit to common metadata that I look for
+eabbr exifs "grc exiftool -s" # show tag names instead of descriptions
 
 ### *** ffmpeg et al
 alias ffmpeg="ffmpeg -hide_banner"
 alias ffprobe="ffprobe -hide_banner"
 alias ffplay="ffplay -hide_banner"
 # FYI some of these are reminders when I revisit ffmpeg (i.e. to know what to get help for)
-ealias ff="ffmpeg"
-ealias ffm="ffmpeg"
-ealias ffh="ffmpeg -h full" # full help is far superior to ffmpeg-all which doesn't really show device specific options/devices/filters/formats/etc
-ealias ffhe="ffmpeg -h encoder="
-ealias ffhd="ffmpeg -h decoder="
-ealias ffhmx="ffmpeg -h muxer="
-ealias ffhdx="ffmpeg -h demuxer="
-ealias ffhf="ffmpeg -h filter="
-ealias ffhb="ffmpeg -h bsf="
-ealias ffhp="ffmpeg -h protocol="
+eabbr ff "ffmpeg"
+eabbr ffm "ffmpeg"
+eabbr ffh "ffmpeg -h full" # full help is far superior to ffmpeg-all which doesn't really show device specific options/devices/filters/formats/etc
+eabbr ffhe "ffmpeg -h encoder="
+eabbr ffhd "ffmpeg -h decoder="
+eabbr ffhmx "ffmpeg -h muxer="
+eabbr ffhdx "ffmpeg -h demuxer="
+eabbr ffhf "ffmpeg -h filter="
+eabbr ffhb "ffmpeg -h bsf="
+eabbr ffhp "ffmpeg -h protocol="
 #
 # ffp = ffprobe
-ealias ffp="ffprobe"
-ealias ffph="ffprobe -h full"
+eabbr ffp "ffprobe"
+eabbr ffph "ffprobe -h full"
 # ffpl = ffplay
-ealias ffpl="ffplay"
-ealias ffplh="ffplay -h full"
+eabbr ffpl "ffplay"
+eabbr ffplh "ffplay -h full"
 
 ### *** sips command
-ealias sipsg="sips -g all"
+eabbr sipsg "sips -g all"
 # mostly will use sRGB Profile, if not then at least I can modify this command for a different profile:
 # -o file/dir => instead of in-place update, write the modified image file to a new file/dir
-ealias sipsm="sips --matchTo '/System/Library/ColorSync/Profiles/sRGB Profile.icc' -o new_image_file "
-ealias sipsx="sips -x profile_extracted_here" # extract a profile from an image
+eabbr sipsm "sips --matchTo '/System/Library/ColorSync/Profiles/sRGB Profile.icc' -o new_image_file "
+eabbr sipsx "sips -x profile_extracted_here" # extract a profile from an image
 # embed = insert a profile w/o modifying the raw pixels
 # match = modify the raw pixels to match the profile, and embed that profile too
-ealias sipsf="sips --formats" # list supported formats
+eabbr sipsf "sips --formats" # list supported formats
