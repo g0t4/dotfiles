8ee1b344fac0ce40b1ce9bbdfaf3282a0837b544
Wes Higbee <wes.mcclure@gmail.com>
2024-02-24 11:00:54 -0600

    eabbr

--- zsh/compat_fish/hashicorp.zsh
+++ zsh/compat_fish/hashicorp.zsh
@@ -1,9 +1,9 @@
 ## packer
-ealias pa='packer'
-ealias pai='packer init .'
-ealias pav='packer validate .'
-ealias paf='packer fmt .'
-ealias pab='packer build .'
+eabbr pa 'packer'
+eabbr pai 'packer init .'
+eabbr pav 'packer validate .'
+eabbr paf 'packer fmt .'
+eabbr pab 'packer build .'
 
 ## vagrant
 # https://www.vagrantup.com/docs/experimental
@@ -16,76 +16,76 @@ ealias pab='packer build .'
 # export VAGRANT_BOX_UPDATE_CHECK_DISABLE=1
 # export VAGRANT_PROVIDER=virtualbox
 
-ealias v='vagrant'
-ealias vlsc='vagrant list-commands'
+eabbr v 'vagrant'
+eabbr vlsc 'vagrant list-commands'
 
 ## Vagrantfile
-ealias vi='vagrant init --minimal'
-ealias vv='vagrant validate'
+eabbr vi 'vagrant init --minimal'
+eabbr vv 'vagrant validate'
 
 ## cloud boxes
-ealias vc='vagrant cloud'
-ealias vcs='vagrant cloud search'
-ealias vcb='vagrant cloud box show'
+eabbr vc 'vagrant cloud'
+eabbr vcs 'vagrant cloud search'
+eabbr vcb 'vagrant cloud box show'
 
 ## local (cached) boxes
-ealias vb='vagrant box'
-ealias vbls='vagrant box list -i'
-ealias vba='vagrant box add'
-ealias vbo='vagrant box outdated'
-ealias vbog='vagrant box outdated --global'
-ealias vbu='vagrant box update' # current vagrant project only
-ealias vbub='vagrant box update --box' # independent of project
-ealias vbpr='vagrant box prune --dry-run'
-ealias vbrm='vagrant box remove'
-ealias vbrep='vagrant box repackage'
+eabbr vb 'vagrant box'
+eabbr vbls 'vagrant box list -i'
+eabbr vba 'vagrant box add'
+eabbr vbo 'vagrant box outdated'
+eabbr vbog 'vagrant box outdated --global'
+eabbr vbu 'vagrant box update' # current vagrant project only
+eabbr vbub 'vagrant box update --box' # independent of project
+eabbr vbpr 'vagrant box prune --dry-run'
+eabbr vbrm 'vagrant box remove'
+eabbr vbrep 'vagrant box repackage'
 
 # any vbox VM => vagrant box (generates embedded Vagrantfile too)
-ealias vpack='vagrant package --base ' # last arg is VM name (see vboxmanage list vms)
+eabbr vpack 'vagrant package --base ' # last arg is VM name (see vboxmanage list vms)
 
 ## query
-ealias vst='vagrant status'
-ealias vgst='vagrant global-status --prune'
+eabbr vst 'vagrant status'
+eabbr vgst 'vagrant global-status --prune'
 
 ## VM state
-ealias vu='vagrant up'
-ealias vpv='vagrant provision'
-ealias vh='vagrant halt'
-ealias vrl='vagrant reload'
-ealias vrlp='vagrant reload --provision'
+eabbr vu 'vagrant up'
+eabbr vpv 'vagrant provision'
+eabbr vh 'vagrant halt'
+eabbr vrl 'vagrant reload'
+eabbr vrlp 'vagrant reload --provision'
 #
-ealias vsp='vagrant suspend'
-ealias vspg='vagrant suspend --all-global'
-ealias vrs='vagrant resume'
+eabbr vsp 'vagrant suspend'
+eabbr vspg 'vagrant suspend --all-global'
+eabbr vrs 'vagrant resume'
 
 ## cleanup
-ealias vd='vagrant destroy'
-ealias vdf='vagrant destroy -f'
+eabbr vd 'vagrant destroy'
+eabbr vdf 'vagrant destroy -f'
 
 ## plugins
-ealias vpl='vagrant plugin'
-  ealias vplls='vagrant plugin list'
+eabbr vpl 'vagrant plugin'
+  eabbr vplls 'vagrant plugin list'
 # include local plugins in list
-  ealias vpllsl='vagrant plugin list --local'
-  ealias vplup='vagrant plugin update'
+  eabbr vpllsl 'vagrant plugin list --local'
+  eabbr vplup 'vagrant plugin update'
 # installing plugins
-  ealias vpli='vagrant plugin install'
-  ealias vplil='vagrant plugin install --local'
-  ealias vplun='vagrant plugin uninstall'
+  eabbr vpli 'vagrant plugin install'
+  eabbr vplil 'vagrant plugin install --local'
+  eabbr vplun 'vagrant plugin uninstall'
 
 # connect
-ealias vs='vagrant ssh'
-ealias vsc='vagrant ssh-config'
-ealias vscmd='vagrant ssh --command'
+eabbr vs 'vagrant ssh'
+eabbr vsc 'vagrant ssh-config'
+eabbr vscmd 'vagrant ssh --command'
 # TODO add alias to use ssh-config file that is in .vagrant folder with ssh command without needing ssh-config IIRC
 
 # snapshots
-ealias vsn='vagrant snapshot'
-ealias vsnls='vagrant snapshot list'
+eabbr vsn 'vagrant snapshot'
+eabbr vsnls 'vagrant snapshot list'
 # named snapshots
-ealias vsns='vagrant snapshot save'
-ealias vsnr='vagrant snapshot restore'
-ealias vsnrm='vagrant snapshot delete'
+eabbr vsns 'vagrant snapshot save'
+eabbr vsnr 'vagrant snapshot restore'
+eabbr vsnrm 'vagrant snapshot delete'
 # stack based snapshots
-ealias vsnpu='vagrant snapshot push'
-ealias vsnpo='vagrant snapshot pop'
+eabbr vsnpu 'vagrant snapshot push'
+eabbr vsnpo 'vagrant snapshot pop'
