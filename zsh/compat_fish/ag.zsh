2073d1dcd2d5671405fb719f757a40149041e7cd
Wes Higbee <wes.mcclure@gmail.com>
2024-02-24 10:42:12 -0600

    use eabbr in compat layer

--- zsh/compat_fish/ag.zsh
+++ zsh/compat_fish/ag.zsh
@@ -11,15 +11,15 @@ alias ag='ag --nogroup --color-match "$__color_matching_text"'
 # FYI can defer expand color variable so order of startup files is irrelevant
 # --nogroup => disable grouping to show file/line per match to click to open in vscode (via iterm links)
 
-ealias agi="ag -i"
-ealias agh="ag --hidden" # search hidden files (including vcs ignores)
-ealias agu="ag -u" # unrestricted # by default .gitignore/.hgignore/.ignore are excluded
+eabbr agi 'ag -i'
+eabbr agh 'ag --hidden' # search hidden files (including vcs ignores)
+eabbr agu 'ag -u' # unrestricted # by default .gitignore/.hgignore/.ignore are excluded
 
 # I am used to these params, don't currently need to alias them:
 #  -g and -G myself
 #  -A/-B or -C # num of context lines to show # default = 2 for both
 
-ealias agl="ag -l" # print file name only, not matched content
-ealias agL="ag -L" # print files w/o content match
-ealias agw="ag --word-regexp" # match whole words
-ealias agz="ag --search-zip" # search inside zip files (gz,xz only)
+eabbr agl 'ag -l' # print file name only, not matched content
+eabbr agL 'ag -L' # print files w/o content match
+eabbr agw 'ag --word-regexp' # match whole words
+eabbr agz 'ag --search-zip' # search inside zip files (gz,xz only)
