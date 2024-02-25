dd2541c85fc2e24e9b34314024bdbb8619b5c5d2
Wes Higbee <wes.mcclure@gmail.com>
2024-02-24 18:39:35 -0600

    bifurcate

---
+++ zsh/universals/3-last/ag-specific.zsh
@@ -0,0 +1,25 @@
+# *** ag color options ***
+#   also uses ansi color codes like GREP
+#   options:
+#     --color-line-number # Default is 1;33. (fg: bold, yellow)
+#     --color-match # Default is 30;43. (fg: default, bg: yellow)
+#     --color-path # Default is 1;32. (fg: bold, green)
+#
+
+# FYI colors are defined by fish/zsh respectively in color-specific.{fish,zsh}
+alias ag='ag --nogroup --color-match "$__color_matching_text"'
+# FYI can defer expand color variable so order of startup files is irrelevant
+# --nogroup => disable grouping to show file/line per match to click to open in vscode (via iterm links)
+
+eabbr agi 'ag -i'
+eabbr agh 'ag --hidden' # search hidden files (including vcs ignores)
+eabbr agu 'ag -u' # unrestricted # by default .gitignore/.hgignore/.ignore are excluded
+
+# I am used to these params, don't currently need to alias them:
+#  -g and -G myself
+#  -A/-B or -C # num of context lines to show # default = 2 for both
+
+eabbr agl 'ag -l' # print file name only, not matched content
+eabbr agL 'ag -L' # print files w/o content match
+eabbr agw 'ag --word-regexp' # match whole words
+eabbr agz 'ag --search-zip' # search inside zip files (gz,xz only)
