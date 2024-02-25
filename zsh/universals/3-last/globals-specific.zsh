a787c3b9d899f88f7784d37545840e7fc47718ee
Wes Higbee <wes.mcclure@gmail.com>
2024-02-25 00:43:38 -0600

    format xml

--- zsh/universals/3-last/globals-specific.zsh
+++ zsh/universals/3-last/globals-specific.zsh
@@ -5,6 +5,7 @@ ealias pgr='| grep -i' -g
 ealias pjq='| jq .' -g
 ealias pbat='| bat -l' -g
 ealias pyml='| bat -l yml' -g
+ealias pxml='| bat -l xml' -g
 ealias phelp='| bat -l help' -g
 ealias pini='| bat -pl ini' -g
 ealias psh='| bat -pl sh' -g
