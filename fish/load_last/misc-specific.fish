7e2146e816194f77e105ed1e87212ee3705454ca
Wes Higbee <wes.mcclure@gmail.com>
2024-03-06 02:05:38 -0600

    test drive 'oy' => '-o yaml'

--- fish/load_last/misc-specific.fish
+++ fish/load_last/misc-specific.fish
@@ -160,6 +160,8 @@ if command -q kubectl
     abbr karn 'grc kubectl api-resources --namespaced=true'
     abbr karg 'grc kubectl api-resources --namespaced=false' # (g)lobal
 
+    # TODO how do I feel about this:
+    abbr --position=anywhere -- oy '-o yaml'
     # grc kubectl options
 
     # kubectl alpha
