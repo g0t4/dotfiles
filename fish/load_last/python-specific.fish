891f20d7878cda8f28f65f38d17839d2c280f222
Wes Higbee <wes.mcclure@gmail.com>
2024-03-08 11:24:40 -0600

    reuse vea in abbr expansion

--- fish/load_last/python-specific.fish
+++ fish/load_last/python-specific.fish
@@ -1,1 +1,6 @@
 abbr vea 'source .venv*/bin/activate.fish' # override zsh version's /activate
+
+# add function so this can be embedded in other abbr expansions
+function vea --wraps=source
+    source .venv*/bin/activate.fish
+end
