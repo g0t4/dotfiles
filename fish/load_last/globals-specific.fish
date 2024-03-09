00c732837b96244f6f2b396792bbffd9ba258232
Wes Higbee <wes.mcclure@gmail.com>
2024-03-09 10:22:39 -0600

    pmd

--- fish/load_last/globals-specific.fish
+++ fish/load_last/globals-specific.fish
@@ -1,12 +1,13 @@
-abbr --position anywhere --  pgr '| grep -i'
-abbr --position anywhere --  pjq '| jq .'
 abbr --position anywhere --  pbat '| bat -l'
-abbr --position anywhere --  pyml '| bat -l yml'
-abbr --position anywhere --  pxml '| bat -l xml'
+abbr --position anywhere --  pgr '| grep -i'
 abbr --position anywhere --  phelp '| bat -l help'
 abbr --position anywhere --  pini '| bat -pl ini'
-abbr --position anywhere --  psh '| bat -pl sh'
+abbr --position anywhere --  pjq '| jq .'
+abbr --position anywhere --  pmd '| bat -pl md'
 abbr --position anywhere --  prb '| bat -pl rb'
+abbr --position anywhere --  psh '| bat -pl sh'
+abbr --position anywhere --  pxml '| bat -l xml'
+abbr --position anywhere --  pyml '| bat -l yml'
 
 abbr --position anywhere --  hC '| hexdump -C'
 # i.e.    echo -n $IFS | hexdump -C
