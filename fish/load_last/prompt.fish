e11df438f9cf17aa7c487c50be501b08fbbb9287
Wes Higbee <wes.mcclure@gmail.com>
2024-03-09 09:19:32 -0600

    fix python icon overlapping next character in prompt

--- fish/load_last/prompt.fish
+++ fish/load_last/prompt.fish
@@ -15,7 +15,7 @@ function prompt_login --description 'display user name for the prompt'
 
     # show python icon if in a venv
     if test -n "$VIRTUAL_ENV"
-        echo -n -s (set_color cyan) \ue73c (set_color normal)
+        echo -n -s (set_color cyan) \ue73c (set_color normal) " "
     end
 
     if string match -q "mbp*" $hostname
