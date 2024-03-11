085bfee55f6272782c9af25507667fce39fb98e3
Wes Higbee <wes.mcclure@gmail.com>
2024-03-11 16:44:01 -0500

    use builtin sorting in hub-tool

--- fish/load_last/docker-specific.fish
+++ fish/load_last/docker-specific.fish
@@ -237,7 +237,8 @@ abbr dh "hub-tool"
 # hub-tool repo ls roboxes
 abbr dhr "hub-tool repo ls"
 # hub-tool tag ls roboxes/debian12
-abbr --set-cursor='!' -- dht "hub-tool tag ls --platforms ! | sort" # --all variant?
+abbr dht "hub-tool tag ls --sort=name=desc --platforms" # sort updated/name[=asc/desc] .. name sort sorts by versions such that :8 is before :8.1 is before :8.1.1 which makes sense (:8 ~= :8 "latest", thats commo
+abbr dhtu "hub-tool tag ls --sort=updated=desc --platforms" # FYI updated is default, still make it explicit doesn't hurt (ie if this changes)
 abbr --set-cursor='!' -- dhtj "hub-tool tag ls --format json ! | jq" # fyi json includes --platforms by default # ? or just use pjq abbr instead of | jq and ! set-cursor
 abbr dhti "hub-tool tag inspect"
 
