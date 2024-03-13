f9df6dbb77d97c26cc2aab5f6c153c7a9990b9e8
Wes Higbee <wes.mcclure@gmail.com>
2024-03-13 14:02:14 -0500

    fix

--- zsh/universals/3-last/docker-specific.zsh
+++ zsh/universals/3-last/docker-specific.zsh
@@ -237,7 +237,7 @@ abbr dhr "hub-tool repo ls"
 # hub-tool tag ls roboxes/debian12
 abbr dht "hub-tool tag ls --sort=name=desc --platforms"
 abbr dhtu "hub-tool tag ls --sort=updated=desc --platforms"
-aabbr dhtj "hub-tool tag ls --format json" # fyi json includes --platforms by default
+abbr dhtj "hub-tool tag ls --format json" # fyi json includes --platforms by default
 abbr dhti "hub-tool tag inspect"
 
 # FYI hub APIs:
