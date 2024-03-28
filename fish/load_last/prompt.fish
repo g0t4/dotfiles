59f6d704ef45b8474c01a60b296a01b0a75ed87d
Wes Higbee <wes.mcclure@gmail.com>
2024-03-28 15:22:45 -0500

    notes

--- fish/load_last/prompt.fish
+++ fish/load_last/prompt.fish
@@ -15,6 +15,7 @@ function prompt_login --description 'display user name for the prompt'
 
     # SSH indicator 
     if set -q SSH_CONNECTION
+        # keep in mind, have to update dotfiles for remote machines to pick up changes so this can't ever be assumed to be universal
         echo -n -s (set_color cyan) \ue0a0 (set_color normal) " "
     end
 
