74a9bfe4707b0bcdaaacd75235f3d30db88c8c28
Wes Higbee <wes.mcclure@gmail.com>
2024-04-05 12:51:04 -0500

    notes

--- pwsh/helpers/misc-helpers.ps1
+++ pwsh/helpers/misc-helpers.ps1
@@ -6,3 +6,6 @@ function wcl() {
   & $_python $_wcl_py $args
 
 }
+
+# TODO wrap z command like I did with fish shell
+#   see notes in wcl.py about how to approach this to obviate the need to add the path to z history in wcl.py too
