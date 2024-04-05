a1d5d45fbf07e8a1d8bff6494393bab1e9ddc80a
Wes Higbee <wes.mcclure@gmail.com>
2024-04-05 13:08:56 -0500

    get z_pwsh_impl wrapper started

--- pwsh/helpers/misc-helpers.ps1
+++ pwsh/helpers/misc-helpers.ps1
@@ -10,7 +10,7 @@ function wcl() {
 # TODO wrap z command like I did with fish shell
 #   see notes in wcl.py about how to approach this to obviate the need to add the path to z history in wcl.py too
 #   see misc-specific.fish
-# function z
+# function z_fish_impl
 #     # TLDR = wcl + z
 #     # FYI still uses z fish completions (b/c same name)
 
@@ -31,3 +31,32 @@ function wcl() {
 #         __z $argv
 #     end
 # end
+
+function z_pwsh_impl {
+  # TODO add z_pwsh_impl such that it wraps z command... right now z command (not this one) takes precedence so this doesn't plugin ... and I wanna wait before I fix that... to see if I feel a need for this again in pwsh...
+  # FYI mostly working, but FYI this only detects a github.com URL repo format, not yet using org/repo alone...
+
+  # TLDR = wcl + z
+  # FYI still uses z fish completions (b/c same name)
+
+  if ($args -match "github.com") {
+    # if a repo url then clone and/or cd to it
+
+    # assume all args are compat w/ wcl (i.e. --dry-run)
+    $_path = wcl --path-only @args
+
+    if (Test-Path $_path) {
+      # PRN wcl anyways to get latest?
+      Set-Location $_path
+    }
+    else {
+      wcl @args
+      Set-Location $_path
+    }
+  }
+  else {
+    # call z and pass (spread) same args:
+    z @args
+  }
+
+}
