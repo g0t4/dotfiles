76780ce1d100d116f65bc906e2dd6ca588e3bc6f
Wes Higbee <wes.mcclure@gmail.com>
2024-05-22 23:53:56 -0500

    refactor services

--- zsh/universals/3-last/ask-openai/single.py
+++ zsh/universals/3-last/ask-openai/single.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from os import getenv
 from openai import OpenAI
 from keyrings.cryptfile.cryptfile import CryptFileKeyring
@@ -6,9 +7,56 @@ import sys
 import platform
 
 
+Service = namedtuple('Service', 'service_name account_name base_url model')
+
+def use_groq():
+
+    return Service(
+        service_name='groq',
+        account_name='ask',
+        base_url='https://api.groq.com/openai/v1',
+        model='llama3-70b-8192'
+        # groq https://console.groq.com/docs/models
+        #   llama3-8b-8192, llama3-70b-8192, mixtral-8x7b-32768, gemma-7b-it
+    )
+
+def use_openai():
+
+    # openai https://platform.openai.com/docs/models
+    # gpt4 models: https://platform.openai.com/docs/models/gpt-4-turbo-and-gpt-4
+    # *** gpt4 turbo (adds vision capabilities): # does this add anything else besides vision to the turbo preview?
+    # model = "gpt-4-turbo" # currently => "gpt-4-turbo-2024-04-09" # TODO test this vs preview for my use case
+    # model = "gpt-4-turbo-2024-04-09" # thru Dec 2023
+    # *** gpt4 turbo previews:
+    # ! model = "gpt-4o"  # thru Oct 2023
+    # model = "gpt-4-turbo-preview"  # currently => "gpt-4-0125-preview" (last used before gpt-4o)
+    # model = "gpt-4-0125-preview" # thru Dec 2023 - aka gpt4 turbo
+    # model = "gpt-4-1106-preview", # thru Apr 2023 - aka gpt4 turbo # ! this model is the first I used and it works great (used late 2023/early 2024)
+    #
+    # *** gpt 3.5:
+    # model="gpt-3.5-turbo-1106",
+    # gpt-4 "turbo" and gpt-3.5-turbo are both fast, so use gpt-4 for accuracy (else 3.5 might need to be re-run/fixed which can cost more)
+
+    return Service(
+        service_name='openai',
+        account_name='ask',
+        model='gpt-4o',
+        base_model=None
+    )
+
+def use_lmstudio():
+    return Service(
+        service_name='openai',
+        account_name='ask',
+        base_url="http://localhost:1234/v1",
+        model='gpt-4o'
+    )
+
 def generate_command(context: str):
 
-    use_groq = False
+    # use = use_lmstudio()
+    # use = use_openai()
+    use = use_groq()
 
     if platform.system() == 'Linux':
         # https://pypi.org/project/keyrings.cryptfile/
@@ -27,48 +75,21 @@ def generate_command(context: str):
         kr.keyring_key = getenv("KEYRING_CRYPTFILE_PASSWORD")
         keyring.set_keyring(kr)  # tell keyring to use kr (not other backends, and not try to setup keyring.cryptfile backend instance itself, b/c then it prompts for password)
 
-    service_name = 'groq' if use_groq else 'openai'
-    account_name = 'ask'
-    password = keyring.get_password(service_name, account_name)
+    password = keyring.get_password(use.service_name, use.account_name)
 
     # windows => open Credential Manager => Windows Credentials tab => Generic Credentials section (add new)...  service_name => Internet/NetworkAddress, account_name => username
     # macos => open Keychain Access => kind=app password, (security add-generic-password IIRC)
 
     if password is None:
-        print(f"No password found for {account_name} in {service_name}")
+        print(f"No password found for {use.account_name} in {use.service_name}")
         sys.exit(1)
 
-    # grok base_url https://console.groq.com/docs/openai
-    base_url = "https://api.groq.com/openai/v1" if use_groq else None
-    client = OpenAI(api_key=password, base_url=base_url)
-
-    if use_groq:
-        # groq https://console.groq.com/docs/models
-        #   llama3-8b-8192, llama3-70b-8192, mixtral-8x7b-32768, gemma-7b-it
-        model = "llama3-70b-8192"
-        # wow llama3-* are fast... not sure yet about quality, time will tell
-        # print("Using Groq")
-    else:
-        # openai https://platform.openai.com/docs/models
-        #
-        # gpt4 models: https://platform.openai.com/docs/models/gpt-4-turbo-and-gpt-4
-        # *** gpt4 turbo (adds vision capabilities): # does this add anything else besides vision to the turbo preview?
-        # model = "gpt-4-turbo" # currently => "gpt-4-turbo-2024-04-09" # TODO test this vs preview for my use case
-        # model = "gpt-4-turbo-2024-04-09" # thru Dec 2023
-        # *** gpt4 turbo previews:
-        model = "gpt-4o"  # thru Oct 2023
-        # model = "gpt-4-turbo-preview"  # currently => "gpt-4-0125-preview" (last used before gpt-4o)
-        # model = "gpt-4-0125-preview" # thru Dec 2023 - aka gpt4 turbo
-        # model = "gpt-4-1106-preview", # thru Apr 2023 - aka gpt4 turbo # ! this model is the first I used and it works great (used late 2023/early 2024)
-        #
-        # *** gpt 3.5:
-        # model="gpt-3.5-turbo-1106",
-        # gpt-4 "turbo" and gpt-3.5-turbo are both fast, so use gpt-4 for accuracy (else 3.5 might need to be re-run/fixed which can cost more)
+    client = OpenAI(api_key=password, base_url=use.base_url)
 
     try:
 
         completion = client.chat.completions.create(
-            model=model,
+            model=use.model,
             messages=[{
                 "role": "system",
                 "content": "You are a command line expert. Respond with a single, valid, complete command line. I intend to execute it. No explanation. No markdown blocks"
