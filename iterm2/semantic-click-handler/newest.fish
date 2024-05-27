5de66672867f6cafd8b033d2ca23ac54ab44a3f2
Wes Higbee <wes.mcclure@gmail.com>
2024-05-27 15:57:11 -0500

    notes

--- iterm2/semantic-click-handler/newest.fish
+++ iterm2/semantic-click-handler/newest.fish
@@ -19,10 +19,12 @@
 # *** end docs ***
 #
 # *** FYI I used this value in iterm2 semantic history text box:
-#    $HOME/repos/wes-config/wes-bootstrap/subs/dotfiles/iterm2/semantic-click-handler/newest.fish "\1" "\2" "unused" "unused" "\5"
-#      leave "" around items so a blank value doesn't result in shifting the other args
-#      I had an old note about "" around \1 causing issues b/c its already got "\ " escaing of spaces, but its not causing issues with "" around it so leave unles issues arise
-#      not using \3 \4 and I had an old note about them causing issues so I replaced with "unused" so the other args stay in the same position
+#   $HOME/repos/wes-config/wes-bootstrap/subs/dotfiles/iterm2/semantic-click-handler/newest.fish \1 "\2" "unused" "unused" \5
+#       \1,\5 => iTerm escapes "\ " the paths, so I don't need "", AND if I add "" then logic below sees them as "\ " not " " and then fails (ie test if directory)
+#           PRN if I have further issues with escaping, I should look in iTerm2 source/docs for how it interpolates...
+#       \2 => leave "\2" because if there is no line number then the blank means the args shift left (\3 => \2, \4 => \3, etc)
+#       \3 \4 => unused => not using \3 \4 and I had an old note about them causing issues so I replaced with "unused" so the other args stay in the same position
+#
 
 function call_code
     if test -x /opt/homebrew/bin/code
@@ -48,21 +50,19 @@ set text_before_click $argv[3]
 set text_after_click $argv[4]
 set working_directory $argv[5]
 
-exit 1
-
-if test -d $clicked_path
+if test -d "$clicked_path"
     # if clicked path is a directory then open it in vscode
 
     # PRN change to use finder to open? unsure as I don't usually click open directories
-    call_code $clicked_path
+    call_code "$clicked_path"
 
-    exit 0
+    exit 1
 end
 
 # open vscode scoped to the repo root directory
-set repo_root (git rev-parse --show-toplevel $working_directory)
+set repo_root (git rev-parse --show-toplevel "$working_directory")
 call_code \
-    --goto $clicked_path:$line_number \
-    $repo_root
+    --goto "$clicked_path:$line_number" \
+    "$repo_root"
 
-exit
+exit 1
