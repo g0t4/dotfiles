b70b014f244ea5533a5a4a40523d0cfc0cf86d3e
Wes Higbee <wes.mcclure@gmail.com>
2024-06-02 15:47:51 -0500

    tab completion of repo names

--- fish/load_last/pythons-specific.fish
+++ fish/load_last/pythons-specific.fish
@@ -11,6 +11,9 @@ end
 complete -c wcl --no-files
 complete -c wcl --long-option path-only --description 'Only print the path'
 complete -c wcl --long-option dry-run
+# complete my repository names:
+# gh api /users/g0t4/repos --jq '.[].name' # later use .full_name for other org repos?
+complete -c wcl -a '(gh repo list --json name --jq .[].name --limit 1000)' -f
 
 # PRN complete with gh repo list... with my repos
 # complete -c wcl -a '(gh repo list --json nameWithOwner --jq .[].nameWithOwner --limit 1000)' -f
