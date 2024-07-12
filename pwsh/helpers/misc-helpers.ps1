a872830607a05dbff25ca92e244e4a6433483a2c
Wes Higbee <wes.mcclure@gmail.com>
2024-07-12 00:09:15 -0500

    consolidate

--- pwsh/helpers/misc-helpers.ps1
+++ pwsh/helpers/misc-helpers.ps1
@@ -50,3 +50,35 @@ ealias pxml '| bat -l xml' -Anywhere # shortened
 ealias pyml '| bat -l yml' -Anywhere # shortened
 ealias hC '| hexdump -C' -Anywhere
 # todo more in fish's misc for kubectl command
+
+
+
+# powershell alias helpers
+function Get-AliasByDefinition {
+
+  param (
+    [string] $Definition
+  )
+
+  Get-Alias -Definition $Definition   
+}
+set-alias gald "Get-AliasByDefinition"
+
+function _cd_cmd() {
+  param([string]$cmd) 
+  # change to the directory containing the command
+  Get-Command $cmd -All | `
+    Select-Object -ExpandProperty Source | `
+    Split-Path -Parent | `
+    Set-Location
+}
+set-alias cdcmd _cd_cmd
+
+function _gcm_path() {
+  param([string]$cmd) 
+
+  Get-Command $cmd -All | `
+    Select-Object -ExpandProperty Source 
+}
+
+set-alias gcmp _gcm_path
