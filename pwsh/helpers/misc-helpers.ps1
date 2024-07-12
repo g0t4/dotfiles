8b06bfc749680e25fcb99c3035ce50b1d0a78a5d
Wes Higbee <wes.mcclure@gmail.com>
2024-07-12 00:26:04 -0500

    consolidate

--- pwsh/helpers/misc-helpers.ps1
+++ pwsh/helpers/misc-helpers.ps1
@@ -82,3 +82,16 @@ function _gcm_path() {
 }
 
 set-alias gcmp _gcm_path
+
+
+
+
+# *** clipboard helpers
+# make it appear as if pbcopy/paste are available
+# assumption: this only runs on windows+pwsh
+ealias pbcopy "Set-Clipboard" # macos equiv
+ealias pbpaste "Get-Clipboard" # macos equiv
+ealias pwdcp "pwd | Set-Clipboard" # expand to pwsh equivalent
+ealias wdcp "pwd | Set-Clipboard" # expand to pwsh equivalent
+# FYI look at clipcopy/paste in omz, or fish_*_copy/paste for how other tools wrap system specific backends (ie to make pbcopy/paste an entrypoint in pwsh+win)
+
