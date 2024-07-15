434f2fcd7089372f469133931118a8a6e1e1f9f7
Wes Higbee <wes.mcclure@gmail.com>
2024-07-15 14:11:50 -0500

    split

--- zsh/universals/3-last/ask-openai/vim.py
+++ zsh/universals/3-last/ask-openai/vim.py
@@ -20,7 +20,8 @@ def generate_command(passed_context: str, use: Service):
         Respond with a single, valid vim command line. Their command line will be replaced with your response. So it can be reviewed and executed. 
         No explanation. No markdown. No markdown with backticks ` nor ```. 
 
-        If the user mentions another vim mode (i.e. normal, insert, etc) then if possible return a command to switch to that mode and execute whatever they asked about. For example, if the user asks how to delete a line in normal mode, you could answer `:normal dd`. 
+        If the user mentions another vim mode (i.e. normal, insert, etc) then if possible return a command to switch to that mode and execute whatever they asked about. 
+        For example, if the user asks how to delete a line in normal mode, you could answer `:normal dd`. 
         """)
         # alternative?:   If the user mentions another vim mode (i.e. normal, insert, etc) then your answer doesn't need to be a valid command. For example, you could answer that in normal mode you can use `dd` to delete a line.
 
