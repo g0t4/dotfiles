937804b6ff11fd6e2e8226f1a7733038f2fca9a3
Wes Higbee <wes.mcclure@gmail.com>
2024-08-04 01:53:19 -0600

    llama3.1 groq.com

--- zsh/universals/3-last/ask-openai/services.py
+++ zsh/universals/3-last/ask-openai/services.py
@@ -18,9 +18,11 @@ def use_groq(model: Optional[str] = None):
         name='groq',
         api_key=get_api_key('groq', 'ask'),
         base_url='https://api.groq.com/openai/v1',
-        model=model if model else 'llama3-70b-8192',
+        model=model if model else 'llama-3.1-70b-versatile',
         # groq https://console.groq.com/docs/models
-        #   llama3-8b-8192, llama3-70b-8192, mixtral-8x7b-32768, gemma-7b-it
+        #   llama3-8b-8192, llama3-70b-8192, mixtral-8x7b-32768, gemma-7b-it, gemma2-9b-it
+        #   llama-3.1-405b-reasoning, llama-3.1-70b-versatile, llama-3.1-8b-instant
+        #   llama3-groq-70b-8192-tool-use-preview, llama3-groq-8b-8192-tool-use-preview
     )
 
 
