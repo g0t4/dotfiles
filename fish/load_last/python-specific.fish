49fac3891e911e2409374bf80f351e5d38332b87
Wes Higbee <wes.mcclure@gmail.com>
2024-10-04 17:08:35 -0500

    woops dup

--- fish/load_last/python-specific.fish
+++ fish/load_last/python-specific.fish
@@ -72,34 +72,7 @@ abbr pipupp 'pip3 install --upgrade pip'
 abbr pipun 'pip3 uninstall -y $(pip3 freeze)'
 abbr pipunu 'pip3 uninstall -y $(pip3 freeze --user)' # --user site (if applicable)
 
-function wcl
 
-    set -l _python3 "$WES_DOTFILES/.venv/bin/python3"
-    set -l _script_py "$WES_DOTFILES/zsh/compat_fish/pythons/wcl.py"
-
-    $_python3 $_script_py $argv
-
-end
-
-# completions:
-complete -c wcl --no-files
-complete -c wcl --long-option path-only --description 'Only print the path'
-complete -c wcl --long-option dry-run
-# complete my repository names:
-# gh api /users/g0t4/repos --jq '.[].name' # later use .full_name for other org repos?
-complete -c wcl -a '(gh repo list --json name --jq .[].name --limit 1000)' -f
-
-# PRN complete with gh repo list... with my repos
-# complete -c wcl -a '(gh repo list --json nameWithOwner --jq .[].nameWithOwner --limit 1000)'
-
-function wrc
-
-    set -l _python3 "$WES_DOTFILES/.venv/bin/python3"
-    set -l _script_py "$WES_DOTFILES/zsh/compat_fish/pythons/wrc.py"
-
-    $_python3 $_script_py $argv
-
-end
 
 # *** wcl wrappers
 function wcl
