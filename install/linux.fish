b70a041f021cf7c482213d9219e608c4f9f3c605
Wes Higbee <wes.mcclure@gmail.com>
2024-10-05 22:10:39 -0500

    pynvim

--- install/linux.fish
+++ install/linux.fish
@@ -5,3 +5,6 @@ sudo timedatectl set-timezone 'America/Chicago'
 sudo hostnamectl set-hostname 'foo'
 
 sudo apt install -y python3 python3-pip
+# pynvim for wilder in vim
+sudo apt install -y python3-pynvim
+
