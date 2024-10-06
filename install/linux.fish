9e0dff93d0053befae91ce5b60a44deef2016bfb
Wes Higbee <wes.mcclure@gmail.com>
2024-10-05 23:43:41 -0500

    debian/ubuntu pkgs with comments about each so I can install AIO and yet mark why I want a given pkg

--- install/linux.fish
+++ install/linux.fish
@@ -4,9 +4,33 @@
 sudo timedatectl set-timezone 'America/Chicago'
 sudo hostnamectl set-hostname 'foo'
 
-sudo apt install -y python3 python3-pip
 
-# node for github/copilot.vim
-# pynvim for wilder in vim
-sudo apt install -y python3-pynvim nodejs
+# Define list of packages w/ rationale AIO in the comments
+set -l packages "
+  python3 python3-pip
+  python3-pynvim # for wilder in vim
+  nodejs # for github/copilot.vim
+  eza # IIRC only newer debian/ubuntu versions have this?
+  cargo # treeify
+
+  # networking:
+  avahi-utils  # mDNS
+  httpie
+  jq
+  iputils-ping
+  iproute2
+  net-tools
+  whois
+  dnsutils
+  nmap
+  mtr-tiny # combines traceroute and ping, so not adding traceroute too?
+
+  # tcpdump
+  # wireshark
+  # netcat
+  # socat
+"
+set -l package_list (echo $packages | sed 's/#.*//' | grep -v "^\s*\$" | grep -v "^\s*#.*")
+eval "sudo apt install $package_list"
+# fish does not do variable expansion like bash, i.e. splitting on whitespace... so just build the commad to run and eval it
 
