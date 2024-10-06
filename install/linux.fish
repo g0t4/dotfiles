ce08163b5a331f591bb937865516b1c31bced98e
Wes Higbee <wes.mcclure@gmail.com>
2024-10-05 22:12:55 -0500

    installs

--- install/linux.fish
+++ install/linux.fish
@@ -5,6 +5,8 @@ sudo timedatectl set-timezone 'America/Chicago'
 sudo hostnamectl set-hostname 'foo'
 
 sudo apt install -y python3 python3-pip
+
+# node for github/copilot.vim
 # pynvim for wilder in vim
-sudo apt install -y python3-pynvim
+sudo apt install -y python3-pynvim nodejs
 
