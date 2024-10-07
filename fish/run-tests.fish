6457f5b765c6249f4c897b5d2205c0546e50b035
Wes Higbee <wes.mcclure@gmail.com>
2024-10-06 19:03:39 -0500

    run all tests

---
+++ fish/run-tests.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+# ensure can run from anywhere
+set script_path (status current-filename)
+set script_dir (dirname $script_path)
+
+fishtape $script_dir/functions/*.fish
+
+# PRN figure out what best way is to run all/some fish tests, maybe vscode test runner setup?
