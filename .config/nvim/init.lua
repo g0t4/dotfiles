b35ee2d0e392aa7a187fddcbbce7eeee34bd5473
Wes Higbee <wes.mcclure@gmail.com>
2024-10-11 10:47:33 -0500

    test a context menu for nvim-tree, cool... would nee dto add more actions obviously... me thinks learning key mappings would be best... though a searchable version of g? would also be useful

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -356,7 +356,7 @@ packer.startup(function()
     -- TODO telescope integration => actions menu (https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#creating-an-actions-menu-using-telescope)
     -- FYI `g?` shows help overlay with keymaps for actions
     -- supports mouse too
-    -- TODO map Cmd+B to close, then use Cmd+Shift+E to open and find file but not Toggle version 
+    -- TODO map Cmd+B to close, then use Cmd+Shift+E to open and find file but not Toggle version
 
 
 
@@ -369,6 +369,87 @@ packer.startup(function()
     -- FYI cmd+shift+e => F2 in iterm settings
     vim.api.nvim_set_keymap('n', '<F2>', ':NvimTreeFindFileToggle<CR>', { noremap = true, silent = true }) -- dont want to use ctrl+l anyways, so set this up to stop it
 
+
+    local tree_actions = {
+        {
+            name = "Create node",
+            handler = require("nvim-tree.api").fs.create,
+        },
+        {
+            name = "Remove node",
+            handler = require("nvim-tree.api").fs.remove,
+        },
+        {
+            name = "Trash node",
+            handler = require("nvim-tree.api").fs.trash,
+        },
+        {
+            name = "Rename node",
+            handler = require("nvim-tree.api").fs.rename,
+        },
+        {
+            name = "Fully rename node",
+            handler = require("nvim-tree.api").fs.rename_sub,
+        },
+        {
+            name = "Copy",
+            handler = require("nvim-tree.api").fs.copy.node,
+        },
+
+        -- ... other custom actions you may want to display in the menu
+    }
+
+
+    local function tree_actions_menu(node)
+        local entry_maker = function(menu_item)
+            return {
+                value = menu_item,
+                ordinal = menu_item.name,
+                display = menu_item.name,
+            }
+        end
+
+        local finder = require("telescope.finders").new_table({
+            results = tree_actions,
+            entry_maker = entry_maker,
+        })
+
+        local sorter = require("telescope.sorters").get_generic_fuzzy_sorter()
+
+        local default_options = {
+            finder = finder,
+            sorter = sorter,
+            attach_mappings = function(prompt_buffer_number)
+                local actions = require("telescope.actions")
+
+                -- On item select
+                actions.select_default:replace(function()
+                    local state = require("telescope.actions.state")
+                    local selection = state.get_selected_entry()
+                    -- Closing the picker
+                    actions.close(prompt_buffer_number)
+                    -- Executing the callback
+                    selection.value.handler(node)
+                end)
+
+                -- The following actions are disabled in this example
+                -- You may want to map them too depending on your needs though
+                actions.add_selection:replace(function() end)
+                actions.remove_selection:replace(function() end)
+                actions.toggle_selection:replace(function() end)
+                actions.select_all:replace(function() end)
+                actions.drop_all:replace(function() end)
+                actions.toggle_all:replace(function() end)
+
+                return true
+            end,
+        }
+
+        -- Opening the menu
+        require("telescope.pickers").new({ prompt_title = "Tree menu" }, default_options):find()
+    end
+
+    vim.keymap.set("n", "<C-Space>", tree_actions_menu, { buffer = buffer, noremap = true, silent = true })
 
     -- TODO can I map [shift]+ctrl+tab to move forward/backward through files to edit? (like in vscode)
     --    edit #
