2d31cf05e1013d8bd1ed55ef82e4c7286a739a0f
Wes Higbee <wes.mcclure@gmail.com>
2024-10-11 02:58:50 -0500

    which-key

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -369,6 +369,35 @@ packer.startup(function()
     -- :TSInstallInfo
     --
 
+    -- which-key.nvim
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup {
+                delay = 1000, -- before open (ms)
+
+                plugins = {
+                    presets = {
+                        motions = true, -- show help for motions too => type `d` and wait (although it doesn't show d again for line?)
+                    }
+                }
+
+                -- default configures keymap triggers for every mode
+                --   so when you pause mid key combo, it pops open... not if you rapidly enter the key combo
+                --   also gives you time to look and pick w/o timeout on keys (IIGC timeoutlen limits this)
+                --   would be good to increase delay me thinks, lets wait and see though
+                --        delay = function(ctx)
+                --             return ctx.plugin and 0 or 200
+                --           end,
+
+                --
+                --   pulls desc attr of each map, so set those!
+                -- opts.triggers includes { "<auto>", mode = "nixsotc" },
+                -- optional - for icons - mini.icons or nvim-web-devicons
+            }
+        end
+    }
+
 
     -- highlight just word under cursor:
     use "RRethy/vim-illuminate" -- FYI integrates with treesitter! :TSModuleInfo adds illuminate column
