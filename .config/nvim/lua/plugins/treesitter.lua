9f9579b6a3fb7693afbad0c6c2ea77276f552489
Wes Higbee <wes.mcclure@gmail.com>
2024-10-14 10:10:23 -0500

    fix up treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,9 @@ return {
 
     {
         'nvim-treesitter/nvim-treesitter',
-        -- run = ':TSUpdate',
+        build = ":TSUpdate",
 
-        build = function()
+        config = function()
             -- nvim-treesitter[lua]: Could not create tree-sitter-lua-tmp
             -- mkdir: tree-sitter-lua-tmp: File exists
             -- TODO when do I wanna do this part of tree sitter? this keeps running the same install sync step on every load...  definitely don't want that here
@@ -13,11 +13,15 @@ return {
             require 'nvim-treesitter.configs'.setup {
                 ensure_installed = { "c", "lua", "python", "javascript", "typescript", "html", "css", "json", "yaml", "markdown", "vim" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
                 sync_install = false,
-                auto_install = true,                                                                                                       -- PRN try tree-sitter CLI too, outside of neovim
+                -- auto_install = true,                                                                                                       -- PRN try tree-sitter CLI too, outside of neovim
                 -- ignore_install
                 highlight = {
                     enable = true, -- enable for all
                     disable = {},  -- confirmed TSModuleInfo shows X for these languages
+                },
+                indent = {
+                    enable = true,
+                    disable = {},
                 },
                 -- additional_vim_regex_highlighting = false, -- not having any effect on my regex highlighting... is that intended?
                 -- doesn't look like it's doing anything right now.. no languages are marked as highlighted (nor anything else)
