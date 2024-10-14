4aad32480beb4c592f413275ae815cc8e6a60b7b
Wes Higbee <wes.mcclure@gmail.com>
2024-10-14 10:36:07 -0500

    notes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,14 +6,10 @@ return {
         build = ":TSUpdate",
 
         config = function()
-            -- nvim-treesitter[lua]: Could not create tree-sitter-lua-tmp
-            -- mkdir: tree-sitter-lua-tmp: File exists
-            -- TODO when do I wanna do this part of tree sitter? this keeps running the same install sync step on every load...  definitely don't want that here
-            --
             require 'nvim-treesitter.configs'.setup {
                 ensure_installed = { "c", "lua", "python", "javascript", "typescript", "html", "css", "json", "yaml", "markdown", "vim" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
                 sync_install = false,
-                -- auto_install = true,                                                                                                       -- PRN try tree-sitter CLI too, outside of neovim
+                auto_install = true,                                                                                                       -- auto install on entering buffer (must have tree-sitter CLI, IIUC)
                 -- ignore_install
                 highlight = {
                     enable = true, -- enable for all
@@ -25,6 +21,9 @@ return {
                 },
                 -- additional_vim_regex_highlighting = false, -- not having any effect on my regex highlighting... is that intended?
                 -- doesn't look like it's doing anything right now.. no languages are marked as highlighted (nor anything else)
+                -- TODO folding with treesitter?
+                -- vim.wo.foldmethod = 'expr'
+                -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
             }
             -- TSModuleInfo shows what features (highlight, illuminate[if plugin enabled], indent, incremental_selection)
         end
