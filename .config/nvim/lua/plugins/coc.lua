fcdbf6595229257de491a48088f7201a29ebf02e
Wes Higbee <wes.mcclure@gmail.com>
2024-10-14 14:19:15 -0500

    ok on rwest

--- .config/nvim/lua/plugins/coc.lua
+++ .config/nvim/lua/plugins/coc.lua
@@ -150,16 +150,13 @@ return {
                 imap <S-M-f> <Esc>:call CocAction('format')<CR>a
                 nmap <S-M-f> :call CocAction('format')<CR>
 
-" TODO review rest of suggested COC config (indent when done reviewing):
-
-"" TODO do I want this?
-"augroup mygroup
-"  autocmd!
-"  " Setup formatexpr specified filetype(s)
-"  autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
-"  " Update signature help on jump placeholder
-"  autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-"augroup end
+                augroup mygroup
+                  autocmd!
+                  " Setup formatexpr specified filetype(s)
+                  autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
+                  " Update signature help on jump placeholder
+                  autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+                augroup end
 
                 " Applying code actions to the selected code block
                 " Example: `<leader>aap` for current paragraph
@@ -181,45 +178,45 @@ return {
                 " Run the Code Lens action on the current line
                 nmap <leader>cl  <Plug>(coc-codelens-action)
 
-" Map function and class text objects
-" NOTE: Requires 'textDocument.documentSymbol' support from the language server
-xmap if <Plug>(coc-funcobj-i)
-omap if <Plug>(coc-funcobj-i)
-xmap af <Plug>(coc-funcobj-a)
-omap af <Plug>(coc-funcobj-a)
-xmap ic <Plug>(coc-classobj-i)
-omap ic <Plug>(coc-classobj-i)
-xmap ac <Plug>(coc-classobj-a)
-omap ac <Plug>(coc-classobj-a)
+                " Map function and class text objects
+                " NOTE: Requires 'textDocument.documentSymbol' support from the language server
+                xmap if <Plug>(coc-funcobj-i)
+                omap if <Plug>(coc-funcobj-i)
+                xmap af <Plug>(coc-funcobj-a)
+                omap af <Plug>(coc-funcobj-a)
+                xmap ic <Plug>(coc-classobj-i)
+                omap ic <Plug>(coc-classobj-i)
+                xmap ac <Plug>(coc-classobj-a)
+                omap ac <Plug>(coc-classobj-a)
 
-" Remap <C-f> and <C-b> to scroll float windows/popups
-if has('nvim-0.4.0') || has('patch-8.2.0750')
-  nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-  nnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
-  inoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
-  inoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
-  vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-  vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
-endif
+                " Remap <C-f> and <C-b> to scroll float windows/popups
+                if has('nvim-0.4.0') || has('patch-8.2.0750')
+                  nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+                  nnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+                  inoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
+                  inoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
+                  vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+                  vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+                endif
 
-" Use CTRL-S for selections ranges
-" Requires 'textDocument/selectionRange' support of language server
-nmap <silent> <C-s> <Plug>(coc-range-select)
-xmap <silent> <C-s> <Plug>(coc-range-select)
+                " Use CTRL-S for selections ranges
+                " Requires 'textDocument/selectionRange' support of language server
+                nmap <silent> <C-s> <Plug>(coc-range-select)
+                xmap <silent> <C-s> <Plug>(coc-range-select)
 
-" Add `:Format` command to format current buffer
-command! -nargs=0 Format :call CocActionAsync('format')
+                " Add `:Format` command to format current buffer
+                command! -nargs=0 Format :call CocActionAsync('format')
 
-" Add `:Fold` command to fold current buffer
-command! -nargs=? Fold :call     CocAction('fold', <f-args>)
+                " Add `:Fold` command to fold current buffer
+                command! -nargs=? Fold :call     CocAction('fold', <f-args>)
 
-" Add `:OR` command for organize imports of the current buffer
-command! -nargs=0 OR   :call     CocActionAsync('runCommand', 'editor.action.organizeImport')
+                " Add `:OR` command for organize imports of the current buffer
+                command! -nargs=0 OR   :call     CocActionAsync('runCommand', 'editor.action.organizeImport')
 
-" Add (Neo)Vim's native statusline support
-" NOTE: Please see `:h coc-status` for integrations with external plugins that
-" provide custom statusline: lightline.vim, vim-airline
-set statusline^=%{coc#status()}%{get(b:,'coc_current_function','')}
+                " Add (Neo)Vim's native statusline support
+                " NOTE: Please see `:h coc-status` for integrations with external plugins that
+                " provide custom statusline: lightline.vim, vim-airline
+                set statusline^=%{coc#status()}%{get(b:,'coc_current_function','')}
 
 
             ]])
