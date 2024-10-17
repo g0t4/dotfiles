8b54b5cf89f6c29e3d7f4553d8995d255f6b341b
Wes Higbee <wes.mcclure@gmail.com>
2024-10-17 10:15:54 -0500

    better name

--- .config/nvim/lua/early.lua
+++ .config/nvim/lua/early.lua
@@ -7,7 +7,7 @@
 
 --" Uncomment the following to have Vim jump to the last position when reopening a file
 vim.cmd([[
-    augroup JumpToLastPosition
+    augroup RestoreCursorPosition 
         autocmd!
         autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
     augroup END
