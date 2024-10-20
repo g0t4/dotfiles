ff645e4dd054f5f2633325b290cb35db0bd3fdd1
Wes Higbee <wes.mcclure@gmail.com>
2024-10-19 22:37:23 -0500

    dont enable avante unless > nvim 0.10 =>  friggin ubuntu is a year out of date even on 24.10

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,71 @@
+local avante =
+{
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+
+    -- config docs: https://github.com/yetone/avante.nvim?tab=readme-ov-file#default-setup-configuration
+    opts = {
+        -- FYI defaults to claude, recommends claude too.. I should try both
+        provider = "copilot",
+        -- auto_suggestions_provider = "claude" -- or "copilot" or? which is better try both
+
+        behaviour = {
+            auto_suggestions = false, -- Experimental stage
+            auto_set_highlight_group = true,
+            auto_set_keymaps = true,
+            auto_apply_diff_after_generation = false,
+            support_paste_from_clipboard = false,
+        },
+
+    },
+
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "stevearc/dressing.nvim",      -- for vim.ui.select
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",        -- ui component library
+        --- The below dependencies are optional,
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua",      -- for providers='copilot'
+        {
+            -- support for image pasting (FOR REAL?)
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- recommended settings
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true,
+                    },
+                    -- required for Windows users
+                    use_absolute_path = true,
+                },
+            },
+        },
+        {
+            -- Make sure to set this up properly if you have lazy=true
+            'MeanderingProgrammer/render-markdown.nvim',
+            opts = {
+                file_types = { "markdown", "Avante" },
+            },
+            ft = { "markdown", "Avante" },
+        },
+    },
+}
+
+-- avante requires 0.10+
+local version = vim.version()
+if version.major == 0 and version.minor < 10 then
+    avante = {}
+end
+
 return {
     {
         'github/copilot.vim',
@@ -33,66 +101,5 @@ return {
             ]])
         end,
     },
-
-    {
-        "yetone/avante.nvim",
-        event = "VeryLazy",
-        lazy = false,
-        version = false, -- set this if you want to always pull the latest change
-
-        -- config docs: https://github.com/yetone/avante.nvim?tab=readme-ov-file#default-setup-configuration
-        opts = {
-            -- FYI defaults to claude, recommends claude too.. I should try both
-            provider = "copilot",
-            -- auto_suggestions_provider = "claude" -- or "copilot" or? which is better try both
-
-            behaviour = {
-                auto_suggestions = false, -- Experimental stage
-                auto_set_highlight_group = true,
-                auto_set_keymaps = true,
-                auto_apply_diff_after_generation = false,
-                support_paste_from_clipboard = false,
-            },
-
-        },
-
-        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-        build = "make",
-        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "stevearc/dressing.nvim",      -- for vim.ui.select
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",        -- ui component library
-            --- The below dependencies are optional,
-            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            "zbirenbaum/copilot.lua",      -- for providers='copilot'
-            {
-                -- support for image pasting (FOR REAL?)
-                "HakonHarnes/img-clip.nvim",
-                event = "VeryLazy",
-                opts = {
-                    -- recommended settings
-                    default = {
-                        embed_image_as_base64 = false,
-                        prompt_for_file_name = false,
-                        drag_and_drop = {
-                            insert_mode = true,
-                        },
-                        -- required for Windows users
-                        use_absolute_path = true,
-                    },
-                },
-            },
-            {
-                -- Make sure to set this up properly if you have lazy=true
-                'MeanderingProgrammer/render-markdown.nvim',
-                opts = {
-                    file_types = { "markdown", "Avante" },
-                },
-                ft = { "markdown", "Avante" },
-            },
-        },
-    }
-
+    avante,
 }
