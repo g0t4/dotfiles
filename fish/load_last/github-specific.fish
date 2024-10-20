9b1fa7087530d0e7ad05092a98d3df98c9e04c78
Wes Higbee <wes.mcclure@gmail.com>
2024-10-20 09:16:42 -0500

    depoliticize, sick of this crap

--- fish/load_last/github-specific.fish
+++ fish/load_last/github-specific.fish
@@ -52,3 +52,52 @@ function __gh_repo_create_clone_with_ignores
     commit_gitignores_for macos linux windows archives images video vim
     # PRN add to zsh's z history databaseend
 end
+
+
+function __gh_depoliticize
+    # gh repo list  --json name,defaultBranchRef,isPrivate,isFork g0t4 --limit 1000 > tmp
+    # cat tmp | jq 'map(select(.defaultBranchRef.name == "main" and .isFork == false and (.name | startswith("course") | not)))'
+    #
+    # check for local repos that might be on wrong branch
+    # for f in *; test -d $f; and echo -n $f"   "; and git -C $f branch --show-current  ;end | grep main | grep -v "^course"
+
+
+    # open page to view before/after, can check this after done with changes by refreshing
+    gh repo view --web 
+
+    if not git pull --rebase
+        echo "Failed to pull latest, aborting..."
+        return -1
+    end
+
+    # list all branches before
+    git branch -a
+
+    if not git status | grep -q "On branch main"
+        echo "Not on main branch, aborting..."
+        return -1
+    end
+
+    # create master if not exists
+    if not git branch -a | grep -q "master"
+        git checkout -b master
+        git push --set-upstream origin $(git_current_branch)
+    end
+
+    # ok to recall
+    gh repo edit --default-branch master
+
+    if not git status | grep -q "On branch master"
+        echo "Not on master branch, aborting..."
+        return -1
+    end
+
+    if not git branch -d main
+        echo "Failed to delete main branch, aborting..."
+        return -1
+    end
+
+    git push origin --delete main
+
+
+end
