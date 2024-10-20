baa945fc2ac80d08f99e2d8c1cbc6a51abc11cc5
Wes Higbee <wes.mcclure@gmail.com>
2024-10-19 23:25:18 -0500

    notes

--- .config/nvim/links.sh
+++ .config/nvim/links.sh
@@ -6,6 +6,7 @@
 # links below
 # install deps: npm/node, deno (markdown), rg (telescope), luarocks
 # use `:checkhealth` in nvim to see whats missing
+# careful, if build/run step on lazy plugin fails (i.e. peek md uses deno, another uses npm... if it fails I don't see a way to re-run it short of remove plugin's config and then Sync/Clean and then add back and try again... why does :Lazy not have a reinstall option?)
 
 
 # current links I am using for vim dotfiles
