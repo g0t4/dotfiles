c7f4d09dd0f8468c775002e7915e932df5d56249
Wes Higbee <wes.mcclure@gmail.com>
2024-10-20 10:13:23 -0500

    wire up fish-lsp directly w/o :CocInstall foo

---
+++ .config/nvim/coc-install.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+
+
+# IIUC these don't have :CocInstall wrappers... instead just point to them directly?
+echo "installing fish-lsp"
+wcl ndonfris/fish-lsp
+z ndonfris/fish-lsp
+yarn install
+
