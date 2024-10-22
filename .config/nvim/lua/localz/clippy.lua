2d2c17a82e1d5b581bf877257c63ed480933044a
Wes Higbee <wes.mcclure@gmail.com>
2024-10-22 01:04:38 -0500

    notes

--- .config/nvim/lua/localz/clippy.lua
+++ .config/nvim/lua/localz/clippy.lua
@@ -1,6 +1,8 @@
 -- see:   :h osc52...
 -- TODO revisit later
 
+-- IDEAS in this osc52 plugin (predates nvim builtin support for osc52) that still has relevant config ideas:
+--    https://github.com/ojroques/nvim-osc52
 
 local function copy(lines, _)
     require('osc52').copy(table.concat(lines, '\n'))
