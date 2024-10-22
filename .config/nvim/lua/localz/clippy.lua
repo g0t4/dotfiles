7915b8b7e29a1b8565e11b58a5a6e813e0e2b1db
Wes Higbee <wes.mcclure@gmail.com>
2024-10-22 00:52:40 -0500

    osc52 remote clipboard

---
+++ .config/nvim/lua/localz/clippy.lua
@@ -0,0 +1,26 @@
+-- see:   :h osc52...
+-- TODO revisit later
+
+
+local function copy(lines, _)
+    require('osc52').copy(table.concat(lines, '\n'))
+end
+
+local function paste()
+    return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+end
+
+vim.g.clipboard = {
+    name = 'osc52',
+    copy = { ['+'] = copy, ['*'] = copy },
+    paste = { ['+'] = paste, ['*'] = paste },
+}
+
+-- Now the '+' register will copy to system clipboard using OSC52
+vim.keymap.set('n', '<leader>c', '"+y')
+vim.keymap.set('n', '<leader>cc', '"+yy')
+
+-- Note that if you set your clipboard provider like the example above, copying
+-- text from outside Neovim and pasting with <kbd>p</kbd> won't work. But you can
+-- still use the paste shortcut of your terminal emulator (usually
+-- <kbd>ctrl+shift+v</kbd>).
