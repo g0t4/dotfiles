a73403ce28cff7bb27c0c6b0d76b9e4107273825
Wes Higbee <wes.mcclure@gmail.com>
2024-10-22 01:05:49 -0500

    notes

--- .config/nvim/lua/localz/clippy.lua
+++ .config/nvim/lua/localz/clippy.lua
@@ -3,6 +3,16 @@
 
 -- IDEAS in this osc52 plugin (predates nvim builtin support for osc52) that still has relevant config ideas:
 --    https://github.com/ojroques/nvim-osc52
+--
+-- i.e.
+--
+-- function copy()
+--   if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+--     require('osc52').copy_register('+')
+--   end
+-- end
+--
+-- vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
 
 local function copy(lines, _)
     require('osc52').copy(table.concat(lines, '\n'))
