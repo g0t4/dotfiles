1d950f7a9c47face1d1f006cf4aa0d7e98725956
Wes Higbee <wes.mcclure@gmail.com>
2024-10-22 01:47:13 -0500

    workon osc

--- .config/nvim/lua/localz/clippy.lua
+++ .config/nvim/lua/localz/clippy.lua
@@ -5,43 +5,49 @@ vim.o.clipboard = 'unnamedplus' -- use +/* which on macOS are wired to system cl
 -- so, normally `yy` would yank to the " register, which is an isolated clipboard within vim, hence the need to use "+yy to yank to the system clipboard
 -- so, setting unnamedplus => makes `yy` == `"+yy` and `p` == `"+p` etc.
 
-
--- TODO can I just setup to always use osc52 escape codes and forget about macos/linux etc if I am always using a shell that supports them?
-
+-- clipboard provider
+-- this is how nvim wires up +/* registers to system clipboard
+-- FYI `:registers` will show if there is a clipboard provider for +/* registers
+-- on ubuntu machine, over SSH, I see
+--     clipboard: No provider. Try ":checkhealth" or ":h clipboard".
 --
--- -- see:   :h osc52...
--- -- IDEAS in this osc52 plugin (predates nvim builtin support for osc52) that still has relevant config ideas:
--- --    https://github.com/ojroques/nvim-osc52
--- --
--- -- i.e.
--- --
--- -- function copy()
--- --   if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
--- --     require('osc52').copy_register('+')
--- --   end
--- -- end
--- --
--- -- vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
+
+TODO can I just setup to always use osc52 escape codes and forget about macos/linux etc if I am always using a shell that supports them?
 --
--- local function copy(lines, _)
---     require('osc52').copy(table.concat(lines, '\n'))
+--
+-- see:   :h osc52...
+-- IDEAS in this osc52 plugin (predates nvim builtin support for osc52) that still has relevant config ideas:
+--    https://github.com/ojroques/nvim-osc52
+--
+-- i.e.
+--
+-- function copy()
+--   if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+--     require('osc52').copy_register('+')
+--   end
 -- end
 --
--- local function paste()
---     return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
--- end
+-- vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
 --
--- vim.g.clipboard = {
---     name = 'osc52',
---     copy = { ['+'] = copy, ['*'] = copy },
---     paste = { ['+'] = paste, ['*'] = paste },
--- }
+local function copy(lines, _)
+    require('osc52').copy(table.concat(lines, '\n'))
+end
 --
--- -- Now the '+' register will copy to system clipboard using OSC52
--- vim.keymap.set('n', '<leader>c', '"+y')
--- vim.keymap.set('n', '<leader>cc', '"+yy')
+local function paste()
+    return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+end
 --
--- -- Note that if you set your clipboard provider like the example above, copying
--- -- text from outside Neovim and pasting with <kbd>p</kbd> won't work. But you can
--- -- still use the paste shortcut of your terminal emulator (usually
--- -- -- <kbd>ctrl+shift+v</kbd>).
+vim.g.clipboard = {
+    name = 'osc52',
+    copy = { ['+'] = copy, ['*'] = copy },
+    paste = { ['+'] = paste, ['*'] = paste },
+}
+--
+-- Now the '+' register will copy to system clipboard using OSC52
+vim.keymap.set('n', '<leader>c', '"+y')
+vim.keymap.set('n', '<leader>cc', '"+yy')
+--
+-- Note that if you set your clipboard provider like the example above, copying
+-- text from outside Neovim and pasting with <kbd>p</kbd> won't work. But you can
+-- still use the paste shortcut of your terminal emulator (usually
+-- -- <kbd>ctrl+shift+v</kbd>).
