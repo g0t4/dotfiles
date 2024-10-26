aa8d23e25067be9e9be61f6321001ec2b78fe4dd
Wes Higbee <wes.mcclure@gmail.com>
2024-10-25 23:50:37 -0500

    notes

--- .config/nvim/lua/shada-local.lua
+++ .config/nvim/lua/shada-local.lua
@@ -15,8 +15,14 @@ local function set_shada_for_workspace()
     local hash = vim.fn.sha256(dir)
     -- TODO sha256 takes 10ms to run :( ... faster way? what does vscode use, doesn't it store some workspace state centrally?
     local shada_path = "~/.local/share/nvim/shada/workspaces/" .. hash .. ".shada"
+    -- TODO refactor to put auto sessions here too? probably rename to drop "shada" or at least make the hash available so not need to recompute
 
     vim.opt.shadafile = shada_path
 end
 
+-- WHY do this?
+--   privacy (don't jump list back to another project, i.e. during screencast)
+--   separate workspaces, jumplist/marks s/b per project, not global... like vscode
+--      and cmd history, also belongs per project (though I can see more of an argument for global cmd history but since I don't use it much I don't think it will matter)
+--   should work well with some sort of auto session save config/plugin that tracks a session in accordance with this workspace dir too.. so I can reuse the hash file above
 set_shada_for_workspace()
