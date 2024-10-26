3207f68223c6474bafcc5b14b0abd36557a8e325
Wes Higbee <wes.mcclure@gmail.com>
2024-10-26 00:11:26 -0500

    quick hack to setup an auto session (load on startup, save on exit)... honestly I don't think I will mind this!

--- .config/nvim/lua/shada-local.lua
+++ .config/nvim/lua/shada-local.lua
@@ -14,10 +14,28 @@ local function set_shada_for_workspace()
     local dir = get_git_root() or vim.fn.getcwd()
     local hash = vim.fn.sha256(dir)
     -- TODO sha256 takes 10ms to run :( ... faster way? what does vscode use, doesn't it store some workspace state centrally?
-    local shada_path = "~/.local/share/nvim/shada/workspaces/" .. hash .. ".shada"
+    local workspace_dir = "~/.config/nvim/shada/workspaces/"
+    workspace_dir = vim.fn.expand(workspace_dir) -- expand ~, else it will be literally ~ and yeah not what you want
+    local shada_path = workspace_dir .. hash .. "/shada"
     -- TODO refactor to put auto sessions here too? probably rename to drop "shada" or at least make the hash available so not need to recompute
 
     vim.opt.shadafile = shada_path
+
+    local session_file = workspace_dir .. hash .. "/session.vim"
+
+    -- Load session on startup
+    if vim.fn.filereadable(vim.fn.expand(session_file)) == 1 then
+        print("Loading session: " .. session_file)
+        vim.cmd("source " .. session_file)
+    end
+
+    -- Save session on exit
+    vim.cmd("autocmd VimLeavePre * mksession! " .. session_file)
+
+    -- Session notes:
+    --   what if I pass file names to nvim, shouldn't I also open those in addition to whatever is open as of last session save?
+    --   if open multiple instances, all bets are off but that is fine b/c its rare (usually just if I wanna test dotfiles w/o quitting nvim editing instance)... also vscode always would reopen in already open instance so I dont know there is any logic for multiple instances when there is a "shared" session...
+    --   I LOVE RESUMING the last open file!!!
 end
 
 -- WHY do this?
