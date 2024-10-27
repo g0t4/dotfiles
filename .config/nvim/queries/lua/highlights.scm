ab2481dee6a962410b94c659e141454fe0cf6be2
Wes Higbee <wes.mcclure@gmail.com>
2024-10-27 03:07:42 -0500

    fix with ordering for now

--- .config/nvim/queries/lua/highlights.scm
+++ .config/nvim/queries/lua/highlights.scm
@@ -12,8 +12,8 @@
 ]
 [
  ; todo would it make sense to make each language specific? so I put -- ihn ... I'd prefer copy pasta this file and have a global catchall regex on front to match whatever comment start there is ... why is this not matching comment contents? is there a way to do that?
-  (comment) @comment_asterisks (#match? @comment_asterisks "^(--|\")\\s\\*+[^!]") ; TODO fix matching ***! too
   (comment) @comment_asterisks_bang (#match? @comment_asterisks_bang "^(--|\")\\s\\*\\*\\*!")
+  (comment) @comment_asterisks (#match? @comment_asterisks "^(--|\")\\s\\*+[^!]") ; TODO fix matching ***! too
 ]
 [
   ; not match start only:
