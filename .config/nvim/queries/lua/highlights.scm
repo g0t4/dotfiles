58c8462c872d3c679f377c614975c08f1473b8a7
Wes Higbee <wes.mcclure@gmail.com>
2024-10-27 02:49:55 -0500

    start on why \s isn't working

--- .config/nvim/queries/lua/highlights.scm
+++ .config/nvim/queries/lua/highlights.scm
@@ -1,4 +1,10 @@
 ; extends
+;
+; TODO map to more languages, add prefixes for various comment types, almost want a script to generate this file per language actually :) then not even need the wild regex?
+;
+; #match? docs:   https://neovim.io/doc/user/treesitter.html#treesitter-predicate-vim-match%3F
+;    is match nvim specific?
+;
 [
   ; must put most specific first, somehow that dictates which matching hlgroup wins if multiple attached... not sure I can do ! not in treesitter matches so yeah I have to overlap
   (comment) @comment_todo_bang (#match? @comment_todo_bang "TODO!")
@@ -6,7 +12,7 @@
 ]
 [
  ; todo would it make sense to make each language specific? so I put -- ihn ... I'd prefer copy pasta this file and have a global catchall regex on front to match whatever comment start there is ... why is this not matching comment contents? is there a way to do that?
-  (comment) @comment_asterisks (#match? @comment_asterisks "^(--|\") \\*\\*\\*[^!]")
+  (comment) @comment_asterisks (#match? @comment_asterisks "^(--|\")[ \t]\\*+[^!]")
   (comment) @comment_asterisks_bang (#match? @comment_asterisks_bang "^(--|\") \\*\\*\\*!")
 ]
 [
