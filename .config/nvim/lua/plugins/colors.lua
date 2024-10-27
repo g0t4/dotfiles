fdb62713fa7e07a6a6cf6715058bd7689c89159c
Wes Higbee <wes.mcclure@gmail.com>
2024-10-27 00:38:04 -0500

    statusline

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -3,39 +3,55 @@ return {
     -- {
     --     'Mofiqul/vscode.nvim'
     -- }, -- use "vscode" ... I added this in neovim, though my other theme is fine too it seem
+
     {
         -- FYI onedarkpro supports this OOB so I will try it
         "nvim-lualine/lualine.nvim",
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
-            require("lualine").setup {
-                -- default: https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#default-configuration
+            function StatusLine_Line_Column()
+                return "L:" .. vim.fn.line(".") .. "/C:" .. vim.fn.col(".")
+            end
 
+            function StatusLine_Line()
+                return "Line " .. vim.fn.line(".")
+            end
+
+            function StatusLine_Column()
+                return "Col " .. vim.fn.col(".")
+            end
+
+            require("lualine").setup {
+                -- default: https://github.com/nvim-lualine/lualine.nvim#default-configuration
                 -- options = {
                 --     theme = "codedark",
                 --     section_separators = { "", "" },
                 --     component_separators = { "", "" },
                 -- },
-
                 -- FYI =>    :lua print(vim.inspect(require('lualine').get_config()))
-                -- TODO port my copilot indicator
                 sections = {
-                    lualine_a = { "mode" }, -- default: "mode"
-                    lualine_b = { "branch", "diff" }, -- default: "branch", "diff", "diagnostics"
-                    lualine_c = { "filename" }, -- default: "filename"
-                    lualine_x = { "encoding", "fileformat", "filetype" }, -- default: "encoding", "fileformat", "filetype"
-                    lualine_y = { "progress" }, -- default: "progress"
-                    lualine_z = { "location" } -- default: "location"
+                    -- commandline shows mode already so why put it here too? plus lualine has color changes
+                    -- lualine_a = { 'buffers' }, -- TODO "buffers" looks interesting! shows tabs for each file... might be what I've been wanting?
+                    lualine_a = { '' },
+                    lualine_b = { { -- FYI wrap in {} to customize component options
+                        --   https://github.com/nvim-lualine/lualine.nvim#filename-component-options
+                        "filename",
+                        path = 1, -- 1 = relative path, 4 = filename+parentdir sounds interesting
+                        -- relative path, 4 filename+parentdir sounds interesting
+                    } },          -- filename includes modified
+                    lualine_c = { "filetype" },
+                    lualine_x = { "GetStatusLineCopilot" },
+                    lualine_y = { StatusLine_Line, StatusLine_Column, "progress" },
+                    lualine_z = { '' },
                 },
-
-                -- inactive_sections = {
-                --     lualine_a = {},
-                --     lualine_b = {},
-                --     lualine_c = { "filename" },
-                --     lualine_x = { "location" },
-                --     lualine_y = {},
-                --     lualine_z = {},
-                -- },
+                inactive_sections = {
+                    lualine_a = {},             -- default ""
+                    lualine_b = { "filename" }, -- default ""
+                    lualine_c = { "" },         -- default "filename"
+                    lualine_x = { "location" }, -- default "location"
+                    lualine_y = {},             -- default ""
+                    lualine_z = {},             -- default ""
+                },
             }
         end,
     },
