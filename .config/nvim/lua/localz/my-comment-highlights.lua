bc05c6ed01b1419b4b2a9a8877eb51070d2fd298
Wes Higbee <wes.mcclure@gmail.com>
2024-10-26 20:20:14 -0500

    and yeah now I realize... this isn't matching just comments... FOOOOO yuck, I like the syntax approach and can deal for now until I find a plugin to fix my nested language issues and conflicting styles

--- .config/nvim/lua/localz/my-comment-highlights.lua
+++ .config/nvim/lua/localz/my-comment-highlights.lua
@@ -114,14 +114,16 @@ vim.cmd("nnoremap <leader>pi :Inspect<CR>") -- prefer over pd/pc I made, b/c thi
 -- })
 
 
-if false then
+-- disable with flip true/false, for perf testing
+if true then
 
     -- Step 1: Define the highlight group for TODOs
     vim.api.nvim_set_hl(0, 'CommentTODO', { fg = "#ffcc00" })
     vim.api.nvim_set_hl(0, 'CommentTODOBang', { bg = "#ffcc00", fg = "#1f1f1f", bold = true })
     vim.api.nvim_set_hl(0, 'CommentPRN', { fg = "#27AE60" })
     vim.api.nvim_set_hl(0, 'CommentPRNBang', { bg = "#27AE60", fg = "#1f1f1f", bold = true })
-
+    vim.api.nvim_set_hl(0, 'CommentAsterisk', { fg = "#ff00c3" })
+    vim.api.nvim_set_hl(0, 'CommentAsteriskBang', { bg = "#ff52d1", fg = "#1f1f1f", bold = true })
 
     -- Step 2: Function to highlight TODO comments
     local function highlight_todo()
@@ -159,6 +161,21 @@ if false then
             if start_col then
                 return start_col, end_col, "CommentPRN"
             end
+
+
+            -- ***! asterisk
+            start_col, end_col = line:find("[#/%-]*%s*%*!.*")
+            if start_col then
+                return start_col, end_col, "CommentAsteriskBang"
+            end
+            -- * single or more asterisk
+            start_col, end_col = line:find("[#/%-]*%s*%*.*")
+            if start_col then
+                return start_col, end_col, "CommentAsterisk"
+            end
+
+
+
 
 
             --
