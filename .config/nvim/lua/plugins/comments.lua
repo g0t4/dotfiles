011b3c937c18737f64f212f523ed250d7e64269a
Wes Higbee <wes.mcclure@gmail.com>
2024-10-27 23:24:24 -0500

    notes

---
+++ .config/nvim/lua/plugins/comments.lua
@@ -0,0 +1,20 @@
+-- TODO move comments related settings/plugins here
+--
+--
+
+-- TODO try plugins that help autoformat in comments
+-- - i.e. that autoformat a list like this
+-- - and will then setup matching next line
+-- - and reformat/reindent existing lists? (consistency would rock)
+--  - I am very inconsistent about my lists in comments
+--
+
+
+-- SEPARATE:
+-- - highlights of comments (move here?)
+-- - check out how vim-notes impls highlights of comments, just curious: https://peterodding.com/code/vim/notes#customizing_syntax_highlighting_of_notes
+--     - I don't think this is for comments but I wanna see how it approaches notes highlighting that is similar to what I am doing w/ comments
+--
+--
+
+return {}
