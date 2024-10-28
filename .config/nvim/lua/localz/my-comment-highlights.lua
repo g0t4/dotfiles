3ce099e0fb5aa4a89ce4585d7d43fd87419b87f6
Wes Higbee <wes.mcclure@gmail.com>
2024-10-28 01:14:14 -0500

    fix

--- .config/nvim/lua/localz/my-comment-highlights.lua
+++ .config/nvim/lua/localz/my-comment-highlights.lua
@@ -12,7 +12,7 @@ vim.cmd("nnoremap <leader>pi :Inspect<CR>") -- prefer over pd/pc I made, b/c thi
 -- -- TODO! remove once I am happy with new treesitter based highlights that aren't conflicting at all given treesitter highlights take precedence (IIUC) over "legacy" syntax highlights
 -- vim.cmd [[
 --     hi clear Comment " clear it fixes the fg color ... b/c then yeah a comment doesn't have a fg color... ok... but can I add back color as a lower precedence rule?
---     -- FYI... treesitter-highlight-priority ... sets nvim_buf_set_extmark() to 100.. must have to do with why it wins over syntax match highlights (prior way I did it)
+--     " FYI... treesitter-highlight-priority ... sets nvim_buf_set_extmark() to 100.. must have to do with why it wins over syntax match highlights (prior way I did it)
 -- ]]
 -- vim.api.nvim_create_autocmd("BufReadPost", {
 --     callback = function()
