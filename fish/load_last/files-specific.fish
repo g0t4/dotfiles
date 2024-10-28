17e6d5873e19bbb8c8e7b950fd564de6afbf17d7
Wes Higbee <wes.mcclure@gmail.com>
2024-10-28 15:24:21 -0500

    review

--- fish/load_last/files-specific.fish
+++ fish/load_last/files-specific.fish
@@ -410,15 +410,14 @@ function oz
 end
 function nz
     z $argv
-    nvim (_repo_root)
+    nvim
 end
 export EDITOR="vim"
 
 
 #### nvim:
 # FYI I changed session restore to use passed files/paths to open after session restored, so now I want that to only be if I explicitly ask for a file to be opened and not show dir every time I open, show last file open in most cases... I might need to alter this later
-abbr nh nvim # IIAC don't need the "." here.. probably don't need it for code/zed either, I think that was an accidental carry over from zr/cr alias w/ repo root
-#abbr nr 'nvim "$(_repo_root)"' # TODO do I want to change this to set cwd too or leave it which is like a filter on the files in a subset of repo (ie in telescope)
+abbr nh nvim
 abbr nr --function nr_expand
 function nr_expand
     if not string match --quiet (_repo_root) (pwd)
@@ -429,9 +428,8 @@ end
 
 abbr nd --function nd_expand
 function nd_expand
-    if not string match --quiet --regex dotfiles (pwd)
-        # if not in dotfiles, then "z to it"
-        echo -n "z dotfiles; "
+    if not string match --quiet $WES_DOTFILES (pwd)
+        echo -n "cd $WES_DOTFILES; "
     end
-    echo nvim # .config/nvim/init.lua  (let last open file drive the file to show and/or telescope/jumplist) can change easily
+    echo nvim
 end
