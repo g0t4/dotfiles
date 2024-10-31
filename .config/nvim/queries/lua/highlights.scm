698b5bae5c833cddb976f0370d3b7168e3298108
Wes Higbee <wes.mcclure@gmail.com>
2024-10-31 01:20:06 -0500

    allow # comments in fish scripts

--- .config/nvim/queries/lua/highlights.scm
+++ .config/nvim/queries/lua/highlights.scm
@@ -15,8 +15,8 @@
 ]
 [
  ; todo would it make sense to make each language specific? so I put -- ihn ... I'd prefer copy pasta this file and have a global catchall regex on front to match whatever comment start there is ... why is this not matching comment contents? is there a way to do that?
-  (comment) @comment_asterisks_bang (#match? @comment_asterisks_bang "^(--|\")\\s\\*\\*\\*!")
-  (comment) @comment_asterisks (#match? @comment_asterisks "^(--|\")\\s\\*+[^!]") ; TODO fix matching ***! too... damn weird regex... are these not greedy?
+  (comment) @comment_asterisks_bang (#match? @comment_asterisks_bang "^(--|\"|#)\\s\\*\\*\\*!")
+  (comment) @comment_asterisks (#match? @comment_asterisks "^(--|\"|#)\\s\\*+[^!]") ; TODO fix matching ***! too... damn weird regex... are these not greedy?
 ]
 [
   ; not match start only:
@@ -24,19 +24,19 @@
   (comment) @comment_prn (#match? @comment_prn "(PRN|FYI)[^!]")
 
   ; FYI if I wanna limit to start of comment:
-  ;(comment) @comment_prn_bang (#match? @comment_prn_bang "^(--|\")\\s(PRN|FYI)!")
-  ;(comment) @comment_prn (#match? @comment_prn "^(--|\")\\s(PRN|FYI)")
+  ;(comment) @comment_prn_bang (#match? @comment_prn_bang "^(--|\"|#)\\s(PRN|FYI)!")
+  ;(comment) @comment_prn (#match? @comment_prn "^(--|\"|#)\\s(PRN|FYI)")
 ]
 [
   ; !!! => comment_triple_bang
-  (comment) @comment_triple_bang (#match? @comment_triple_bang "^(--|\")\\s!!!")
+  (comment) @comment_triple_bang (#match? @comment_triple_bang "^(--|\"|#)\\s!!!")
   ; gotta use start for ! single bang... else matches my others, though maybe I can change hl group orders to fix that?
-  (comment) @comment_single_bang (#match? @comment_single_bang "^(--|\")\\s![^!]") ; -- REDO others like this so not double labeling when not suppposed to
+  (comment) @comment_single_bang (#match? @comment_single_bang "^(--|\"|#)\\s![^!]") ; -- REDO others like this so not double labeling when not suppposed to
 ]
 [
   ; ???+ => comment_triple_question
-  (comment) @comment_triple_question (#match? @comment_triple_question "^(--|\")\\s\\?\\?\\?+")
+  (comment) @comment_triple_question (#match? @comment_triple_question "^(--|\"|#)\\s\\?\\?\\?+")
   ; ?|?? => comment_single_question (\\? == ?) and \\?? means optional ? (0 or 1)
-  (comment) @comment_single_question (#match? @comment_single_question "^(--|\")\\s\\?\\??[^\\?]")
+  (comment) @comment_single_question (#match? @comment_single_question "^(--|\"|#)\\s\\?\\??[^\\?]")
 ]
 
