d18e58ad5bda973a555171b2887cf2dd9acd3ffd
Wes Higbee <wes.mcclure@gmail.com>
2024-11-01 00:14:40 -0500

    tweak neoscroll timings

--- .config/nvim/lua/plugins/code.lua
+++ .config/nvim/lua/plugins/code.lua
@@ -70,25 +70,48 @@ return {
         config = function()
             local neoscroll = require('neoscroll')
             -- additional/custom keybindings, why aren't PageUp/Down OOB? any reason why I shouldn't remap those too?
+            -- default mappings: https://github.com/karb94/neoscroll.nvim/blob/master/lua/neoscroll/init.lua#L411-L424
             local keymap = {
-                ["<PageUp>"] = function() neoscroll.ctrl_b({ duration = 450 }) end,
-                ["<PageDown>"] = function() neoscroll.ctrl_f({ duration = 450 }) end,
-                -- IIUC these are builtin defaults (or close to them):
-                -- ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 250 }) end,
-                -- ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 250 }) end,
-                -- ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 450 }) end,
-                -- ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 450 }) end,
-                -- ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 }) end,
-                -- ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 100 }) end,
-                -- ["zt"]    = function() neoscroll.zt({ half_win_duration = 250 }) end,
-                -- ["zz"]    = function() neoscroll.zz({ half_win_duration = 250 }) end,
-                -- ["zb"]    = function() neoscroll.zb({ half_win_duration = 250 }) end,
+                -- scroll full page:
+                ["<C-b>"]      = function() neoscroll.ctrl_b({ duration = 350 }) end, -- 450 default
+                ["<C-f>"]      = function() neoscroll.ctrl_f({ duration = 350 }) end, -- 450 default
+                ["<PageUp>"]   = function() neoscroll.ctrl_b({ duration = 350 }) end, -- not mapped by neoscroll
+                ["<PageDown>"] = function() neoscroll.ctrl_f({ duration = 350 }) end, -- not mapped by neoscroll
+                --
+                -- scroll half page:
+                -- ["<C-u>"]      = function() neoscroll.ctrl_u({ duration = 250 }) end, -- 250 default
+                -- ["<C-d>"]      = function() neoscroll.ctrl_d({ duration = 250 }) end, -- 250 default
+                --
+                -- scroll a few line(s):
+                ["<C-y>"]      = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 50 }) end, -- 100 default
+                ["<C-e>"]      = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 50 }) end,  -- 100 default (feels slow)
+                --
+                -- scroll cursor line to top/middle/bottom:
+                -- 150 feels fine (even slow sometimes) for these, esp zz which often has a minimal move to make
+                ["zt"]         = function() neoscroll.zt({ half_win_duration = 150 }) end, -- 250 half_win_duration default
+                ["zz"]         = function() neoscroll.zz({ half_win_duration = 150 }) end, -- 250 half_win_duration default
+                ["zb"]         = function() neoscroll.zb({ half_win_duration = 150 }) end, -- 250 half_win_duration default, too slow when move is large (top line to bottom = 2x250 = 500ms)
             }
             local modes = { 'n', 'v', 'x' }
             for key, func in pairs(keymap) do
                 vim.keymap.set(modes, key, func)
             end
             neoscroll.setup({
+                mappings = {
+                    -- FYI make sure you uncomment here if you comment out customizations above
+                    -- remove keys that you want to customize, else neoscroll will redefine them
+                    '<C-u>', -- 250 default works good
+                    '<C-d>', -- 250 default works good
+                    -- '<C-b>',
+                    -- '<C-f>',
+                    -- '<C-y>',
+                    -- '<C-e>',
+                    -- 'zt',
+                    -- 'zz',
+                    -- 'zb',
+                    -- 'G', -- I expect this to be jarring and would never wanna wait 250ms per page up/down!
+                    -- 'gg',
+                },
                 -- easing = "quartic", -- I think I prefer linear default
             })
         end
