9f73659bef27fd98f57c60a6864e6ec6a8245438
Wes Higbee <wes.mcclure@gmail.com>
2024-11-04 11:16:41 -0600

    cleanup

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -20,21 +20,6 @@ return {
     },
 
     -- {
-    --     "g0t4/test-nvim",
-    --     --
-    --     -- dir = "~/repos/github/g0t4/test-nvim", -- uncomment to use local checkout (takes precedence over name/url convention)
-    --     --  FYI can also use `dev = true` and then setup lazy package manager's config.dev to path of local projects... perhaps if I get a lot of local checkouts... for now lets use dir here which works fine for now (WAIT FOR THE PAIN)
-    --     --     see:   ~/.local/share/nvim/lazy/lazy.nvim/doc/lazy.nvim.txt:665
-    --     --
-    --     config = function()
-    --         require("test-nvim")
-    --     end,
-    --     dependencies = {
-    --         "rcarriga/nvim-notify",
-    --     },
-    -- },
-
-    -- {
     --     "https://github.com/folke/noice.nvim",
     --     config = function()
     --         require("noice").setup({
