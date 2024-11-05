acac9d5f87e178c2dc9623f9648b7fcbb576e4f0
Wes Higbee <wes.mcclure@gmail.com>
2024-11-04 21:05:05 -0600

    test out ThePrimeagen/refactoring, ri works, cool!

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -19,6 +19,18 @@ return {
         end,
     },
 
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        lazy = false,
+        config = function()
+            require("refactoring").setup()
+        end,
+    },
+
     -- {
     --     "https://github.com/folke/noice.nvim",
     --     config = function()
