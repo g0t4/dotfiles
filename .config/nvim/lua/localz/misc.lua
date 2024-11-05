404602dd2a5fde8164b6243725dc86c22075b0e4
Wes Higbee <wes.mcclure@gmail.com>
2024-11-05 02:23:16 -0600

    hack together a new F1 in visual mode, hopefully to encourage me to use visual selections more and learn them better!

--- .config/nvim/lua/localz/misc.lua
+++ .config/nvim/lua/localz/misc.lua
@@ -37,3 +37,29 @@ vim.api.nvim_set_keymap('n', '<F1>', ':help <C-R><C-W><CR>', { noremap = true, s
 -- in visual mode, press F1 to search for selected text
 -- TODO write func to see if selected text is empty and do word under cursor instead => right now its just copying the letter under the cursor, which is fine for now but not ideal
 vim.api.nvim_set_keymap('x', '<F1>', 'y:help <C-R>"<CR>', { noremap = true, silent = true })
+vim.keymap.set('v', '<F1>', function()
+    -- TODO make this handle 'n' normal mode too?
+    -- local mode = vim.fn.visualmode()
+
+    -- marks store start and end of visual selection
+    --    this is why '<,'> is inserted into command line when you select text! now it makes sense! ' == mark, </> are the mark "register" names
+    local start_pos = vim.fn.getpos("'<")
+    local start_line = start_pos[2]
+    local start_col = start_pos[3]
+    local end_pos = vim.fn.getpos("'>")
+    local end_line = end_pos[2]
+    local end_col = end_pos[3]
+
+    print("start/end", vim.inspect(start_pos), "/", vim.inspect(end_pos))
+    if start_line == end_line or start_col == end_col then
+        print("only one char")
+        vim.cmd('normal! w') -- selects word under cursor (since one char alone isn't really a selection and if it is then this won't change it!)
+        -- think of this as not requiring user to make simple selections, do it for them
+    end
+
+    -- yank visual selection into " register
+    vim.cmd('normal! ""y')
+
+    local search_term = vim.fn.getreg("\"")
+    vim.cmd('help ' .. search_term)
+end)
