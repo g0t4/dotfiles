54fb31776a26481b9798d3d78fc27e395d696a91
Wes Higbee <wes.mcclure@gmail.com>
2024-11-06 02:37:03 -0600

    tinker

--- .config/nvim/lua/localz/ideas.lua
+++ .config/nvim/lua/localz/ideas.lua
@@ -1,38 +1,66 @@
--- use cmd line events to store history of cmdline and add undo/redo support?
--- use wilder as a reference to how to use the cmd line / change it / etc
--- could start with just the history for the current cmdline (clear on leave)
-vim.cmd [[
+-- !!! SUPER BUGGY, just spitballing ideas
 
-function! CmdlineHistory_add(cmdline)
-    "call luaeval("vim.notify('Current input: ' .. vim.inspect(vim.fn.getcmdline()))")
-    if !exists('g:cmdline_history')
-        let g:cmdline_history = []
-    endif
-    call add(g:cmdline_history, a:cmdline)
-endfunction
+function cmdline_enter()
+    _G.cmdline_history = {}
+    _G.cmdline_history_index = -1
+    _G.cmdline_last_cmdline = ''
+end
 
-function! CmdlineHistory_clear()
-    let g:cmdline_history = []
-endfunction
+function cmdline_changed()
+    local cmdline = vim.fn.getcmdline()
 
-augroup cmdline_history
-    autocmd!
-    "autocmd CmdlineLeave * call CmdlineHistory_add(getcmdline())
-    autocmd CmdlineChanged * call CmdlineHistory_add(getcmdline())
-    autocmd CmdlineEnter * call CmdlineHistory_clear()
-augroup END
+    if cmdline == _G.cmdline_last_cmdline then
+        return
+    end
 
-]]
+    -- find first difference between cmdline and last_cmdline, just to test spaces as delimiter
+    local index = 0
+    while index < #cmdline and index < #_G.cmdline_last_cmdline do
+        if cmdline:sub(index + 1, index + 1) ~= _G.cmdline_last_cmdline:sub(index + 1, index + 1) then
+            break
+        end
+        index = index + 1
+    end
+    local diff_cmdline = cmdline:sub(index + 1, index + 1)
+    local diff_last_cmdline = _G.cmdline_last_cmdline:sub(index + 1, index + 1)
 
+    if diff_cmdline == ' ' then
+        -- add to history
+        table.insert(_G.cmdline_history, cmdline)
+        _G.cmdline_history_index = #_G.cmdline_history
+        _G.cmdline_last_cmdline = cmdline
+        return
+    end
+    -- vim.notify('diff_cmdline: ' .. diff_cmdline)
+    _G.cmdline_last_cmdline = cmdline
+
+    -- TODO what happens when pull back old command? just need to add it right? but in that case first diff isn't a space
+    -- TODO more cases here for deleting spaces
+    -- TODO handle edits mid-cmdline (if needed, need to make test cases)
+end
+
+vim.api.nvim_create_augroup('cmdline_history', { clear = true })
+
+vim.api.nvim_create_autocmd('CmdlineChanged', {
+    group = 'cmdline_history',
+    callback = cmdline_changed,
+})
+
+vim.api.nvim_create_autocmd('CmdlineEnter', {
+    group = 'cmdline_history',
+    callback = cmdline_enter,
+})
+
+-- *** undo
 vim.keymap.set('c', '<C-z>', function()
-    if vim.g.cmdline_history_index == nil then
-        vim.g.cmdline_history_index = #vim.g.cmdline_history - 1
-    elseif vim.g.cmdline_history_index > 0 then
-        vim.g.cmdline_history_index = vim.g.cmdline_history_index - 1
+    if _G.cmdline_history_index == nil then
+        _G.cmdline_history_index = #_G.cmdline_history - 1
+    elseif _G.cmdline_history_index > 0 then
+        _G.cmdline_history_index = _G.cmdline_history_index - 1
     else
         return
     end
-    local last_cmd_line = vim.g.cmdline_history[vim.g.cmdline_history_index]
+    local last_cmd_line = _G.cmdline_history[_G.cmdline_history_index]
     if last_cmd_line == nil then
         vim.fn.setcmdline('')
         return
@@ -40,15 +68,16 @@ vim.keymap.set('c', '<C-z>', function()
     vim.fn.setcmdline(last_cmd_line)
 end)
 
+-- *** redo
 vim.keymap.set('c', '<C-y>', function()
-    if vim.g.cmdline_history_index == nil then
+    if _G.cmdline_history_index == nil then
         return -- nothing to redo until after first undo
-    elseif vim.g.cmdline_history_index < #vim.g.cmdline_history - 1 then
-        vim.g.cmdline_history_index = vim.g.cmdline_history_index + 1
+    elseif _G.cmdline_history_index < #_G.cmdline_history - 1 then
+        _G.cmdline_history_index = _G.cmdline_history_index + 1
     else
         return
     end
-    local last_cmd_line = vim.g.cmdline_history[vim.g.cmdline_history_index]
+    local last_cmd_line = _G.cmdline_history[_G.cmdline_history_index]
     if last_cmd_line == nil then
         vim.fn.setcmdline('')
         return
@@ -58,6 +87,6 @@ end)
 
 function Watch_cmdline()
     start_watching(function()
-        return vim.g.cmdline_history
+        return _G.cmdline_history
     end)
 end
