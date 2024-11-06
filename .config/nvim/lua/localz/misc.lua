9080fbf37731ec5b4951088a1a04c4bb1f2afb1b
Wes Higbee <wes.mcclure@gmail.com>
2024-11-05 18:30:14 -0600

    add lua completions to user cmd

--- .config/nvim/lua/localz/misc.lua
+++ .config/nvim/lua/localz/misc.lua
@@ -97,5 +97,10 @@ vim.api.nvim_create_autocmd("FileType", {
 -- *** :Dump vim.g.foo
 -- TODO completion for <args>, lua expression completion
 -- Am I the only who hates typing :lua print(vim.inspect(...))?
-vim.api.nvim_create_user_command('Dump', "lua print(vim.inspect(<args>))", { nargs = '*' })
-
+vim.api.nvim_create_user_command('Dump', "lua print(vim.inspect(<args>))", {
+    nargs = '*',
+    complete = "lua", -- completes like using :lua command
+})
+-- vim.cmd [[
+--     command! -nargs=1 -complete=lua Dump lua print(vim.inspect(<args>))
+-- ]]
