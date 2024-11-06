ed1a4eb02367769a19a2d692cb9e9dfaf6ba4890
Wes Higbee <wes.mcclure@gmail.com>
2024-11-06 02:41:09 -0600

    stub out test file

---
+++ .config/nvim/lua/tests.lua
@@ -0,0 +1,8 @@
+-- :PlenaryBustedFile .config/nvim/lua/tests.lua
+
+describe("test", function()
+    it("tests", function()
+        assert.is.equals(2, 1 + 1)
+        assert.is_true(true)
+    end)
+end)
