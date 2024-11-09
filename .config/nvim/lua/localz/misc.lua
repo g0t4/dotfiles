30cb2c14746c9b8984cfcf48f7ee501639890a2e
Wes Higbee <wes.mcclure@gmail.com>
2024-11-09 02:25:35 -0600

    quit help windows with q

--- .config/nvim/lua/localz/misc.lua
+++ .config/nvim/lua/localz/misc.lua
@@ -68,7 +68,7 @@ end)
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "help",
     callback = function()
-        vim.api.nvim_buf_set_keymap(0, "n", "q", ":q<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "q", ":q<CR>", { noremap = true, silent = true, buffer = true })
         -- PRN others?
     end,
 })
