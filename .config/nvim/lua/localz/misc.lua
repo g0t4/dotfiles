fbd9e9ebe11fde55523d9296a21d585b6f4539f1
Wes Higbee <wes.mcclure@gmail.com>
2024-11-09 01:58:28 -0600

    move

--- .config/nvim/lua/localz/misc.lua
+++ .config/nvim/lua/localz/misc.lua
@@ -9,26 +9,6 @@ vim.cmd([[
                   \ | wincmd p | diffthis
 ]])
 
-
-
-
--- *** Ctrl+S to save http://vim.wikia.com/wiki/Saving_a_file
-vim.cmd("nnoremap <c-s> :w<CR>")
-vim.cmd("vnoremap <c-s> <Esc><c-s>gv") -- esc=>normal mode => save => reselect visual mode, not working... figure out later
-vim.cmd("inoremap <c-s> <c-o><c-s>")
-
--- F9 == quit all
-vim.cmd("nnoremap <F9> :qall<CR>")
--- perhaps I am doing something wrong if I need F9.. but I love this, open lots of tabs to test neovim config changes and just wanna close w/o BS... also love one click quit if no changes
--- FYI F10 is F9 + re-run nvim (in keyboard maestro to relaunch nvim after quitting)
-
-
--- map [Shift]+Ctrl+Tab to move forward/backward through files to edit, in addition to Ctrl+o/i
---   that is my goto key combo, perhaps I should learn o/i instead... feel like many apps use -/+ for this, vscode for shizzle
-vim.api.nvim_set_keymap('n', '<C-->', '<C-o>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-S-->', '<C-i>', { noremap = true, silent = true })
---  FYI in iTerm => Profiles -> Keys -> Key Mappings -> removed "send 0x1f" on "ctrl+-" ... if that breaks something, well you have this note :)
-
 -- *** help
 --
 -- start typing :help then Ctrl+R, Ctrl+W takes word under cursor
