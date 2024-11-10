6ff5fa5c479cc895740882d847baa5b47745a8b3
Wes Higbee <wes.mcclure@gmail.com>
2024-11-09 23:08:39 -0600

    darn forgot I have alt+right for accept next word of suggestions

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -9,7 +9,8 @@ for _, arrow in ipairs({ "right", "left", "up", "down" }) do
     -- <Cmd>wincmd == one keymap for both modes (n/i) + preserve mode after switch
     --   also, this adds 4 fewer keymaps overall, so when I use :map to list them all, I see fewer
     local dir = arrow == "left" and "h" or arrow == "right" and "l" or arrow == "up" and "k" or "j"
-    vim.keymap.set({ "i", "n" }, "<M-" .. arrow .. ">", "<Cmd>wincmd " .. dir .. "<CR>", default_options)
+    -- disable "i" insert mode b/c I use alt+right for accept next work in suggestions
+    vim.keymap.set({ "n" }, "<M-" .. arrow .. ">", "<Cmd>wincmd " .. dir .. "<CR>", default_options)
 end
 -- FYI can use Shift+Alt+arrows to move some other thing, might even want that for window moves if not something else b/c that is what I use in iterm2 for switching panes in a split tab/window
 
