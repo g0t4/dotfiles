42cb59006fa79d3b4b4cb32ff7ce831b0be2e6f3
Wes Higbee <wes.mcclure@gmail.com>
2024-11-10 15:15:32 -0600

    notes

--- .config/nvim/queries/lua/highlights.scm
+++ .config/nvim/queries/lua/highlights.scm
@@ -1,5 +1,7 @@
 ; extends
 ;
+; FYI https://github.com/nvim-treesitter/nvim-treesitter/tree/master/queries (use as a ref for building queries - has every language imaginable)
+;
 ; TODO map to more languages, add prefixes for various comment types, almost want a script to generate this file per language actually :) then not even need the wild regex?
 ;
 ; #match? docs:   https://neovim.io/doc/user/treesitter.html#treesitter-predicate-vim-match%3F
@@ -10,6 +12,7 @@
   ; must put most specific first, somehow that dictates which matching hlgroup wins if multiple attached... not sure I can do ! not in treesitter matches so yeah I have to overlap
   ;   THIS must have smth to do with issues I had with the syntax matches ... it seemed like first hl-group applied wins (for the attrs it sets and so subsequent can apply a diff characteristic (fg/bg) but not override prev capture groups or smth)...
   ;   hl group ordering doesn't matter here, if I fliop bang after non-bang, then the fg comes from the non-bang even on the bangs... flip it and then the bang wins b/c it sets both fg and bg... and I tried flipping hlgroup definitioon ordering and that did not matter
+  ; TODO - both FYI and TODO are not matching first comment in a file, has to be content before it... fix these to match on any comment in file like I do with others here
   (comment) @comment_todo_bang (#match? @comment_todo_bang "TODO!")
   (comment) @comment_todo (#match? @comment_todo "TODO[^!]")
 ]
