d44f57357149df943a50ede5ff708418a7d44261
Wes Higbee <wes.mcclure@gmail.com>
2024-11-14 09:54:12 -0600

    for now treat zsh as bash

--- .config/nvim/ftplugin/zsh.lua
+++ .config/nvim/ftplugin/zsh.lua
@@ -1,1 +1,14 @@
+-- ensure don't override base ftplugin for zsh:
+vim.cmd('runtime! ftplugin/zsh.lua')
+
 -- print("hi from zsh")
+--
+
+-- for now, use bash file type for zsh given treesitter lacks zsh support
+vim.bo.filetype = "bash"
+-- alternatively, disable treesitter for zsh
+-- TSDisable highlight zsh
+-- then vim's syntax highlighting will kick in (not much better)
+
+-- PRN get zsh nested in markdown code blocks highlighted? I don't care much about zsh, but it is a good thing to learn to do, maybe with a diff lang?
+-- zsh might not be recognized as markdown code block type?
