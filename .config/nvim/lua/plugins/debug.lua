ed56cad97153d2c6b2205900a7503a4507335c3b
Wes Higbee <wes.mcclure@gmail.com>
2024-11-18 10:22:18 -0600

    use nvim-dap-python to configure nvim-dap for debugpy, then next I will use manual config

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -1,3 +1,31 @@
+function setup_python_dap()
+    -- local dap = require('dap')
+    -- dap.adapters.python = function(cb, config)
+    --     if config.request == 'attach' then
+    --         ---@diagnostic disable-next-line: undefined-field
+    --         local port = (config.connect or config).port
+    --         ---@diagnostic disable-next-line: undefined-field
+    --         local host = (config.connect or config).host or '127.0.0.1'
+    --         cb({
+    --             type = 'server',
+    --             port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+    --             host = host,
+    --             options = {
+    --                 source_filetype = 'python',
+    --             },
+    --         })
+    --     else
+    --         cb({
+    --             type = 'executable',
+    --             command = 'path/to/virtualenvs/debugpy/bin/python',
+    --             args = { '-m', 'debugpy.adapter' },
+    --             options = {
+    --                 source_filetype = 'python',
+    --             },
+    --         })
+    --     end
+    -- end
+end
 
 return {
 
@@ -34,11 +62,11 @@ return {
     --     end,
     -- },
 
-    -- {
-    --     "mfussenegger/nvim-dap-python",
-    --     config = function()
-    --         require("dap-python").setup("python")
-    --     end,
-    -- }
+    {
+        "mfussenegger/nvim-dap-python",
+        config = function()
+            require("dap-python").setup("python")
+        end,
+    }
 
 }
