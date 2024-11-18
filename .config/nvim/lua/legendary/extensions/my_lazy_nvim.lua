b281b7b8142ce3c1ab855c289696a0ee6e601a9f
Wes Higbee <wes.mcclure@gmail.com>
2024-11-18 03:33:31 -0600

    use rhs not desc, maybe add both?

--- .config/nvim/lua/legendary/extensions/my_lazy_nvim.lua
+++ .config/nvim/lua/legendary/extensions/my_lazy_nvim.lua
@@ -11,21 +11,19 @@ return function()
     for _, plugin in pairs(LazyNvimConfig.plugins) do
         local keys = vim.tbl_get(plugin or {}, '_', 'handlers', 'keys') or {}
         for _, keymap in pairs(keys) do
-            if keymap.desc and #keymap.desc > 0 then
-                -- we don't need the implementation, since
-                -- lazy.nvim will have already bound it. We
-                -- just need the description-only item to appear
-                -- in the legendary.nvim finder.
-                local legendary_keymap = {
-                    -- for backwards compatibility, if keymap.lhs is missing, using an old lazy.nvim so it will be keymap[1]
-                    keymap.lhs or keymap[1],
-                    description = keymap.desc,
-                    mode = keymap.mode, ---@type string|string[]|nil
-                }
-                require('legendary').keymap(legendary_keymap)
-            end
+            -- we don't need the implementation, since
+            -- lazy.nvim will have already bound it. We
+            -- just need the description-only item to appear
+            -- in the legendary.nvim finder.
+            local legendary_keymap = {
+                -- for backwards compatibility, if keymap.lhs is missing, using an old lazy.nvim so it will be keymap[1]
+                keymap.lhs or keymap[1],
+                -- FYI description MUST be set or its not shown
+                description = keymap.rhs,
+                -- description = keymap.desc,
+                mode = keymap.mode, ---@type string|string[]|nil
+            }
+            require('legendary').keymap(legendary_keymap)
         end
     end
 end
-
-
