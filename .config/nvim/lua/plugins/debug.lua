f927e435892815f6f596799cd9cedd78b4389bf9
Wes Higbee <wes.mcclure@gmail.com>
2024-11-18 02:00:48 -0600

    debuggers

---
+++ .config/nvim/lua/plugins/debug.lua
@@ -0,0 +1,20 @@
+return {
+
+    -- vimspector vs nvim-dap?
+    --   others?
+    -- wants:
+    --   DAP support
+    --   best nvim integration and experience, not necessarily all debugger features ever
+    --   I rarely use debuggers so I can compromise and don't need all the features
+    -- [vimspector](https://github.com/puremourning/vimspector)
+    --   [languages](https://github.com/puremourning/vimspector/wiki/Additional-Language-Support)
+    --   pros:
+    --   cons:
+    -- nvim-dap:
+    --   pros:
+    --   cons:
+    --
+    -- differences not as important to me:
+    --   nvim-dap is nvim only, vimspector works in vim... not sure which way this is pro or con, right now I am focused on nvim config (would be much work to go back to just vim)
+
+}
