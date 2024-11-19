ade26152c4984ef990c595980d7f93ef7da71787
Wes Higbee <wes.mcclure@gmail.com>
2024-11-19 17:18:38 -0600

    notes

---
+++ .config/nvim/ftplugin/lua.lua
@@ -0,0 +1,13 @@
+
+
+-- TODO review formatoptions
+--   o = insert vim.o.comments on `o` and `O`
+--      r = same for enter
+--   autowrap lines
+--      t = auto wrap regular text
+--      c = auto wrap comments + insert comments leader too
+--  j = remove comment leader when joining lines
+--- q = gq formats comments
+---
+
+-- TODO address any issues w/ custom lua settings I have scattered elsewhere, gather them here?
