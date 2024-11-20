3105ee72a51a9d08d0ac58597cb08cbce624a12e
Wes Higbee <wes.mcclure@gmail.com>
2024-11-20 02:08:08 -0600

    vimspector

--- .config/nvim/lua/plugins/debug-vimspector.lua
+++ .config/nvim/lua/plugins/debug-vimspector.lua
@@ -7,6 +7,25 @@ return {
 
     {
         'puremourning/vimspector',
+        config = function()
+            -- articles to read/try:
+            -- - https://puremourning.github.io/vimspector-web/demo-setup.html
+            -- - https://dev.to/iggredible/debugging-in-vim-with-vimspector-4n0m
+            -- reference: https://puremourning.github.io/vimspector/
+            -- schemas: https://puremourning.github.io/vimspector/schema/
+
+
+            -- FYI, later port to lua if useful to do so
+            vim.cmd [[
+                " todo turn this off and see what difference it makes once debugging is working
+                let g:vimspector_enable_mappings = 'HUMAN'
+                " ?? only make these work in "debug" tabs?
+
+                " alternatives to mouse hover which isn't likely gonna work in terminals (nor in nvim IIUC)
+                nmap <Leader>di <Plug>VimspectorBalloonEval
+                xmap <Leader>di <Plug>VimspectorBalloonEval
+            ]]
+        end,
     },
 
 }
