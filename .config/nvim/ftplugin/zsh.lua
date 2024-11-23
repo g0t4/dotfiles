44d2e810f8524f6f55b01e213ec0a9377084f059
Wes Higbee <wes.mcclure@gmail.com>
2024-11-23 01:39:42 -0600

    fix zsh.vim not being called - no syntax highlighting except for first opened zsh file from session restore

--- .config/nvim/ftplugin/zsh.lua
+++ .config/nvim/ftplugin/zsh.lua
@@ -1,8 +1,14 @@
 -- ensure don't override base ftplugin for zsh:
-vim.cmd('runtime! ftplugin/zsh.lua')
+vim.cmd('runtime! ftplugin/zsh.vim')
+-- FYI ensure the above is valid, otherwise seems to silently fail (and then things like syntax highlighting are mysteriously broken)
+-- FYI search rtp:
+--    :echo globpath(&rtp, 'syntax/zsh.vim', 1)
+--    :echo globpath(&rtp, 'syntax/zsh.*', 1)
 
--- for now, use bash file type for zsh given treesitter lacks zsh support
-vim.bo.filetype = "bash"
+
+-- TODO do I want to just use bash file type for zsh?
+--   EVEN THE SYNTAX HIGHLIGHTING is not great for zsh in zsh.vim (above)
+-- vim.bo.filetype = "bash"
 -- alternatively, disable treesitter for zsh
 -- TSDisable highlight zsh
 -- then vim's syntax highlighting will kick in (not much better)
