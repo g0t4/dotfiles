2003a315c6b2dcc202e6467fa3ccda73d3a48745
Test User <test@example.com>
2024-12-03 10:43:59 -0600

    try this one

--- fish/load_last/prompt.fish
+++ fish/load_last/prompt.fish
@@ -18,11 +18,11 @@ function prompt_login --description 'display user name for the prompt'
         return
     end
 
-    # SSH indicator 
+    # SSH indicator  or 
     if set -q SSH_CONNECTION
         # keep in mind, have to update dotfiles for remote machines to pick up changes so this can't ever be assumed to be universal
         #echo -n -s (set_color cyan) \ue0a0 (set_color normal) " "
-        echo -n -s (set_color cyan) \ueb3a (set_color normal)
+        echo -n -s (set_color cyan) \ue0a2 (set_color normal) " "
     end
 
     if test -f /.dockerenv
