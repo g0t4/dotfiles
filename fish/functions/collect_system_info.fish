1abcd4f0a648223dabe4006046325940e8ab6ff9
Wes Higbee <wes.mcclure@gmail.com>
2024-12-12 19:20:07 -0600

    my mods

--- fish/functions/collect_system_info.fish
+++ fish/functions/collect_system_info.fish
@@ -1,17 +1,16 @@
 function collect_system_info
-    set -l os (uname)
-    set -l os_version (sw_vers -productVersion 2>/dev/null || echo "N/A")
-    set -l arch (uname -m)
-    set -l shell_version $FISH_VERSION
-    set -l git_version (git --version | cut -d" " -f3)
-    set -l python3_version (python3 --version 2>/dev/null || echo "N/A")
-    set -l brew_version (brew --version 2>/dev/null | head -n1 | cut -d" " -f2 || echo "N/A")
-    
-    printf "%s\n" \
-        "OS: $os $os_version" \
-        "Architecture: $arch" \
-        "Shell: fish $shell_version" \
-        "Git: $git_version" \
-        "Python3: $python3_version" \
-        "Homebrew: $brew_version"
+    if $IS_MACOS
+        uname -smv # -v = os version, -m = arch, -s = os name
+        #set -l os_version (sw_vers -productVersion 2>/dev/null || echo "N/A")
+    else if $IS_LINUX
+        cat /etc/os-release | grep PRETTY_NAME
+        uname -m
+    end
+    echo "hostname: $(hostname)"
+
+    if command -q brew
+        echo "brew --prefix: $(brew --prefix)"
+        echo "brew list: $(brew list | xargs echo)"
+        echo
+    end
 end
