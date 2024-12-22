badd431314a61d68abcabdefba9eaab044eee9b5
Wes Higbee <wes.mcclure@gmail.com>
2024-12-22 03:17:36 -0600

    not needed

--- zsh/universals/3-last/ask-openai/devtools.py
+++ zsh/universals/3-last/ask-openai/devtools.py
@@ -1,5 +1,4 @@
 import sys
-import textwrap
 
 from services import args_to_use, Service
 
@@ -7,7 +6,7 @@ from services import args_to_use, Service
 #    I found a few seettings for delays and set them to 0 and its sliightly helped but 200ms still fels like 1+ seconds (vs test at CLI where I run the test fish script that calls same thing)
 #   FYI run test fish script like KM would run is 200ms overhead w/o import openai and generate_command... so smth else might be slowing down KM... though it is hard to say my perception of time is ridiculous :) ... half a second likely feels like 2 to me
 
-system_message = textwrap.dedent("""
+system_message = """
 You are a chrome devtools expert.
 The user is working in the devtools Console in the Brave Beta Browser.
 The user needs help completing a javascript command.
@@ -17,8 +16,7 @@ Respond with a single, valid javascript command line. Their command line will be
 No explanation. No markdown. No markdown with backticks ` nor ```.
 
 An example of a command line could be `find the first div on the page` and a valid response would be `document.querySelector('div')`
-""")
-
+"""
 
 
 def generate_command(passed_context: str, use: Service):
