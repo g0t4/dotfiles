aa911c9cf7277721614001a5aec8ffabeadee9d8
Wes Higbee <wes.mcclure@gmail.com>
2024-12-22 03:44:39 -0600

    run security directly 45 ms, much beter

---
+++ zsh/universals/3-last/ask-openai/tests/security.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+def main():
+    try:
+        cmd = ['security', 'find-generic-password', '-s', 'openai', '-a', 'ask', '-w']
+        result = subprocess.run(cmd, text=True, capture_output=True, check=True)
+        print("Output:", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("Command failed!")
+        print("Error:", e.stderr)
