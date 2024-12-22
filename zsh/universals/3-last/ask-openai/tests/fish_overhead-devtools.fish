ff150fe2c63a1db9c626bdedd77a967bd39cd848
Wes Higbee <wes.mcclure@gmail.com>
2024-12-22 04:29:58 -0600

    ntoes

--- zsh/universals/3-last/ask-openai/tests/fish_overhead-devtools.fish
+++ zsh/universals/3-last/ask-openai/tests/fish_overhead-devtools.fish
@@ -1,6 +1,15 @@
 #!/opt/homebrew/bin/fish
 
 # STDIN==clipboard
+# TEST WITH:
+#    make sure set ask_use_* to match groq below or w/e you use below
+#       FYI, --ollama is fastest b/c no security key lookup
+#    hyperfine ./tests/fish_overhead-devtools.fish
+#    first, comment out generate in suggest.py so we only count overhead before/after API call
+#    the compare it to python directly:
+#    hyperfine -- 'echo foo | python3 devtools.py --groq'
+
+
 
 set -l _python3 "$WES_DOTFILES/.venv/bin/python3"
 set -l _single_py "$WES_DOTFILES/zsh/universals/3-last/ask-openai/devtools.py"
