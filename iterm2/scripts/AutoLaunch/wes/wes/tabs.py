de243a80f955737548c64fb44d47eb3fc3825714
Wes Higbee <wes.mcclure@gmail.com>
2024-12-23 14:22:59 -0600

    split

---
+++ iterm2/scripts/AutoLaunch/wes/wes/tabs.py
@@ -0,0 +1,24 @@
+from common import *
+
+
+async def close_other_tabs(connection):
+    current_tab = await get_current_tab(connection)
+    if current_tab is None:
+        return
+
+    assert current_tab.window is not None
+    for tab in current_tab.window.tabs:
+        if tab != current_tab:
+            await tab.async_close()
+
+
+async def new_tab_then_close_others(connection):
+    prior_window = await get_current_window(connection)
+    if prior_window is None:
+        return
+
+    # make new tab and close all other tabs in current window
+    new_tab = await prior_window.async_create_tab()
+    for tab in prior_window.tabs:
+        if tab != new_tab:
+            await tab.async_close(force=True)
--- iterm2/scripts/AutoLaunch/wes/wes/wes.py
+++ iterm2/scripts/AutoLaunch/wes/wes/wes.py
@@ -1,9 +1,10 @@
 import iterm2
 
-from scrape_ask import copy_screen_to_clipboard
-from f9command import on_f9
+from scrape_ask import *
+from f9command import *
 from common import *
-from og_ask import ask_openai
+from og_ask import *
+from tabs import *
 
 
 async def main(connection: iterm2.Connection):
@@ -40,29 +41,6 @@ async def main(connection: iterm2.Connection):
         while True:
             keystroke = await mon.async_get()
             await keystroke_handler(keystroke)
-
-
-async def close_other_tabs(connection):
-    current_tab = await get_current_tab(connection)
-    if current_tab is None:
-        return
-
-    assert current_tab.window is not None
-    for tab in current_tab.window.tabs:
-        if tab != current_tab:
-            await tab.async_close()
-
-
-async def new_tab_then_close_others(connection):
-    prior_window = await get_current_window(connection)
-    if prior_window is None:
-        return
-
-    # make new tab and close all other tabs in current window
-    new_tab = await prior_window.async_create_tab()
-    for tab in prior_window.tabs:
-        if tab != new_tab:
-            await tab.async_close(force=True)
 
 
 iterm2.run_forever(main)
