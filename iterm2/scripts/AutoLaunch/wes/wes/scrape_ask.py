6bf814574e1ca486a6ea8aec577ea77d4c847b60
Wes Higbee <wes.mcclure@gmail.com>
2024-12-23 08:03:26 -0600

    hack a cpoy of this

--- iterm2/scripts/AutoLaunch/wes/wes/scrape_ask.py
+++ iterm2/scripts/AutoLaunch/wes/wes/scrape_ask.py
@@ -9,6 +9,16 @@ import itertools
 
 from common import get_session
 
+# TODO MAKE COMMON VERSION AND TOGGLE
+DEBUG = True
+
+
+def log(msg):
+    # FYI check Cmd+Alt+J to see iterm2 logs (script console), with this output
+    if not DEBUG:
+        return
+    print(msg)
+
 
 async def copy_screen_to_clipboard(connection: iterm2.Connection):
     app = await iterm2.async_get_app(connection)
@@ -56,6 +66,70 @@ async def copy_screen_to_clipboard(connection: iterm2.Connection):
     changed_text = [l for l in changed_text if l != ""]
     print(f"diff: {changed_text}")
     # TODO concat lines? or?
-    pyperclip.copy("\n".join(changed_text))
+    current_command = "\n".join(changed_text)
+    # pyperclip.copy(current_command) # don't need now!
 
-    # await session.async_send_text("TODO finish ask-openai scrape")
+    # TODO extract shared logic with the other ask-openai async script! will be cool to do this async too!
+
+    # try get ask_os from shell integration, should be correct unless using a remote that doesn't have shell integration in the underlying shell that launched the REPL that is running and complete here (i.e. fish + gdb)
+    ask_os = await session.async_get_variable("user.ask_os")
+    if ask_os is None:
+        # fallback to iterm2's host os (not specific to a remote shell)
+        ask_os = platform.system()
+
+    use, client = get_ask_client()
+
+    messages = [{
+        "role":
+        "system",
+        "content":
+        "You are a command line expert. Respond with a single, valid, complete command line. I intend to execute it. No explanation. No markdown. DO NOT respond with leading ``` nor trailing ```"
+    }, {
+        "role": "user",
+        "content": f"env: shell={jobName} on uname={ask_os}\nquestion: {current_command}"
+    }]
+    # TODO LOGGING like other scripts (have toggle in one spot)
+    print(f"messages: {messages[1]['content']}")
+
+    # await task_clear  # ? why can't I put this after try/catch (smth happens with timing to not actually clear the prompt if I do that, but only on remote pi7.lan?)
+    # ALREADY cleared b/c I had to, to get the diff (hopefully clear!)
+
+    # TODO uncomment and patch up anthropic support when I MERGE this with other one in a comomn helper
+    # if use.name == "anthropic":
+    #     # PRN impl streaming anthropic here based on httpx only
+    #     # TODO impl it elsewhere and plug it in here, testing by restarting wes.py is a PITA
+    #     from single import get_anthropic_suggestion
+    #     command = get_anthropic_suggestion(current_command, use)
+    #     await session.async_send_text(command)
+    #     return
+
+    # *** request completion
+    try:
+        response_stream = await client.chat.completions.create(model=use.model, messages=messages, max_tokens=200, stream=True)
+    except Exception as e:
+        # TODO test timeouts?
+        log(f"Error contacting OpenAI: {e}")
+        await session.async_send_text(f"Error contacting API endpoint: {e}")
+        return
+
+    # *** stream the reponse chunks
+    # TODO write some tests for sanitizing and use a seam here
+
+    first_chunk = True
+    async for chunk in response_stream:
+        # FYI w.r.t ``` ... deepseek-chat listens to request to not use ``` and ``` but deepseek-coder always returns ```... that actually makes sense for the coder...
+        if chunk.choices[0].delta.content is not None:
+            # strip new lines to avoid submitting commands prematurely, is there an alternate char I could use to split the lines still w/o submitting (would have to check what the shell supports, if anything is possible)... one downside to not being a part of the line editor.. unless there is a workaround? not that I care much b/c multi line commands are not often necessary...
+            sanitized = chunk.choices[0].delta.content.replace("\n", " ")
+            if first_chunk:
+                print(f"first_chunk: {sanitized}")
+                sanitized = re.sub(r'```', '', sanitized).lstrip()
+                print(f"sanitized: {sanitized}")
+                print(f"sanitized hex: {sanitized.encode('utf-8').hex()}")
+                first_chunk = sanitized == ""  # stay in "first_chunk" mode until first non-empty chunk
+                await session.async_send_text(sanitized)
+            else:
+                await session.async_send_text(sanitized)
+            # TODO is there a way to detect last chunk?
+    # after last chunk, can I remove ending ``` and spaces? it might span multiple last chunks btw so wouldn't just be able to keep track of last chunk, would need entire response and then detect if ends with ``` and spaces and then delete those chars?
+    # ideally I would have some sort of streaming mechanism that would detect leading/trailing ``` and spaces... and then no correction is needed to delete chars`
