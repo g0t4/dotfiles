b309736e68075095b4e6c9fd81c634b429bb4098
Wes Higbee <wes.mcclure@gmail.com>
2024-12-23 15:24:45 -0600

    re-enable! awesome

--- iterm2/scripts/AutoLaunch/wes/wes/scrape_ask.py
+++ iterm2/scripts/AutoLaunch/wes/wes/scrape_ask.py
@@ -86,7 +86,7 @@ async def copy_screen_to_clipboard(connection: iterm2.Connection, history: bool 
 
     # await task_clear  # in this case, already waited for clear above
 
-    # await ask_openai_async_type_response(session, messages)
+    await ask_openai_async_type_response(session, messages)
 
 
 async def get_previous_output(session: iterm2.Session):
