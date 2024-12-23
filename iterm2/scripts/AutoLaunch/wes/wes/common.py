6eb04272c749a21df58295aaf9852e3134316978
Wes Higbee <wes.mcclure@gmail.com>
2024-12-23 12:58:48 -0600

    cleanup some warnings

--- iterm2/scripts/AutoLaunch/wes/wes/common.py
+++ iterm2/scripts/AutoLaunch/wes/wes/common.py
@@ -5,8 +5,19 @@ async def get_session(connection: iterm2.Connection):
 
     # TODO is there a simpler way already built into the API?
     app = await iterm2.async_get_app(connection)
-    window = app.current_terminal_window
+    if app is None:
+        print("No current app")
+        return
+    window = app.current_window
     if window is None:
         print("No current terminal window")
         return
-    return window.current_tab.current_session
+    tab = window.current_tab
+    if tab is None:
+        print("No current tab")
+        return
+    session = tab.current_session
+    if session is None:
+        print("No current session")
+        return
+    return session
