4da4c1caeea5c89642957fb8bb94e63fe6923dfe
Wes Higbee <wes.mcclure@gmail.com>
2024-12-23 01:55:51 -0600

    ~/.editrc

--- install/symlinks.fish
+++ install/symlinks.fish
@@ -2,3 +2,6 @@
 
 # start moving general symlinks to dotfiles here (consolidate them?)
 ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/inputrc/.inputrc ~/.inputrc
+
+# editline:
+ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/editrc/.editrc ~/.editrc
