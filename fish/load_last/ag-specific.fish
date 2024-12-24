e1c0dc36cbcf449365d7d4485dfdc08c4dcfed72
Wes Higbee <wes.mcclure@gmail.com>
2024-12-24 10:08:01 -0600

    format + work on agimages (add ico, also start agimages-global to look in common spots for a search term)

--- fish/load_last/ag-specific.fish
+++ fish/load_last/ag-specific.fish
@@ -8,17 +8,17 @@
 
 # FYI colors are defined by fish/zsh respectively in color-specific.{fish,zsh}
 function ag
-  command ag --nogroup --color-match "$__color_matching_text" $argv
+    command ag --nogroup --color-match "$__color_matching_text" $argv
 end
 # FYI can defer expand color variable so order of startup files is irrelevant
 # --nogroup => disable grouping to show file/line per match to click to open in vscode (via iterm links)
 
-abbr --set-cursor='!' --  agi 'ag -i "!"'
-abbr --set-cursor='!' --  agg 'ag -ig "!"'
-abbr --set-cursor='!' --  agh 'ag --hidden -i "!"' # match hidden files, but not ignored files
-abbr --set-cursor='!' --  aggh 'ag --hidden -ig "!"' # match hidden files, but not ignored files
-abbr --set-cursor='!' --  agu 'ag --unrestricted -i "!"' # match hidden files + ignored files
-abbr --set-cursor='!' --  aggu 'ag --unrestricted -ig "!"' # match hidden files + ignored files
+abbr --set-cursor='!' -- agi 'ag -i "!"'
+abbr --set-cursor='!' -- agg 'ag -ig "!"'
+abbr --set-cursor='!' -- agh 'ag --hidden -i "!"' # match hidden files, but not ignored files
+abbr --set-cursor='!' -- aggh 'ag --hidden -ig "!"' # match hidden files, but not ignored files
+abbr --set-cursor='!' -- agu 'ag --unrestricted -i "!"' # match hidden files + ignored files
+abbr --set-cursor='!' -- aggu 'ag --unrestricted -ig "!"' # match hidden files + ignored files
 # ignored files: .ignore, .gitignore, --ignore, etc
 # hidden files: .config, .git (dotfiles/dirs)
 
@@ -30,9 +30,49 @@ abbr agl 'ag -l' # print file name only, not matched content
 abbr agL 'ag -L' # print files w/o content match
 abbr agw 'ag --word-regexp' # match whole words
 abbr agz 'ag --search-zip' # search inside zip files (gz,xz only)
+
+
+
+
+
+
+
 function agimages
-    for f in (ag --unrestricted -i -g "\.(png|jpg|jpeg|gif|bmp|tiff|webp|svg|icns)")
+    set look_in_dir $argv[1] # optional
+    for f in (ag --unrestricted -i -g "\.(png|jpg|jpeg|gif|bmp|tiff|webp|svg|icns|ico)" $look_in_dir)
         echo $f
         imgcat $f
     end
+
+    ## TODOS
+    # TODO why does imgcat fail on some svgs? can I detect and avoid that? => look at ~/Applications/DataSpell.app/Contents/plugins/pycharm-ds-customization/jupyter-web/fontawesome-webfont.svg
+    # TODO can I limit size of image?
+end
+
+
+
+
+
+
+
+
+
+
+function _agimages-dir
+    for p in $argv[1]/*
+        if string match -q -r $argv[2] $p then
+            log_ --brblue "## $p"
+            agimages $p
+        end
+    end
+end
+function agimages-global
+    #-- take a search term, find in common places, i.e. /Applications/X.app/*
+    _agimages-dir /Applications "$argv"
+    _agimages-dir ~/Applications "$argv"
+    # what other spots?
+
+    # this direction would be useful if agimages also takes an optional search term to match the file name (in addition to the extension)
+    # simple-icons repo
+    #_agimages-dir ~/repos/github/simple-icons/simple-icons
 end
