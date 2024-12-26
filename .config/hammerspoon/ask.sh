9e5b3665d16855159b5b9e03a974c28a93948abb
Wes Higbee <wes.mcclure@gmail.com>
2024-12-26 01:04:04 -0600

    use bash script right to streamdeck button, actually both this and KM macro have same (almost nil overhead)... you can see if you unselect text and watch how fast text is selected to copy it, it's nearly instant... so I don't need this actually but it might be preferable? ... time is taken to wait for initial chunk (that is gonna be fixed with groq ;) )

---
+++ .config/hammerspoon/ask.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO is this faster than going through KM macro, IIAC it is.. there feels like a half-full second delay in KM macro execution of shell scripts
+
+/opt/homebrew/bin/hs -c 'AskOpenAIStreaming()'
+
