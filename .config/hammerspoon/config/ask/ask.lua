938e9765d7b8056b5606b6ae7ad7f7be9f8f8c1a
Wes Higbee <wes.mcclure@gmail.com>
2024-12-26 00:54:06 -0600

    timing done for now and yes 30ms seems fine so far, can go to 50ms and be absolutely fine even 100 would be probably ok

--- .config/hammerspoon/config/ask/ask.lua
+++ .config/hammerspoon/config/ask/ask.lua
@@ -20,49 +20,41 @@ function M.AskOpenAIStreaming()
     end
 
     local app = hs.application.frontmostApplication()
+    -- print_elapsed("frontmost") -- < 0.5ms
     -- todo use this to decide how to copy the current context... i.e. in AppleScript context I expect to already copy the relevant question part... whereas in devtools I just wanna grab the full command line and so I don't wanna have to select it myself...
     -- ALSO use app to select prompt!
     -- MAYBE even use context of the app (i.e. in devtools) to decide what prompt to use
     --    COULD also have diff prmopts tied to streamdeck buttons (app specific) if I find it useful to control the prompt instead of trying to guess it based on current app... (app by app basis that I care to do this for)
-
-    -- print_elapsed("frontmost") -- < 0.5ms
+    -- CAREFUL NOT TO INTRODUCE A TON OF OVERHEAD HERE in app detection
 
     if app:name() == "iTerm2" then
         hs.alert.show("use iterm specific shortcut for ask-openai")
         return
     end
-
     -- print_elapsed("appname") -- < 0.5ms
 
-    -- -- trigger select all =>
     -- TODO pass app as last arg to keyStroke? does it matter for timing?
-    -- -- *** standalone 200ms!!!
-    -- TODO how long can keystroke delay be?
-    local keystrokeDelay = 25000 -- worked: 100ms, 50ms
-    hs.eventtap.keyStroke({ "cmd" }, "a", keystrokeDelay)
-    -- OK 100ms is working fine for now... and feels maybe fast enough?
-    print_elapsed("select all")
-    -- -- trigger copy
-    hs.eventtap.keyStroke({ "cmd" }, "c", keystrokeDelay)
-    -- -- *** standalone 200ms!!!
-    print_elapsed("copy")
+    local keystrokeDelay = 30000 -- worked: 100ms, 50ms,30m (works, can try lower)... wow we're good at even 25ms/20ms IMO
+    -- FYI make sure to remove all print statements when testing delays b/c that adds overhead between keystrokes (if used) and will not be there in reality
+    -- FYI 5ms sometimes worked then other times failed... 20 is NBD
     -- https://www.hammerspoon.org/docs/hs.eventtap.html#keyStroke (200ms is default keystroke delay)
+    --
+    -- trigger select all:
+    hs.eventtap.keyStroke({ "cmd" }, "a", keystrokeDelay)
+    -- trigger copy:
+    hs.eventtap.keyStroke({ "cmd" }, "c", keystrokeDelay)
 
     -- HOW else can I copy faster? or get the text?!
     -- FYI can try applescript but I dont think that's necessary, unless issuing keystrokes is also slow, the 200 ms is artificial (potentially)
     -- can system events select all and copy for the front most app faster?
 
 
-    -- get prompt from clipboard:
-    local prompt = hs.pasteboard.getContents()
-    print_elapsed("got prompt")
-    print("Prompt:", prompt)
+    local prompt = hs.pasteboard.getContents() -- < 0.6ms
+    -- print("Prompt:", prompt)
 
-    if true then
-        return
-    end
-
-
+    -- if true then
+    --     return
+    -- end
 
     -- TODO lookup ask-open service from ~/.local/share/ask/service
     -- JUST cache it on startup, cuz I can always trigger reload config for ask-openai to switch it -- ZERO latency feels best and is the goal for this rewrite
