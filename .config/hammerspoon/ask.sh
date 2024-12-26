0a4d2f4b9f96224750107e2e11c310ad178d1d89
Wes Higbee <wes.mcclure@gmail.com>
2024-12-26 01:05:54 -0600

    notes

--- .config/hammerspoon/ask.sh
+++ .config/hammerspoon/ask.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# TODO is this faster than going through KM macro, IIAC it is.. there feels like a half-full second delay in KM macro execution of shell scripts
+# FYI this is no faster than using KM macro => shell script
+# unselect text in devtools, watch how fast its selected aftger button press
+#   button to this script is same speed to select text as the button to KM macro to run a similar script, almost instant
+# delay is clearly visible after text is selected and before the first chunk arrives
 
 /opt/homebrew/bin/hs -c 'AskOpenAIStreaming()'
 
