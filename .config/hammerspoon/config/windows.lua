0e5316936a37f600caed3285de66b075a86a6e8c
Wes Higbee <wes.mcclure@gmail.com>
2024-12-30 16:01:25 -0600

    bounce back screen move

--- .config/hammerspoon/config/windows.lua
+++ .config/hammerspoon/config/windows.lua
@@ -2,12 +2,25 @@ local tripleModifiers = { "cmd", "shift", "ctrl" }
 local doubleModifiers = { "cmd", "shift" }
 hs.loadSpoon("WinWin")
 hs.hotkey.bind(tripleModifiers, "Right", function()
+    -- leave this as right only, no bounce... only so I can see if I start using right... cuz I bet with bounce I will just use left exclusively since that's all I was using before
     spoon.WinWin:moveToScreen("right")
 end)
 hs.hotkey.bind(tripleModifiers, "Left", function()
-    -- now I can do left too (bettersnap only had cycle displays key)
+    -- SUPER cool to see most logic resides in hs.screen/hs.window... I don't really even need WinWin/WIndowHalfsAndThirds
+
+    -- FYI can impl smth like hs.screen.toWest() but would need to find the furthest west screen
+    --    https://github.com/Hammerspoon/hammerspoon/blob/master/extensions/screen/screen.lua#L305-L342
+    -- HONESTLY I only have two screens at most so I don't care that much :) do the bounce until I have 3
+
+    local win = hs.window.focusedWindow()
+    local beforeScreen = win:screen()
     spoon.WinWin:moveToScreen("left")
-    -- PRN if cannot move to the left, cycle to the right most display? or let it stop as it is now?
+    local afterScreen = win:screen()
+    if beforeScreen ~= afterScreen then
+        return
+    end
+    -- bound back when on leftmost, mirror what I got used to with bettersnap (albeit this is not cycling but bouncing)
+    spoon.WinWin:moveToScreen("right")
 end)
 
 
