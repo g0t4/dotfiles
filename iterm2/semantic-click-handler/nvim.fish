4c4c8d6b6c441a50bb40b11f8b266a9d864af6c5
Wes Higbee <wes.mcclure@gmail.com>
2025-01-04 01:49:03 -0600

    w00t first pass works to open nvim window w/ clicked path! and workspace set to repo root (or cwd if not repo)

---
+++ iterm2/semantic-click-handler/nvim.fish
@@ -0,0 +1,83 @@
+#!/opt/homebrew/bin/fish
+
+# *** docs from iterm2 prefs => profile => advanced tab => semantic history ***
+# In this mode semantic history will only be activated when you click on an existing file name.
+# You can provide substitutions as follows:
+#   \1 will be replaced with the filename.
+#   \2 will be replaced with the line number.
+#   \3 will be replaced with the text before the click.
+#   \4 will be replaced with the text after the click.
+#   \5 will be replaced with the working directory.
+#
+# This is also an interpolated string evaluated in the context of the current session. In addition to the usual variables, the following substitutions are available:
+#   \(semanticHistory.path) will be replaced with the filename.
+#   \(semanticHistory.lineNumber) will be replaced with the line number.
+#   \(semanticHistory.columnNumber) will be replaced with the column number.
+#   \(semanticHistory.prefix) will be replaced with the text before the click.
+#   \(semanticHistory.suffix) will be replaced with the text after the click.
+#   \(semanticHistory.workingDirectory) will be replaced with the working directory.
+# *** end docs ***
+#
+# *** FYI I used this value in iterm2 semantic history text box:
+#   set drop down to "Run command"
+#      TODO try "Always run command" which can work with text that is not an existing file => i.e. parse text for things like github "org/repo" in output, and open in browser at github.com?
+#   $HOME/repos/wes-config/wes-bootstrap/subs/dotfiles/iterm2/semantic-click-handler/advanced.fish \1 "\2" "unused" "unused" \5
+#       \1,\5 => iTerm escapes "\ " the paths, so I don't need "", AND if I add "" then logic below sees them as "\ " not " " and then fails (ie test if directory)
+#           PRN if I have further issues with escaping, I should look in iTerm2 source/docs for how it interpolates...
+#       \2 => leave "\2" because if there is no line number then the blank means the args shift left (\3 => \2, \4 => \3, etc)
+#       \3 \4 => unused => not using \3 \4 and I had an old note about them causing issues so I replaced with "unused" so the other args stay in the same position
+#
+
+## TROUBLESHOOTING (iterm2 shows output on a failure, so add a failure to inspect these values)
+for i in (seq 1 6)
+    echo "[DEBUG] arg $i: $argv[$i]"
+end
+
+set clicked_path $argv[1] # dir or file, must exist if iTerm2 allowed to click open it
+set line_number $argv[2]
+set text_before_click $argv[3]
+set text_after_click $argv[4]
+set working_directory $argv[5]
+
+# *** handle directory clicks ***
+if test -d "$clicked_path"
+    # if clicked path is a directory...
+    open "$clicked_path" # open dir in Finder (default handler)
+    # call_code "$clicked_path" # open dir in vscode
+    exit 0
+end
+
+# # USE file command to decide if I want to use default handlers to open (or specific handler)
+set _mime_type (file --brief --mime-type "$clicked_path")
+echo "[DEBUG]: mime type: $_mime_type"
+
+# *** pdfs ***
+if string match --quiet "$_mime_type" application/pdf
+    # find test cases:     ag -ig "pdf\$"
+    open "$clicked_path" # open w/ default handler
+    exit 0
+end
+
+# *** images ***
+if string match --quiet --regex "image/.*" "$_mime_type"
+    # find test cases:     ag -ig "jpg|gif|png"
+    open "$clicked_path" # open w/ default handler
+    # PRN open w/ vscode instead? I don't really mind that aside from issues with svgs
+    exit 0
+end
+
+# *** default => vscode ***
+# scope vscode to the repo root, else working directory if not a repo
+set repo_root $(git -C $working_directory rev-parse --show-toplevel 2>/dev/null)
+if test $status -ne 0
+    set repo_root $working_directory
+end
+
+# hack to get uv to run w/ venv (todo remove need for cd here)
+cd $WES_DOTFILES/iterm2/semantic-click-handler
+# why doesn't `uv run` find the venv in the parent dir of the script?
+uv run nvim.py \
+    "$clicked_path" "$line_number" "$text_before_click" \
+    "$text_after_click" "$working_directory" "$repo_root"
+
+#exit 1 # if you wanna see debug STDOUT messages above, uncomment this line and click "View" in the iTerm2 dialog
--- iterm2/semantic-click-handler/nvim.py
+++ iterm2/semantic-click-handler/nvim.py
@@ -1,17 +1,41 @@
+from re import I
 import iterm2
 
+import sys
 
+clicked_path = sys.argv[1]
+line_number = sys.argv[2]
+text_before_click = sys.argv[3]
+text_after_click = sys.argv[4]
+working_directory = sys.argv[5]
+repo_root = sys.argv[6]
+
+print(f"py - clicked_path: {clicked_path}")
+print(f"py - line_number: {line_number}")
+print(f"py - text_before_click: {text_before_click}")
+print(f"py - text_after_click: {text_after_click}")
+print(f"py - working_directory: {working_directory}")
+print(f"py - repo root: {repo_root}")
+
+
+# exit(0) # for testing, uncomment to stop here
+
+# foo
 async def open_nvim_window(connection: iterm2.Connection):
     # neat thing is, this new nvim window if started with nvim then when nvim is closed, window closes too! no shell to go back to (so this becomes very much like what I had with vscode before)
+
     profile = iterm2.LocalWriteOnlyProfile()
-    profile.set_custom_directory("/Users/wesdemos/repos")
+    profile.set_custom_directory(repo_root)
     profile.set_initial_directory_mode(iterm2.InitialWorkingDirectory.INITIAL_WORKING_DIRECTORY_CUSTOM)
 
     # advanced dirs lets you set window/tab/pane specific dirs (not one working dir), so I don't need that for now
     # profile.set_advanced_working_directory_window_directory("/Users/wesdemos/repos")
     # profile.set_initial_directory_mode(iterm2.InitialWorkingDirectory.INITIAL_WORKING_DIRECTORY_ADVANCED)
 
-    profile.set_command("/opt/homebrew/bin/nvim")
+    cmd = f"/opt/homebrew/bin/nvim '{clicked_path}'"
+    if line_number:
+        cmd += f" +{line_number}"
+    profile.set_command(cmd)
     profile.set_use_custom_command("Yes")
 
     # IIUC command/profile_customizations are mutually exclusive
