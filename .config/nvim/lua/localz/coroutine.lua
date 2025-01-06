4898612dae38394cc3864cf13521134cc78770a6
Wes Higbee <wes.mcclure@gmail.com>
2025-01-06 04:24:58 -0600

    twice only

--- .config/nvim/lua/localz/coroutine.lua
+++ .config/nvim/lua/localz/coroutine.lua
@@ -1,13 +1,11 @@
-co = coroutine.create(function(a, b, c)
-    while true do
-        print("co", a, b, c)
-        coroutine.yield()
-        --  sleep for a second, call callback that calls yield
-    end
+co = coroutine.create(function(a)
+    print("co", a, coroutine.status(co))
+    x = coroutine.yield(a)
+    print("co", x, coroutine.status(co))
+    --  sleep for a second, call callback that calls yield
 end)
-print("co", coroutine.status(co))
+print("out", coroutine.status(co))
 coroutine.resume(co, 1, 2, 3) --> co  1  2  3
-print("co", coroutine.status(co))
+print("out", coroutine.status(co))
 coroutine.resume(co, 4, 5, 6) --> co  4  5  6
-print("co", coroutine.status(co))
-coroutine.resume(co, 7, 8, 9) --> co  7  8  9
+print("out", coroutine.status(co))
