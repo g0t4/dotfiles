b48b8dab9b10a447fb94a38764cf297c9b789d81
Wes Higbee <wes.mcclure@gmail.com>
2025-01-05 22:20:35 -0600

    fix for dir creating nested too

--- install/symlinks.fish
+++ install/symlinks.fish
@@ -3,17 +3,17 @@
 mkdir -p ~/.config
 
 # start moving general symlinks to dotfiles here (consolidate them?)
-ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/inputrc/.inputrc ~/.inputrc
+ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/inputrc/.inputrc ~/.
 
 # editline:
-ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/editrc/.editrc ~/.editrc
+ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/editrc/.editrc ~/.
 
 # hammerspoon:
 #
 mkdir -p ~/.hammerspoon
-ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/hammerspoon/init.lua ~/.hammerspoon/init.lua
-ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/hammerspoon/config ~/.hammerspoon/config
+ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/hammerspoon/init.lua ~/.hammerspoon/.
+ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/hammerspoon/config ~/.hammerspoon/.
 
 # ghostty
 mkdir -p ~/.config/ghostty
-ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/ghostty/config ~/.config/ghostty/config
+ln -s ~/repos/wes-config/wes-bootstrap/subs/dotfiles/.config/ghostty/config ~/.config/ghostty/.
