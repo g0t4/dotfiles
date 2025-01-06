c59773806e447af31d11626299656781e364990f
Wes Higbee <wes.mcclure@gmail.com>
2025-01-06 04:19:13 -0600

    status

--- .config/nvim/lua/localz/coroutine.lua
+++ .config/nvim/lua/localz/coroutine.lua
@@ -2,8 +2,12 @@ co = coroutine.create(function(a, b, c)
     while true do
         print("co", a, b, c)
         coroutine.yield()
+        --  sleep for a second, call callback that calls yield
     end
 end)
+print("co", coroutine.status(co))
 coroutine.resume(co, 1, 2, 3) --> co  1  2  3
-coroutine.resume(co, 1, 2, 3) --> co  1  2  3
-coroutine.resume(co, 1, 2, 3) --> co  1  2  3
+print("co", coroutine.status(co))
+coroutine.resume(co, 4, 5, 6) --> co  4  5  6
+print("co", coroutine.status(co))
+coroutine.resume(co, 7, 8, 9) --> co  7  8  9
