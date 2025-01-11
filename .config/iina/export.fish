fbf318728f7aad4d3add4fd088d7e6a9090c02a3
Wes Higbee <wes.mcclure@gmail.com>
2025-01-10 23:45:18 -0600

    export plist for iina in xml format, strip keys I dont wanna track

---
+++ .config/iina/export.fish
@@ -0,0 +1,31 @@
+#!/usr/bin/env fish
+
+# defaults export com.colliderli.iina iina.plist
+
+# export as xml
+plutil -convert xml1 ~/Library/Preferences/com.colliderli.iina.plist -o test.plist
+
+# alternatively, I could allow list the keys to keep
+#   I like idea of denylist b/c then I am forced to review new keys from settings I might've changed recently though
+
+# FYI seems like settings keys start lowercase, so might wanna filter on that primarily or just when reviewing use that to consider what to keep
+
+# TODO move common keys to a shared file to filter from all plists once I add this to more apps
+set rm_keys MainWindowLastPosition NSFullScreenMenuItemEverywhere \
+    NSNavPanelExpandedSizeForOpenMode NSOSPLastRootDirectory \
+    recentDocuments "NSWindow Frame IINAWelcomeWindow" \
+    "NSWindow Frame IINAInspectorPanel" "NSWindow Frame IINAPreferenceWindow" \
+    "NSWindow Frame NSColorPanel" "NSWindow Frame NSNavPanelAutosaveName" \
+    "NSToolbar Configuration com.apple.NSColorPanel" \
+    "NSSplitView Subview Frames NSColorPanelSplitView" \
+    iinaLastPlayedFilePath iinaLastPlayedFilePosition \
+    controlBarPositionHorizontal controlBarPositionVertical
+
+# exclude anything that would vary (ie window positions, last played file)
+for key in $rm_keys
+    /usr/libexec/PlistBuddy -c "Delete ':$key'" test.plist
+end
+
+
+# critical settings:
+#   currentInputConfigName => VLC Default  # for now, use vlc keys that I already know
