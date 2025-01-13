83fd75751c15333b4e1dc5f5ce759102361a22ae
Wes Higbee <wes.mcclure@gmail.com>
2025-01-13 02:41:04 -0600

    open apps

--- .config/hammerspoon/config/learn/hsapp.lua
+++ .config/hammerspoon/config/learn/hsapp.lua
@@ -12,10 +12,17 @@ Dump(vlc)
 -- IF app running:
 --    { <userdata 1> -- hs.application: VLC }
 local ppt = hs.application.applicationsForBundleID("com.microsoft.PowerPoint") -- returns hs.application object
-Dump("ppt", ppt)
-
+Dump("ppt", ppt) -- FYI so far... not showing ppt instance that is running... must be bundle id difference?
+-- TODO how can I lookup bundle id of running apps?
 -- local pptInfo = hs.application.infoForBundleID("com.microsoft.PowerPoint") -- returns hs.application object
 -- Dump("pptInfo", pptInfo) -- UTT infos, etc -- IIAC Info.plist(s) are used to make this
+
+-- IIUC use name in App Switcher
+-- hs.application.launchOrFocus("PowerPoint") -- DOES NOT WORK
+-- hs.application.launchOrFocus("Microsoft PowerPoint") -- works
+-- hs.application.launchOrFocus("vlc") -- works
+
+
 
 
 -- TODO? hs.application.find
