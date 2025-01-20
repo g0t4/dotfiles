cf39c39a99a4dcf18e11aff2206709dfcf5994fd
Wes Higbee <wes.mcclure@gmail.com>
2025-01-19 18:39:12 -0600

    bring to top

--- .config/hammerspoon/config/uielements.lua
+++ .config/hammerspoon/config/uielements.lua
@@ -165,6 +165,9 @@ local function ensureWebview()
         return
     end
     printWebView:frame(rect)
+    print("raising")
+    printWebWindow:raise() -- ensure on top for Hammerspoon app
+    printWebWindow:application():activate() -- ensure Hammerspoon app is front most too (else all app windows remain in the background)
 end
 
 
