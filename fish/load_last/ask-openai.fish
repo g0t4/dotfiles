fadab77b88a4d3eada37aebda51eac22b55b7eb2
Wes Higbee <wes.mcclure@gmail.com>
2025-01-21 15:02:26 -0600

    add model completions using /models API, duh

--- fish/load_last/ask-openai.fish
+++ fish/load_last/ask-openai.fish
@@ -44,8 +44,33 @@ function ask_use_ollama
     _ask_write_state --ollama $argv
 end
 
-complete --command ask_use_ollama --no-files \
-    -a "(ollama list | tail -n +2 | cut -d ' ' -f1)"
+function get_openai_models
+
+    # if first arg is ollama, use that instaed:
+    if test "$argv[1]" = ollama
+        set base_url http://localhost:11434/v1/models
+        set api_key ""
+    else if test "$argv[1]" = groq
+        set base_url https://api.groq.com/openai/v1/models
+        set api_key (security find-generic-password -s groq -a ask -w)
+    else if test "$argv[1]" = deepseek
+        set base_url https://api.deepseek.com/v1/models
+        set api_key (security find-generic-password -s deepseek -a ask -w)
+    else
+        set base_url https://api.openai.com/v1/models
+        set api_key (security find-generic-password -s openai -a ask -w)
+    end
+
+    curl -H "Authorization: Bearer $api_key" \
+        -fsSL $base_url \
+        | jq ".data[].id" -r
+
+end
+
+complete --command ask_use_ollama --no-files -a '(get_openai_models ollama)'
+complete --command ask_use_openai --no-files -a '(get_openai_models)'
+complete --command ask_use_groq --no-files -a '(get_openai_models groq)'
+complete --command ask_use_deepseek --no-files -a '(get_openai_models deepseek)'
 
 function ask_openai
 
