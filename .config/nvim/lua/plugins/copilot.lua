abf8a1a14ab234ac080220eeb85e22231df761aa
Wes Higbee <wes.mcclure@gmail.com>
2025-01-23 08:26:43 -0600

    switch local

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -164,7 +164,8 @@ local llm_nvim = {
 
 
             -- TODO if it works, try all sorts of models (you've never tested any of them for this use case!)
-            url = "http://build21.lan:11434", -- llm-ls uses "/v1/completions"
+            -- url = "http://build21.lan:11434", -- llm-ls uses "/v1/completions"
+            url = "http://localhost:11434",
 
         })
     end
