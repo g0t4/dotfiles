e9e24718f304fbac3b9f288e3570e35d7a73d841
Wes Higbee <wes.mcclure@gmail.com>
2025-01-24 11:45:42 -0600

    notes

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,3 +1,3 @@
-
-print("my ftplugin/lua.lua")
 -- run in after so you guarantee that this is last to run (wins) vs bundled nvim ftplugin/lua.* scripts
+
+-- print("my ftplugin/lua.lua") -- uncomment in other places, including nvim's bundled scripts to double check order (or use :scriptnames, IIRC those are in order)
