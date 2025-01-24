3a04cf5f82042048489dc878f647be41f3e41cc6
Wes Higbee <wes.mcclure@gmail.com>
2025-01-24 11:37:47 -0600

    add after dir as reminder

---
+++ .config/nvim/after/ftplugin/lua.lua
@@ -0,0 +1,3 @@
+
+print("my ftplugin/lua.lua")
+-- run in after so you guarantee that this is last to run (wins) vs bundled nvim ftplugin/lua.* scripts
--- .config/nvim/ftplugin/lua.lua
+++
@@ -1,3 +0,0 @@
-
--- TODO does defining this override the one installed (bundled) with nvim?
--- TODO did I add this to do override it? what happens if removed (check later)
