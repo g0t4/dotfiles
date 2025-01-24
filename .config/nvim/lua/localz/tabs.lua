575e780c8f4938536c8c9d051c0e0a57259353cf
Wes Higbee <wes.mcclure@gmail.com>
2025-01-24 10:12:34 -0600

    review

--- .config/nvim/lua/localz/tabs.lua
+++ .config/nvim/lua/localz/tabs.lua
@@ -1,6 +1,6 @@
 -- definitely move this elsewhere later
 -- wrap settings
-vim.o.wrap = false  -- global nowrap, consider local settings for this instead
+vim.o.wrap = false -- global nowrap, consider local settings for this instead
 vim.o.textwidth = 0 -- disable globally, add back if I find myself missing it
 vim.o.wrapmargin = 0 -- disable globally, add back if I find myself missing it
 -- TODO ftplugin/ is overriding textwidth for vim script... grrr
@@ -8,13 +8,16 @@ vim.o.wrapmargin = 0 -- disable globally, add back if I find myself missing it
 -- *** tabs
 -- I chose option 2 (always insert spaces, leave tabs as is with default tabstop=8)
 vim.o.expandtab = true -- insert spaces for tabs
-vim.o.softtabstop = 4  -- b/c expandtab is set, this is the width of an inserted tab in spaces
-vim.o.shiftwidth = 4   -- shifting: << >>
+vim.o.softtabstop = 4 -- b/c expandtab is set, this is the width of an inserted tab in spaces
+vim.o.shiftwidth = 4 -- shifting: << >>
 -- vim.o.tabstop -- leave as is (8) so existing uses of tabs match width likely intended
 
 -- *** show whitespace
 vim.opt.listchars = { tab = '→ ', trail = '·', space = '⋅' } -- FYI also `eol:$`
-vim.cmd("command! ToggleShowWhitespace if &list | set nolist | else | set list | endif")
+vim.cmd([[
+    command! ToggleShowWhitespace if &list | set nolist | else | set list | endif
+]])
+
 
 -- TODO: port from vimrc
 -- " *** review `autoindent`/`smartindent`/`cindent` and `smarttab` settings, I think I am fine as is but I should check
