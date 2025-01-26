f3cabb493092080ef870a073978b99b3cbd51140
Wes Higbee <wes.mcclure@gmail.com>
2025-01-26 00:07:09 -0600

    notes

--- iterm2/scripts/AutoLaunch/wes/wes/asyncs.py
+++ iterm2/scripts/AutoLaunch/wes/wes/asyncs.py
@@ -26,18 +26,19 @@ async def ask_openai_async_type_response(session, messages):
             stream=True)
     except Exception as e:
         # TODO test timeouts?
-        log(f"Error contacting OpenAI: {e}")
+        log(f"Error contacting Anthropic: {e}")
         await session.async_send_text(f"Error contacting API endpoint: {e}")
         return
 
     # *** stream the reponse chunks
     # TODO write some tests for sanitizing and use a seam here
 
+    # ! TODO... what actually differs here vs OpenAI...  can I just merge this logic into one handler, is it a diff field in response or?
     first_chunk = True
     async for chunk in response_stream:
         if chunk.choices[0].delta.content is not None:
             # strip new lines to avoid submitting commands prematurely, is there an alternate char I could use to split the lines still w/o submitting (would have to check what the shell supports, if anything is possible)... one downside to not being a part of the line editor.. unless there is a workaround? not that I care much b/c multi line commands are not often necessary...
-            sanitized = chunk.choices[0].delta.content.replace("\n", " ")
+            sanitized = chunk.choices[0].delta.content.replace("\n", " ") # TODO oh man this is not good.. I wanna keep new lines... I also might need to use semicolon (or shell specific separator to do this)
             if first_chunk:
                 log(f"first_chunk: {sanitized}")
                 sanitized = re.sub(r'```', '', sanitized).lstrip()
@@ -47,6 +48,6 @@ async def ask_openai_async_type_response(session, messages):
                 await session.async_send_text(sanitized)
             else:
                 await session.async_send_text(sanitized)
-            # TODO is there a way to detect last chunk?
+            # TODO is there a way to detect last chunk? => actually IIRC yes... there is a stop reason on each chunk, IIRC
     # after last chunk, can I remove ending ``` and spaces? it might span multiple last chunks btw so wouldn't just be able to keep track of last chunk, would need entire response and then detect if ends with ``` and spaces and then delete those chars?
     # ideally I would have some sort of streaming mechanism that would detect leading/trailing ``` and spaces... and then no correction is needed to delete chars`
