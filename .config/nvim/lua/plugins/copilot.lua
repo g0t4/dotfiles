7135a8e68242991505659c497769ffd9bb4c3fee
Wes Higbee <wes.mcclure@gmail.com>
2025-01-26 03:56:12 -0600

    verbose for now

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -414,7 +414,9 @@ return {
             -- model = "llama3.2-vision:11b", -- ollama list
             model = "qwen2.5-coder:3b",
             use_api_ollama = true,
-            api_url = "http://build21.lan:11434/v1/chat/completions"
+            api_url = "http://build21.lan:11434/v1/chat/completions",
+
+            verbose = true,
         },
 
         dependencies = {
