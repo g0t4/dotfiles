ef219dcce060364333e6887074e683c433db6471
Wes Higbee <wes.mcclure@gmail.com>
2025-01-31 12:12:02 -0600

    notes

--- install/arch/rocm.sh
+++ install/arch/rocm.sh
@@ -5,4 +5,11 @@ sudo pacman --needed --noconfirm -S \
 # not in path (which is fine, dont add to path, can just mod path for using hipcc, etc)
 /opt/rocm/bin/rocminfo
 
+
+# whisper.cpp uses openblas (do not install system wide, conflicts with hipblas used by ggml above, ugh)
+# wait are cblas/openblas/hipblas interchangeable? or not sure hip/cblas conflict actually they seem to work together
+sudo pacman --noconfirm -S openblas
+
+# TODO or opencl?
+
 # see examples repo for test cases of hipblas compiler hipcc
