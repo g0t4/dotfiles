5d07de03db5419d875748ca7389f44e1bca9583e
Wes Higbee <wes.mcclure@gmail.com>
2025-01-31 11:49:17 -0600

    notes

--- install/arch/rocm.sh
+++ install/arch/rocm.sh
@@ -2,5 +2,7 @@
 sudo pacman --needed --noconfirm -S \
     rocm-hip-sdk rocminfo rocm-hip-runtime hipblas
 
-# not in path
+# not in path (which is fine, dont add to path, can just mod path for using hipcc, etc)
 /opt/rocm/bin/rocminfo
+
+# see examples repo for test cases of hipblas compiler hipcc
