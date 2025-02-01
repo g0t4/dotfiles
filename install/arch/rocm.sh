9f93ea818886f8b7240ce7924167ff7dc8d770ff
Wes Higbee <wes.mcclure@gmail.com>
2025-02-01 17:58:48 -0600

    dedup

--- install/arch/rocm.sh
+++ install/arch/rocm.sh
@@ -1,32 +1,10 @@
-#
-# *** llama.cpp, first get it compiling w/o hipblas
-cmake -B build
-cmake --build build --config Release -j 12
-
-# *** ok nvm skip whisper for now, I can come back to it... go for the gold w/ llama.cpp
-# + HIP:   https://github.com/ggerganov/llama.cpp/blob/master/docs/build.md#hip
-#   hipblas is needed for amd gpus (offidially listed):   https://github.com/ggerganov/llama.cpp/blob/master/docs/build.md#hip
-#
-# activate hipcc "env"
-set PATH /opt/rocm/bin $PATH
-# -- fixes missing -lamdhip64 error
-set LIBRARY_PATH /opt/rocm/lib $LIBRARY_PATH
-export LIBRARY_PATH
-set LD_LIBRARY_PATH /opt/rocm/lib $LD_LIBRARY_PATH
-export LD_LIBRARY_PATH
-set CPATH /opt/rocm/include $CPATH
-export CPATH
-export LDFLAGS="-L/opt/rocm/lib"
-#
 # confirm device:
 rocminfo | grep gfx | head -1 | awk '{print $2}'
-#
-HIPCXX="$(hipconfig -l)/clang" HIP_PATH="$(hipconfig -R)" \
-    cmake -S . -B build -DGGML_HIP=ON -DAMDGPU_TARGETS=gfx1030 -DCMAKE_BUILD_TYPE=Release \
-    && cmake --build build --config Release -- -j 16
-# WORKS!
-# not compiled w/ libcurl ... so I have to download models, that's fine
-#
+
+
+
+
+
 # test model:
 # https://huggingface.co/Qwen/Qwen2.5-Coder-0.5B-Instruct-GGUF/tree/main # pick one, i.e.:
 wget https://huggingface.co/Qwen/Qwen2.5-Coder-0.5B-Instruct-GGUF/resolve/main/qwen2.5-coder-0.5b-instruct-q4_k_m.gguf
