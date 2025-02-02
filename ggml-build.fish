a3272a8b2032d0de9c13ab908409e9a89faf3de3
Wes Higbee <wes.mcclure@gmail.com>
2025-02-01 18:00:10 -0600

    review

---
+++ ggml-build.fish
@@ -0,0 +1,9 @@
+
+
+# *** ggml builds:
+#
+# AMD GPU support
+sudo pacman --needed --noconfirm -S \
+    rocm-hip-sdk rocminfo rocm-hip-runtime hipblas
+
+
--- install/arch/rocm.sh
+++ install/arch/rocm.sh
@@ -1,3 +1,9 @@
+# AMD GPU support
+sudo pacman --needed --noconfirm -S \
+    rocm-hip-sdk rocminfo rocm-hip-runtime hipblas
+
+
+
 # confirm device:
 rocminfo | grep gfx | head -1 | awk '{print $2}'
 
@@ -24,26 +30,4 @@ wget https://huggingface.co/DuckyBlender/racist-phi3-GGUF/resolve/main/racist-ph
 
 
 
-
-
-
-# *** ggml builds:
-# NOTES FROM HIPBLAS+ggml test builds (did not get working YET:)
-#
-# AMD GPU support
-sudo pacman --needed --noconfirm -S \
-    rocm-hip-sdk rocminfo rocm-hip-runtime hipblas
-
-# not in path (which is fine, dont add to path, can just mod path for using hipcc, etc)
-/opt/rocm/bin/rocminfo
-# see examples repo for test cases of hipblas compiler hipcc
-
-
-# *** whisper.cpp
-#   can I use hipblas w/ whisper? ... repo has many refs to AMD/ROCM... must be possible via hipblas IIAC? or is whipser mostly CPU insructrs... and thus OpenBlas?
-#
-# whisper docs mention openblas
-# whisper.cpp uses openblas (do not install system wide, conflicts with hipblas used by ggml above, ugh)
-# wait are cblas/openblas/hipblas interchangeable? or not sure hip/cblas conflict actually they seem to work together
-sudo pacman --noconfirm -S openblas
 
