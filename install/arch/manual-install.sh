1e62f81371f71b4a74c9d998f4a15eab69cdf163
Wes Higbee <wes.mcclure@gmail.com>
2025-02-02 09:17:23 -0600

    arch

--- install/arch/manual-install.sh
+++ install/arch/manual-install.sh
@@ -17,7 +17,9 @@ pacman --needed --noconfirm -S tree less neovim \
     eza jq httpie nmap sudo git bat wget \
     ollama ollama-rocm \
     the_silver_searcher \
-    base-devel cmake ffmpeg man-db
+    base-devel cmake ffmpeg man-db \
+    core/inetutils
+# inetutils => hostname cmd
 
 # FYI older python versions, use uv to install based on .python-version in a repo dir
 
