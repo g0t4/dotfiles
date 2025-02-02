37261b11e6b3040ebc8fefc68d3bf3a377bfcad1
Wes Higbee <wes.mcclure@gmail.com>
2025-02-01 19:11:13 -0600

    alias name

--- install/arch/rocm/llama-cpp-infill.fish
+++ install/arch/rocm/llama-cpp-infill.fish
@@ -11,7 +11,8 @@
 ./build/bin/llama-server \
     -hf ggml-org/Qwen2.5-Coder-3B-Q8_0-GGUF \
     --port 8012 -ngl 99 -fa -ub 1024 -b 1024 \
-    --ctx-size 0 --cache-reuse 256 --host 0.0.0.0
+    --ctx-size 0 --cache-reuse 256 --host 0.0.0.0 \
+    --log-colors --alias qwen-infill
 # can I pass a models directory and serve all of them?
 
 
@@ -31,13 +32,14 @@ curl http://ollama:8012/v1/models
 # TODO use model alias to avoid filename path
 curl -X POST http://ollama:8012/infill \
     -d '{
+   "model": "qwen-infill",
    "raw":true,
    "num_predict":40,
-   "model": "/home/wes/.cache/llama.cpp/ggml-org_Qwen2.5-Coder-7B-Q8_0-GGUF_qwen2.5-coder-7b-q8_0.gguf",
    "stream":true,
    "input_prefix": "a = 1 + 2",
    "input_suffix": "c = a + b"
 }'
+# "model": "/home/wes/.cache/llama.cpp/ggml-org_Qwen2.5-Coder-7B-Q8_0-GGUF_qwen2.5-coder-7b-q8_0.gguf",
 # prompt is optional - TODO try it
 #"prompt":"",
 
