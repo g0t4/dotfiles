6705f9f7570ad66b776538fc0a14e3bf24927dd2
Wes Higbee <wes.mcclure@gmail.com>
2025-02-08 14:29:30 -0600

    ls w/ size

--- fish/load_last/files-specific.fish
+++ fish/load_last/files-specific.fish
@@ -1,7 +1,8 @@
 
 # *** ls
 # still not using exa/eza (not worth hassle, esp b/c fish builtin ls/la colors work well enough)
-abbr lat "ls -alt"
+abbr lat "ls -alht" # -t == sort by time
+abbr las "ls -alhS" # -S == sort by size
 
 # this was in release notes for 3.6.0! regex just added (among other changes)
 #    https://fishshell.com/docs/3.6/relnotes.html
