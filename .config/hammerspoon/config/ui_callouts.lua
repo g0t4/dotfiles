9e7ebac8cd3c414325be6373e046cf06e81928c6
Wes Higbee <wes.mcclure@gmail.com>
2025-02-17 17:54:12 -0600

    review

--- .config/hammerspoon/config/ui_callouts.lua
+++ .config/hammerspoon/config/ui_callouts.lua
@@ -129,23 +129,23 @@ hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "T", function()
                 -- stream is just move alert not position
                 highlightCurrentElement()
             end
-            -- function(error)
-            --     -- right now my sources don't levearge error (nor complete) events... so just ignore
-            --     print("[ERROR] what to do here?", error)
-            -- end,
-            -- function()
-            --     print("[COMPLETE] what to do here?")
-            -- end
+        -- function(error)
+        --     -- right now my sources don't levearge error (nor complete) events... so just ignore
+        --     print("[ERROR] what to do here?", error)
+        -- end,
+        -- function()
+        --     print("[COMPLETE] what to do here?")
+        -- end
         )
     else
         M.moves = nil
         M.subscription:unsubscribe() -- subscription cleanup is all... really can skip this here
-        -- separately need to stop the upstream event source (do not comingle unsub w/ stop source, usually you might have multiple subs and would want to separately control the subs vs source)
+        removeHighlight() -- clear the callout/tooltips
         if M.stop_moves then
+            -- separately need to stop the upstream event source (do not comingle unsub w/ stop source, usually you might have multiple subs and would want to separately control the subs vs source)
             M.stop_moves()
             M.stop_moves = nil
         end
-        removeHighlight() -- clear the callout/tooltips
     end
 end)
 
