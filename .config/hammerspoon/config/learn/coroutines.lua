f27b9a7e4976e9f1a373c6e8eadcc607cedcb810
Wes Higbee <wes.mcclure@gmail.com>
2025-02-22 02:42:33 -0600

    later can do job queue... just wanna wrap mind around coroutines in lua so gonna stop at runner for a single coroutine job1

--- .config/hammerspoon/config/learn/coroutines.lua
+++ .config/hammerspoon/config/learn/coroutines.lua
@@ -10,18 +10,36 @@ local function slowSearch(callback)
     hs.timer.doAfter(1, function() callback("surprise butt sex") end):start()
 end
 
-local co_outer = coroutine.create(function()
-    print('outer starts: ')
-    local result = coroutine.yield(slowSearch)
-    print('outer result: ', result)
+local job1 = coroutine.create(function()
+    print('job1 started: ')
+    local result1 = coroutine.yield(slowSearch)
+    print('job1 result1: ', result1)
+    local result2 = coroutine.yield(slowSearch)
+    print('job1 result2:', result2)
 end)
+
+
+local job2 = coroutine.create(function()
+    print('job2 started: ')
+    local result1 = coroutine.yield(slowSearch)
+    print('job2 result1: ', result1)
+    local result2 = coroutine.yield(slowSearch)
+    print('job2 result2:', result2)
+end)
+
+-- PRN add job queue (push all work here, including new coroutines that call a yielded func... and maps the result back to the original coroutine... would need to yield though to avoid deadlocks? lock between result and resume coroutuine?
+local queue = {}
+
+table.insert(queue, job1)
+
+
 -- todo loop until done... can always yield nothing to effectively say I am giving up CPU for someone else
 -- coroutine.status()
-local ok, asyncFunc = coroutine.resume(co_outer)
+local ok, asyncFunc = coroutine.resume(job1)
 -- if (asyncFunc ~= nil) then
 asyncFunc(function(...)
     print("fuck")
-    coroutine.resume(co_outer, ...)
+    coroutine.resume(job1, ...)
 end)
 -- end
 
