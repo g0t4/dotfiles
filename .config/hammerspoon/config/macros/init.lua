0fdc45ddd94937515777304b76c7bae8c35a8d8c
Wes Higbee <wes.mcclure@gmail.com>
2025-02-22 01:54:17 -0600

    review

--- .config/hammerspoon/config/macros/init.lua
+++ .config/hammerspoon/config/macros/init.lua
@@ -22,18 +22,18 @@ function MacroFcpxFindXSlider()
         DumpHtml(searchTask)
     end
 
-    --   AXDescription = "Title Inspector"
     -- OMG finding button to show panels is nearly instant!! I can use this as a first search (Fallback) if fixed path doesn't work!
     --     OR Can I just search every time?
     --     FYI must set count = 1 to be fast
     local criteria = { attribute = "AXDescription", value = "Title Inspector" } -- 270ms to 370ms w/ count=1
-    FindOneElement(GetFcpxAppElement(), criteria, afterSearch)
 
     -- slider (deeply nested)
     -- local criteria = { attribute = "AXHelp", value = "Y Slider" } -- 2.5s w/ count=1 (~10+ w/o)
-    -- setTimeout doesn't seem to work for elementSearch?!
 
-    -- FYI takes < half time if I know there's only one item I want to find!
+    FindOneElement(GetFcpxAppElement(), criteria, afterSearch)
+
+    -- NOTES:
+    -- takes < half time if I know there's only one item I want to find!
     --   taking 5 seconds for a full search in FCPX...
     --   deosn't mean I can't search but I need to narrow my search scope (i.e. find panel I want and search there instead of globally in app!)
     --     search can be used to provide some flexibility when controls rearrange or otherwise aren't consistently in same spots
