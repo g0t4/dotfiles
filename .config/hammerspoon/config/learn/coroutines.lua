36ce80e4bd0a70ada3aaa5cab8a68725f0e7db88
Wes Higbee <wes.mcclure@gmail.com>
2025-02-22 02:38:35 -0600

    start impl loop

--- .config/hammerspoon/config/learn/coroutines.lua
+++ .config/hammerspoon/config/learn/coroutines.lua
@@ -1,20 +1,34 @@
+-- function wrap(func, callback)
+--     local co = coroutine.create(function()
+--         func(callback)
+--         coroutine.yield()
+--     end)
+-- end
 
-
-
-
-
+local function slowSearch(callback)
+    -- simulate slow search
+    hs.timer.doAfter(1, function() callback("surprise butt sex") end):start()
+end
 
 local co_outer = coroutine.create(function()
-    require "socket"
-    local socket = require 'socket'
-    socket.sleep(1)
-    print('foo')
-    --
-    -- function sleep(sec)
-    --     socket.select(nil, nil, sec)
-    -- end
-    --
-    -- sleep(1)
+    print('outer starts: ')
+    local result = coroutine.yield(slowSearch)
+    print('outer result: ', result)
 end)
-coroutine.resume(co_outer)
+-- todo loop until done... can always yield nothing to effectively say I am giving up CPU for someone else
+-- coroutine.status()
+local ok, asyncFunc = coroutine.resume(co_outer)
+-- if (asyncFunc ~= nil) then
+asyncFunc(function(...)
+    print("fuck")
+    coroutine.resume(co_outer, ...)
+end)
+-- end
 
+
+
+
+-- FYI blocking sleep:
+-- require "socket"
+-- local socket = require 'socket'
+-- socket.sleep(1)
