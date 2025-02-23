ca10fc3b3fe7a142fdde3f3c3207ba03fcc4fa67
Wes Higbee <wes.mcclure@gmail.com>
2025-02-23 16:27:18 -0600

    try out EmmyLua stubs for hs.* modules

--- .config/hammerspoon/config/spoons.lua
+++ .config/hammerspoon/config/spoons.lua
@@ -27,3 +27,13 @@ package.path = package.path .. ";" .. spoons_repo
 --         "J",
 --     }
 -- })
+
+-- *** EmmyLua generate stubs for hs.* modules
+-- - first run takes a second to generate stubs
+-- - also if outdated, IIAC on file timestamps?
+-- - based on discussion in:
+--   - https://github.com/Hammerspoon/hammerspoon/pull/2530
+--   - https://github.com/Hammerspoon/Spoons/pull/240
+-- PRN could set a script that runs automatically when neovim starts in just my hammerspoon config dir
+print("loading EmmyLua to generate stubs if needed... can do this manually if this is too slow here as I only need this for neovim lua LS... not for hammerspoon app itself")
+hs.loadSpoon("EmmyLua") -- <2ms to check is fine... NBD to run all the time
