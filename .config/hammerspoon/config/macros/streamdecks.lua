7c6407c40e9515221e1957c3c694d44ddd45a720
Wes Higbee <wes.mcclure@gmail.com>
2025-02-23 17:40:43 -0600

    streamdeck!

--- .config/hammerspoon/config/macros/streamdecks.lua
+++ .config/hammerspoon/config/macros/streamdecks.lua
@@ -27,11 +27,37 @@ end
 -- TODO turn this into hot reload for just streamdeck lua scripts?
 reloadOnMacrosChanges()
 
-hs.streamdeck.init(function(connected, deck)
-    -- YAY IT READS ALL OF MY DECKS!!!!!
+
+-- TODO test dials on streamdeck plus
+--   ALL/REGULAR:
+--      :buttonCallback(fn)
+--   PLUS touchscreen:
+--      hs.streamdeck:screenCallback(fn)
+--      pressed/released AND rotated (plus only, IIUC)
+--
+--
+
+--
+-- @param connected boolean
+-- @param deck hs.streamdeck
+function onDeviceDiscovery(connected, deck)
     print("Discovered streamdeck", hs.inspect(deck), "connected:", connected)
     print("devices: ", hs.streamdeck.numDevices())
-end)
+    -- setBrightness
+    print("serialNumber: ", deck:serialNumber())
+    print("firmwareVersion: ", deck:firmwareVersion())
+    -- use serialNumber to identify which device is which
+    -- serial ends in "9","8","1" (all start with "CL" too)... PLUS starts wtih "A"
+    local cols, rows = deck:buttonLayout()
+    print("  cols:", cols, " rows:", rows)
+    -- PRN deck:setBrightness(80) -- 0 to 100 (FYI persists across restarts of hammerspoon... IIAC only need to set this once when I wanna change it)
+
+
+
+    print(hs.inspect(getmetatable(deck)))
+end
+
+hs.streamdeck.init(onDeviceDiscovery) -- onDeviceConnected)
 
 -- FYI at this point, there are no devices available, wait for them to connect (each one)
 
