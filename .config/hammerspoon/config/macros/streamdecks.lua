f625e37c2f829faf9d2e68dcb0c3e224b5d8bd7e
Wes Higbee <wes.mcclure@gmail.com>
2025-02-23 16:44:09 -0600

    derp

--- .config/hammerspoon/config/macros/streamdecks.lua
+++ .config/hammerspoon/config/macros/streamdecks.lua
@@ -3,7 +3,6 @@
 --    light up the dials for adjusting its specific properties...
 --  I can do this with an accessibility observer
 
-local streamdeck = require("hs.streamdeck")
 
 function reloadOnMacrosChanges(path)
     local scriptPath = path or "/Users/wesdemos/.hammerspoon/config/macros"
@@ -14,8 +13,7 @@ function reloadOnMacrosChanges(path)
     --     hs.alert.show("Reloaded: " .. scriptPath)
     -- end
 
-    local pathwatcher = require("hs.pathwatcher")
-    local watcher = pathwatcher.new(scriptPath, function(files, flagTables)
+    local watcher = hs.pathwatcher.new(scriptPath, function(files, flagTables)
         -- for _, file in ipairs(files) do ... hot reload each? (FYI symlinks might not match if checking based on file name
         print("Reloading script: ", scriptPath)
         hs.reload() -- crude, reload config as a pseudo restart
@@ -29,6 +27,6 @@ end
 -- TODO turn this into hot reload for just streamdeck lua scripts?
 reloadOnMacrosChanges()
 
-streamdeck.discoveryCallback = function(connected, deck)
+hs.streamdeck.discoveryCallback(function(connected, deck)
     print("Discovered streamdeck", deck.name, "connected:", connected)
-end
+end)
