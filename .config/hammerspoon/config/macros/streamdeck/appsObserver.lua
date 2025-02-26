23bd0c5a644aa93189af4df09996666b8818d446
Wes Higbee <wes.mcclure@gmail.com>
2025-02-26 12:53:27 -0600

    actually lets route to per event funcs

--- .config/hammerspoon/config/macros/streamdeck/appsObserver.lua
+++ .config/hammerspoon/config/macros/streamdeck/appsObserver.lua
@@ -6,21 +6,27 @@ AppsObserver.__index = AppsObserver
 ---@return AppsObserver
 function AppsObserver:new()
     local o = setmetatable({}, AppsObserver)
+
     o.watcher = hs.application.watcher.new(function(appName, eventType, hsApp)
-        -- btw this wrapper func exists b/c lua won't let me do new(o:onAppEvent)
-        --   and I don't want the logic buried in here
-        o:onAppEvent(appName, eventType, hsApp)
+        if eventType == hs.application.watcher.activated then
+            o:onAppActivated(appName, hsApp)
+        elseif eventType == hs.application.watcher.deactivated then
+            o:onAppDeactivated(appName, hsApp)
+        end
     end)
+
     return o
 end
 
-function AppsObserver:onAppEvent(appName, eventType, hsApp)
-    if eventType == hs.application.watcher.activated then
-        print("app activated", appName)
-    elseif eventType == hs.application.watcher.deactivated then
-        print("app deactivated", appName)
-        -- TODO cleanup (FYI happens after other app activates)
-    end
+function AppsObserver:onAppActivated(appName, hsApp)
+    print("app activated", appName)
+    -- TODO do something with the app
+end
+
+function AppsObserver:onAppDeactivated(appName, hsApp)
+    print("app deactivated", appName)
+    -- FYI happens after other app activates
+    -- TODO cleanup
 end
 
 function AppsObserver:start()
