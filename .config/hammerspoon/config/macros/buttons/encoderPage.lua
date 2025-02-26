856523d514e85ee0c23f73b992768670a4991793
Wes Higbee <wes.mcclure@gmail.com>
2025-02-26 04:26:59 -0600

    detect direction

--- .config/hammerspoon/config/macros/buttons/encoderPage.lua
+++ .config/hammerspoon/config/macros/buttons/encoderPage.lua
@@ -76,16 +76,72 @@ function EncoderPage:onScreenTouched(interaction, xStart, yStart, xStop, yStop)
 
     --  "shortPress", "longPress" or "swipe"
     -- for "shortPress" and "longPress" only use xFirst/yFirst, b/c xLast/yLast are 0
-    local message = interaction .. " screen "
     if interaction == "swipe" then
         stopEncoderNumber = math.floor(xStop / pixelsPerEncoder) + 1
         if stopEncoderNumber > self._numberOfEncoders then
             print("stop encoder not mapped: " .. stopEncoderNumber)
             return
         end
-        message = message .. "from " .. startEncoderNumber ..
+
+
+
+        -- compute velocity of swipe?
+        --   swipe appears to be time constrained, not based on when I pickup my finger
+        --   so, distance traveled is directly proportional to speed of swipe
+        local xDistance = math.abs(xStop - xStart)
+        local yDistance = math.abs(yStop - yStart)
+        local distance = math.floor(math.sqrt(math.pow(xDistance, 2) + math.pow(yDistance, 2)))
+
+
+        -- compute orientation? swipe up/down, left/right, or diagonal left/right? can map each to different actions
+        local ratio = math.floor(xDistance / yDistance * 1000) / 1000
+        local direction = ""
+        -- FYI ratio > 5 worked well too in initial testing.. .if 4 is too low
+        --   just test the gesture near the boundary between directions
+        local threshold = 4
+        if ratio > threshold then
+            -- horizontal swipe
+            if xStop > xStart then
+                -- PRN use up/down left/right so its distinct versus diagonal swipes?
+                --    also, up/down/left/right is easier to understand
+                direction = "right"
+                -- direction = "east"
+            else
+                direction = "left"
+                -- direction = "west"
+            end
+        elseif ratio < (1 / threshold) then
+            -- vertical swipe
+            if yStop > yStart then
+                direction = "down"
+                -- direction = "south"
+            else
+                direction = "up"
+                -- direction = "north"
+            end
+        else
+            if xStop > xStart then
+                if yStop > yStart then
+                    direction = "south-east"
+                else
+                    direction = "north-east"
+                end
+            else
+                if yStop > yStart then
+                    direction = "south-west"
+                else
+                    direction = "north-west"
+                end
+            end
+        end
+
+
+        local message = interaction ..
+            " " .. direction ..
+            " " .. distance .. " pixels" ..
+            " from enc" .. startEncoderNumber ..
             " (" .. xStart .. ", " .. yStart .. ")" ..
-            " to " .. stopEncoderNumber .. " (" .. xStop .. ", " .. yStop .. ")"
+            " to enc" .. stopEncoderNumber .. " (" .. xStop .. ", " .. yStop .. ")"
         print(message)
         return
     end
@@ -93,8 +149,9 @@ function EncoderPage:onScreenTouched(interaction, xStart, yStart, xStop, yStop)
     -- short/longPress:
     assert(xStop == 0, "xLast must be 0 for interactions other than swipe")
     assert(yStop == 0, "yLast must be 0 for interactions other than swipe")
-    message = message .. "above encoder " .. startEncoderNumber ..
-        " (" .. xStart .. ", " .. yStart .. ")"
+    local message = interaction ..
+        "above encoder " .. startEncoderNumber ..
+        " at (" .. xStart .. ", " .. yStart .. ")"
     print(message)
 end
 
