0ae860d3462ca7077661be5e8260a455a063e57e
Wes Higbee <wes.mcclure@gmail.com>
2025-02-26 10:58:45 -0600

    split

---
+++ .config/hammerspoon/config/macros/streamdeck/deckController.lua
@@ -0,0 +1,78 @@
+local ButtonsController = require("config.macros.streamdeck.buttonsController")
+local EncodersController = require("config.macros.streamdeck.encodersController")
+require("config.macros.streamdeck.helpers")
+
+---@class DeckController
+---@field deck hs.streamdeck
+---@field name string
+---@field private buttons ButtonsController
+---@field private encoders EncodersController
+local DeckController = {}
+DeckController.__index = DeckController
+
+---@param deck hs.streamdeck
+---@return DeckController
+function DeckController:new(deck)
+    o = setmetatable({}, self)
+    o.deck = deck
+    o.name = getDeckName(deck)
+    o.buttons = o.name:find("XL$") and ButtonsController:newXL(deck) or ButtonsController:newPlus(deck)
+    -- TODO add empty encoder controller instead of nil? with 0 encoders?
+    o.encoders = o.name:find("Plus$") and EncodersController:newPlus(deck) or nil
+    return o
+end
+
+function DeckController:__tostring()
+    return "DeckController<" .. self.name .. ">"
+end
+
+---@param deck hs.streamdeck
+---@param interaction string
+---@param xFirst number
+---@param yFirst number
+---@param xLast number
+---@param yLast number
+function DeckController:onScreenTouched(deck, interaction, xFirst, yFirst, xLast, yLast)
+    if self.encoders == nil then
+        error("onScreenPressed: no encoders on " .. self.name)
+        return
+    end
+    self.encoders:onScreenTouched(interaction, xFirst, yFirst, xLast, yLast)
+end
+
+function DeckController:onEncoderPressed(deck, encoderNumber, pressedOrReleased, turnedLeft, turnedRight)
+    if self.encoders == nil then
+        error("onEncoderPressed: no encoders on " .. self.name)
+        return
+    end
+    self.encoders:onEncoderPressed(encoderNumber, pressedOrReleased, turnedLeft, turnedRight)
+end
+
+function DeckController:onButtonPressed(deck, buttonNumber, pressedOrReleased)
+    if self.buttons == nil then
+        error("onButtonPressed: no buttons on " .. self.name)
+        return
+    end
+    self.buttons:onButtonPressed(buttonNumber, pressedOrReleased)
+end
+
+function DeckController:start()
+    self.deck:screenCallback(function(deck, interaction, xFirst, yFirst, xLast, yLast)
+        self:onScreenTouched(deck, interaction, xFirst, yFirst, xLast, yLast)
+    end)
+    self.deck:encoderCallback(function(deck, encoderNumber, pressedOrReleased, turnedLeft, turnedRight)
+        self:onEncoderPressed(deck, encoderNumber, pressedOrReleased, turnedLeft, turnedRight)
+    end)
+    self.deck:buttonCallback(function(deck, buttonNumber, pressedOrReleased)
+        self:onButtonPressed(deck, buttonNumber, pressedOrReleased)
+    end)
+end
+
+function DeckController:stop()
+    -- clear callbacks == STOP
+    self.deck:screenCallback(nil)
+    self.deck:encoderCallback(nil)
+    self.deck:buttonCallback(nil)
+end
+
+return DeckController
