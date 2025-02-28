6793e3efb7211f724ef4330a7c6e6d1c61baf8fc
Wes Higbee <wes.mcclure@gmail.com>
2025-02-28 17:47:53 -0600

    continue to test how I like composing funcs instead

--- .config/hammerspoon/config/macros/streamdeck/profiles/fcpx.lua
+++ .config/hammerspoon/config/macros/streamdeck/profiles/fcpx.lua
@@ -24,7 +24,7 @@ FcpxObserver:addProfile("2XL", function(_, deck)
 
 
         LuaButton:new(25, deck, drawTextIcon("Save\nCurrent\nFrame", deck), menu({ "File", "Share", "Save Current Frame…" })),
-        MenuButton:new(26, deck, drawTextIcon("Save\n4K", deck), { "File", "Share", "Apple Devices 4K..." }),
+        LuaButton:new(26, deck, drawTextIcon("Save\n4K", deck), menu({ "File", "Share", "Apple Devices 4K..." })),
 
     }
 end)
