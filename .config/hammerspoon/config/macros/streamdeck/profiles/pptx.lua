5ac1c4edeba29d595e96695c401c41289e21fc7b
Wes Higbee <wes.mcclure@gmail.com>
2025-02-28 19:29:55 -0600

    pptx

--- .config/hammerspoon/config/macros/streamdeck/profiles/pptx.lua
+++ .config/hammerspoon/config/macros/streamdeck/profiles/pptx.lua
@@ -23,6 +23,13 @@ end)
 
 PptxObserver:addProfile("3XL", function(_, deck)
     return {
+        LuaButton:new(1, deck, hsIcon("pptx/shapes/align-left.png"), menu({ "Arrange", "Align or Distribute", "Align Left" })),
+        LuaButton:new(2, deck, hsIcon("pptx/shapes/align-center.png"), menu({ "Arrange", "Align or Distribute", "Align Center" })),
+        LuaButton:new(3, deck, hsIcon("pptx/shapes/align-right.png"), menu({ "Arrange", "Align or Distribute", "Align Right" })),
+
+        LuaButton:new(8, deck, hsIcon("pptx/shapes/align-top.png"), menu({ "Arrange", "Align or Distribute", "Align Top" })),
+        LuaButton:new(16, deck, hsIcon("pptx/shapes/align-middle.png"), menu({ "Arrange", "Align or Distribute", "Align Middle" })),
+        LuaButton:new(24, deck, hsIcon("pptx/shapes/align-bottom.png"), menu({ "Arrange", "Align or Distribute", "Align Bottom" })),
     }
 end)
 
