77a5a1c740a089a24900d23ccadbe4e77dddd943
Wes Higbee <wes.mcclure@gmail.com>
2025-03-03 13:32:41 -0600

    test image file load timing, not as bad

--- .config/hammerspoon/config/macros/streamdeck/profiles/defaults.lua
+++ .config/hammerspoon/config/macros/streamdeck/profiles/defaults.lua
@@ -87,35 +87,36 @@ end
 
 local memoized_hsCircleIcon = memoize(hsCircleIcon)
 -- local memoized_hsCircleIcon = hsCircleIcon -- no memoize
-
-local function myTimingTest()
-    local startTime = GetTime()
-    local xlDeck = {
-        buttonSize = {
-            w = 96,
-            h = 96
-        }
+local TestXLDeck = {
+    buttonSize = {
+        w = 96,
+        h = 96
     }
+}
+local function myTimingTest()
+    -- TODO test setButtonColor
+    -- TODO test setButtonImage (precreated and perfectly sized?) images<F7>
+    local startTime = GetTime()
     local base = {
         -- *** BINGO image creation is SLOW!!!
         -- !!! (is it most of the time?... maybe reset sucked b/c I didn't reuse the same image instance?)
 
-        MaestroButton:new(1, xlDeck, memoized_hsCircleIcon("#FFFF00", xlDeck),
+        MaestroButton:new(1, TestXLDeck, memoized_hsCircleIcon("#FFFF00", TestXLDeck),
             "foo", "Highlight color yellow"),
 
-        MaestroButton:new(1, xlDeck, memoized_hsCircleIcon("#00FF00", xlDeck),
+        MaestroButton:new(1, TestXLDeck, memoized_hsCircleIcon("#00FF00", TestXLDeck),
             "foo", "Highlight color yellow"),
-        MaestroButton:new(1, xlDeck, memoized_hsCircleIcon("#00FF00", xlDeck),
+        MaestroButton:new(1, TestXLDeck, memoized_hsCircleIcon("#00FF00", TestXLDeck),
             "foo", "Highlight color yellow"),
-        MaestroButton:new(1, xlDeck, memoized_hsCircleIcon("#00FF00", xlDeck),
+        MaestroButton:new(1, TestXLDeck, memoized_hsCircleIcon("#00FF00", TestXLDeck),
             "foo", "Highlight color yellow"),
 
         -- #FCE5CD (highlight light orange 3) => increase saturation for button color: #FFC690
-        MaestroButton:new(2, xlDeck, memoized_hsCircleIcon("#FFC690", xlDeck, "rec"),
+        MaestroButton:new(2, TestXLDeck, memoized_hsCircleIcon("#FFC690", TestXLDeck, "rec"),
             "foo", "highlight light orange 3"),
 
         -- "none" == remove highlight (background color)
-        MaestroButton:new(3, xlDeck, memoized_hsCircleIcon("#FFFFFF", xlDeck, "none"),
+        MaestroButton:new(3, TestXLDeck, memoized_hsCircleIcon("#FFFFFF", TestXLDeck, "none"),
             "foo", "highlight none"),
 
         -- -- changes text color (not highlight) => looks nice! (could be veritcal middle aligned but this is FINE for now)
@@ -129,9 +130,59 @@ local function myTimingTest()
 end
 
 
+local function myTimingTestHSIcon()
+    local startTime = GetTime()
+    local deck = TestXLDeck
+
+    -- 3 hsIcon file loads => 2 to 4ms total... not bad (IIAC image size is a factor)
+    -- 26 => 11-13ms
+    -- TODO check image size factor (to help me size appropriately too, extra reason to get size right)
+    local base = {
+        MaestroButton:new(1, deck, hsIcon("pptx/colors/fill-pink.png"), "02BF881E-47AF-4812-830F-4765B6AABC41"),
+        MaestroButton:new(9, deck, hsIcon("pptx/colors/line-pink.png"), "FBAD1498-E64F-4A26-8B41-59D4B59C4F6B"),
+        MaestroButton:new(17, deck, hsIcon("pptx/colors/text-pink.png"), "F5068DE4-4EA3-4B4E-ABE9-44A358D380B1"),
+
+        MaestroButton:new(2, deck, hsIcon("pptx/colors/fill-blue.png"), "D5CD851F-5A99-46E1-A922-4EF11726BD8A"),
+        MaestroButton:new(10, deck, hsIcon("pptx/colors/line-blue.png"), "5250FA4D-74BE-4E29-8EFB-3F9DA4183923"),
+        MaestroButton:new(18, deck, hsIcon("pptx/colors/text-blue.png"), "A8963B4C-825B-4BBC-9C4B-105B1FDBB253"),
+
+        MaestroButton:new(3, deck, hsIcon("pptx/colors/fill-purple.png"), "EE2F4019-8615-4A6C-9CE8-840D9FA27778"),
+        MaestroButton:new(11, deck, hsIcon("pptx/colors/line-purple.png"), "4AD5641B-4BAF-47D6-A6B8-D8D93041F23C"),
+        MaestroButton:new(19, deck, hsIcon("pptx/colors/text-purple.png"), "09063A55-0CE1-4AE3-BAC2-8DF341AF619D"),
+
+        MaestroButton:new(4, deck, hsIcon("pptx/colors/fill-yellow.png"), "70299E82-5094-44CD-98EB-EE783BE3FA0E"),
+        MaestroButton:new(12, deck, hsIcon("pptx/colors/line-yellow.png"), "289D7C69-74B6-4127-B7EF-4C054EADD65E"),
+        MaestroButton:new(20, deck, hsIcon("pptx/colors/text-yellow.png"), "E13771C5-F241-4C3A-8064-7765F6F30369"),
+
+        MaestroButton:new(5, deck, hsIcon("pptx/colors/fill-orange.png"), "C4E8F125-C719-4125-A3A2-1492642DC054"),
+        MaestroButton:new(13, deck, hsIcon("pptx/colors/line-orange.png"), "2AA28BE5-58A4-403C-8ABD-3FC69152E5B3"),
+        MaestroButton:new(21, deck, hsIcon("pptx/colors/text-orange.png"), "F4CB8BE1-1199-4472-948F-07D5BBF11464"),
+
+        MaestroButton:new(6, deck, hsIcon("pptx/colors/fill-green.png"), "EB427AC4-F6EE-4C4A-8972-47C7D91F1C92"),
+        MaestroButton:new(14, deck, hsIcon("pptx/colors/line-green.png"), "D1E5B8B5-3160-4FF7-B6F3-F6812576AEFD"),
+        MaestroButton:new(22, deck, hsIcon("pptx/colors/text-green.png"), "3634BCDF-9426-47ED-94DB-5A2B6AE29C66"),
+
+        MaestroButton:new(7, deck, hsIcon("pptx/colors/fill-red.png"), "7E4D159E-F181-4D32-B567-033AD826CD6A"),
+        MaestroButton:new(15, deck, hsIcon("pptx/colors/line-red.png"), "39368956-2C5B-41B6-8ACE-AC26D8BE2BFC"),
+        MaestroButton:new(23, deck, hsIcon("pptx/colors/text-red.png"), "6D19B684-4DA2-4566-8F76-FCAACB39FED9"),
+
+        MaestroButton:new(8, deck, hsIcon("pptx/colors/fill-inky-blue.png"), "796DFBC5-8B80-4422-9AA5-D19DE2A055D4"),
+        MaestroButton:new(16, deck, hsIcon("pptx/colors/line-inky-blue.png"), "6769734A-DC6E-4D4E-9E20-641AD9197005"),
+        MaestroButton:new(24, deck, hsIcon("pptx/colors/text-inky-blue.png"), "7F18E471-F989-4DA0-967D-1E935E3E0FC3"),
+
+        -- TODO verify the action for these two:
+        MaestroButton:new(27, deck, hsIcon("pptx/recording/record-pptx.png"), "B0F6834D-5E1D-4E08-AD92-3C2F87886CC0"),
+        MaestroButton:new(28, deck, hsIcon("pptx/recording/record-mouse-only.png"), "C998899A-74A5-4329-8B94-6E1DE875F32B"),
+
+    }
+
+    print("myTestFunc took " .. GetElapsedTimeInMilliseconds(startTime) .. "ms")
+end
+
 FallbackProfiles:addProfilePage(DECK_2XL, PAGE_1, function(_, deck)
     return {
         -- row 4:
+        LuaButton:new(31, deck, drawTextIcon("Timing hsIcon file", deck), myTimingTestHSIcon),
         LuaButton:new(32, deck, drawTextIcon("Timing Test", deck), myTimingTest)
     }
 end)
