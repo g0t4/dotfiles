603862a06f4fbe539300b48b9def7bb0c95d87aa
Wes Higbee <wes.mcclure@gmail.com>
2025-03-03 17:50:39 -0600

    add prefix too

--- .config/hammerspoon/config/macros/streamdeck/profiles/pptx.lua
+++ .config/hammerspoon/config/macros/streamdeck/profiles/pptx.lua
@@ -92,48 +92,46 @@ PptxObserver:addProfilePage(DECK_2XL, PAGE_1, function(_, deck)
     --   w/o even adding caching BMPs shaved 10ms+ off of overall time to change to PPTX on 2XL deck (this one)
 
     local useBmp = true
-    local prefix = "pptx/colors/bmp-96/"
-    local suffix = ".bmp"
-    if not useBmp then
-        prefix = "pptx/colors/"
-        suffix = ".png"
-    end
-    function hsIconBmp(name)
-        return hsIcon(prefix .. name .. suffix)
+    function hsIconBmp(prefix, name)
+        if useBmp then
+            return hsIcon(prefix .. "bmp-96/" .. name .. ".bmp")
+        else
+            return hsIcon(prefix .. name)
+        end
     end
 
     return {
-        MaestroButton:new(1, deck, hsIconBmp("fill-pink.png"), "02BF881E-47AF-4812-830F-4765B6AABC41"),
-        MaestroButton:new(9, deck, hsIconBmp("line-pink.png"), "FBAD1498-E64F-4A26-8B41-59D4B59C4F6B"),
-        MaestroButton:new(17, deck, hsIconBmp("text-pink.png"), "F5068DE4-4EA3-4B4E-ABE9-44A358D380B1"),
+        MaestroButton:new(1, deck, hsIconBmp("pptx/colors/", "fill-pink.png"), "02BF881E-47AF-4812-830F-4765B6AABC41"),
+        MaestroButton:new(9, deck, hsIconBmp("pptx/colors/", "line-pink.png"), "FBAD1498-E64F-4A26-8B41-59D4B59C4F6B"),
+        MaestroButton:new(17, deck, hsIconBmp("pptx/colors/", "text-pink.png"), "F5068DE4-4EA3-4B4E-ABE9-44A358D380B1"),
 
-        MaestroButton:new(2, deck, hsIconBmp("fill-blue.png"), "D5CD851F-5A99-46E1-A922-4EF11726BD8A"),
-        MaestroButton:new(10, deck, hsIconBmp("line-blue.png"), "5250FA4D-74BE-4E29-8EFB-3F9DA4183923"),
-        MaestroButton:new(18, deck, hsIconBmp("text-blue.png"), "A8963B4C-825B-4BBC-9C4B-105B1FDBB253"),
+        MaestroButton:new(2, deck, hsIconBmp("pptx/colors/", "fill-blue.png"), "D5CD851F-5A99-46E1-A922-4EF11726BD8A"),
+        MaestroButton:new(10, deck, hsIconBmp("pptx/colors/", "line-blue.png"), "5250FA4D-74BE-4E29-8EFB-3F9DA4183923"),
+        MaestroButton:new(18, deck, hsIconBmp("pptx/colors/", "text-blue.png"), "A8963B4C-825B-4BBC-9C4B-105B1FDBB253"),
 
-        MaestroButton:new(3, deck, hsIconBmp("fill-purple.png"), "EE2F4019-8615-4A6C-9CE8-840D9FA27778"),
-        MaestroButton:new(11, deck, hsIconBmp("line-purple.png"), "4AD5641B-4BAF-47D6-A6B8-D8D93041F23C"),
-        MaestroButton:new(19, deck, hsIconBmp("text-purple.png"), "09063A55-0CE1-4AE3-BAC2-8DF341AF619D"),
+        MaestroButton:new(3, deck, hsIconBmp("pptx/colors/", "fill-purple.png"), "EE2F4019-8615-4A6C-9CE8-840D9FA27778"),
+        MaestroButton:new(11, deck, hsIconBmp("pptx/colors/", "line-purple.png"), "4AD5641B-4BAF-47D6-A6B8-D8D93041F23C"),
+        MaestroButton:new(19, deck, hsIconBmp("pptx/colors/", "text-purple.png"), "09063A55-0CE1-4AE3-BAC2-8DF341AF619D"),
 
-        MaestroButton:new(4, deck, hsIconBmp("fill-yellow.png"), "70299E82-5094-44CD-98EB-EE783BE3FA0E"),
-        MaestroButton:new(12, deck, hsIconBmp("line-yellow.png"), "289D7C69-74B6-4127-B7EF-4C054EADD65E"),
-        MaestroButton:new(20, deck, hsIconBmp("text-yellow.png"), "E13771C5-F241-4C3A-8064-7765F6F30369"),
+        MaestroButton:new(4, deck, hsIconBmp("pptx/colors/", "fill-yellow.png"), "70299E82-5094-44CD-98EB-EE783BE3FA0E"),
+        MaestroButton:new(12, deck, hsIconBmp("pptx/colors/", "line-yellow.png"), "289D7C69-74B6-4127-B7EF-4C054EADD65E"),
+        MaestroButton:new(20, deck, hsIconBmp("pptx/colors/", "text-yellow.png"), "E13771C5-F241-4C3A-8064-7765F6F30369"),
 
-        MaestroButton:new(5, deck, hsIconBmp("fill-orange.png"), "C4E8F125-C719-4125-A3A2-1492642DC054"),
-        MaestroButton:new(13, deck, hsIconBmp("line-orange.png"), "2AA28BE5-58A4-403C-8ABD-3FC69152E5B3"),
-        MaestroButton:new(21, deck, hsIconBmp("text-orange.png"), "F4CB8BE1-1199-4472-948F-07D5BBF11464"),
+        MaestroButton:new(5, deck, hsIconBmp("pptx/colors/", "fill-orange.png"), "C4E8F125-C719-4125-A3A2-1492642DC054"),
+        MaestroButton:new(13, deck, hsIconBmp("pptx/colors/", "line-orange.png"), "2AA28BE5-58A4-403C-8ABD-3FC69152E5B3"),
+        MaestroButton:new(21, deck, hsIconBmp("pptx/colors/", "text-orange.png"), "F4CB8BE1-1199-4472-948F-07D5BBF11464"),
 
-        MaestroButton:new(6, deck, hsIconBmp("fill-green.png"), "EB427AC4-F6EE-4C4A-8972-47C7D91F1C92"),
-        MaestroButton:new(14, deck, hsIconBmp("line-green.png"), "D1E5B8B5-3160-4FF7-B6F3-F6812576AEFD"),
-        MaestroButton:new(22, deck, hsIconBmp("text-green.png"), "3634BCDF-9426-47ED-94DB-5A2B6AE29C66"),
+        MaestroButton:new(6, deck, hsIconBmp("pptx/colors/", "fill-green.png"), "EB427AC4-F6EE-4C4A-8972-47C7D91F1C92"),
+        MaestroButton:new(14, deck, hsIconBmp("pptx/colors/", "line-green.png"), "D1E5B8B5-3160-4FF7-B6F3-F6812576AEFD"),
+        MaestroButton:new(22, deck, hsIconBmp("pptx/colors/", "text-green.png"), "3634BCDF-9426-47ED-94DB-5A2B6AE29C66"),
 
-        MaestroButton:new(7, deck, hsIconBmp("fill-red.png"), "7E4D159E-F181-4D32-B567-033AD826CD6A"),
-        MaestroButton:new(15, deck, hsIconBmp("line-red.png"), "39368956-2C5B-41B6-8ACE-AC26D8BE2BFC"),
-        MaestroButton:new(23, deck, hsIconBmp("text-red.png"), "6D19B684-4DA2-4566-8F76-FCAACB39FED9"),
+        MaestroButton:new(7, deck, hsIconBmp("pptx/colors/", "fill-red.png"), "7E4D159E-F181-4D32-B567-033AD826CD6A"),
+        MaestroButton:new(15, deck, hsIconBmp("pptx/colors/", "line-red.png"), "39368956-2C5B-41B6-8ACE-AC26D8BE2BFC"),
+        MaestroButton:new(23, deck, hsIconBmp("pptx/colors/", "text-red.png"), "6D19B684-4DA2-4566-8F76-FCAACB39FED9"),
 
-        MaestroButton:new(8, deck, hsIconBmp("fill-inky-blue.png"), "796DFBC5-8B80-4422-9AA5-D19DE2A055D4"),
-        MaestroButton:new(16, deck, hsIconBmp("line-inky-blue.png"), "6769734A-DC6E-4D4E-9E20-641AD9197005"),
-        MaestroButton:new(24, deck, hsIconBmp("text-inky-blue.png"), "7F18E471-F989-4DA0-967D-1E935E3E0FC3"),
+        MaestroButton:new(8, deck, hsIconBmp("pptx/colors/", "fill-inky-blue.png"), "796DFBC5-8B80-4422-9AA5-D19DE2A055D4"),
+        MaestroButton:new(16, deck, hsIconBmp("pptx/colors/", "line-inky-blue.png"), "6769734A-DC6E-4D4E-9E20-641AD9197005"),
+        MaestroButton:new(24, deck, hsIconBmp("pptx/colors/", "text-inky-blue.png"), "7F18E471-F989-4DA0-967D-1E935E3E0FC3"),
 
         -- TODO ADD BMPS here too:
         -- TODO verify the action for these two:
