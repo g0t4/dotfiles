20bb88185b05c4a8cfc6fc2007a887418013a2d0
Wes Higbee <wes.mcclure@gmail.com>
2025-03-10 09:34:53 -0500

    get session

--- iterm2/scripts/AutoLaunch/wes/wes/tabs.py
+++ iterm2/scripts/AutoLaunch/wes/wes/tabs.py
@@ -31,6 +31,9 @@ async def wes_cmd_n_override(connection: iterm2.Connection):
         # this is not possible AFAIK right now b/c you have to have a window open to invoke keyboard monitor handlers
         await iterm2.Window.async_create(connection)
         return
+    session = await get_session(connection)
+    if session is None:
+        return
 
     window = await iterm2.Window.async_create(connection)
 
