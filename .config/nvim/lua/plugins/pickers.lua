915169de8880e6a3e800bf03472244a7f646ce09
Wes Higbee <wes.mcclure@gmail.com>
2025-03-13 15:55:44 -0500

    reformat

--- .config/nvim/lua/plugins/pickers.lua
+++ .config/nvim/lua/plugins/pickers.lua
@@ -177,7 +177,20 @@ return {
                         -- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/pickers/layout_strategies.lua
                         -- layout_strategy = 'cursor', -- popup right where cursor is at? not sure I will always be expecting that... try and find out, FYI works w/ theme=dropdown
 
-                        find_command = { "rg", "--files", "--no-ignore", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/.venv/*", "--glob", "!**/node_modules/*", "--glob", "!**/iterm2env/*" },
+                        find_command = {
+                            "rg",
+                            "--files",
+                            "--no-ignore",
+                            "--hidden",
+                            "--glob",
+                            "!**/.git/*",
+                            "--glob",
+                            "!**/.venv/*",
+                            "--glob",
+                            "!**/node_modules/*",
+                            "--glob",
+                            "!**/iterm2env/*"
+                        },
                         -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
                         -- PRN use git_files and fallback to find_files: Falling back to find_files if git_files can't find a .git directory, wouldn't this be missing new files?
 
