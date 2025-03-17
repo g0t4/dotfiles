d8929edd5c159a3f97c90d77df1d9a69f5e0b7de
Wes Higbee <wes.mcclure@gmail.com>
2025-03-17 10:01:28 -0500

    add verbose user@host too

--- fish/load_last/prompt.fish
+++ fish/load_last/prompt.fish
@@ -35,6 +35,10 @@ function prompt_login --description 'display user name for the prompt'
     #if test -n "$VIRTUAL_ENV"
     #    echo -n -s (set_color cyan) \ue73c (set_color normal) " "
     #end
+    if set -q show_verbose_prompt
+        echo -n -s $USER@$hostname
+        return
+    end
 
     # show hostname
     if string match -q "mbp*" $hostname
