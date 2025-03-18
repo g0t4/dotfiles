b3b95939b900abb4d52fe10dc135eab83c92f1c1
Wes Higbee <wes.mcclure@gmail.com>
2025-03-18 10:18:54 -0500

    copy GitHub links.. add permalink and line ranges   and new leader based keymaps

--- .config/nvim/lua/localz/github-links.lua
+++ .config/nvim/lua/localz/github-links.lua
@@ -1,46 +1,60 @@
 
 vim.cmd [[
 
-    command! CopyGitHubLink call CopyGitHubLink()
+    command! CopyGitHubLink call CopyGitHubLink(v:false)
+    command! CopyGitHubPermaLink call CopyGitHubLink(v:true)
 
-    function! BuildGitHubLink()
-      " Get current file path relative to the git root
-      let l:filepath = system('git rev-parse --show-prefix') . expand('%')
+    function! BuildGitHubLink(start_line, end_line, is_permalink)
 
-      " Get current branch
-      let l:branch = system('git rev-parse --abbrev-ref HEAD')
+      " current file path relative to the repo root
+      let l:file_path_in_repo = system('git rev-parse --show-prefix') . expand('%')
 
-      " Get GitHub remote URL and clean it up
+      if a:is_permalink
+          let l:commit_ish = system('git rev-parse --short HEAD')
+      else
+          let l:commit_ish = system('git rev-parse --abbrev-ref HEAD')
+      end
+
       let l:remote_url = system('git config --get remote.origin.url')
       let l:remote_url = substitute(l:remote_url, 'git@github.com:', 'https://github.com/', '')
       let l:remote_url = substitute(l:remote_url, '.git\n$', '', '')
 
-      " Get current line number
-      let l:line_number = line('.')
-
-      " Build the full GitHub link
-      let l:github_url = l:remote_url . '/blob/' . l:branch . '/' . l:filepath . '#L' . l:line_number
+      let l:github_url = l:remote_url . '/blob/' . l:commit_ish . '/' . l:file_path_in_repo . '#L' . a:start_line
+      if a:start_line != a:end_line
+         let l:github_url = l:github_url . '-' . a:end_line
+      endif
 
       " trim new lines
       let l:github_url = substitute(l:github_url, '\n', '', 'g')
       return l:github_url
     endfunction
 
-    function! CopyGitHubLink()
-        let l:github_url = BuildGitHubLink()
+    function! CopyGitHubLink(is_permalink) range
+        let l:github_url = BuildGitHubLink(a:firstline, a:lastline, a:is_permalink)
         let @+ = l:github_url
         echo 'GitHub link copied: ' . l:github_url
     endfunction
 
-    command! OpenGitHubLink call OpenGitHubLink()
+    command! OpenGitHubLink call OpenGitHubLink(v:false)
+    command! OpenGitHubPermaLink call OpenGitHubLink(v:true)
 
-    function! OpenGitHubLink()
-        let l:github_url = BuildGitHubLink()
+    function! OpenGitHubLink(is_permalink)
+        let l:github_url = BuildGitHubLink(a:firstline, a:lastline, a:is_permalink)
         " # has to be escaped, smth with vim dispatch (no alternate file name to substitute for '#')
         let l:escaped_url = substitute(l:github_url, '#', '\\#', 'g')
         execute 'silent !open "' . l:escaped_url . '"'
     endfunction
 
+    " PRN can use `gh` prefix if collisions w/ other needs
+    "   right now I use `g` for references `gr` `gi`
+    "   and it feels somewhat fitting to have `go` to go to (open) GitHub!
+    "   then gl/gp can be for copying link/permalink, could do `gcp`/`gcl` too
+    nnoremap <silent> <leader>go :call OpenGitHubLink(v:false)<CR>
+    vnoremap <silent> <leader>go :call OpenGitHubLink(v:false)<CR>
+    nnoremap <silent> <leader>gl :call CopyGitHubLink(v:false)<CR>
+    vnoremap <silent> <leader>gl :call CopyGitHubLink(v:false)<CR>
+    nnoremap <silent> <leader>gp :call CopyGitHubLink(v:true)<CR>
+    vnoremap <silent> <leader>gp :call CopyGitHubLink(v:true)<CR>
 
 ]]
 
