d24799f8d058169d892b06283b5d10b6fa228044
Wes Higbee <wes.mcclure@gmail.com>
2025-03-22 23:43:09 -0500

    trying iron.nvim!

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,9 +1,123 @@
 return {
-    -- TODO try https://github.com/folke/neoconf.nvim
-    -- project specific config (but wait until I need this, i.e. maybe if I need diff formatters or rules)
-    --  OR find smth similar? I am not keen on the idea of using json instead of lua/vimscript
-    -- !!! import vscode settings?! DEFINITELY NEED TO TEST THAT for ideas for what to configure yet
+
+
+    -- *** JUPYTER / REPLs ***
+    --  HONESTLY, I just wanna be able to run python code like a notebook but I hate the NB interface (esp VSCode)..
+    --    want to keep a session open
+    --    - ** not re-run a script every time I change it
+    --    - saves load times (esp w/ datasets/models that are huge)
+    --    - * REPL integration so I don't have to copy/paste
+    --      - ipython REPL (color / tab completion)
+    --    - maybe: cell deliniation
+    --      - actually, just an easy selection mechanism (maybe just use vim motions)
+    --      - maybe setup a shortcut <leader>c or w/e and have it grep for a start/end marker I pick? selects between cell border comments
     --
+    --  - *first, try w/o jupyter notebook?
+    --     this will be superior in some ways b/c I can run adhoc code too (in the REPL) and don't have to put it in a cell for quick testing
+    --     also, REPL route supports other languages  (conjure, iron.nvim)
+    --
+    -- CONSIDERING:
+    --   REPL integration:
+    --   "Olical/conjure"
+    --   "Vigemus/iron.nvim"
+    --
+    --   JUPYTER notebook support:
+    --   "kiyoon/jupynium.nvim", -- probably best option if I wanna try full jupyter integration (drives a separate, synchronized nb instance)
+    --   "jupyter-vim/jupyter-vim"
+    --
+    -- *** WIP iron.nvim ***
+    {
+        -- FULLY EVALUATE iron.nvim before touching anything else
+        --  Likes => all the keymaps make 100% sense, esp `isb` for block (cell deliniator)
+        --    multiple languages (i.e. python, lua, shell)
+        --  Unsure => terminal windows, thus far I have been apprehensive and prefer iterm panes, but I can give it a try and see
+        --   PRN, add motion or keymap to select a "cell"
+        "Vigemus/iron.nvim",
+        config = function()
+            local iron = require("iron.core")
+            local view = require("iron.view")
+            local common = require("iron.fts.common")
+
+            iron.setup {
+                config = {
+                    scratch_repl = true, -- discard repls?
+                    -- TODO how can I turn of close prompt or not have to switch to repl to confirm yes/no... and why not just have a "restore" instead so no confirm to close?
+                    repl_definition = {
+                        sh = {
+                            -- command: either a table, or a func that returns a table
+                            command = { "fish" },
+                        },
+                        python = {
+                            command = { "ipython", "--no-autoindent" },
+                            -- command = { "python3" },
+                            format = common.bracketed_paste_python,
+                            block_deviders = { "# %%", "#%%" }, -- TODO TRY BLOCK DIVIDERS with which motion?
+
+                            -- use iterm to split pane, not sure this does what ChatGPT thought it would do :)... this just runs iterm in a nested terminal window
+                            -- command = { "osascript", "-e", [[tell app "iTerm" to tell the current window to create tab with default profile]] },
+                            -- format = require("iron.fts.common").bracketed_paste,
+                        }
+                    },
+                    repl_filetype = function(bufnr, ft)
+                        -- set repl filetype (to language used)
+                        return ft
+                    end,
+                    repl_open_cmd = view.split.vertical("50%"),
+                    -- When repl_open_cmd is an array table, IronRepl uses first cmd:
+                    -- repl_open_cmd = {
+                    --   view.split.vertical.rightbelow("%40"), -- cmd_1: open a repl to the right
+                    --   view.split.rightbelow("%25")  -- cmd_2: open a repl below
+                    -- }
+                },
+
+                -- no keymaps by default (Amen):
+                keymaps = {
+                    toggle_repl = "<space>ir", -- toggles the repl open and closed.
+                    -- If repl_open_command is a table, then:
+                    -- toggle_repl_with_cmd_1 = "<space>rv",
+                    -- toggle_repl_with_cmd_2 = "<space>rh",
+                    restart_repl = "<space>iR", -- calls `IronRestart` to restart the repl
+
+                    -- send keymaps (<leader>is prefix currently):
+                    send_motion = "<space>isc", -- motion right after!
+                    visual_send = "<space>isc", -- send selection
+                    send_file = "<space>isf", -- *
+                    send_line = "<space>isl", -- *
+                    send_paragraph = "<space>isp", -- * think {}
+                    send_until_cursor = "<space>isu", -- run file to cursor
+                    send_mark = "<space>ism",
+                    send_code_block = "<space>isb", -- *** OMG YES, works with block_deviders (above)
+                    send_code_block_and_move = "<space>isn",
+                    mark_motion = "<space>imc",
+                    mark_visual = "<space>imc",
+                    remove_mark = "<space>imd",
+                    cr = "<space>is<cr>",
+                    interrupt = "<space>is<space>",
+                    exit = "<space>isq",
+                    clear = "<space>icl", -- why isn't this clearing? just submits prompt
+                    -- btw, if I change to terminal, go into insert mode I can use ctrl+l to clear it
+                    --   maybe I should setup terminals so ctrl+l always clears (doesn't open nvim-tree)?
+                    --   though if I really start using builtin terminal then maybe I will want ctrl-L to open to PWD?
+                    --   btw, what is the insert mode for a terminal buffer?
+                    --      right now I can't exit this terminal insert mode... only enter it with `i`
+                    --      also doesn't show blue like regular buffer in status bar
+                },
+                highlight = {
+                    -- can set hl group to control style too
+                    italic = true -- IIUC is for last line run
+                },
+                ignore_blank_lines = true, -- when sending visual select lines, IIAC to not sumbmit extra prompt lines before/after/between sent commands
+            }
+        end
+
+    },
+
+
+    -- *** TESTING ***
+    -- TODO try testing, instead of / in addition to Plenary
+    -- {
+    --     "nvim-neotest/neotest",
+    -- },
 
     {
         -- FYI use `:Notifications` to see history of notifications
