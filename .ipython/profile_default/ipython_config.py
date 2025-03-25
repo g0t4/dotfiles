14cc47c593fd961f5a277ac739b15fcdb24c8cbd
Wes Higbee <wes.mcclure@gmail.com>
2025-03-24 21:13:09 -0500

    notes

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,5 +1,9 @@
 c = get_config()  #noqa
 
+# FYI I setup a second profile and left all defaults in it, that way this file is not bloated but I also have a reference
+#   ipython profile create foo-lookup-defaults
+#   cat ~/.ipython/profile_foo-lookup-defaults/ipython_config.py | grep -i foo
+
 c.TerminalInteractiveShell.confirm_exit = False
 
 # turn off that banner w/ version info when first open REPL (i.e. in nvim)
