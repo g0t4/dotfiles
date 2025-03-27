959f8137d07440201c022da9fe3fab4f378d9b0d
Wes Higbee <wes.mcclure@gmail.com>
2025-03-27 09:51:10 -0500

    notes

---
+++ .config/nvim/lua/plugins/wip/builtin-lsp.lua
@@ -0,0 +1,12 @@
+-- switch to using native LSP + mason (to install LS IIUC)...
+--   instead of coc
+--   I already use several standalone LS's with coc, it wouldn't be much diff than that
+--   and IIUC should perfom much better
+--   with same complexity in terms of config
+--
+-- this seems like a good starting point
+--   https://github.com/jmbuhr/quarto-nvim-kickstarter/blob/main/lua/plugins/lsp.lua#L4
+--
+-- remap all key bindings that I use with coc
+-- have telescope use its builtin lsp pickers (instead of telescope-coc extension)
+-- etc
