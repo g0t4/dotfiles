b59f9ec2be8375f76bf1fcc88b8ce3b1230d7825
Wes Higbee <wes.mcclure@gmail.com>
2025-04-04 16:44:21 -0500

    new

---
+++ misc/browsers/google-docs/dispatchMouseEvents.js
@@ -0,0 +1,28 @@
+
+function dispatchMouseEvents(targetSelector, mouseEvents) {
+    const targets = document.querySelectorAll(targetSelector);
+    if (!targets.length)
+        throw Error("no target found for selector", { targetSelector });
+    if (targets.length > 1)
+        throw Error("too many targets found for selector", { targetSelector });
+
+    const moreTarget = document.querySelector(targetSelector);
+
+    mouseEvents.forEach((type) => {
+        // simulate mouse events
+        const event = new MouseEvent(type, {
+            bubbles: true,
+            cancelable: true,
+            view: window,
+        });
+        moreTarget.dispatchEvent(event);
+        console.log(`Dispatched: ${type}`);
+    });
+}
+function dispatchMouseDownUpEvents(targetSelector) {
+    const mouseEvents = ["mousedown", "mouseup"];
+    return dispatchMouseEvents(targetSelector, mouseEvents);
+}
+
+dispatchMouseDownUpEvents("#moreButton");
+
