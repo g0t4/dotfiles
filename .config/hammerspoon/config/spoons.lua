087324826db8b054c3d12d5cd6c2f1e74ea8a2f2
Wes Higbee <wes.mcclure@gmail.com>
2025-04-05 11:57:08 -0500

    update spoons

--- .config/hammerspoon/config/spoons.lua
+++ .config/hammerspoon/config/spoons.lua
@@ -12,3 +12,18 @@ package.path = package.path .. ";" .. spoons_repo
 -- --   AS If the first load of these spoons fixes the issue... IIAC after reboot it won't work again
 -- --   INEVITABLY though it stops working again (IIGC after machine reboot) - todo check restart/logout and see if repro
 -- --   FIX WHEN I CAN RELIABLY REPRO this... IIRC I could get it to repro across reload configs, so I might need to do some more testing on that
+
+
+function UpdateSpoonsRepo()
+    -- pull latest on spoons repo
+    -- TODO if needed, why not make a method that does this => run cmd, dump output and nothing fancy... just pass a cmd and it does rest (executeAndLog)
+    local output, status, type, rc = hs.execute("/usr/bin/env fish --no-config -c 'cd $HOME/repos/github/Hammerspoon/Spoons && git pull'", true)
+    if output ~= nil or output ~= "" then
+        -- FYI OSC codes are printed, its fine
+        print("STDOUT:" .. tostring(output))
+    end
+    if rc ~= 0 then
+        print("Exit code: " .. status)
+        print("Type:      " .. type)
+    end
+end
