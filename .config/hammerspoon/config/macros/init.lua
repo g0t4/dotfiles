94f64258ff21da6d7c1e8620a911ace1e9dd4b4e
Wes Higbee <wes.mcclure@gmail.com>
2025-04-05 17:46:28 -0500

    use menu for cmd+a

--- .config/hammerspoon/config/macros/init.lua
+++ .config/hammerspoon/config/macros/init.lua
@@ -406,6 +406,13 @@ end
 function SearchForDevToolsTextArea(callback)
     local focusedWindow, app = GetBraveFocusedWindowElement()
 
+    -- trigger select all which is needed to get/replace the user prompt/question
+    local selected           = app:asHSApplication():selectMenuItem({ "Edit", "Select All" })
+    if (not selected) then
+        print("failed to select all")
+        return
+    end
+
     -- AXTextArea '' - Console prompt
     -- AXHighestEditableAncestor: AXTextArea '' - Console prompt<hs.axuielement>
     --    todo look for presence of AXHighestEditableAncestor?
