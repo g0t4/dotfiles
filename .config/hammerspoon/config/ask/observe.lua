da49fc09276a68d8931e8dcc234f5a24904490bc
Wes Higbee <wes.mcclure@gmail.com>
2025-04-06 05:02:43 -0500

    reivew

--- .config/hammerspoon/config/ask/observe.lua
+++ .config/hammerspoon/config/ask/observe.lua
@@ -1,23 +1,3 @@
--- local events = {
---     "AXUIElementCreated",
---     "AXWindowCreated",
---     "AXWindowMoved",
---     "AXWindowResized",
---     "AXFocusedUIElementChanged",
---     "AXValueChanged",
---     "AXMenuOpened",
---     "AXMenuClosed",
---     "AXRowExpanded",
---     "AXRowCollapsed",
---     "AXSelectedTextChanged",
---     "AXTitleChanged",
---     "AXSheetCreated",
---     "AXSheetDismissed",
---     "AXMenuItemSelected",
---     "AXLiveRegionChanged",
---     "AXElementBusyChanged",
---     "AXNotification",
--- }
 local M = {}
 M.skip = false
 M.observer = nil
@@ -34,39 +14,8 @@ end
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "D", function()
     M.stopObserving()
 
-    -- EACH key stroke triggers to value changed events (two sep objects)... this might be a good way to capture the element involved in devtools!
-    --    one of these must be a backing field for the other?
-    --
-    --    webArea('DevTools')
-    --    textArea(desc='Console prompt')
-    --
-    -- [AX EVENT] app:window(1):group(1):group(1):group(1):group(1):scrollArea(1):webArea(1):group(1):group(1):group(1):group(1):group(1)
-    --   :group(1):group(1):group(1):group(2):group(1):group(1):group(2):group(1):group(1):group(1):group(1):group(1):group(1)
-    --   :group(1):group(1):group(2):group(2):group(1):group(1):group(1):group(2):textArea(1) test this outs,ss - AXValueChanged
-    --   {
-    --   AXTextChangeElement = <userdata 1> -- hs.axuielement: AXTextArea (0x600010745e78),
-    --   AXTextChangeValues = { {
-    --       AXTextChangeValue = "s",
-    --       AXTextChangeValueLength = 1,
-    --       AXTextChangeValueStartMarker = <userdata 2> -- hs.axuielement.axtextmarker: (0x600010745f38),
-    --       AXTextEditType = 3
-    --     } },
-    --   AXTextStateChangeType = 1
-    -- }
-    -- [AX EVENT] app:window(1):group(1):group(1):group(1):group(1):scrollArea(1):webArea(1)  - AXValueChanged
-    --   {
-    --   AXTextChangeElement = <userdata 1> -- hs.axuielement: AXTextArea (0x60000c2a6f38),
-    --   AXTextChangeValues = { {
-    --       AXTextChangeValue = "s",
-    --       AXTextChangeValueLength = 1,
-    --       AXTextChangeValueStartMarker = <userdata 2> -- hs.axuielement.axtextmarker: (0x60000c2a6ef8),
-    --       AXTextEditType = 3
-    --     } },
-    --   AXTextStateChangeType = 1
-    -- }
-
     local app = hs.application.frontmostApplication()
-    print("[AXObserver] Watching accessibility events for:", app:name())
+    print("[AXObserver] Watching ax events for ask-openai for app:", app:name())
     local appPID = app:pid()
     local appElement = hs.axuielement.applicationElement(app)
 
