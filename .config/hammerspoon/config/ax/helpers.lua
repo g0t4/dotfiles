45904bdeaf1e512734755525255835c3aaf787a7
Wes Higbee <wes.mcclure@gmail.com>
2025-04-05 20:32:44 -0500

    move

--- .config/hammerspoon/config/ax/helpers.lua
+++ .config/hammerspoon/config/ax/helpers.lua
@@ -471,6 +471,25 @@ function BuildHammerspoonLuaTo(toElement)
     return ConcatIntoLines(refChain)
 end
 
+function CombineClausesWithLineContinuations(clauses)
+    -- assumes this is one set of clauses for a single line of code
+    -- and that I can use a line continuation between any of the consecutive clauses
+    --  - i.e. won't result in line continuation in the middle of a string literal
+    local lines = EnumTableValues(clauses)
+        :foldl(function(accum, current)
+            local last = accum[#accum]
+            if string.len(last) + string.len(current) > 80 then
+                table.insert(accum, current)
+                -- FYI don't forget return has to come last in a block (obscure detail I wanted to write down one time)
+                return accum
+            end
+            accum[#accum] = last .. current
+            return accum
+        end, { "" })
+
+    return table.concat(lines, "¬\n    ") -- four spaces on ident too
+end
+
 function ConcatIntoLines(refChain, maxLineLength)
     maxLineLength = maxLineLength or 120
     local lines = { "" }
