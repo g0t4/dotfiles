ff5f78ecbdd9a9bd2c4bb8bdf4570b847139771a
Wes Higbee <wes.mcclure@gmail.com>
2025-04-05 19:16:44 -0500

    notes

--- .config/hammerspoon/config/macros/init.lua
+++ .config/hammerspoon/config/macros/init.lua
@@ -349,10 +349,19 @@ function FindOneElement(app, criteria, callback)
 end
 
 -- *** streamdeck Keyboard Maestro wrapper to catch errors and log them and rethrow for KM to show too ***
--- I want error in hammerspoon logs too
--- I also like the notification from KM b/c that is immediately visible
--- then when I go to look into the issue I want HS logs
 function StreamDeckKeyboardMaestroRunner(what)
+    -- I want error in hammerspoon logs too
+    -- I also like the notification from KM b/c that is immediately visible
+    --   FYI AFAICT KM will use exit code to decide when to show for NOTIFICATIONS
+    --   BUT, it seems to always show on ANY STDOUT if you choose window as the option in KM
+    --   IOTW AFAICT I only wanna use notifications in KM for "output"
+    -- then when I go to look into the issue I want HS logs
+    --
+    -- FYI w/o this its easy to think unhandled exceptions are being swallowed when its just KM not showing errors (b/c you said ignore results, rightly so b/c results all the time are annoying)... anyways use this to always log them)
+    -- TODO can I just wire up something such that anyone that calls hs CLI, the errors are caught and logged... then I wouldn't need this
+    --     anyone that calls hs CLI the errors will go to their CLI instance's STDOUT...
+    --     thus wrap here to catch here too
+    --
     -- anything printed/unhandled here is going to show in logs for hs CLI => only for KM notifications in the case of KM calling this
     xpcall(function()
         -- keep all parsing inside too so I catch those errors and show in HS logs too
