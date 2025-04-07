7c12564f03116426505b3eec9c673c38e5edabae
Wes Higbee <wes.mcclure@gmail.com>
2025-04-07 14:45:46 -0500

    more samples of OSC when CD'ing

--- .config/nvim/lua/plugins/osc-reference.lua
+++ .config/nvim/lua/plugins/osc-reference.lua
@@ -1,3 +1,47 @@
+--
+
+-- FYI ^[ == \x1b
+
+-- TODO do I have old code setting 133;A/B using fish_prompt_mark/end ? and is that still needed in fish 4?
+--   certainly not causing issues but I thought fish4 has it baked in now
+--   probaly leave it for now b/c some envs I still use fish 3.7 (i.e. f*#$%R ubunut/debian)
+
+-- *** OSC for cdless dir change (using fish alt+left/right to move left/right and redraw prompt)
+-- ^[]1337;SetUserVar=split_path=L1VzZXJzL3dlc2RlbW9zL3JlcG9zL2dpdGh1Yi9nMHQ0L2RvdGZpbGVz
+-- ^[]7;file://mbp21/Users/wesdemos/repos/github/g0t4/dotfiles
+-- ^[]133;A;special_key=1
+-- ^[]133;D;0
+-- ^[]1337;RemoteHost=wesdemos@mbp21
+-- ^[]1337;CurrentDir=/Users/wesdemos/repos/github/g0t4
+-- ^[]1337;SetUserVar=ask_shell=ZmlzaA==
+-- ^[]1337;SetUserVar=ask_os=RGFyd2lu
+-- ^[]133;A
+-- ^[]133;B
+-- ^[]133;A;special_key=1
+-- ^[]133;D;0
+-- ^[]1337;RemoteHost=wesdemos@mbp21
+-- ^[]1337;CurrentDir=/Users/wesdemos/repos/github/g0t4/dotfiles
+-- ^[]1337;SetUserVar=ask_shell=ZmlzaA==
+-- ^[]1337;SetUserVar=ask_os=RGFyd2lu
+-- ^[]133;A
+-- ^[]133;B
+
+
+-- *** OSC REF for cd command to new dir too
+-- ^[]133;C
+-- ^[]133;C;
+-- ^[]133;
+-- ^[]1337;SetUserVar=split_path=L1VzZXJzL3dlc2RlbW9zL3JlcG9zL2dpdGh1Yi9nMHQ0
+-- ^[]7;file://mbp21/Users/wesdemos/repos/github/g0t4
+-- ^[]133;D;0
+-- ^[]133;A;special_key=1
+-- ^[]133;D;0
+-- ^[]1337;RemoteHost=wesdemos@mbp21
+-- ^[]1337;CurrentDir=/Users/wesdemos/repos/github/g0t4
+-- ^[]1337;SetUserVar=ask_shell=ZmlzaA==
+-- ^[]1337;SetUserVar=ask_os=RGFyd2lu
+-- ^[]133;A
+-- ^[]133;B
 
 -- I used this to figure out how to grab command output in REPL using tmp file, then wait for command to complete 133;D
 
