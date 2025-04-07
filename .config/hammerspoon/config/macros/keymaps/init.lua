de5f3cbd80b49a1a60ef157ed4322abf5663dc0c
Wes Higbee <wes.mcclure@gmail.com>
2025-04-06 19:40:56 -0500

    notes

---
+++ .config/hammerspoon/config/macros/keymaps/init.lua
@@ -0,0 +1,24 @@
+
+
+-- no idea this existed already!
+--
+-- I can improve upon paletro by making my own popup keymap
+--   can hard code what apps not to use it in ...
+--   can make it search the hotkey symbols too not just the name of the menu entries
+--   can IIAC use hotkeys not in menus too, right?
+--
+-- https://www.hammerspoon.org/docs/hs.hotkey.html#getHotkeys
+-- hs.hotkey.getHotkeys()
+--
+--
+-- https://www.hammerspoon.org/docs/hs.hotkey.html#showHotkeys
+--
+--
+--
+-- check if already used when making new hotkeys
+--   hs.hotkey.assignable(mods, key)
+--
+--
+
+
+
