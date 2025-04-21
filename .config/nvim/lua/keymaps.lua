7f956ee09b6b2ba3ebdb04ee6aaf2735735f2c1f
Wes Higbee <wes.mcclure@gmail.com>
2025-04-21 18:24:20 -0500

    comments

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -161,13 +161,12 @@ vim.cmd [[
     " <C-/> is 1 chord (eqiv to 1 keystroke), not 3 key/chors in a row
     " only issue C-/ won't work with trailing motions, i.e. gc{
     vmap <C-/> gcc
-
-    " PRN add a toggle comments command?, each line is treated separately instead of a multi line comment/uncomment
-
-    " TODO find a comments plugin that extends the neovim builtin, basic comment commands
-
-    " TODO learn more about when/why to use selectmode vs visual mode, have to `:set selectmode=mouse` or key/cmd ... by default its empty for my config
 ]]
+
+
+
+
+
 
 
 -- [<count>]gcl<motion>
