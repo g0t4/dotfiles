7554e620b5d6de5c051dfecf663cca260e2a502a
Wes Higbee <wes.mcclure@gmail.com>
2025-04-24 03:44:43 -0500

    notes

---
+++ .config/nvim/lua/plugins/completions/nvim_insert_completions.lua
@@ -0,0 +1,7 @@
+-- what I care about:
+--  complete require("foo.<TAB> to correct paths
+--  F12 go to files
+--  see my coc keymaps
+--    gd/gr goto def, usages, etc
+--
+--  enter to accept, tab is reserved for copilot du jour plugin
