39925135c359356901a7b10d17ec998633f757bc
Wes Higbee <wes.mcclure@gmail.com>
2025-04-25 10:21:23 -0500

    split out a new file for zeta.lua plugin config

---
+++ .config/nvim/lua/plugins/ai/zeta.lua
@@ -0,0 +1,9 @@
+local zeta_nvim_plugin = {
+    "g0t4/zeta.nvim",
+    -- event = { "CmdlineEnter", "InsertEnter" },
+    dir = "~/repos/github/g0t4/zeta.nvim",
+    opts = {},
+}
+
+
+return zeta_nvim_plugin
--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -617,13 +617,8 @@ local ask_openai_plugin = {
     }
 }
 
-local zeta_nvim_plugin = {
-    "g0t4/zeta.nvim",
-    enabled = vim.tbl_contains(use_ai, "g0t4/zeta.nvim"),
-    -- event = { "CmdlineEnter", "InsertEnter" },
-    dir = "~/repos/github/g0t4/zeta.nvim",
-    opts = {},
-}
+local zeta_nvim_plugin = require("plugins.ai.zeta")
+zeta_nvim_plugin.enabled = vim.tbl_contains(use_ai, "g0t4/zeta.nvim")
 
 -- avante requires 0.10+
 local version = vim.version()
