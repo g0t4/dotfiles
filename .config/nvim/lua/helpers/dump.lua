57eea5addbb74e41c8dfaee9c466dee222d819df
Wes Higbee <wes.mcclure@gmail.com>
2025-05-02 20:26:09 -0500

    create BufferDump module called "dump"... maybe rename it later - stop using BufferDupm* prefix to namespace it globally

--- .config/nvim/lua/helpers/dump.lua
+++ .config/nvim/lua/helpers/dump.lua
@@ -1,6 +1,10 @@
--- *** :Dump vim.g.foo
--- TODO completion for <args>, lua expression completion
+--
+-- * :Dump vim.g.foo
 -- Am I the only who hates typing :lua print(vim.inspect(...))?
+--
+-- TODO completion for <args>, lua expression completion
+-- vim.lua_omnifunc() for lua expression completion
+--
 vim.api.nvim_create_user_command('Dump', "lua print(vim.inspect(<args>))", {
     nargs = '*',
     complete = "lua", -- completes like using :lua command
@@ -10,10 +14,9 @@ vim.api.nvim_create_user_command('Dump', "lua print(vim.inspect(<args>))", {
 -- ]]
 --
 
-function _BufferDumpTest()
-    local inspected = { a = "foo", b = "bar" }
-    BufferDump(inspected, inspected)
-end
+
+-- * DumpBuffer module
+local M = {}
 
 ---@return integer|nil # first window_id for buffer
 local function window_id_for_buffer(bufnr)
@@ -22,13 +25,13 @@ local function window_id_for_buffer(bufnr)
     return window_ids[1]
 end
 
-local function is_buffer_visible(bufnr)
+function M.is_visible(bufnr)
     local window_id = window_id_for_buffer(bufnr)
     return window_id ~= nil
 end
 
-dump_bufnr = nil
-dump_channel = nil
+M.dump_bufnr = nil
+M.dump_channel = nil
 
 vim.api.nvim_create_autocmd("VimLeavePre", {
     callback = function()
@@ -43,71 +46,70 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
     end,
 })
 
+--- does not open the buffer
+--- only creates it if it doesn't already exist
+local function ensure_buffer_exists()
+    if M.dump_bufnr ~= nil then
+        return
+    end
+
+    -- create buffer first time
+    M.dump_bufnr = vim.api.nvim_create_buf(true, false) -- listed, scratch
+
+    -- * terminal backing:
+    -- inspired by `:h TermHl` which also explains the following:
+    -- by default, there's no external process
+    --   instead it echos input to its STDOUT
+    --   STDOUT connects to the buffer so you can see the output in the buffer
+    -- KEEP IN MIND: there is no shell running, nor anything else
+    --   that would have to be started too, and then connected
+    M.dump_channel = vim.api.nvim_open_term(M.dump_bufnr, {})
+    -- why use a temrinal window?
+    --   doesn't have scroll issues like regular buffer
+    --   not stuck with scroll until last line in buffer is at topline)
+    --   supports ansi color sequences (from my inspect helper)
+    --
+    -- set modifiable, so I can programatically change (i.e. clear) the buffer
+    --   otherwise, by default, terminal buffers are not modifiable
+    vim.api.nvim_set_option_value('modifiable', true, { buf = M.dump_bufnr })
+
+    -- -- * non-terminal backing:
+    -- -- set nofile to avoid saving on quit
+    -- vim.api.nvim_set_option_value('buftype', 'nofile', { buf = dump_bufnr })
+
+    -- ensure listed w/ name:
+    --   I want users to easily find it should they want to
+    vim.api.nvim_set_option_value('buflisted', true, { buf = M.dump_bufnr })
+    vim.api.nvim_buf_set_name(M.dump_bufnr, 'buffer_dump')
+end
+
 local function ensure_buffer_is_open()
+    -- TODO! test this works here after creating buffer (before opening it in a window)
     -- note the current window so I can switch back to it when done opening the buffer/window
     local original_window_id = vim.api.nvim_get_current_win()
 
-    -- create buffer first time
-    if dump_bufnr == nil then
-        dump_bufnr = vim.api.nvim_create_buf(true, false) -- listed, scratch
-
-        -- * terminal backing:
-        -- inspired by `:h TermHl` which also explains the following:
-        -- by default, there's no external process
-        --   instead it echos input to its STDOUT
-        --   STDOUT connects to the buffer so you can see the output in the buffer
-        -- KEEP IN MIND: there is no shell running, nor anything else
-        --   that would have to be started too, and then connected
-        dump_channel = vim.api.nvim_open_term(dump_bufnr, {})
-        -- why use a temrinal window?
-        --   doesn't have scroll issues like regular buffer
-        --   not stuck with scroll until last line in buffer is at topline)
-        --   supports ansi color sequences (from my inspect helper)
-        --
-        -- set modifiable, so I can programatically change (i.e. clear) the buffer
-        --   otherwise, by default, terminal buffers are not modifiable
-        vim.api.nvim_set_option_value('modifiable', true, { buf = dump_bufnr })
-
-        -- -- * non-terminal backing:
-        -- -- set nofile to avoid saving on quit
-        -- vim.api.nvim_set_option_value('buftype', 'nofile', { buf = dump_bufnr })
-
-        -- ensure listed w/ name:
-        --   I want users to easily find it should they want to
-        vim.api.nvim_set_option_value('buflisted', true, { buf = dump_bufnr })
-        vim.api.nvim_buf_set_name(dump_bufnr, 'buffer_dump')
-    end
-
     -- ensure buffer is visible
     -- vim.api.nvim_win_set_buf(0, bufnr)
-    if not is_buffer_visible(dump_bufnr) then
+    if not M.is_visible(M.dump_bufnr) then
         vim.api.nvim_command("vsplit")
-        vim.api.nvim_win_set_buf(0, dump_bufnr)
+        vim.api.nvim_win_set_buf(0, M.dump_bufnr)
     end
 
     vim.api.nvim_set_current_win(original_window_id)
 end
-local function buffer_dump(append, ...)
-    ensure_buffer_is_open()
-    buffer_dump_background(append, ...)
-end
-function buffer_dump_background(append, ...)
+
+local function buffer_dump_background(append, ...)
     -- TMP this is not here long term, just for now since my original code all assumes buffer opens if not already
     ensure_buffer_is_open() -- TODO! once I figure out how I want it to work in background, then remove this
-
-
-
-    -- TODO ok now how can I continue to write to the buffer in the background?
-    -- closing it, switching files (buffers) loses its contents since its not a real file
 
     -- PRN use with my existing Dump command in nvim?
     -- dump into a buffer instead of print/echo/etc
     --    that way I don't need to use `:mess` (and `:mess clear`, etc)
 
-    assert(dump_bufnr ~= nil)
+    assert(M.dump_bufnr ~= nil)
 
     if not append then
-        BufferDumpClear()
+        M.clear()
     end
 
     local args = { ... }
@@ -123,7 +125,7 @@ function buffer_dump_background(append, ...)
         -- send output to terminal, so it processes the ANSI color sequences
         -- and output comes over STDOUT back to the buffer
         -- vim.api.nvim_chan_send(dump_channel, table.concat(formatted_args, "\n") .. "\n")
-        vim.api.nvim_chan_send(dump_channel, arg .. "\n")
+        vim.api.nvim_chan_send(M.dump_channel, arg .. "\n")
 
         -- * non-terminal backing:
         -- FYI had to split on "\n" for each arg too, so every line is separate
@@ -136,54 +138,44 @@ function buffer_dump_background(append, ...)
 
     -- TODO not working with term backed buffer?
     -- move cursor to bottom of buffer
-    local dump_window_id = window_id_for_buffer(dump_bufnr)
+    local dump_window_id = window_id_for_buffer(M.dump_bufnr)
     assert(dump_window_id ~= nil)
     vim.fn.win_execute(dump_window_id, "normal G")
 end
 
---- FYI this only APPENDS (for now)
-function BufferDumpArray(array)
-    -- pass an array table that explicitly should be dumped with one item per line
-    -- otherwise, vim.inspect will collapse onto one line... perhaps vim.inspect has flags to pass?
-    -- ** SUPER USEFUL vim.iter
-    vim.iter(array):each(function(_index, item)
-        BufferDumpAppend(item)
-    end)
-end
+function M.header(...)
+    ensure_buffer_exists()
 
-function BufferDumpHeader(...)
     local header = string.format("%s", table.concat({ ... }, " "))
     header = "\n" .. "---------- " .. header .. " ----------\n"
     buffer_dump_background(true, header)
 end
 
-function BufferDump(...)
-    buffer_dump_background(false, ...)
-end
-
-function BufferDumpClear()
-    if dump_bufnr == nil then
+function M.clear()
+    if M.dump_bufnr == nil then
         return
     end
     -- * clear the buffer first
     -- both regular and terminal buffers, if modifiable:
-    vim.api.nvim_buf_set_lines(dump_bufnr, 0, -1, false, {})
+    vim.api.nvim_buf_set_lines(M.dump_bufnr, 0, -1, false, {})
 end
 
-function BufferDumpAppend(...)
+function M.append(...)
     -- assume buffer is open (or explicitly closed) and its fine to append w/o a care for showing it
     buffer_dump_background(true, ...)
 end
 
-function BufferDumpOpen()
+function M.ensure_open()
     ensure_buffer_is_open()
 end
 
 ---@return integer|nil bufnr, integer|nil window_id
-function GetBufferDumpNumbers()
-    if dump_bufnr == nil then
+function M.get_numbers()
+    if M.dump_bufnr == nil then
         return nil, nil
     end
     -- for special cases where I just wanna reuse this buffer
-    return dump_bufnr, window_id_for_buffer(dump_bufnr)
+    return M.dump_bufnr, window_id_for_buffer(M.dump_bufnr)
 end
+
+return M
