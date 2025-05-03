0941221632030e403bce72c61c8fcbe33eef56cc
Wes Higbee <wes.mcclure@gmail.com>
2025-05-03 03:52:12 -0500

    format

--- .config/nvim/lua/localz/comment-highlights.lua
+++ .config/nvim/lua/localz/comment-highlights.lua
@@ -24,21 +24,20 @@ vim.keymap.set('n', '<leader>pt', function()
     print(text)
 end)
 
-vim.api.nvim_set_hl(0, '@comment_todo', { fg = '#ffcc00' })                                         -- TODO test
-vim.api.nvim_set_hl(0, '@comment_todo_bang', { bg = '#ffcc00', fg = "#1f1f1f", bold = true })       -- TODO! test
+vim.api.nvim_set_hl(0, '@comment_todo', { fg = '#ffcc00' }) -- TODO test
+vim.api.nvim_set_hl(0, '@comment_todo_bang', { bg = '#ffcc00', fg = "#1f1f1f", bold = true }) -- TODO! test
 --
-vim.api.nvim_set_hl(0, '@comment_asterisks', { fg = '#ff00c3' })                                    -- *** test
-vim.api.nvim_set_hl(0, '@comment_asterisks_bang', { bg = '#ff00c3', fg = "#1f1f1f", bold = true })  -- ***! test
+vim.api.nvim_set_hl(0, '@comment_asterisks', { fg = '#ff00c3' }) -- *** test
+vim.api.nvim_set_hl(0, '@comment_asterisks_bang', { bg = '#ff00c3', fg = "#1f1f1f", bold = true }) -- ***! test
 
-vim.api.nvim_set_hl(0, '@comment_prn', { fg = "#27AE60" })                                          -- PRN test
-vim.api.nvim_set_hl(0, '@comment_prn_bang', { bg = "#27AE60", fg = "#1f1f1f", bold = true })        -- PRN! test
+vim.api.nvim_set_hl(0, '@comment_prn', { fg = "#27AE60" }) -- PRN test
+vim.api.nvim_set_hl(0, '@comment_prn_bang', { bg = "#27AE60", fg = "#1f1f1f", bold = true }) -- PRN! test
 
-vim.api.nvim_set_hl(0, '@comment_single_bang', { fg = "#cc0000" })                                  -- ! test
-vim.api.nvim_set_hl(0, '@comment_triple_bang', { bg = "#cc0000", fg = "#ffffff", bold = true })     -- !!! test
+vim.api.nvim_set_hl(0, '@comment_single_bang', { fg = "#cc0000" }) -- ! test
+vim.api.nvim_set_hl(0, '@comment_triple_bang', { bg = "#cc0000", fg = "#ffffff", bold = true }) -- !!! test
 
-vim.api.nvim_set_hl(0, '@comment_single_question', { fg = "#3498DB" })                              -- ? test
+vim.api.nvim_set_hl(0, '@comment_single_question', { fg = "#3498DB" }) -- ? test
 vim.api.nvim_set_hl(0, '@comment_triple_question', { bg = "#3498DB", fg = "#1f1f1f", bold = true }) -- ??? test
 
-vim.api.nvim_set_hl(0, '@comment_cell_devider', { underline = true })                               --%%
-vim.api.nvim_set_hl(0, '@comment_cell_devider_bang', { bold = true, underline=true})                --%%! test
-
+vim.api.nvim_set_hl(0, '@comment_cell_devider', { underline = true }) --%%
+vim.api.nvim_set_hl(0, '@comment_cell_devider_bang', { bold = true, underline = true }) --%%! test
