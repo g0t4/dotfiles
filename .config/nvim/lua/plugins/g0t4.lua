08e944b1b9dd9548fa9233cf0842cfaa8682b919
Wes Higbee <wes.mcclure@gmail.com>
2025-05-03 18:20:03 -0500

    add in new devtools.nvim plugin

--- .config/nvim/lua/plugins/g0t4.lua
+++ .config/nvim/lua/plugins/g0t4.lua
@@ -1,6 +1,13 @@
 return {
-    -- enabled = false, -- comment out to enable
-    "g0t4/illuminate.nvim",
-    dir = "~/repos/github/g0t4/illuminate.nvim",
-    opts = {}
+    {
+        -- enabled = false, -- comment out to enable
+        "g0t4/illuminate.nvim",
+        dir = "~/repos/github/g0t4/illuminate.nvim",
+        opts = {}
+    },
+    {
+        "g0t4/devtools.nvim",
+        dir = "~/repos/github/g0t4/devtools.nvim",
+        opts = {}
+    }
 }
