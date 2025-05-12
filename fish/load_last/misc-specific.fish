bfe7ed399e1619b99dd8536181c100d2a3658f2d
Wes Higbee <wes.mcclure@gmail.com>
2025-05-12 08:50:19 -0500

    seda abbr

--- fish/load_last/misc-specific.fish
+++ fish/load_last/misc-specific.fish
@@ -842,8 +842,8 @@ if $IS_MACOS
     abbr --set-cursor='!' sedp "gsed -Ei 's/!//g' **/*.py"
     abbr --command gsed --position=anywhere "*p" "**/*.py"
     # all - use brace expansion for multiple file types
-    abbr --set-cursor='!' seda "gsed -Ei 's/!//g' **/{*.lua,*.md}"
-    abbr --command gsed --position=anywhere "*a" "**/{*.lua,*.md}"
+    abbr --set-cursor='!' seda "gsed -Ei 's/!//g' **/{*.lua,*.md,*.py}"
+    abbr --command gsed --position=anywhere "*a" "**/{*.lua,*.md,*.py}"
     #
     # todo make sede the default now that I am using gsed on macOS?
     abbr --set-cursor='!' sede "gsed -Ei 's/!//g'"
@@ -860,8 +860,8 @@ else
     abbr --command sed --position=anywhere "*m" "**/*.md"
     abbr --set-cursor='!' sedp "sed -Ei 's/!//g' **/*.py"
     abbr --command sed --position=anywhere "*p" "**/*.py"
-    abbr --set-cursor='!' seda "sed -Ei 's/!//g' **/{*.lua,*.md}"
-    abbr --command sed --position=anywhere "*a" "**/{*.lua,*.md}"
+    abbr --set-cursor='!' seda "sed -Ei 's/!//g' **/{*.lua,*.md,*.py}"
+    abbr --command sed --position=anywhere "*a" "**/{*.lua,*.md,*.py}"
     #
     abbr --set-cursor='!' sede "sed -Ei 's/!//g'"
     abbr --set-cursor='!' sedd "sed --debug -i 's/!//g'"
