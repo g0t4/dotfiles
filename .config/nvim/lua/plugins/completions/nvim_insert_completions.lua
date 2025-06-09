f15467795f0936c501450160524beb83536e473b
Wes Higbee <wes.mcclure@gmail.com>
2025-06-09 11:29:03 -0500

    notes

--- .config/nvim/lua/plugins/completions/nvim_insert_completions.lua
+++ .config/nvim/lua/plugins/completions/nvim_insert_completions.lua
@@ -5,3 +5,17 @@
 --    gd/gr goto def, usages, etc
 --
 --  enter to accept, tab is reserved for copilot du jour plugin
+
+
+-- PRN later try lua in nvim's LSP => focus specifically on API for symbols? is it superior to coc?
+-- require('lspconfig').lua_ls.setup {
+--   settings = {
+--     Lua = {
+--       runtime = { version = 'LuaJIT' },
+--       diagnostics = { globals = { 'vim' } },
+--       workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+--       telemetry = { enable = false },
+--     },
+--   },
+-- }
+
