5fcf6d29ca2bf67fb8ce861843bb5ff8d47af4eb
Wes Higbee <wes.mcclure@gmail.com>
2025-06-11 00:51:10 -0500

    why not sooner... why did I always give up on vi mode in the shell?! it is PERFECT for modifying commandlines

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -3,6 +3,10 @@
 # - https://fishshell.com/docs/current/language.html#configuration
 # - https://fishshell.com/docs/current/tutorial.html#tut-config
 
+# ok lets do it! I want vi bindings!
+#   TODO move shit you need, don't just switch back to emacs crap
+fish_vi_key_bindings
+
 # TODO review feature flags: https://fishshell.com/docs/current/language.html#future-feature-flags
 # TODO review options, i.e.:
 set fish_greeting ""
