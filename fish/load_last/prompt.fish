df52e8b33ba297c21227f60a024bca0845e4062c
Wes Higbee <wes.mcclure@gmail.com>
2025-06-18 09:43:32 -0500

    return early

--- fish/load_last/prompt.fish
+++ fish/load_last/prompt.fish
@@ -7,28 +7,30 @@ function fish_mode_prompt
     #   N has fat cursor block, also obvious
     #   V visual... well when I use that I probably have a selection! so that's also obvious
     # LETS leave this off? or?
-    if test "$fish_key_bindings" = fish_vi_key_bindings
-        or test "$fish_key_bindings" = fish_hybrid_key_bindings
-        switch $fish_bind_mode
-            case default
-                set_color --bold red
-                echo '[N]'
-            case insert
-                set_color --bold green
-                echo '[I]'
-            case replace_one
-                set_color --bold green
-                echo '[R]'
-            case replace
-                set_color --bold cyan
-                echo '[R]'
-            case visual
-                set_color --bold magenta
-                echo '[V]'
-        end
-        set_color normal
-        echo -n ' '
+    if not test "$fish_key_bindings" = fish_vi_key_bindings
+        and not test "$fish_key_bindings" = fish_hybrid_key_bindings
+        return
     end
+
+    switch $fish_bind_mode
+        case default
+            set_color --bold red
+            echo '[N]'
+        case insert
+            set_color --bold green
+            echo '[I]'
+        case replace_one
+            set_color --bold green
+            echo '[R]'
+        case replace
+            set_color --bold cyan
+            echo '[R]'
+        case visual
+            set_color --bold magenta
+            echo '[V]'
+    end
+    set_color normal
+    echo -n ' '
 end
 
 function fish_vcs_prompt --description 'Print all vcs prompts'
