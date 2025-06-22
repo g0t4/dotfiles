9952f57bc6a56162ee618e9cb9fd1ff6ce68a02c
Wes Higbee <wes.mcclure@gmail.com>
2025-06-21 22:43:09 -0500

    spike an idea for more vim like motions in fish shell

--- fish/load_first/binds.fish
+++ fish/load_first/binds.fish
@@ -10,6 +10,62 @@ function bind_both_modes_default_and_insert
     bind -M insert $argv
 end
 
+# * idea... to simulate my favorite vim motions/command combos
+#   unfortunately, "word" doesn't match between fish and vim... fish treats - as a boundary too
+#   so I am going with word == bigword
+bind -M default d,i,w _diw
+function _diw
+    commandline -f backward-bigword
+    commandline -f kill-bigword
+end
+
+bind -M default c,i,w _ciw
+function _ciw
+    commandline -f backward-bigword
+    commandline -f kill-bigword
+    set fish_bind_mode insert
+    # alternatively use `bind -m insert` to switch modes after the bound function completes
+end
+
+# * half baked ideas:
+#
+bind -M default v,i,w _viw
+function _viw
+    set fish_bind_mode visual
+    commandline -f backward-bigword
+    commandline -f begin-selection
+    commandline -f forward-bigword
+    # this does select the text, but I am not sure what else I can do with it?
+end
+#
+# function _copy_selection
+#     set -l buf (commandline)
+#     set -l start (commandline --selection-start)
+#     set -l end (commandline --selection-end)
+#
+#     echo $start/$end
+#
+#
+#     if test $start -lt 0
+#         return # no selection
+#     end
+#
+#     set -l selection (string sub --start (math $start) --end (math $end + 1) -- "$buf")
+#
+#     echo -n $selection pbcopy # use xclip/wl-copy if not macOS
+#     commandline -f cancel
+# end
+#
+# copy word? (but don't delete/kill it)
+# bind -M default y,i,w _yiw
+# function _yiw
+#     set fish_bind_mode visual
+#     commandline -f backward-bigword
+#     commandline -f begin-selection
+#     commandline -f forward-bigword
+#     _copy_selection
+# end
+#
 
 # TODO! should I move these bindings to nvim too?!
 # undo/redo should work in insert mode too
