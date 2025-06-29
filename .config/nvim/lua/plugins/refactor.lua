eafc0c2eef704d1ae848c8ffa4194e1cc7ea5f9a
Wes Higbee <wes.mcclure@gmail.com>
2025-06-28 19:40:39 -0500

    keymaps for print refactoring

--- .config/nvim/lua/plugins/refactor.lua
+++ .config/nvim/lua/plugins/refactor.lua
@@ -48,8 +48,8 @@ return {
             -- -- statement (or set two remaps for either one). This remap must be made in normal mode.
             -- -- vim.keymap.set("n", "<leader>rp", function() require('refactoring').debug.printf({ below = false }) end)
             -- -- Print var
-            -- vim.keymap.set({ "x", "n" }, "<leader>rv", function() require('refactoring').debug.print_var() end)
-            -- vim.keymap.set("n", "<leader>rc", function() require('refactoring').debug.cleanup({}) end)
+            vim.keymap.set({ "x", "n" }, "<leader>rp", function() require('refactoring').debug.print_var() end)
+            vim.keymap.set("n", "<leader>rpc", function() require('refactoring').debug.cleanup({}) end)
 
             -- easiest to trigger...
             -- * refactor menu
