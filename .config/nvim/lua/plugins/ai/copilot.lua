6d19a0aae36c85cafd251ff20d53d32961714363
Wes Higbee <wes.mcclure@gmail.com>
2025-06-30 00:54:07 -0500

    moved testing lsp config over to ask-openai plugin

--- .config/nvim/lua/plugins/ai/copilot.lua
+++ .config/nvim/lua/plugins/ai/copilot.lua
@@ -15,26 +15,6 @@ local lsp_ask_openai = {
     -- TODO make sure I don't double define nvim-lspconfig plugin? have one spot register it and then just config server here?
     "neovim/nvim-lspconfig",
     config = function()
-        local lspconfig = require("lspconfig")
-        local configs = require("lspconfig.configs")
-
-        if not configs.ask_language_server then
-            configs.ask_language_server = {
-                default_config = {
-                    cmd = {
-                        os.getenv("HOME") .. "/repos/github/g0t4/ask-openai.nvim/.venv/bin/python",
-                        "-m",
-                        "lsp.server",
-                    },
-                    cmd_cwd = os.getenv("HOME") .. "/repos/github/g0t4/ask-openai.nvim/lua/ask-openai/rag",
-                    filetypes = { "lua" },
-                    root_dir = require("lspconfig.util").root_pattern(".git", "."),
-
-                },
-            }
-        end
-
-        lspconfig.ask_language_server.setup({})
     end
 }
 
