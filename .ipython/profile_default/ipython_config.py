01a2258d566afddecf01cb5d324223fc629ce16f
Wes Higbee <wes.mcclure@gmail.com>
2025-06-30 23:05:47 -0500

    remap Ctrl-W in ipython, so it stops on . (dots) as a word boundary...

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -27,3 +27,72 @@ c.TerminalIPythonApp.display_banner = False
 from IPython.core.ultratb import VerboseTB
 
 VerboseTB.tb_highlight = "bg:ansiyellow ansiblack"  # I like the yellow, just want black text
+
+# * change keymaps
+c.TerminalInteractiveShell.shortcuts = [
+    {
+        # change how ctrl+w treats word boundary chars... somehow this triggers it to respect . (dot) as a word separator
+        # test this with:
+        #  import foo.bar<Ctrl-W>
+        #  now it should stop on . (not wipe out entire module name)
+        "new_keys": ["c-w"],
+        "command": "prompt_toolkit:named_commands.backward_kill_word",
+        "create": True,
+    }
+]
+
+#  - IPython:shortcuts.handle_return_or_newline_or_execute
+#  - IPython:shortcuts.reformat_and_execute
+#  - IPython:shortcuts.quit
+#  - IPython:shortcuts.previous_history_or_previous_completion
+#  - IPython:shortcuts.next_history_or_next_completion
+#  - IPython:shortcuts.dismiss_completion
+#  - IPython:shortcuts.reset_buffer
+#  - IPython:shortcuts.reset_search_buffer
+#  - IPython:shortcuts.suspend_to_bg
+#  - IPython:shortcuts.indent_buffer
+#  - IPython:shortcuts.newline_autoindent
+#  - IPython:shortcuts.open_input_in_editor
+#  - IPython:auto_match.parenthesis
+#  - IPython:auto_match.brackets
+#  - IPython:auto_match.braces
+#  - IPython:auto_match.raw_string_parenthesis
+#  - IPython:auto_match.raw_string_bracket
+#  - IPython:auto_match.raw_string_braces
+#  - IPython:auto_match.double_quote
+#  - IPython:auto_match.single_quote
+#  - IPython:auto_match.docstring_double_quotes
+#  - IPython:auto_match.docstring_single_quotes
+#  - IPython:auto_match.skip_over
+#  - IPython:auto_match.delete_pair
+#  - IPython:auto_suggest.accept_or_jump_to_end
+#  - IPython:auto_suggest.accept
+#  - IPython:auto_suggest.accept_word
+#  - IPython:auto_suggest.accept_token
+#  - IPython:auto_suggest.discard
+#  - IPython:auto_suggest.swap_autosuggestion_up
+#  - IPython:auto_suggest.swap_autosuggestion_down
+#  - IPython:auto_suggest.up_and_update_hint
+#  - IPython:auto_suggest.down_and_update_hint
+#  - IPython:auto_suggest.accept_character
+#  - IPython:auto_suggest.accept_and_move_cursor_left
+#  - IPython:auto_suggest.accept_and_keep_cursor
+#  - IPython:auto_suggest.backspace_and_resume_hint
+#  - IPython:auto_suggest.resume_hinting
+#  - prompt_toolkit:completion.display_completions_like_readline
+#  - IPython:shortcuts.win_paste
+#  - prompt_toolkit:named_commands.beginning_of_line
+#  - prompt_toolkit:named_commands.backward_char
+#  - prompt_toolkit:named_commands.kill_line
+#  - prompt_toolkit:named_commands.backward_kill_word
+#  - prompt_toolkit:named_commands.yank
+#  - prompt_toolkit:named_commands.undo
+#  - prompt_toolkit:named_commands.edit_and_execute
+#  - prompt_toolkit:named_commands.backward_word
+#  - prompt_toolkit:named_commands.capitalize_word
+#  - prompt_toolkit:named_commands.kill_word
+#  - prompt_toolkit:named_commands.downcase_word
+#  - prompt_toolkit:named_commands.uppercase_word
+#  - prompt_toolkit:named_commands.yank_pop
+#  - prompt_toolkit:named_commands.yank_last_arg
+#
