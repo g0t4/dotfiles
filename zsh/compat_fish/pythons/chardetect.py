23873bf3366e97964e00456da2fd62a13f37376d
Wes Higbee <wes.mcclure@gmail.com>
2025-07-10 09:49:12 -0500

    skip dirs for now

--- zsh/compat_fish/pythons/chardetect.py
+++ zsh/compat_fish/pythons/chardetect.py
@@ -1,11 +1,20 @@
 import chardet
 import sys
+from pathlib import Path
 from rich import print
 
 files = sys.argv[1:]
 # print(f'{files=}')
 
 for file in files:
+    file_path = Path(file)
+    if file_path.is_dir():
+        # this comes up if you do `chardetect *` ...
+        # skipping saves you from needing to use `cardetect *.*`
+        # ? should I add recursive flag and check these then?
+        print(f"Skipping directory: {file}")
+        continue
+
     with open(file, "rb") as f:
         raw_data = f.read()
         result = chardet.detect(raw_data)
