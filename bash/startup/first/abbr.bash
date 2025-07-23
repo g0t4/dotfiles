077edc249734694b802aa6a05cf3873e38c27f04
Wes Higbee <wes.mcclure@gmail.com>
2025-07-23 03:22:37 -0500

    test if I can pass args this way too... YUP!

--- bash/startup/first/abbr.bash
+++ bash/startup/first/abbr.bash
@@ -3,6 +3,8 @@ declare -A abbrs=(
     [gdlc]="git log --patch HEAD~1..HEAD"
 )
 expand_abbr() {
+    local tmp=$1
+    echo $tmp
     local cmd=$READLINE_LINE
     local expanded="${abbrs[$cmd]}"
     if [[ "$expanded" != "" ]]; then
@@ -15,9 +17,9 @@ expand_abbr() {
     READLINE_POINT=${#READLINE_LINE} # then, move cursor to end too
 }
 # bind -x '"\C-a":expand_abbr'
-bind -x '" ":expand_abbr' # on space works!
+bind -x '" ":expand_abbr space' # on space works!
 # TODO ok on Enter then submit the command after!!
-bind -x '"\C-m":expand_abbr' # on enter (Ctrl-m) works!
+bind -x '"\C-m":expand_abbr enter' # on enter (Ctrl-m) works!
 
 abbr() {
     # PRN handle options (if any) when I add abbrs that use them
