e67813fe21af3343cf3d65ca1cdf1bce87cb577c
Wes Higbee <wes.mcclure@gmail.com>
2025-07-23 14:14:49 -0500

    work on some bash abbrs for troubleshooting

--- bash/startup/first/abbr.bash
+++ bash/startup/first/abbr.bash
@@ -63,3 +63,12 @@ abbr n nvim
 
 abbr dils "docker image ls"
 abbr dcls "docker container ls"
+
+# declare -p abbrs  # sanity check
+abbr declarep "declare -p"
+# would be cool to get a full blown snippet system in bash (and other shells)...
+#  ea => echo "${placeholder1[@]}" # put cursor on placeholder1 slot
+#     I could do this with cursor positioning like --set-cursor in fish abbrs
+#     and I had an IMPL of that in zsh prior
+# shellcheck disable=SC2016 # expressions in single quotes don't expand, yup that's the point here!
+abbr ea 'echo "${name[@]}"'
