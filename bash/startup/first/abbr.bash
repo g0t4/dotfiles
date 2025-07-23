cd063e83a17aae9bf503ca5400418a1bf0bb8990
Wes Higbee <wes.mcclure@gmail.com>
2025-07-23 03:53:03 -0500

    ok for now just use command not found handler to do expansion (if availabole)...

--- bash/startup/first/abbr.bash
+++ bash/startup/first/abbr.bash
@@ -3,28 +3,29 @@ declare -A abbrs=(
     [gdlc]="git log --patch HEAD~1..HEAD"
 )
 expand_abbr() {
-    local bound_key=$1
     local cmd=$READLINE_LINE
     local expanded="${abbrs[$cmd]}"
     if [[ "$expanded" != "" ]]; then
         # expand and add space:
-        READLINE_LINE="${expanded}"
+        READLINE_LINE="${expanded} " # $'\n'  # this inserts new line like multiline! close but not yet
     else
         # otherwise just add space:
-        READLINE_LINE="${cmd}"
+        READLINE_LINE="${cmd} "
     fi
     READLINE_POINT=${#READLINE_LINE} # then, move cursor to end too
-    if [[ $bound_key == "enter" ]]; then
-        echo -ne '\r'
-    else
-        READLINE_LINE="${READLINE_LINE} "
-    fi
-
+    # bind has an accept-line! I wish I could trigger it... why can't I?!
 }
-# bind -x '"\C-a":expand_abbr'
 bind -x '" ":expand_abbr " "' # on space works!
-# TODO ok on Enter then submit the command after!!
-bind -x '"\C-m":expand_abbr enter' # on enter (Ctrl-m) works!
+
+command_not_found_handle() {
+    # TODO would be nice to use bind -x to expand the abbr TOO and then have that submit the command with bind's `accept-line`...
+    # just haven't figured out how to trigger that yet!
+    # inserting $'\n' literally adds a new line
+    local expanded="${abbrs[$1]}"
+    if [[ "$expanded" != "" ]]; then
+        eval "$expanded"
+    fi
+}
 
 abbr() {
     # PRN handle options (if any) when I add abbrs that use them
