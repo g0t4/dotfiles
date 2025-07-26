abd616ec448b44be6650f4967b11b716d0268d9c
Wes Higbee <wes.mcclure@gmail.com>
2025-07-26 01:48:27 -0500

    review

--- bash/startup/first/_utils.bash
+++ bash/startup/first/_utils.bash
@@ -1,14 +1,14 @@
-
 indexed_array_contains() {
+    # FYI ideally would use associative array so O(1) lookup vs O(n) below
 
     # usage:
     #   arr=(foo bar baz)
     #   indexed_array_contains "bar" "${arr[@]}" && echo "found"
 
-    local val=$1
+    local value="$1"
     shift # remove val (first positional param) off of $@
     for item in "$@"; do
-        [[ $item == "$val" ]] && return 0
+        [[ "$item" == "$value" ]] && return 0
     done
     return 1
 }
