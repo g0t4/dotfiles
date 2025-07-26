9143dc01835fbf9d2098822a6bcbe49fb011da41
Wes Higbee <wes.mcclure@gmail.com>
2025-07-25 23:25:57 -0500

    review nvim bash snippets and sync changes to related abbrs... for basically command line snippets - prefer more explanator snippet names b/c tab completion will handle the length issue - easier to find all for_* related snippets by typing for_<TAB>!

--- bash/startup/bind-specific.bash
+++ bash/startup/bind-specific.bash
@@ -15,19 +15,19 @@ abbr declarep "declare -p % | bat -l bash" --set-cursor
 #     jump positions too with Ctrl-J or similar (like nvim)
 # shellcheck disable=SC2016 # expressions in single quotes don't expand, yup that's the point here!
 abbr d@='"${%[@]}"' --position=anywhere --set-cursor
-abbr d@#='${#%[@]}' --position=anywhere --set-cursor
+abbr d_array_length='${#%[@]}' --position=anywhere --set-cursor
 abbr d*='"${%[*]}"' --position=anywhere --set-cursor
 abbr dx='"${%}"' --position=anywhere --set-cursor
 abbr dxu='${%}' --position=anywhere --set-cursor
-abbr ed*='declare -p'
-abbr ed@#='echo ${#%[@]}' --set-cursor
-abbr edi='echo _"${%[0]}"_' --set-cursor # array item, adds [0] as a convenience
-abbr edx='echo _"${%}"_' --set-cursor
-abbr edxu='echo _${%}_' --set-cursor
+abbr echo_array_length='echo ${#%[@]}' --set-cursor
+abbr echo_scalar='echo _"${%}"_' --set-cursor
+abbr echo_array='declare -p % | bat -l bash'
+abbr echo_array_element='echo _"${%[0]}"_' --set-cursor # array item, adds [0] as a convenience
 #
 # aliased, see which I prefer
 abbr for_in_array='for item in "${%[@]}"; do echo $item; done' --set-cursor
-abbr f@='for item in "${%[@]}"; do echo $item; done' --set-cursor
+abbr for_in_range='for i in "{1..10%}"; do echo $i; done' --set-cursor
+abbr for_in_array_indicies='for i in "${!%[@]}"; do echo "${i} ${name[$i]}"; done' --set-cursor
 
 abbr pIFS "echo -n \"\${IFS}\" | hexdump -C" # block word splitting, or it will split it's own characters :)
 
