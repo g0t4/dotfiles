269e806983c8b6398650917bdb1beed420016d28
Wes Higbee <wes.mcclure@gmail.com>
2025-07-28 12:32:03 -0500

    map some emacs keymaps for bash expansions into vi mode

--- bash/startup/binds.bash
+++ bash/startup/binds.bash
@@ -1,18 +1,37 @@
 #
 # *** vi mode
-set -o vi
-# FYI find differences:
-#    icdiff <((set -o vi; bind -p)) <((set -o emacs; bind -p))
+# set -o vi
+# fix vi-insert vs emacs differences:
+#   icdiff <((bind -m vi-insert -p)) <((bind -m emacs -p ))
 #
 # FIX self-insert differences
-#    icdiff <((set -o vi; bind -p | grep -i self-insert)) <((set -o emacs; bind -p | grep -i self-insert))
-#
-bind '"\\": self-insert' # missing in vi mode?! found in emacs mode:   bind -p | grep '\\"'
+#    icdiff <((bind -m vi-insert -p)) <((bind -m emacs -p )) | grep -i self-insert
+#    ok and now this isn't an issue... WTH? maybe one of my bindings was wrong when testing and I deleted \\?
+# bind -m vi-insert '"\\": self-insert' # missing in vi mode?! found in emacs mode:   bind -p | grep '\\"'
 #
 # FYI make sure to bind keymaps for vi mode too (-m vi-insert) (-m vi-command)
 # choices: emacs, emacs-standard, emacs-meta, emacs-ctlx, vi, vi-move, vi-command, and vi-insert
 # also emacs keymaps I like, rebound for vi-insert
 bind -m vi-insert '"\e.": yank-last-arg'
+#
+# emacs expand key sequences (no collision with vi-insert mode)
+#  that way I can still demo w/ the same defaults
+bind -m vi-insert '"\eg": glob-complete-word'
+bind -m vi-insert '"\C-x*": glob-expand-word'
+bind -m vi-insert '"\e^": history-expand-line'
+bind -m vi-insert '"\e\C-e": shell-expand-line'
+#  wow shell-expand-line messes up:
+#  foo=(a "b c" " " ef)
+#  echo "${foo[@]}" # invoke shell-expand-line... doesn't quote the expanded args?!
+#  # this is the result, wow:
+#  echo a b c   ef
+bind -m vi-insert '"\e&": tilde-expand'
+#
+# maybe adds (unbound by default in emacs):
+# alias-expand-line
+# history-and-alias-expand-line
+
+# FYI \C-x\C-u is mapped in vi-insert to undo, works good too!
 
 # *** custom yank
 
