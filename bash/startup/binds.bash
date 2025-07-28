c63037bdfbfbc8a59898e8030e9cbc2c19467bc9
Wes Higbee <wes.mcclure@gmail.com>
2025-07-28 10:45:56 -0500

    albeit unnecessary, bind Esc-k in vi-insert keymap too

--- bash/startup/binds.bash
+++ bash/startup/binds.bash
@@ -1,4 +1,10 @@
+#
+# *** vi mode
 set -o vi
+# FYI make sure to bind keymaps for vi mode too (-m vi-insert) (-m vi-command)
+# choices: emacs, emacs-standard, emacs-meta, emacs-ctlx, vi, vi-move, vi-command, and vi-insert
+
+# *** custom yank
 
 copy_and_clear_line() {
     # current line only, FYI in vi mode you can use `yy` or `dd` to copy/nuke entire line
@@ -7,6 +13,8 @@ copy_and_clear_line() {
 }
 
 bind -x '"\ek": copy_and_clear_line'
+bind -m vi-insert -x '"\ek": copy_and_clear_line'
+# FYI I should be using yy/dd in vi mode... but I am going to leave Esc-k too just to have it be consistent...
 # FYI use \C-k for Ctrl-k vs \ek for Esc-k ... cannot do: \Ck nor \e-k
 
 # *** bind
@@ -19,5 +27,3 @@ abbr bind_list_macros "bind -s"
 #
 abbr bind_list_bash_funcs "bind -X"
 # TODO bind -x doesn't work to give executable format... is there another way to match -s/-p (little s/p) ... -S/-P/-X are all "human readable" ... why isn't there an exec format for -X?!
-
-
