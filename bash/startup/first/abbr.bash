320927563f527c60723ec4b667f704e9290414ce
Wes Higbee <wes.mcclure@gmail.com>
2025-07-29 14:16:01 -0500

    new snippets for if empty / not empty in bash

--- bash/startup/first/abbr.bash
+++ bash/startup/first/abbr.bash
@@ -411,6 +411,9 @@ list_abbrs() {
             # still need to add "" though and to expand the quoted var requires "" so... just yeah:
             opts+=("--regex" "\"$quoted_regex\"")
         fi
+        if [[ -n "${cmd_only}" ]]; then
+            opts+=("--command" "$cmd_only")
+        fi
         if [[ -n "$set_cursor" ]]; then
             opts+=("--set-cursor=$set_cursor")
         fi
