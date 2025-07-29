a7523ed1358055cc18c0c40458b1d3f5c67daea7
Wes Higbee <wes.mcclure@gmail.com>
2025-07-28 19:26:30 -0500

    ok using ctrl-x,ctrl-i for my virtual complete key seq

--- bash/startup/first/abbr.bash
+++ bash/startup/first/abbr.bash
@@ -255,17 +255,16 @@ bind -m vi-insert "\"\C-m\": \"$key_seq_expand_abbr_enter$key_seq_accept_line\""
 # * expand on <Tab>
 key_seq_expand_abbr_tab='\C-x\C-['
 #
-# key_seq_complete='\C-x\C-]' # if I want my own key seq again for accept-line
-key_seq_complete='\C-i' # OOB complete, don't need another virtual key seq
+key_seq_complete='\C-x\C-i' # use this for a new complete key seq since I wanna replace the only one which is Ctrl-i
 #
 # emacs keymap:
 bind -m emacs -x "\"$key_seq_expand_abbr_tab\": expand_abbr enter"
-# bind -m emacs "\"$key_seq_complete\": complete" # this appears redundant given I am using OOB \C-j but it doesn't hurt to leave it
+bind -m emacs "\"$key_seq_complete\": complete" # this appears redundant given I am using OOB \C-j but it doesn't hurt to leave it
 bind -m emacs "\"\C-m\": \"$key_seq_expand_abbr_tab$key_seq_complete\""
 #
 # vi-insert keymap:
 bind -m vi-insert -x "\"$key_seq_expand_abbr_tab\": expand_abbr enter"
-# bind -m vi-insert "\"$key_seq_complete\": complete"
+bind -m vi-insert "\"$key_seq_complete\": complete"
 bind -m vi-insert "\"\C-m\": \"$key_seq_expand_abbr_tab$key_seq_complete\""
 
 command_exists() {
