c6e955acc679699690564239aec4c595d56fabdf
Wes Higbee <wes.mcclure@gmail.com>
2025-07-30 11:13:27 -0500

    test expand after opening ( parens for subshell - broken test

--- bash/startup/first/abbr.bash
+++ bash/startup/first/abbr.bash
@@ -932,6 +932,16 @@ test_expand_abbr() {
     expect_equal "$READLINE_LINE" "echo hello; bar "
     expect_equal "$READLINE_POINT" 16
 
+    label_test "should expand command position after a standalone ( subshell opener"
+    reset_abbrs
+    abbr foo bar
+    READLINE_LINE="echo hello ( foo"
+    READLINE_POINT=16
+    expand_abbr " "
+    expect_equal "$READLINE_LINE" "echo hello ( bar "
+    expect_equal "$READLINE_POINT" 17
+
+
     # TODO assume separators for pipelines/lists indicate next word is in command position
     # simple command is what I have now:
     #   gst<SPACE> => git status
