b3deade4eb4eb6db1a17aa12a960018ae4d54f7a
Wes Higbee <wes.mcclure@gmail.com>
2025-07-30 11:10:31 -0500

    format

--- bash/startup/misc.abbrs.bash
+++ bash/startup/misc.abbrs.bash
@@ -33,10 +33,10 @@ abbr read_prompt 'read -rp "Prompt: "'                               # Prompted 
 abbr read_array 'read -ra arr'                                       # Read into array, splitting on $IFS
 abbr read_loop_demo 'while IFS= read -r line; do echo "$line"; done' # Loop over lines (think cat, for demo purposes)
 # * mapfile/readarray
-abbr mapfile_lines 'mapfile -t lines <' # Common pattern for lines[], -t strip trail delim (newline)
+abbr mapfile_lines 'mapfile -t lines <'                     # Common pattern for lines[], -t strip trail delim (newline)
 abbr --set-cursor mapfile_n_lines 'mapfile -n % -t lines <' # read N lines
-abbr --set-cursor mapfile_str 'mapfile -t <<<"$%"' # From a multiline string
-abbr --set-cursor mapfile_cmd 'mapfile -t < <(%)'   # From command output
+abbr --set-cursor mapfile_str 'mapfile -t <<<"$%"'          # From a multiline string
+abbr --set-cursor mapfile_cmd 'mapfile -t < <(%)'           # From command output
 
 abbr pstree_bash_shell 'pstree -p $$'
 
