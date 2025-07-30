2c71118ee43cf1e77ffd1d61d4e1a296a4755063
Wes Higbee <wes.mcclure@gmail.com>
2025-07-29 23:39:13 -0500

    absolute path to generated files

--- bash/fish_to_bash_aliases.fish
+++ bash/fish_to_bash_aliases.fish
@@ -1,20 +1,18 @@
 #!/usr/bin/env fish
 
-# \\\' is to filter out a few records with \' escaped in ' which fish supports but bash does not
-#  PRN rewrite in fish to port easier to bash?
-
-#  BTW... some things may "run" but not work the same if there's a runtime discrepancy
+set file_aliases "$WES_DOTFILES/bash/.generated.aliases.bash"
+set file_skipped "$WES_DOTFILES/bash/.generated.skipped.bash"
 
 # skip options I am not ready to parse yet
 #  ' -- ' after arg ensures not a match in the abbr's "key value" positional args
 # | grep -vE '\-\-(regex).* -- '
 abbr | sort \
     | grep -v "\\\'" \
-    | grep -v "\-- -F" >.generated.aliases.bash
+    | grep -v "\-- -F" >"$file_aliases"
 
 # * produce skipped list BEFORE any mods for regexs (or otherwise)
 # sort both just to be safe, else comm won't work
-comm -23 (abbr | sort | psub) (sort .generated.aliases.bash | psub) >.generated.skipped.bash
+comm -23 (abbr | sort | psub) (sort "$file_aliases" | psub) >"$file_skipped"
 
 # ** MAKE SURE TO TEST THIS IN SAME SHELL (fish, see shebang above)...
 #   b/c bash treats \ as literal inside  '' but fish doesn't
@@ -26,16 +24,18 @@ comm -23 (abbr | sort | psub) (sort .generated.aliases.bash | psub) >.generated.
 #
 # *fish versions:
 # ==> fix \\d to be [0-9]
-# gsed -n 's/\\\\\\\\d/[0-9]/gp'    .generated.aliases.bash # preview w/ drop -i, add -n... add p => /gp
-gsed -i 's/\\\\\\\\d/[0-9]/g' .generated.aliases.bash
+# gsed -n 's/\\\\\\\\d/[0-9]/gp'    "$file_aliases" # preview w/ drop -i, add -n... add p => /gp
+gsed -i 's/\\\\\\\\d/[0-9]/g' "$file_aliases"
 #
-gsed -i 's/\\\\\\\\\./\\\\\./g' .generated.aliases.bash
+gsed -i 's/\\\\\\\\\./\\\\\./g' "$file_aliases"
 # ==> fix \\. to be \. only... which is escaped dot char in regex
 # NOTE there's an extra (9th) \ for \. in this regex! so we don't match on ANY char after \\!
 #
-gsed -i 's/\\\\\\\\b/\\\\b/g' .generated.aliases.bash
+gsed -i 's/\\\\\\\\b/\\\\b/g' "$file_aliases"
 # ==> fix \\b to be \b only in regex
 #
 # FYI bash version of \\d (\ is literal in '' => hence four \ works)
-# gsed -n 's/\\\\d/[0-9]/gp'    .generated.aliases.bash
+# gsed -n 's/\\\\d/[0-9]/gp'    "$file_aliases"
 #
+
+# bash -c "generate_func_wrappers"
