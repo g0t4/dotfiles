51e808917fafcbeb1297f57d40acf41b9458a815
Wes Higbee <wes.mcclure@gmail.com>
2025-07-31 00:18:05 -0500

    vetted

--- bash/startup/full-only/abbrs.snippets.bash
+++ bash/startup/full-only/abbrs.snippets.bash
@@ -10,37 +10,6 @@ abbr --set-cursor mapfile_n_lines 'mapfile -n % -t lines <' # read N lines
 abbr --set-cursor mapfile_str 'mapfile -t <<<"$%"'          # From a multiline string
 abbr --set-cursor mapfile_cmd 'mapfile -t < <(%)'           # From command output
 
-# TODO mirror with sh.snippets from my nvim config...
-# * would be cool to get a full blown snippet system in bash (and other shells)...
-#  ea => echo "${placeholder1[@]}" # put cursor on placeholder1 slot
-#     I could do this with cursor positioning like --set-cursor in fish abbrs
-#     and I had an IMPL of that in zsh prior
-#     jump positions too with Ctrl-J or similar (like nvim)
-# shellcheck disable=SC2016 # expressions in single quotes don't expand, yup that's the point here!
-abbr d@='"${%[@]}"' --position=anywhere --set-cursor
-abbr d_array_length='${#%[@]}' --position=anywhere --set-cursor
-abbr d_string_length='${#%}' --position=anywhere --set-cursor
-abbr d*='"${%[*]}"' --position=anywhere --set-cursor
-# do not always need {}
-abbr dx='"$%"' --position=anywhere --set-cursor
-abbr dxb='"${%}"' --position=anywhere --set-cursor
-abbr dxu='${%}' --position=anywhere --set-cursor
-#
-abbr d_default_value_if_unset='"${%:-default_value}"' --position=anywhere --set-cursor
-abbr d_swap_if_set '"${%:+use_this_if_set}"' --position=anywhere --set-cursor
-abbr d_assign_if_unset '"${%:=assign_this_if_unset}"' --position=anywhere --set-cursor
-abbr d_error_if_unset '"${%:?error_message_if_unset}"' --position=anywhere --set-cursor
-#
-abbr echo_variable='declare -p % | bat -l bash'         # arrays and scalars
-abbr echo_array_element='echo _"${%[0]}"_' --set-cursor # array item, adds [0] as a convenience (could be a 2nd placeholder in a future snippets system)
-abbr echo_array_length='echo ${#%[@]}' --set-cursor
-#
-# aliased, see which I prefer
-abbr for_in_array='for item in "${%[@]}"; do echo $item; done' --set-cursor
-abbr for_in_range='for i in "{1..10%}"; do echo $i; done' --set-cursor
-abbr for_in_array_indicies='for i in "${!%[@]}"; do echo "${i} ${name[$i]}"; done' --set-cursor
-
-
 # * trap
 abbr trapl "trap -l"
 abbr trapp "trap -p"
--- bash/startup/vetted-benign/abbrs.snippets.bash
+++ bash/startup/vetted-benign/abbrs.snippets.bash
@@ -13,6 +13,28 @@ abbr declareu "declare -u"
 abbr --set-cursor declaref "declare -f % | bat -l bash" # function name/definition
 # abbr declareF "declare -F" # function name only # basically a way to check if function is defined
 
+# * (d)ollar expand
+# TODO mirror with sh.snippets from my nvim config...
+# * would be cool to get a full blown snippet system in bash (and other shells)...
+#  ea => echo "${placeholder1[@]}" # put cursor on placeholder1 slot
+#     I could do this with cursor positioning like --set-cursor in fish abbrs
+#     and I had an IMPL of that in zsh prior
+#     jump positions too with Ctrl-J or similar (like nvim)
+# shellcheck disable=SC2016 # expressions in single quotes don't expand, yup that's the point here!
+abbr d@='"${%[@]}"' --position=anywhere --set-cursor
+abbr d_array_length='${#%[@]}' --position=anywhere --set-cursor
+abbr d_string_length='${#%}' --position=anywhere --set-cursor
+abbr d*='"${%[*]}"' --position=anywhere --set-cursor
+# do not always need {}
+abbr dx='"$%"' --position=anywhere --set-cursor
+abbr dxb='"${%}"' --position=anywhere --set-cursor
+abbr dxu='${%}' --position=anywhere --set-cursor
+#
+abbr d_default_value_if_unset='"${%:-default_value}"' --position=anywhere --set-cursor
+abbr d_swap_if_set '"${%:+use_this_if_set}"' --position=anywhere --set-cursor
+abbr d_assign_if_unset '"${%:=assign_this_if_unset}"' --position=anywhere --set-cursor
+abbr d_error_if_unset '"${%:?error_message_if_unset}"' --position=anywhere --set-cursor
+
 # * printf
 abbr --set-cursor='!' pfv 'printf -v ! "%s"'              # capture in variable
 abbr pf 'printf "%s\n"'                                   # Basic safe string output
@@ -26,6 +48,14 @@ abbr --set-cursor='!' pf_repeat 'printf "%0.s=" {1..30!}' # Print 30 '=' chars
 
 # * echo
 abbr --set-cursor echoe "echo -e '%'"
+abbr echo_variable='declare -p % | bat -l bash'         # arrays and scalars
+abbr echo_array_element='echo _"${%[0]}"_' --set-cursor # array item, adds [0] as a convenience (could be a 2nd placeholder in a future snippets system)
+abbr echo_array_length='echo ${#%[@]}' --set-cursor
+
+# * for loops
+abbr for_in_array='for item in "${%[@]}"; do echo $item; done' --set-cursor
+abbr for_in_range='for i in "{1..10%}"; do echo $i; done' --set-cursor
+abbr for_in_array_indicies='for i in "${!%[@]}"; do echo "${i} ${name[$i]}"; done' --set-cursor
 
 # * p(rint) * helpers
 abbr pIFS "echo -n \"\${IFS}\" | hexdump -C" # block word splitting, or it will split it's own characters :)
