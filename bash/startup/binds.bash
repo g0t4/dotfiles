6a07b7b58937ada365c6dc0656a80cf5e2662095
Wes Higbee <wes.mcclure@gmail.com>
2025-07-30 23:58:52 -0500

    split apart binds I want in the course vs not... b/c I won't be using vi-mode (most likely) in the course, that way I am using same set of keymaps

--- bash/startup/binds.bash
+++ bash/startup/binds.bash
@@ -57,20 +57,6 @@ bind -m vi-insert '"\e&": tilde-expand'
 
 # FYI \C-x\C-u is mapped in vi-insert to undo, works good too!
 
-# *** custom yank
-
-copy_and_clear_line() {
-    # current line only, FYI in vi mode you can use `yy` or `dd` to copy/nuke entire line
-    echo -n "$READLINE_LINE" | pbcopy
-    READLINE_LINE=
-}
-
-bind -m emacs -x '"\ek": copy_and_clear_line'
-bind -m vi-insert -x '"\ek": copy_and_clear_line'
-bind -m vi-command -x '"\ek": copy_and_clear_line'
-# FYI I should be using yy/dd in vi mode... but I am going to leave Esc-k too just to have it be consistent...
-# FYI use \C-k for Ctrl-k vs \ek for Esc-k ... cannot do: \Ck nor \e-k
-
 # *** bind
 _abbrs_bind() {
     # syntax highlight bind mappings
