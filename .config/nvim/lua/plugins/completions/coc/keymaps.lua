ac372fb983c88a1eafaadb6b8fd2f4cefe3301fe
Wes Higbee <wes.mcclure@gmail.com>
2025-07-30 20:30:50 -0500

    use table contains so it's easier to add more types

--- .config/nvim/lua/plugins/completions/coc/keymaps.lua
+++ .config/nvim/lua/plugins/completions/coc/keymaps.lua
@@ -126,7 +126,7 @@ local function suck_a_dick_formatter()
 end
 
 local function format_normal()
-    if vim.bo.filetype == "gitconfig" or vim.bo.filetype == "make" then
+    if vim.tbl_contains({ "gitconfig", "make" }, vim.bo.filetype) then
         -- suck_a_dick_formatter()
         vim.cmd("normal! gg=G")
     else
