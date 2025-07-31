9fb5c802bc2b736d3d5ed3e24e519c8be69a0d38
Wes Higbee <wes.mcclure@gmail.com>
2025-07-30 23:54:24 -0500

    ok

--- bash/course.bashrc.sh
+++ bash/course.bashrc.sh
@@ -13,7 +13,7 @@ is_linux() { [[ "$UNAME_S" = "Linux" ]]; }
 BASH_DOTFILES="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source "$BASH_DOTFILES/startup/vetted-benign/early/funcs.bash"
 source "$BASH_DOTFILES/startup/vetted-benign/early/path-init.bash"
-# export WES_DOTFILES="$(realpath "$BASH_DOTFILES"/..)"
+export WES_DOTFILES="$(realpath "$BASH_DOTFILES"/..)"
 #
 # if is_macos; then
 #
