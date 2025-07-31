ccc3f745b61d19b964ca1443c47e8e5b13f646f2
Wes Higbee <wes.mcclure@gmail.com>
2025-07-31 00:25:16 -0500

    copy over full prompt for a second

--- bash/startup/course-only/prompt.bash
+++ bash/startup/course-only/prompt.bash
@@ -12,4 +12,27 @@ PROMPT_ITALIC="\e[3m"
 
 PS1="${PROMPT_CYAN}${PROMPT_BOLD}${PROMPT_UNDERLINE}\W${PROMPT_RESET} \$ "
 
+__last_histcmd=0
+__last_rc=0
+show_rc_when_last_cmd_failed() {
+    local last_rc=$?
+    local curr_histcmd=$HISTCMD
+
+    # edge triggered on change in HISTCMD
+    # HISTCMD only increments when a command is exectued
+    # ENTER on empty cmdline will not increment it b/c that's not a command
+    # thus we only show if HISTCMD changes indicating previous command just failed and this is prompt right after
+    if ((curr_histcmd != __last_histcmd)); then
+        __last_histcmd=$curr_histcmd
+
+        if ((last_rc != 0)); then
+            # prints message ABOVE next prompt, by echo'ing in a PROMPT_COMMAND function
+            echo -e "\033[31mCommand failed with exit code $last_rc\033[0m"
+        fi
+    fi
+}
+
+PROMPT_COMMAND="show_rc_when_last_cmd_failed;prompt_current_dir"
+PS1="${PROMPT_CYAN}${PROMPT_BOLD}${PROMPT_UNDERLINE}\${current_dir}${PROMPT_RESET} \$ "
+
 export PS1
