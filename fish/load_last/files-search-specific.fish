8cbf6b9394701758f112ad135da6524fc3c372d8
Wes Higbee <wes.mcclure@gmail.com>
2025-08-14 05:25:49 -0500

    notes

--- fish/load_last/files-search-specific.fish
+++ fish/load_last/files-search-specific.fish
@@ -408,6 +408,7 @@ function agimages
     set secondary_path_filter $argv[2]
 
     # combine regex + ext regex into one filter, works great
+    # FYI uses smart casing by default so don't add -i here
     fd --unrestricted --regex "$secondary_path_filter"".*\.(png|jpg|jpeg|gif|bmp|tiff|webp|svg|icns|ico)" \
         --exec bash -c 'echo {}; imgcat "{}"' \
         -- $look_in_dir
