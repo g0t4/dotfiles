59a96feae1bcde5be570cd6349f511d6e021e156
Wes Higbee <wes.mcclure@gmail.com>
2025-08-17 10:24:14 -0500

    use new messages open_on_startup config

--- .config/nvim/lua/plugins/g0t4.lua
+++ .config/nvim/lua/plugins/g0t4.lua
@@ -11,17 +11,11 @@ return {
         -- enabled  = false, -- comment out to enable
         "g0t4/devtools.nvim",
         dir = "~/repos/github/g0t4/devtools.nvim",
-        -- opts = {}
-        config = function()
-            require("devtools").setup {}
-            local messages = require "devtools.messages"
-
-            -- open messages automatically for testing
-            vim.schedule(function()
-                messages.ensure_open()
-            end)
-        end
-
+        opts = {
+            messages = {
+                open_on_startup = true,
+            }
+        }
     },
 
 }
