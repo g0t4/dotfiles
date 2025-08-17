b501c262bad6cb5bfa3fceea11a938ad777afe70
Wes Higbee <wes.mcclure@gmail.com>
2025-08-17 10:18:38 -0500

    setup auto open messages panel for easier testing, can comment out when don't need it ...  and then I should make this an official config param to the plugin so I can toggle it in opts

--- .config/nvim/lua/plugins/g0t4.lua
+++ .config/nvim/lua/plugins/g0t4.lua
@@ -11,7 +11,17 @@ return {
         -- enabled  = false, -- comment out to enable
         "g0t4/devtools.nvim",
         dir = "~/repos/github/g0t4/devtools.nvim",
-        opts = {}
+        -- opts = {}
+        config = function()
+            require("devtools").setup {}
+            local messages = require "devtools.messages"
+
+            -- open messages automatically for testing
+            vim.schedule(function()
+                messages.ensure_open()
+            end)
+        end
+
     },
 
 }
