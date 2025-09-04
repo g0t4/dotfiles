742d8b6da355a9578da8625aebd0931e95f4393c
Wes Higbee <wes.mcclure@gmail.com>
2025-09-04 13:19:18 -0500

    ensure delay is within a reasonable range

--- .config/hammerspoon/config/macros/screenpal/co_tests.lua
+++ .config/hammerspoon/config/macros/screenpal/co_tests.lua
@@ -2,9 +2,16 @@ require("config.macros.screenpal.co")
 
 describe("cos", function()
     it("test w/o run_async because test already has a coroutine", function()
-        print("before sleep")
+        local _start = vim.uv.hrtime() / 1e6
         sleep_ms(250)
-        print("after sleep")
+        local _end = vim.uv.hrtime() / 1e6
+        local actual_ms = _end - _start
+        if actual_ms < 200 then
+            error("sleep was not long enough: " .. actual_ms .. " ms")
+        end
+        if actual_ms > 300 then
+            error("sleep was too long: " .. actual_ms .. " ms")
+        end
     end)
 
     it("test run_async works too, bypasses creating coroutine", function()
