16a32e292eb1f3984008b2499110631b62e42d1e
Wes Higbee <wes.mcclure@gmail.com>
2025-09-04 13:32:38 -0500

    qwen3coder30b AskRewrite exrtract a lua class called TestTimer that has start/stop methods on it, and then a method to throw if time not acceptable, pass time in :new too and use +=4% to throw

--- .config/hammerspoon/config/macros/screenpal/co_tests.lua
+++ .config/hammerspoon/config/macros/screenpal/co_tests.lua
@@ -4,18 +4,47 @@ function get_ms()
     return vim.uv.hrtime() / 1e6
 end
 
+---@class TestTimer
+---@field start_time number
+---@field allowed_time number
+local TestTimer = {}
+
+---@param allowed_time number
+---@return TestTimer
+function TestTimer:new(allowed_time)
+    local obj = {
+        start_time = get_ms(),
+        allowed_time = allowed_time
+    }
+    setmetatable(obj, { __index = self })
+    return obj
+end
+
+---@param msg string
+function TestTimer:throw_if_time_not_acceptable(msg)
+    local elapsed = get_ms() - self.start_time
+    local tolerance = self.allowed_time * 0.04
+    local min_time = self.allowed_time - tolerance
+    local max_time = self.allowed_time + tolerance
+
+    if elapsed < min_time or elapsed > max_time then
+        error(msg .. " - elapsed: " .. elapsed .. "ms, expected: " .. self.allowed_time .. "ms ±" .. tolerance .. "ms")
+    end
+end
+
+function TestTimer:start()
+    self.start_time = get_ms()
+end
+
+function TestTimer:stop()
+    self:throw_if_time_not_acceptable("Timer exceeded acceptable time range")
+end
+
 describe("cos", function()
     it("test w/o run_async because test already has a coroutine", function()
-        local _start = get_ms()
+        local timer = TestTimer:new(250)
         sleep_ms(250)
-        local _end = get_ms()
-        local actual_ms = _end - _start
-        if actual_ms < 240 then
-            error("sleep was not long enough: " .. actual_ms .. " ms")
-        end
-        if actual_ms > 260 then
-            error("sleep was too long: " .. actual_ms .. " ms")
-        end
+        timer:stop()
     end)
 
     it("test run_async works too, bypasses creating coroutine", function()
