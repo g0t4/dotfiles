d6cd2ebf2e48b5d78ed5a18c417716b70ec7e3e3
Wes Higbee <wes.mcclure@gmail.com>
2025-09-04 03:49:36 -0500

    only modify some paths if running tests inside of vim (plenary runner)

--- .config/hammerspoon/config/tests/setup.lua
+++ .config/hammerspoon/config/tests/setup.lua
@@ -15,6 +15,13 @@ local function add_plugin_to_package_path(plugin_path)
 end
 
 local function add_rxlua_to_package_path()
+    if vim == nil then
+        -- for lua/busted/hs rx comes from rxlua pkg in luarocks and it all works OOB
+        return
+    end
+
+    -- only add if using vim for running tests (plenary)
+    -- THIS IS NOT A PRIMARY use case it's just a nice to have
     add_plugin_to_package_path(vim.fn.stdpath("data") .. "/lazy/RxLua/")
     -- other possibilities:
     --   -- vim.opt.runtimepath:append("~/.local/share/nvim/lazy/rxlua")
@@ -23,6 +30,16 @@ end
 local function add_devtools_to_package_path()
     add_plugin_to_package_path(os.getenv("HOME") .. "/repos/github/g0t4/devtools.nvim/lua/")
 end
+
+-- FYI two separate runners:
+-- - busted (via luarocks) => already has luarocks setup and working
+--   lua 5.4 via hs
+-- - nvim plenary runner => does not have things like rxlua (or other luarocks pkgs avail OOB)
+--   I supposed I could modify it to do so in my nvim config
+--   but then nvim is also lua 5.1
+-- - hammerspoon itself
+--   yet another runtime (for prod code in my case)
+-- * KEEP IN MIND that each one may need different mods to package path
 
 -- immediately modify package path on import (require)
 add_devtools_to_package_path()
