09cf196675f4a16c436d08aced057a00993b61a1
Wes Higbee <wes.mcclure@gmail.com>
2025-09-04 03:37:58 -0500

    copy over a test setup module from ask-openai... use as basis to make the same here for busted tests (or plenary runner even w/ hs config)

---
+++ .config/hammerspoon/config/tests/setup.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+function M.dump_package_paths()
+    local paths = {}
+    for path in string.gmatch(package.path, "[^;]+") do
+        print(path)
+        table.insert(paths, path)
+    end
+    print()
+end
+
+local function add_plugin_to_package_path(plugin_path)
+    package.path = package.path .. ";" .. plugin_path .. "?.lua"
+    package.path = package.path .. ";" .. plugin_path .. "?/init.lua"
+end
+
+local function add_rxlua_to_package_path()
+    add_plugin_to_package_path(vim.fn.stdpath("data") .. "/lazy/RxLua/")
+    -- other possibilities:
+    --   -- vim.opt.runtimepath:append("~/.local/share/nvim/lazy/rxlua")
+end
+
+local function add_devtools_to_package_path()
+    add_plugin_to_package_path(os.getenv("HOME") .. "/repos/github/g0t4/devtools.nvim/lua/")
+end
+
+function M.modify_package_path()
+    add_devtools_to_package_path()
+    add_rxlua_to_package_path()
+end
+
+return M
