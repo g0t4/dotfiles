5e14c6afb68e3849d53ae6f7c3c98c01bb5c8b30
Wes Higbee <wes.mcclure@gmail.com>
2025-09-04 13:38:17 -0500

    move

--- .config/hammerspoon/config/macros/screenpal/co_tests.lua
+++ .config/hammerspoon/config/macros/screenpal/co_tests.lua
@@ -1,44 +1,5 @@
 require("config.macros.screenpal.co")
-
-local function get_ms()
-    return vim.uv.hrtime() / 1e6
-end
-
----@class TestTimer
----@field start_time number
----@field allowed_time number
-local TestTimer = {}
-
----@param allowed_time number
----@return TestTimer
-function TestTimer:new(allowed_time)
-    local obj = {
-        start_time = get_ms(),
-        allowed_time = allowed_time
-    }
-    setmetatable(obj, { __index = self })
-    return obj
-end
-
----@param msg string
-function TestTimer:throw_if_time_not_acceptable(msg)
-    local elapsed = get_ms() - self.start_time
-    local tolerance = self.allowed_time * 0.04
-    local min_time = self.allowed_time - tolerance
-    local max_time = self.allowed_time + tolerance
-
-    if elapsed < min_time or elapsed > max_time then
-        error(msg .. " - actual: " .. string.format("%.1f", elapsed) .. "ms, expected: " .. self.allowed_time .. "ms ±" .. tolerance .. "ms")
-    end
-end
-
-function TestTimer:start()
-    self.start_time = get_ms()
-end
-
-function TestTimer:stop()
-    self:throw_if_time_not_acceptable("Test timing is outside acceptable time range")
-end
+local TestTimer = require("config.macros.screenpal.test_timing")
 
 describe("cos", function()
     it("test w/o run_async because test already has a coroutine", function()
