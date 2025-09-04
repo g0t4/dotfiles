43cb79d32297e0a82904ce56dd5051707fa53705
Wes Higbee <wes.mcclure@gmail.com>
2025-09-04 13:28:08 -0500

    extract function

--- .config/hammerspoon/config/macros/screenpal/co_tests.lua
+++ .config/hammerspoon/config/macros/screenpal/co_tests.lua
@@ -1,10 +1,14 @@
 require("config.macros.screenpal.co")
 
+function get_ms()
+    return vim.uv.hrtime() / 1e6
+end
+
 describe("cos", function()
     it("test w/o run_async because test already has a coroutine", function()
-        local _start = vim.uv.hrtime() / 1e6
+        local _start = get_ms()
         sleep_ms(250)
-        local _end = vim.uv.hrtime() / 1e6
+        local _end = get_ms()
         local actual_ms = _end - _start
         if actual_ms < 200 then
             error("sleep was not long enough: " .. actual_ms .. " ms")
