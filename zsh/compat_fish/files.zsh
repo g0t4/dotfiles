6f6463e53d63bbed5e0fdd136b159c4e4453880b
Wes Higbee <wes.mcclure@gmail.com>
2025-09-04 22:34:53 -0500

    add -O for stdout extract with tar

--- zsh/compat_fish/files.zsh
+++ zsh/compat_fish/files.zsh
@@ -29,12 +29,13 @@ abbr zx 'z -x'
 
 # tar:
 abbr tarx 'tar -xf' # e(x)tract
+abbr tarx_stdout 'tar -O -xf' # e(x)tract to std(O)ut
 abbr tart 'tar -tf' # lis(t) / (t)est
 abbr tarc 'tar --xz -cf' # create xz (todo use set-position to put cursor in name that already has .txz extension)
 abbr tarcg 'tar --gzip -cf' # create gzip (todo use set-position to put cursor in name that already has .tgz extension)
 abbr tarcb 'tar --bzip2 -cf' # create bzip2 (todo use set-position to put cursor in name that already has .tbz2 extension)
 
-# *** java
+# *** java (zip)
 # jar:
 abbr jarx 'jar -xf' # e(x)tract
 abbr jart 'jar -tf' # lis(t) / (t)est
