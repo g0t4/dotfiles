a9436e42176a0ccce98d68489c5d80de6d179ca9
Wes Higbee <wes.mcclure@gmail.com>
2025-09-09 05:45:30 -0500

    notes

--- .config/hammerspoon/config/macros/screenpal/py/short_silences.py
+++ .config/hammerspoon/config/macros/screenpal/py/short_silences.py
@@ -60,6 +60,9 @@ def get_short_silences():
     runs = []
     if playhead_leftmost_index is not None:
         runs = scan_for_all_short_silences(hunt_mask_CLOSED)
+        # TODO! this is actually fine b/c I can scroll the playhead off screen
+        #   and it doesn't matter now that we use the timeline bg to find ranges and the playhead
+        #   is just to smooth over the one range it overlaps with (if it does)
 
     if DEBUG:
         built = build_range_mask(runs, image)
