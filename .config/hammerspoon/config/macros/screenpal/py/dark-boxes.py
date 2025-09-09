fix off by one on end of mask only, note about revisiting end inclusivity

.config/hammerspoon/config/macros/screenpal/py/dark-boxes.py
@@ -81,20 +81,25 @@ if DEBUG:
 
     stacked = np.vstack([
         # both full size:
-        image,
-        final_mask,
-        built,
+        # image,
+        # final_mask,
+        # built,
 
         # zoom in on run (where playhead is) => can zoom in then and compare edges to see if it lines up!
-        # image[:, 1300:1500],
-        # final_mask[:, 1300:1500],
+        image[:, 1300:1500],
+        final_mask[:, 1300:1500],
+        built[:, 1300:1500],
     ])
     show_and_wait(stacked)
+    assert (np.array_equal(final_mask[:, 1300:1500], built[:, 1300:1500]))
 
 # * serialize response to json in STDOUT
 results = {
     "short_silences": [
         {
+            # FYI for now off by one won't matter much but I should resolve this
+            # TODO! MAKE SURE you are using end inclusivity correctly
+            # TODO!  IOTW figure out which you are using and rename your DTO here
             # divide by 2 for non-retina resolution
             "x_start": int(x_start / 2),
             "x_end": int(x_end / 2),

.config/hammerspoon/config/macros/screenpal/py/visualize.py
@@ -53,5 +53,6 @@ def build_range_mask(x_ranges, image):
     """
     range_mask = np.zeros_like(image)
     for start, end in x_ranges:
-        range_mask[:, start:end] = 255
+        # TODO do you want the end to be inclusive or exclusive?
+        range_mask[:, start:end + 1] = RED
     return range_mask