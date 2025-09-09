41de59995f3a8f2a3ccbc77b4e5de9cf47a8d68c
Wes Higbee <wes.mcclure@gmail.com>
2025-09-08 21:00:35 -0500

    add lua wrapper to call dark-boxes.py

--- .config/hammerspoon/config/macros/screenpal/py/boxes.lua
+++ .config/hammerspoon/config/macros/screenpal/py/boxes.lua
@@ -1,8 +1,9 @@
 ---@param imagePath string
 ---@param callback fun(data: SilencesList)
-function detect_silence_boxes(imagePath, callback)
+function detect_silence_boxes(imagePath, callback, script)
+    filename = filename or "boxes.py"
     local python_exe = os.getenv("HOME") .. "/repos/github/g0t4/dotfiles/.venv/bin/python3"
-    local script = os.getenv("HOME") .. "/repos/github/g0t4/dotfiles/.config/hammerspoon/config/macros/screenpal/py/boxes.py"
+    local script = os.getenv("HOME") .. "/repos/github/g0t4/dotfiles/.config/hammerspoon/config/macros/screenpal/py/" .. filename
     local args = { script, imagePath }
 
     local task = hs.task.new(python_exe, function(exitCode, stdout, stderr)
@@ -19,4 +20,8 @@ function detect_silence_boxes(imagePath, callback)
     end, args)
 
     task:start()
+end
+
+function detect_short_silences(imagePath, callback, script)
+    detect_silence_boxes(imagePath, callback, "dark-boxes.py")
 end
