fed7256c4f2003e099dab3ebf9b8ff6cf52ac26f
Wes Higbee <wes.mcclure@gmail.com>
2025-09-09 05:14:09 -0500

    indent

--- .config/hammerspoon/config/macros/screenpal/py/boxes.py
+++ .config/hammerspoon/config/macros/screenpal/py/boxes.py
@@ -89,35 +89,35 @@ def get_silences():
     }
     print(json.dumps(results))  # output to STDOUT for hs to consume
 
-if DEBUG:
-    # make a divider like the background color #2C313C
-    divider = np.zeros_like(image)
-    divider = divider[:image.shape[0] // 2, :image.shape[1]]  # first half of image
-    divider[:] = [60, 49, 44]  # BGR for #2C313C
+    if DEBUG:
+        # make a divider like the background color #2C313C
+        divider = np.zeros_like(image)
+        divider = divider[:image.shape[0] // 2, :image.shape[1]]  # first half of image
+        divider[:] = [60, 49, 44]  # BGR for #2C313C
 
-    labeled_mask = display_colorful_labeled_regions(labels)
+        labeled_mask = display_colorful_labeled_regions(labels)
 
-    show_and_wait(
-        # image, # include image but not really necessary
-        display_mask_over_image(image, gray_box_mask),
-        display_mask_only(image, gray_box_mask),
-        display_mask_over_image(image, timeline_mask),
-        display_mask_only(image, timeline_mask),
-        display_mask_over_image(image, playhead_mask, alpha=0.5, highlight_color=YELLOW),
-        display_mask_only(image, playhead_mask, highlight_color=YELLOW),
-        divider,
-        display_mask_only(image, gray_box_mask),
-        display_mask_only(image, gray_box_mask_smooth),
-        divider,
-        labeled_mask,
-    )
+        show_and_wait(
+            # image, # include image but not really necessary
+            display_mask_over_image(image, gray_box_mask),
+            display_mask_only(image, gray_box_mask),
+            display_mask_over_image(image, timeline_mask),
+            display_mask_only(image, timeline_mask),
+            display_mask_over_image(image, playhead_mask, alpha=0.5, highlight_color=YELLOW),
+            display_mask_only(image, playhead_mask, highlight_color=YELLOW),
+            divider,
+            display_mask_only(image, gray_box_mask),
+            display_mask_only(image, gray_box_mask_smooth),
+            divider,
+            labeled_mask,
+        )
 
-    if file == "samples/timeline03a.png":
-        # time python3 boxes.py samples/timeline03a.png --debug
-        # PRN use unit test assertions so we can see what differs
-        expected = {"silences": [{"x_start": 754, "x_end": 891}, {"x_start": 1450, "x_end": 1653}]}
-        assert results["silences"] == expected["silences"]
-        print("\n[bold underline green]MATCHED REGULAR SILENCE TEST CASE!")
+        if file == "samples/timeline03a.png":
+            # time python3 boxes.py samples/timeline03a.png --debug
+            # PRN use unit test assertions so we can see what differs
+            expected = {"silences": [{"x_start": 754, "x_end": 891}, {"x_start": 1450, "x_end": 1653}]}
+            assert results["silences"] == expected["silences"]
+            print("\n[bold underline green]MATCHED REGULAR SILENCE TEST CASE!")
 
-        # * final preview mask
-        show_and_wait(image, build_range_mask(merged_x_ranges, image))
+            # * final preview mask
+            show_and_wait(image, build_range_mask(merged_x_ranges, image))
