69898d580fb227701a6657242e9f8c5bfddb5d35
Wes Higbee <wes.mcclure@gmail.com>
2025-09-11 21:55:52 -0500

    remove volume add tool detection from short_silences, setup a new tool detection module

--- .config/hammerspoon/config/macros/screenpal/py/short_silences.py
+++ .config/hammerspoon/config/macros/screenpal/py/short_silences.py
@@ -10,15 +10,12 @@ from visualize import *
 
 DEBUG = __name__ == "__main__"
 
-def get_short_silences():
+def detect_short_silences():
 
     hunt_mask = cv.bitwise_or(timeline_mask, playhead_mask)
     hunt_mask_CLOSED = cv.morphologyEx(hunt_mask, cv.MORPH_CLOSE, np.ones((3, 3), np.uint8))
 
-    look_start = 0
-    look_end = -1
-
-    def detect_short_silences(mask: np.ndarray):
+    def find_ranges(mask: np.ndarray):
         mask_roi = mask[48:, :]  # take 48+ (bottom 50% of mask) for Insert New to match too
         # verify assumption (just to be safe)
         # FYI ends have curved edges, wait until this is an issue... could make mask around curved corners and then pad with neighboring pixels or smth else and add if they are empty nearby or not
@@ -46,52 +43,18 @@ def get_short_silences():
         ranges = [(edges[i], edges[i + 1] - 1) for i in range(0, len(edges), 2)]
         return ranges
 
-    ranges = detect_short_silences(hunt_mask_CLOSED)
-
-    pink = np.array([198, 74, 218])  # BGR pink top shiny part (row 8, index 7th)
-    pink_mask = color_mask(image, pink, 4)
-
-    def detect_volume_add_tool(image):
-        # color in row 8 works, however playhead interferes if overlapping:
-        #  1. union playhead mask (but would find bogus range of just playhead, would need to remove)
-        #     OR allow if playhead overlaps and is the only missing pixels in range
-        #  2. take min/max column == range
-        #     this worked in initial test case, lets see how well it does in reality
-
-        # row is 7th in 0-based index, then minus 2 for border pixels (removed in load_image)
-        row8 = pink_mask[5]
-
-        # FYI if needed, scan_mask(pink_mask)
-
-        non_zero_indices = np.nonzero(row8)[0]
-        min_index = None
-        max_index = None
-        if len(non_zero_indices) > 0:
-            min_index = np.min(non_zero_indices)
-            max_index = np.max(non_zero_indices)
-
-        return min_index, max_index
-
-    min_index, max_index = detect_volume_add_tool(image)
+    ranges = find_ranges(hunt_mask_CLOSED)
 
     if DEBUG:
-        built = build_range_mask(ranges, image)
+        range_preview_mask = build_range_mask(ranges, image)
 
         full = [
-            display_mask_only(image, pink_mask, pink),
             display_mask_only(image, timeline_mask),
             image,
-            built,
+            range_preview_mask,
         ]
 
-        # around_playhead = [
-        #     # can zoom in and compare edges
-        #     image[:, 1300:1500],
-        #     built[:, 1300:1500],
-        # ]
-
-        stacked = np.vstack(full)
-        show_and_wait(stacked)
+        show_and_wait(np.vstack(full))
 
     # * serialize response to json in STDOUT
     results = {
@@ -103,21 +66,13 @@ def get_short_silences():
             } for x_start, x_end in ranges \
                if x_start < x_end
         ],
-        "tool": {}
     }
-
-    if min_index is not None and max_index is not None:
-        # TODO test if volume edit tool range works on pink too
-        # TODO parameterize the search for other colors that I bet use row 8 too!
-        results["tool"] = {
-            "type": "volume_add_tool",
-            "x_start": int(min_index / 2),
-            "x_end": int(max_index / 2),
-        }
 
     # PRN hardcode results for test case
     if DEBUG:
+
         print(json.dumps(results))
+
         if file == "samples/playhead-darkblue1.png":
             # PRN use unit test assertions so we can see what differs
             expected = {"short_silences": [{"x_start": 4, "x_end": 5}, {"x_start": 31, "x_end": 32}, {"x_start": 217, "x_end": 218}, {"x_start": 319, "x_end": 320}, {"x_start": 376, "x_end": 378}, {"x_start": 403,
@@ -126,14 +81,9 @@ def get_short_silences():
             assert results["short_silences"] == expected["short_silences"]
             print("\n[bold underline green]MATCHED SHORT SILENCE TEST CASE!")
 
-        if file == "samples/pink-volume-add-open.png":
-            expected_tool = {"type": "volume_add_tool", "x_start": 289, "x_end": 403}
-            assert results["tool"] == expected_tool
-            print("\n[bold underline green]MATCHED TOOL TEST CASE!")
-
     return results
 
 if DEBUG:
     # time python3 short_silences.py samples/playhead-darkblue1.png --debug
     from rich import print
-    get_short_silences()
+    detect_short_silences()
---
+++ .config/hammerspoon/config/macros/screenpal/py/tools.py
@@ -0,0 +1,79 @@
+import json
+import os
+import sys
+import cv2 as cv
+import numpy as np
+from pathlib import Path
+from functools import reduce
+from shared import *
+from visualize import *
+
+DEBUG = __name__ == "__main__"
+
+def detect_tools():
+
+    pink = np.array([198, 74, 218])  # BGR pink top shiny part (row 8, index 7th)
+    pink_mask = color_mask(image, pink, 4)
+
+    def detect_volume_add_tool(image):
+        # color in row 8 works, however playhead interferes if overlapping:
+        #  1. union playhead mask (but would find bogus range of just playhead, would need to remove)
+        #     OR allow if playhead overlaps and is the only missing pixels in range
+        #  2. take min/max column == range
+        #     this worked in initial test case, lets see how well it does in reality
+
+        # row is 7th in 0-based index, then minus 2 for border pixels (removed in load_image)
+        row8 = pink_mask[5]
+
+        # FYI if needed, scan_mask(pink_mask)
+
+        non_zero_indices = np.nonzero(row8)[0]
+        min_index = None
+        max_index = None
+        if len(non_zero_indices) > 0:
+            min_index = np.min(non_zero_indices)
+            max_index = np.max(non_zero_indices)
+
+        return min_index, max_index
+
+    min_index, max_index = detect_volume_add_tool(image)
+
+    if DEBUG:
+
+        full = [
+            display_mask_only(image, pink_mask, pink),
+            display_mask_only(image, timeline_mask),
+            image,
+        ]
+
+        stacked = np.vstack(full)
+        show_and_wait(stacked)
+
+    # * serialize response to json in STDOUT
+    results = {"tool": {}}
+
+    if min_index is not None and max_index is not None:
+        # TODO test if volume edit tool range works on pink too
+        # TODO parameterize the search for other colors that I bet use row 8 too!
+        results["tool"] = {
+            "type": "volume_add_tool",
+            "x_start": int(min_index / 2),
+            "x_end": int(max_index / 2),
+        }
+
+    # PRN hardcode results for test case
+    if DEBUG:
+
+        print(json.dumps(results))
+
+        if file == "samples/pink-volume-add-open.png":
+            expected_tool = {"type": "volume_add_tool", "x_start": 289, "x_end": 403}
+            assert results["tool"] == expected_tool
+            print("\n[bold underline green]MATCHED TOOL TEST CASE!")
+
+    return results
+
+if DEBUG:
+    # time python3 short_silences.py samples/playhead-darkblue1.png --debug
+    from rich import print
+    detect_tools()
