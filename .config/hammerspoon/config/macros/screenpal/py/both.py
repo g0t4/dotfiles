5b6d41fcb867e62ac6e862ba5aec38fa53120d77
Wes Higbee <wes.mcclure@gmail.com>
2025-09-11 22:07:22 -0500

    renames

--- .config/hammerspoon/config/macros/screenpal/py/both.py
+++ .config/hammerspoon/config/macros/screenpal/py/both.py
@@ -1,6 +1,6 @@
 import json
-from regular_silences import get_regular_silences
-from short_silences import get_short_silences
+from regular_silences import detect_regular_silences
+from short_silences import detect_short_silences
 
 if __name__ == "__main__":
 
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     # FYI just as fast to do both at the same time
     #  IIGC imports eat up most of the time
 
-    combined = get_short_silences()
-    regular = get_regular_silences()
+    combined = detect_short_silences()
+    regular = detect_regular_silences()
 
     # merge into short object since it has multiple keys currently
     combined["regular_silences"] = regular["silences"]
