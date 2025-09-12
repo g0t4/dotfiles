47049229ccb3453afa1b5bbfc035430924a5f438
Wes Higbee <wes.mcclure@gmail.com>
2025-09-11 23:39:35 -0500

    migrate test for tool detection into a test file!

---
+++ .config/hammerspoon/config/macros/screenpal/py/tools_tests.py
@@ -0,0 +1,14 @@
+from tools import detect_tools
+
+def test_detect_tools_with_pink_volume_add_open():
+    detected = detect_tools('samples/pink-volume-add-open.png')
+
+    expected_tool = {"type": "volume_add_tool", "x_start": 289, "x_end": 403}
+
+    assert detected["tool"] == expected_tool
+
+# def test_with_cut_tool_cursor_at_end():
+#     detected = detect_tools('samples/cut-tool/add-end-selected.png')
+
+# def test_with_cut_tool_cursor_at_start():
+#     detected = detect_tools('samples/cut-tool/add-start-selected.png')
