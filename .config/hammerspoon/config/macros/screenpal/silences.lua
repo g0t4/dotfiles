208900aee8694e03c5dd2567b2225e758b7b71a4
Wes Higbee <wes.mcclure@gmail.com>
2025-09-14 06:40:29 -0500

    qwen3coder30b AskRewrite do this for x_end too

--- .config/hammerspoon/config/macros/screenpal/silences.lua
+++ .config/hammerspoon/config/macros/screenpal/silences.lua
@@ -20,6 +20,13 @@ function Silence:x_start_pad_percent(ratio)
     return self.x_start + padding
 end
 
+function Silence:x_end_pad_percent(ratio)
+    -- compute the padding end if silence is shrunk to ratio/1.0
+    -- i.e. padding for 90% of silence would be 10%/2 => 5% of width on each side
+    local padding = self:x_width() * ratio / 2
+    return self.x_end - padding
+end
+
 ---@alias DetectionResults { short_silences: Silence[], regular_silences: Silence[], tool: { type: string, x_start: number, x_end: number}}
 
 ---@class SilencesController
