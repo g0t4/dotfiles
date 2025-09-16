40455079d9df4c0e7adcaa0d1b55b516b775cea9
Wes Higbee <wes.mcclure@gmail.com>
2025-09-16 05:19:00 -0500

    stop testing for now

--- .config/hammerspoon/config/macros/screenpal/windows/volume_menu.lua
+++ .config/hammerspoon/config/macros/screenpal/windows/volume_menu.lua
@@ -21,6 +21,10 @@ function VolumeMenu:find_my_window()
     end
 
     function lookup()
+        -- print("lookup")
+        -- vim.iter(self._windows.windows_by_title):each(function(k, v)
+        --     print("  Window title: " .. k .. " = " .. tostring(v:axTitle()))
+        -- end)
         return self._windows.windows_by_title["SOM-FloatingWindow-Type=edit2.overlayeditfloat-ZOrder=1(Undefined+1)"]
     end
 
@@ -30,6 +34,7 @@ function VolumeMenu:find_my_window()
         win = lookup()
     end
     self._win = win
+    print("Found2: ", win)
     return win
 end
 
@@ -43,12 +48,6 @@ end
 -- Volume<string> -- *** by the way NEW LINE at start, hence wrapped AXValue here
 -- unique ref: app:window('SOM-FloatingWindow-Type=edit2.overlayeditfloat-ZOrder=1(Undefined+1)'):textField()
 
--- FYI button for Narration
---   FYI I swear this had a radio button one time I opened it... heads up if that irregularity arisesj
--- AXDescription: Narration<string>
--- AXEnabled: true<bool>
--- unique ref: app:window('SOM-FloatingWindow-Type=edit2.overlayeditfloat-ZOrder=1(Undefined+1)'):button(1)
-
 ---@param description string
 ---@return hs.axuielement | nil
 function VolumeMenu:get_button_by_description(description)
@@ -56,11 +55,63 @@ function VolumeMenu:get_button_by_description(description)
     local win = self:find_my_window()
     if not win then return end
 
+    -- FYI I have captures with the button on window and also nested... can I verify what it actually is?
+    --   maybe it's never direclty on the window?
+    --    FYI my elem inspector cannot nav the children (down nor up) for this radio button... PRN research that
+    --    or the AX APIs might
+    --     app:window(1):button(1)
+    --     window(2):tabGroup(1):radioButton(1):button(1)
+    --      but then it seems like it is actually here:
+    --        tabGroup(1):button(1) in my testing...
+    --
+    -- elem captures Narration button:
+    -- AXDescription: Narration<string>
+    -- AXEnabled: true<bool>
+    -- unique ref: app:window('SOM-FloatingWindow-Type=edit2.overlayeditfloat-ZOrder=1(Undefined+1)'):button(1)
+    --
+    -- VERSUS:
+    --
+    -- FML sometimes it's nested in tabGroup... (but then seems to be under tabGroup and not radioButton!)
+    -- app:window(2):tabGroup(1):radioButton(1):button(1)
+    -- AXDescription: Narration<string>
+    -- unique ref: app:window('SOM-FloatingWindow-Type=edit2.overlayeditfloat-ZOrder=1(Undefined+1)'):tabGroup()
+    --   :radioButton(''):button()
+
+    -- print("WIN " .. tostring(win:axTitle()))
+    -- vim.iter(win:children()):each(function(e)
+    --     print("  " .. e:axRole() .. ": " .. e:axDescription())
+    -- end)
+
     -- takes <3ms to find the button, that's fine for now, let's not cache controls
     local button = win:button_by_description(description)
     if button and not button:isValid() then
         print("WARNING: Button '" .. description .. "' is not valid, unexpectedly... " .. hs.inspect(button))
     end
+
+    if not button then
+        local tabGroup = win:tabGroup(1)
+        if not tabGroup then
+            print('F3: no tab group') -- debug, remove later
+            return
+        end
+        vim.iter(tabGroup:children()):each(function(e)
+            -- leave this debug code until stuff is rock solid
+            print("  tabGroup child: " .. e:axRole() .. ": " .. e:axDescription())
+        end)
+        --   so, it's tabGroup(1):button(1) in my testing...
+        -- FYI! this is working to select MUTE with the rest of the waits! Aside from this hellish button crap...
+        --    the rest of the code just worked the first time!
+        local button = tabGroup:button(1)
+
+        -- * vet description matches
+        local desc = button:axDescription()
+        if desc ~= description then
+            print("WARNING: tabGroup nested button description does not match! " .. desc .. " vs " .. description)
+            return nil
+        end
+        return button
+    end
+
     return button
 end
 
