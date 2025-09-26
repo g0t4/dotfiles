afe7cff7c2ad60f536e34371cf920420cf27156a
Wes Higbee <wes.mcclure@gmail.com>
2025-09-25 23:24:34 -0500

    move

--- .config/nvim/lua/plugins/lualine/lualine.lua
+++ .config/nvim/lua/plugins/lualine/lualine.lua
@@ -1,4 +1,39 @@
 local cached = {}
+
+
+local function statusline_filetype()
+    --  lua
+    --  why show the icon too? given the icon alone isn't as telling as the filetype, then just show the darn filetype
+    --  AND why show filetype if the filename has the same extension as it!?
+
+    local file_ext = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':e')
+
+    if file_ext == vim.bo.filetype then
+        return ""
+    end
+    return vim.bo.filetype
+end
+
+local function statusline_nvim_tree()
+    return " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") -- only show last part of path (folder name)
+end
+
+local function workspace_name()
+    -- PRN compute on startup so this func isn't even called?
+    if cached.workspace_name then
+        return cached.workspace_name
+    end
+
+    -- TODO if in a git repo, show org/repo
+    local name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t") -- ~ 20us
+    if name == "course-bash" then
+        name = "course"
+    end
+    cached.workspace_name = name
+    return name
+end
+
+
 return {
     {
         "nvim-lualine/lualine.nvim",
@@ -7,38 +42,6 @@ return {
         -- TODO consider "kyazdani42/nvim-web-devicons" (lua rewrite) if some reaosn to do so, i.e. perf? or other forks?
         config = function()
             -- credit / from https://neovimcraft.com/plugin/SmiteshP/nvim-navic/
-
-            local function statusline_filetype()
-                --  lua
-                --  why show the icon too? given the icon alone isn't as telling as the filetype, then just show the darn filetype
-                --  AND why show filetype if the filename has the same extension as it!?
-
-                local file_ext = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':e')
-
-                if file_ext == vim.bo.filetype then
-                    return ""
-                end
-                return vim.bo.filetype
-            end
-
-            local function statusline_nvim_tree()
-                return " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") -- only show last part of path (folder name)
-            end
-
-            local function workspace_name()
-                -- PRN compute on startup so this func isn't even called?
-                if cached.workspace_name then
-                    return cached.workspace_name
-                end
-
-                -- TODO if in a git repo, show org/repo
-                local name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t") -- ~ 20us
-                if name == "course-bash" then
-                    name = "course"
-                end
-                cached.workspace_name = name
-                return name
-            end
 
             require("lualine").setup {
                 -- default: https://github.com/nvim-lualine/lualine.nvim#default-configuration
