e979a4836469ad66a4539c7273c3594fed351fc0
Wes Higbee <wes.mcclure@gmail.com>
2025-09-26 00:00:01 -0500

    review

--- .config/nvim/lua/plugins/lualine/lualine.lua
+++ .config/nvim/lua/plugins/lualine/lualine.lua
@@ -37,41 +37,26 @@ end
 return {
     {
         "nvim-lualine/lualine.nvim",
-
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         -- TODO consider "kyazdani42/nvim-web-devicons" (lua rewrite) if some reaosn to do so, i.e. perf? or other forks?
         config = function()
-            -- credit / from https://neovimcraft.com/plugin/SmiteshP/nvim-navic/
 
             require("lualine").setup {
-                -- default: https://github.com/nvim-lualine/lualine.nvim#default-configuration
                 options = {
-                    -- section_separators = { left = vim.fn.nr2char(0xE0B4), right = vim.fn.nr2char(0xE0B6) },
-                    -- section_separators = { left = "", right = "" },
-                    -- section_separators = { left = "▌", right = "▐" },
+                    -- separators waste space!
                     section_separators = "",
                     component_separators = "",
-                    -- globalstatus  -- only one status line? hrm... might work now that I have inactive windows dimmed in onedarkpro theme
-                    --
                     theme = require("plugins.lualine.theme").theme(),
-
                 },
-                -- FYI =>    :lua print(vim.inspect(require('lualine').get_config()))
-                -- extensions = { 'nvim-tree' }, -- shows root dir (and dirs above it) in statusline... I dont need that, in fact if anything show file path of the file still that was right before open treeview
-                -- FYI do not use a function that may not exist before this is loaded else the component will be gone the entire nvim session (see what I did with copilot statusline)
                 sections = {
                     -- commandline shows mode already so why put it here too? plus lualine has color changes
-                    -- lualine_a = { 'buffers' }, -- TODO "buffers" looks interesting! shows tabs for each file... might be what I've been wanting?
-                    --    also has tabs/windows... interesting (is that for tab strip,  or?)
                     lualine_a = { '' },
                     lualine_b = {
                         -- https://github.com/nvim-lualine/lualine.nvim#filename-component-options
-                        -- 1 = relative path
-                        { "filename", path = 1 }
+                        { "filename", path = 1 } -- 1 = relative path
                     },
-                    -- lualine_c = { "filetype" },
                     lualine_c = { statusline_filetype, unpack(CopilotsStatus()), },
-                    lualine_x = {}, -- todo move copilot back here?
+                    lualine_x = {},
                     lualine_y = {
                         {
                             function()
