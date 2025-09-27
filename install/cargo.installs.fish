736671954566e075e39e4b4f16a4a4c5340cf75c
Wes Higbee <wes.mcclure@gmail.com>
2025-09-27 11:17:12 -0500

    list of cargo installs

---
+++ install/cargo.installs.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+
+cargo install --list
+# --force to update
+
+# JFryy/systemd-lsp
+cargo install systemd-lsp --force
+
+cargo install treeify --force
+
+cargo install --git https://github.com/rydesun/fennel-language-server --force
