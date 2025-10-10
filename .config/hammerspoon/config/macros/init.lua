1059f92f339e937498c407345e9e535aec7ded9c
Wes Higbee <wes.mcclure@gmail.com>
2025-10-10 02:39:02 -0500

    add log dedicated to KM streamdeck runner entrypoint

--- .config/hammerspoon/config/macros/init.lua
+++ .config/hammerspoon/config/macros/init.lua
@@ -1,33 +1,73 @@
 local M = {}
 
+
+---@type file*?
+local log_file
+local log_dir = os.getenv("HOME") .. "/.hammerspoon/logs"
+local filepath = log_dir .. "/streamdeck_keyboardmaestro_runner.log"
+
+---@return file*?
+local function get_log_file()
+    if not log_file then
+        hs.fs.mkdir(log_dir)
+        log_file = io.open(filepath, "w")
+    end
+    return log_file
+end
+
+--- idea => use log file for issues instead of print/STDOUT or persistent notification/alert
+--- intended for use with KM macro that runs lua code by passing it as a parameter
+--- this way I can leave benign prints in my code and not have them show up in KM STDOUT from running `hs` command
+--- AND THEN for failures (uncaught errors) I can log them instead of print to STDOUT
+--- that way I can keep using print w/o needing to see STDOUT on failures in KM + hs macro
+---@param message string
+function log_failure(message)
+    local file = get_log_file()
+    if not file then
+        -- fallback to print (STDOUT) which will go to hs console and/or command output with `hs` command
+        print("ERROR: failed to open log file for writing...\n  message: " .. message)
+        return
+    end
+
+    file:write(message)
+    file:flush()
+end
+
+function alert_and_log_failure(message)
+    log_failure(message)
+    message = message .. " Check " .. filepath
+    -- this pokes me ON ERRORS ONLY... but doesn't stay up long!
+    -- habituate then check my log file for more details (or longer than alert shows)
+    -- BTW on a real error I want this to stay up briefly, not just flicker
+    -- THIS IS NOT INTENDED TO BE FOR ALL PRINTS like notifications was doing
+    -- SO 5 seconds for now, enough to remind me where the log file is
+    --    log file will have stack trace (i.e. file path:line) I can copy out and use in telescope to jump to issue!)
+    --      unlike notification in macOS where I cannot really interact with it b/c SHOW is a mess and the message isn't copyable (AFAIK)... and it truncates past a few short lines and no way to expand it - IOTW a terrible way to log failures)
+    -- I will then fix issue and then it won't popup the annoying alert again!
+    -- * ALSO, now you should TURN OFF STDOUT => notification in KM macro
+    hs.alert.show(message, nil, nil, 5)
+end
+
 -- *** streamdeck Keyboard Maestro wrapper to catch errors and log them and rethrow for KM to show too ***
 function StreamDeckKeyboardMaestroRunner(what)
     -- USED in KM Macros => look at kmsync file:
     --    plutil -p Keyboard\ Maestro\ Macros.kmsync  | grep "StreamDeckKeyboardMaestroRunner"
 
-    -- I want error in hammerspoon logs too
-    -- I also like the notification from KM b/c that is immediately visible
-    --   FYI AFAICT KM will use exit code to decide when to show for NOTIFICATIONS
-    --   BUT, it seems to always show on ANY STDOUT if you choose window as the option in KM
-    --   IOTW AFAICT I only wanna use notifications in KM for "output"
-    -- then when I go to look into the issue I want HS logs
-    --
-    -- FYI w/o this its easy to think unhandled exceptions are being swallowed when its just KM not showing errors (b/c you said ignore results, rightly so b/c results all the time are annoying)... anyways use this to always log them)
-    -- TODO can I just wire up something such that anyone that calls hs CLI, the errors are caught and logged... then I wouldn't need this
-    --     anyone that calls hs CLI the errors will go to their CLI instance's STDOUT...
-    --     thus wrap here to catch here too
-    --
-    -- anything printed/unhandled here is going to show in logs for hs CLI => only for KM notifications in the case of KM calling this
+    -- FYI w/o this its easy to think unhandled exceptions are being swallowed
+    --   when its just KM not showing errors
+    --   (b/c you said ignore results, rightly so b/c results all the time are annoying)...
+    --   AND `hs` CLI doesn't log STDOUT to hs console
     xpcall(function()
         -- keep all parsing inside too so I catch those errors and show in HS logs too
         local func = load(what)
         if func == nil then
-            error("failed to load: " .. what)
+            alert_and_log_failure("failed to load: " .. what)
             return
         end
         func()
-    end, function(errorMsg) print("error: ", errorMsg) end)
-    -- TODO verify it still shows notification for KM?
+    end, function(errorMsg)
+        alert_and_log_failure("StreamDeckKeyboardMaestroRunner error: " .. tostring(errorMsg) .. "\n")
+    end)
 end
 
 require("config.macros.brave")
