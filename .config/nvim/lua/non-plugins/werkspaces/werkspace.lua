2243ecd010c890198e938e946335ab6f24d096d8
Wes Higbee <wes.mcclure@gmail.com>
2025-10-11 02:38:28 -0500

    rename

--- .config/nvim/lua/non-plugins/werkspaces/werkspace.lua
+++ .config/nvim/lua/non-plugins/werkspaces/werkspace.lua
@@ -87,9 +87,9 @@ function restore_session_by_name()
         return
     end
 
-    local name = vim.fn.inputlist({ "Select session to restore:", unpack(sessions) })
-    if name > 0 and name <= #sessions then
-        local name = sessions[name]
+    local name_index = vim.fn.inputlist({ "Select session to restore:", unpack(sessions) })
+    if name_index > 0 and name_index <= #sessions then
+        local name = sessions[name_index]
         local session_file = get_session_file(name)
         if vim.fn.filereadable(session_file) == 1 then
             vim.cmd("source " .. session_file)
@@ -98,7 +98,7 @@ function restore_session_by_name()
             print("Session file not found: " .. session_file)
         end
     else
-        print("Invalid session selection: " .. name)
+        print("Invalid session selection: " .. name_index)
     end
 end
 
