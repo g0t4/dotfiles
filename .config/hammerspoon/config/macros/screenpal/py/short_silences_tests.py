51ef525c0d3b0bed4ed092772b1caea2d59ef144
Wes Higbee <wes.mcclure@gmail.com>
2025-10-17 15:23:32 -0500

    gptoss120b AskRewrite change to float .0 for whole numbers too

--- .config/hammerspoon/config/macros/screenpal/py/short_silences_tests.py
+++ .config/hammerspoon/config/macros/screenpal/py/short_silences_tests.py
@@ -5,18 +5,18 @@ def test_detect_short_silences():
 
     expected = {
         "short_silences": [
-            {"x_start": 4, "x_end": 5.5},
-            {"x_start": 31, "x_end": 32.5},
-            {"x_start": 217, "x_end": 218.5},
-            {"x_start": 319, "x_end": 320.5},
-            {"x_start": 376, "x_end": 378},
-            {"x_start": 403, "x_end": 404.5},
-            {"x_start": 703, "x_end": 743.5},
-            {"x_start": 1024, "x_end": 1025.5},
-            {"x_start": 1228, "x_end": 1229.5},
-            {"x_start": 1423, "x_end": 1464},
-            {"x_start": 1561, "x_end": 1562.5},
-            {"x_start": 1741, "x_end": 1744}
+            {"x_start": 4.0, "x_end": 5.5},
+            {"x_start": 31.0, "x_end": 32.5},
+            {"x_start": 217.0, "x_end": 218.5},
+            {"x_start": 319.0, "x_end": 320.5},
+            {"x_start": 376.0, "x_end": 378.0},
+            {"x_start": 403.0, "x_end": 404.5},
+            {"x_start": 703.0, "x_end": 743.5},
+            {"x_start": 1024.0, "x_end": 1025.5},
+            {"x_start": 1228.0, "x_end": 1229.5},
+            {"x_start": 1423.0, "x_end": 1464.0},
+            {"x_start": 1561.0, "x_end": 1562.5},
+            {"x_start": 1741.0, "x_end": 1744.0},
         ]
     } # yapf: disable
 
