5d475a69c9aab841b7dabc73c55740cf12d97c1d
Wes Higbee <wes.mcclure@gmail.com>
2025-10-17 15:20:49 -0500

    not necessary, but why not modify tests to assert floating point values even if .0

--- .config/hammerspoon/config/macros/screenpal/py/regular_silences_tests.py
+++ .config/hammerspoon/config/macros/screenpal/py/regular_silences_tests.py
@@ -3,6 +3,6 @@ from regular_silences import detect_regular_silences
 def test_detect_regular_silences():
     detected = detect_regular_silences("samples/timeline03a.png")
 
-    expected = {"regular_silences": [{"x_start": 754, "x_end": 891}, {"x_start": 1450, "x_end": 1653}]} # yapf: disable
+    expected = {"regular_silences": [{"x_start": 754.0, "x_end": 891.0}, {"x_start": 1450.0, "x_end": 1653.0}]} # yapf: disable
 
     assert detected["regular_silences"] == expected["regular_silences"]
