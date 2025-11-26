7c5d05bab8fe08486004741e769fa77d9c0c229a
Wes Higbee <wes.mcclure@gmail.com>
2025-11-26 16:54:25 -0600

    register a .harmony filetype and add snippets for it, for openai harmony response format

--- .config/nvim/lua/non-plugins/werkspaces/filetypemods.lua
+++ .config/nvim/lua/non-plugins/werkspaces/filetypemods.lua
@@ -35,6 +35,7 @@ vim.cmd([[
 vim.filetype.add({
     extension = {
         notes = "markdown",
+        harmony = "harmony",
         --
         -- override bash as bash...
         -- FYI this should be the case from the nvim runtime files (but somehow nvim runtime seems to assign sh instead)
