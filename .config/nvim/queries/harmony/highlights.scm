1c0dc3ec840c1f192c0211de1bb71db6a9fd94b5
Wes Higbee <wes.mcclure@gmail.com>
2025-11-28 17:08:31 -0600

    inlined header node too

--- .config/nvim/queries/harmony/highlights.scm
+++ .config/nvim/queries/harmony/highlights.scm
@@ -9,13 +9,13 @@
 ;   (header_developer) @harmony_developer
 ; ]
 [
-  (message (header (header_user))) @harmony_message_user
-  (message (header (header_developer))) @harmony_message_developer
-  (message (header (header_system))) @harmony_message_system
-  (message (header (header_tool_result))) @harmony_message_tool_result
+  (message (header_user)) @harmony_message_user
+  (message (header_developer)) @harmony_message_developer
+  (message (header_system)) @harmony_message_system
+  (message (header_tool_result)) @harmony_message_tool_result
 ]
 [
-  (message (header (header_assistant_analysis))) @harmony_message_assistant_analysis
-  (message (header (header_assistant_commentary))) @harmony_message_assistant_commentary
-  (message (header (header_assistant_final))) @harmony_message_assistant_final
+  (message (header_assistant_analysis)) @harmony_message_assistant_analysis
+  (message (header_assistant_commentary)) @harmony_message_assistant_commentary
+  (message (header_assistant_final)) @harmony_message_assistant_final
 ]
