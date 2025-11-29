52b49829d0456be906d0889c8eea85f4fcd57369
Wes Higbee <wes.mcclure@gmail.com>
2025-11-28 18:09:09 -0600

    these are too primitive to matter

--- .config/nvim/queries/harmony/highlights.scm
+++ .config/nvim/queries/harmony/highlights.scm
@@ -9,9 +9,9 @@
 ;   (header_developer) @harmony_developer
 ; ]
 [
-  (message header:(header_user)) @harmony_message_user
-  (message header:(header_developer)) @harmony_message_developer
-  (message header:(header_system)) @harmony_message_system
+  (message (header_user)) @harmony_message_user
+  (message (header_developer)) @harmony_message_developer
+  (message (header_system)) @harmony_message_system
   (message header:(header_tool_result)) @harmony_message_tool_result
 ]
 [
