e4fcf5d4a65e6a8f8072e32cd1f25a950dbf8560
Wes Higbee <wes.mcclure@gmail.com>
2025-11-29 12:13:35 -0600

    notes

--- .config/nvim/queries/test/injections.scm
+++ .config/nvim/queries/test/injections.scm
@@ -3,23 +3,16 @@
 ;     (output) @injection.content)
 ;   (#set! injection.language "query"))
 ; inject the input block using the captured language attribute
-; ((test
-;   (header
-;     (attributes
-;       (attribute
-;         (parameter) @corpus.lang)))
-;   (input) @corpus.input)
-;   (#set! injection.language @corpus.lang)
-;   (#set! injection.include-children "true"))
 
 ; ; Step 1 (working) - hardcode the language to harmony, baby steps:
 ; ((input) @injection.content
 ;   (#set! injection.language "harmony"))
 
-; ; Step 2 - use language attribute to inject input node's language
+; ; Step 2 - capture both language and content nodes - now, the end user can define the language in the test header!
+;  i.e. :langauge(harmony) or :language(test)
 (test
   (header
     (attributes
       (attribute
-        language: (parameter) @injection.language)))
-  (input) @injection.content)
+        language: (parameter) @injection.language))) ; @injection.language capture - the text of this captured node should be used (if available) as the injection language
+  (input) @injection.content) ; @injection.content capture - captured node's contents are reparsed using the @injection.language!
