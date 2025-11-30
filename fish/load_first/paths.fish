2220617c48b5c2b739540b831e884d1d0abccb00
Wes Higbee <wes.mcclure@gmail.com>
2025-11-30 00:31:45 -0600

    use local build of tree-sitter (supports :cst in tests)

--- fish/load_first/paths.fish
+++ fish/load_first/paths.fish
@@ -51,6 +51,11 @@ if test -d "$HOME/.cargo/bin"
     export PATH="$HOME/.cargo/bin:$PATH"
 end
 
+if test -d "$HOME/repos/github/tree-sitter/tree-sitter/target/release"
+    # target debug build of zed (comment out when done using it as daily driver)
+    export PATH="$HOME/repos/github/tree-sitter/tree-sitter/target/release:$PATH"
+end
+
 if test -d "$HOME/repos/github/zed-industries/zed/target/debug"
     # target debug build of zed (comment out when done using it as daily driver)
     export PATH="$HOME/repos/github/zed-industries/zed/target/debug:$PATH"
