6793e95685888b841a363440bf0621bb5d274481
Wes Higbee <wes.mcclure@gmail.com>
2025-12-01 22:05:41 -0600

    derp, I can handle both captures with one query!

--- .config/nvim/queries/lua/textobjects.scm
+++ .config/nvim/queries/lua/textobjects.scm
@@ -1,9 +1,7 @@
-(string
-  "[["
-  (string_content)
-  "]]") @fucker.around
-
+;
+; FYI open a lua file, :EditQuery, paste in a query and put cursor on a capture and it will highlight it for you in the lua file!!
+;
 (string
   "[["
   (string_content) @fucker.inner
-  "]]")
+  "]]") @fucker.around
