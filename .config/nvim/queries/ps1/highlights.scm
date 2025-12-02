abe54fce10dd98a0ecc0a596d92de3b8ca411698
Wes Higbee <wes.mcclure@gmail.com>
2025-12-01 18:04:30 -0600

    switch all symlinks to use new inherits/extends combo

---
+++ .config/nvim/queries/ps1/highlights.scm
@@ -0,0 +1,4 @@
+; extends
+; inherits: shared
+;
+; TODO! ps1 files were not highlighting with old symlink... so need to find out why later
---
+++ .config/nvim/queries/zsh/highlights.scm
@@ -0,0 +1,4 @@
+; extends
+; inherits: shared
+;
+; TODO not sure that zsh was working with symlinks, I vaguely recall issues and didn't test before/after but just fix it as is... might be smth in common with ps1 issue too
