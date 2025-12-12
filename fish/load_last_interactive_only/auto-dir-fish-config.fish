f3e6e0a81cac39efb900b54474ea22e4c5c9d43a
Wes Higbee <wes.mcclure@gmail.com>
2025-12-12 08:53:16 -0600

    gptoss120b AskRewrite I want a convention whereby a project (often repo, in repo root)... has a local config.fish file that can be checked into a repo (or just added to any dir actually)... when I cd to a different directory, this function should check for the local config.fish file and source it, and right before sourcing it should run (if loaded from past local config) a function called deactivate_local_config_fish() to unregister prior customizations => then call local config.fish which should define a new deactivate_local_config_fish() function as well as just top level have config for what is customized locally... I will use this for example, to setup repo specific abbrs that I want to vary project to project (i.e. ptw_one is a standard example of running one test with ptw with the various options I like to use and the path to the file in the current project... same abbr could be defined in any project with path to the most relevant single test in a given project/repo - this is as-is without any changes from me yet, just verbatim what model generated
    diff --git a/fish/load_last_interactive_only/auto-dir-fish-config.fish b/fish/load_last_interactive_only/auto-dir-fish-config.fish
    new file mode 100644
    index 000000000..753956418
    --- /dev/null
    +++ b/fish/load_last_interactive_only/auto-dir-fish-config.fish
    @@ -0,0 +1,46 @@
    +
    +
    +function _find_local_config
    +    set -l dir (pwd)
    +    while test $dir != /
    +        if test -f $dir/config.fish
    +            echo $dir/config.fish
    +            return
    +        end
    +        set dir (dirname $dir)
    +    end
    +end
    +
    +function _load_local_config
    +    set -l config_path (_find_local_config)
    +    if test -z "$config_path"
    +        return
    +    end
    +
    +    # deactivate previous local config if it exists
    +    if functions -q deactivate_local_config_fish
    +        deactivate_local_config_fish
    +    end
    +
    +    # source the new config
    +    source $config_path
    +
    +    # ensure the new config defined deactivate_local_config_fish
    +    if not functions -q deactivate_local_config_fish
    +        function deactivate_local_config_fish
    +            # no-op placeholder
    +        end
    +    end
    +end
    +
    +# Hook into directory changes
    +if not functions -q __fish_cd_hook_local_config
    +    function __fish_cd_hook_local_config --on-event fish_prompt
    +        _load_local_config
    +    end
    +end
    +
    +# also load when starting a new session
    +_load_local_config
    +
    +

---
+++ fish/load_last_interactive_only/auto-dir-fish-config.fish
@@ -0,0 +1,46 @@
+
+
+function _find_local_config
+    set -l dir (pwd)
+    while test $dir != /
+        if test -f $dir/config.fish
+            echo $dir/config.fish
+            return
+        end
+        set dir (dirname $dir)
+    end
+end
+
+function _load_local_config
+    set -l config_path (_find_local_config)
+    if test -z "$config_path"
+        return
+    end
+
+    # deactivate previous local config if it exists
+    if functions -q deactivate_local_config_fish
+        deactivate_local_config_fish
+    end
+
+    # source the new config
+    source $config_path
+
+    # ensure the new config defined deactivate_local_config_fish
+    if not functions -q deactivate_local_config_fish
+        function deactivate_local_config_fish
+            # no-op placeholder
+        end
+    end
+end
+
+# Hook into directory changes
+if not functions -q __fish_cd_hook_local_config
+    function __fish_cd_hook_local_config --on-event fish_prompt
+        _load_local_config
+    end
+end
+
+# also load when starting a new session
+_load_local_config
+
+
