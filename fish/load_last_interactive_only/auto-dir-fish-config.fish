fdb37c03a9a4c01dc4ba8085152f67d0de5320b3
Wes Higbee <wes.mcclure@gmail.com>
2025-12-12 09:09:21 -0600

    rename

--- fish/load_last_interactive_only/auto-dir-fish-config.fish
+++ fish/load_last_interactive_only/auto-dir-fish-config.fish
@@ -10,18 +10,18 @@ function _find_local_config
 end
 
 function _load_local_config --on-variable PWD
-    # deactivate previous local config if it exists
+
     if functions -q deactivate_local_config_fish
         deactivate_local_config_fish
     end
 
-    set -l config_path (_find_local_config)
-    if test -z "$config_path"
+    set -l local_config_path (_find_local_config)
+    if test -z "$local_config_path"
         return
     end
 
     # source the new config
-    source $config_path
+    source $local_config_path
 
     # ensure the new config defined deactivate_local_config_fish
     if not functions -q deactivate_local_config_fish
