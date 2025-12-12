0416975c5d67e2ca97b0a815d27eb56bd50455f3
Wes Higbee <wes.mcclure@gmail.com>
2025-12-11 23:29:53 -0600

    don't need this commented out, in fact I need it to work for --rcfile demo

--- bash/course.bashrc.sh
+++ bash/course.bashrc.sh
@@ -1,18 +1,18 @@
-# #
-# # * KEEP THIS FILE MINIMAL, only critical, early config should be in here
 #
-# # * uncomment for xtrace-ing (fyi.. can inherit path from outer shell)
-# # export PS1="\W $ "
-# # unset PROMPT_COMMAND # remove default for xtrace, when also comment out the return here:
-# # return
-#
-# source "$BASH_DOTFILES/shared.bashrc.sh"
-#
-# for script in "$BASH_DOTFILES/startup/course-only/"*.bash; do
-#     source "$script"
-# done
-#
-# if is_interactive; then
-#     # some things to remove for first course, but leave in my full bashrc
-#     abbr --remove wc
-# fi
+# * KEEP THIS FILE MINIMAL, only critical, early config should be in here
+
+# * uncomment for xtrace-ing (fyi.. can inherit path from outer shell)
+# export PS1="\W $ "
+# unset PROMPT_COMMAND # remove default for xtrace, when also comment out the return here:
+# return
+
+source "$BASH_DOTFILES/shared.bashrc.sh"
+
+for script in "$BASH_DOTFILES/startup/course-only/"*.bash; do
+    source "$script"
+done
+
+if is_interactive; then
+    # some things to remove for first course, but leave in my full bashrc
+    abbr --remove wc
+fi
