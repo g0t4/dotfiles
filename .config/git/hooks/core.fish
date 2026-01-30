35582d507e2ed3b208d2a4463e1282f751dbb54d
Wes Higbee <wes.mcclure@gmail.com>
2026-01-29 21:40:44 -0600

    use rich print, which will respect STDOUT not a tty, and drop colors... with AI agents committing frequently, the CSI ansi escape seuqences look terrible

--- .config/git/hooks/core.fish
+++ .config/git/hooks/core.fish
@@ -9,14 +9,14 @@ end
 
 if test -d "$repo/.ctags.d"; and test -f Makefile
     # assume Makefile target for tags
-    log_ --apple_pink --bold "make tags"
+    rich --print "[#FF2D55 bold] make tags"
     make tags
 end
 
 if test -f "$repo/.rag.yaml"
     # convention => .rag.yaml config file
     #  that way I explicitly have to make the file and opt-in
-    log_ --apple_pink --bold rag_indexer
+    rich --print "[#FF2D55 bold] rag_indexer"
 
     #  PRN I should have an option inside to toggle off the auto rag_indexer... that I think can go inside the python code
     #     heck even this check could go into the python code
