e75ec9b1ddd17376db8a4f24f76679ba50f8fd2d
Wes Higbee <wes.mcclure@gmail.com>
2026-02-01 13:49:19 -0600

    idea for MRU cache for fzf pickers

--- fish/load_last_interactive_only/files-specific.fish
+++ fish/load_last_interactive_only/files-specific.fish
@@ -793,13 +793,46 @@ function _fzf_nested_file_widget -d "Paste selected file into command line"
     commandline -f repaint
 end
 
-function _fzf_nested_file_unrestricted_widget -d "Paste selected file (including hidden) into command line"
-    set -l file (fd --type f . -u | fzf --height 50% --border)
+set -g FZF_MRU_FILE ~/.cache/fzf-mru-files
+function __fzf_mru_read
+    test -f $FZF_MRU_FILE; or return
+    cat $FZF_MRU_FILE | while read -l path
+        test -e "$path"; and echo "$path"
+    end
+end
+function __fzf_mru_write --argument path
+    mkdir -p (dirname $FZF_MRU_FILE)
+
+    begin
+        echo "$path"
+        test -f $FZF_MRU_FILE; and grep -Fxv -- "$path" $FZF_MRU_FILE
+    end | head -n 200 > $FZF_MRU_FILE
+end
+function _fzf_nested_file_unrestricted_widget \
+    -d "Paste selected file (including hidden) into command line"
+
+    set -l file (
+        begin
+            __fzf_mru_read
+            fd --type f . -u | grep -Fxv -f $FZF_MRU_FILE 2>/dev/null
+        end | fzf --height 50% --border
+    )
+
     if test -n "$file"
+        __fzf_mru_write "$file"
         commandline -i -- (string escape -- "$file")
     end
+
     commandline -f repaint
 end
+
+# function _fzf_nested_file_unrestricted_widget -d "Paste selected file (including hidden) into command line"
+#     set -l file (fd --type f . -u | fzf --height 50% --border)
+#     if test -n "$file"
+#         commandline -i -- (string escape -- "$file")
+#     end
+#     commandline -f repaint
+# end
 
 function _fzf_nested_both_file_and_dirs_widget -d "Paste selected file or directory into command line"
     # btw `diff_two_commands 'fd --type f --type d' 'fd'` differ in symlinks (at least)
