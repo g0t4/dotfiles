266c564aea54507752c4404a097a761964d2e80c
Wes Higbee <wes.mcclure@gmail.com>
2026-02-27 07:20:05 -0600

    nuke old

--- zsh/universals/3-last/ask-openai/chat_non_stream.py
+++ zsh/universals/3-last/ask-openai/chat_non_stream.py
@@ -38,34 +38,6 @@ def generate_non_streaming(passed_context: str, system_message: str, max_tokens:
         HumanMessage(content=passed_context),
     ]
 
-    service = get_selected_service()
-    if service.name == "anthropic":
-        # FYI importing langchain_anthropic is ALSO slow b/c anthropic package eager loads basically everything on import!
-        #  TBH this is NBD for my wes.py daemon b/c it loads once
-        #    annoying for my rarely used ctrl-b keymap (not a daemon, takes hit every time)
-        #  I setup a POC (proof of concept) to optimize this massively:
-        #    https://github.com/anthropics/anthropic-sdk-python/issues/1211
-        #    already 35% to 60% reduction in import time for several key modules
-        from langchain_anthropic import ChatAnthropic
-        model = ChatAnthropic(
-            model_name=service.model,
-            api_key=service.api_key,
-            timeout=TIMEOUT_SECONDS,
-            stop=None,
-        )
-    else:
-        # FYI importing langchain_openai is slow b/c openai.types is missing lazy loading with 100s of pydantic based types:
-        # https://github.com/openai/openai-python/issues/2819
-        # IOTW import langchain_openai suffers b/c it imports openai (which takes 220 to 300ms)
-        from langchain_openai import ChatOpenAI
-        model = ChatOpenAI(
-            model=service.model,
-            api_key=service.api_key,
-            base_url=service.base_url,
-            timeout=TIMEOUT_SECONDS,
-            # max_retries=2
-        )
-
     # TODO add temperature (optional) to Service? (perhaps rename to ServiceModel or split out generation args somehow)
     # PRN timeout?
     ai_message = model.invoke(messages, max_tokens=max_tokens)
