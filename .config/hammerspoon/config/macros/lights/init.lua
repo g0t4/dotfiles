eaa4b0264a5fb4d71c1ee870e5134f7cd8fa9aa9
Wes Higbee <wes.mcclure@gmail.com>
2026-03-12 14:58:27 -0500

    inline set_lights so not passing values but just use local vars

--- .config/hammerspoon/config/macros/lights/init.lua
+++ .config/hammerspoon/config/macros/lights/init.lua
@@ -56,7 +56,15 @@ local function set_hsl_channels(dmx_channels, channel_start, master_intensity, h
     dmx_channels[channel_start + 7] = lightness_low_byte
 end
 
-local function set_lights(right_intensity, right_temp, back_opts, left_intensity, left_temp)
+function StreamDeckBuild26()
+    local temp = 5000
+    local left_intensity = 5
+    local left_temp = temp
+    local right_intensity = 10
+    local right_temp = temp
+    -- -- TODO lightness, seems to map to a temp value... how does that work? just on HSL 16 bit mode
+    local back = { master = 20, hue = 240, saturation = 100, lightness = 0 } -- rear/kick/accent
+
     local dmx_channels = {}
 
     -- left (key) light: CCT 16bit (uses 5/8 channels)
@@ -67,7 +75,7 @@ local function set_lights(right_intensity, right_temp, back_opts, left_intensity
 
     -- back light HSL 16bit (uses 8/8 channels)
     set_hsl_channels(dmx_channels, 17,
-        back_opts.master, back_opts.hue, back_opts.saturation, back_opts.lightness)
+        back.master, back.hue, back.saturation, back.lightness)
 
     -- TODO how can I target a subset of channels without a massive comma delimited string?
     --    i.e. can't I send just 21 to 28? and not need the empty commas in between?
@@ -75,16 +83,6 @@ local function set_lights(right_intensity, right_temp, back_opts, left_intensity
     local cmd = "/opt/homebrew/bin/ola_set_dmx -u 1 --dmx " .. dmx_string
     print(cmd)
     hs.execute(cmd)
-end
-
-function StreamDeckBuild26()
-    local temp = 5000
-    set_lights(
-        5, temp -- left (key)
-        -- TODO lightness, seems to map to a temp value... how does that work? just on HSL 16 bit mode
-        { master = 20, hue = 240, saturation = 100, lightness = 0 }, -- rear/kick/accent
-        10, temp, -- right (fill)
-    )
 end
 
 function StreamDeckDmxOff()
