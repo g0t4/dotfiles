ba65137aa126508b928c0f8956c82adbe24cff07
Wes Higbee <wes.mcclure@gmail.com>
2026-03-29 00:13:47 -0500

    make ansible abbrs for fish shell only... I am going to start moving away from even maintaining zsh abbrs... I would much rather just install fish everywhere than bother with zsh

--- fish/load_last_interactive_only/ansibles.fish
+++ fish/load_last_interactive_only/ansibles.fish
@@ -1,3 +1,16 @@
+# pass an option (key) filter => 0 or more matching option names and details of each!
+abbr aclsn "_ansible-config_options_name_contains"
+function _ansible-config_options_name_contains
+    set -l filter $argv
+    command ansible-config list | yq "with_entries(select(.key | test(\"(?i).*$filter.*\")))"
+end
+abbr aclsd "_ansible-config_option_details_contains"
+function _ansible-config_option_details_contains
+    # TODO verify works in fish
+    set -l key $1
+    command ansible-config list | yq "with_entries(select(.. | has(\"$key\")))"
+end
+
 abbr ap "ansible-playbook"
   abbr apv "ansible-playbook -v"
   abbr apvv "ansible-playbook -vv"
@@ -51,3 +64,4 @@ abbr ai "ansible-inventory"
   abbr aih "ansible-inventory --host "
 
 abbr apull "ansible-pull"
+
