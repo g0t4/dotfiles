fc977de551b9b31b7e0644baa1b05b6cf22b29d2
Wes Higbee <wes.mcclure@gmail.com>
2026-04-06 15:11:32 -0500

    gptoss120b use fish_add_path

--- fish/load_first/paths.fish
+++ fish/load_first/paths.fish
@@ -32,16 +32,16 @@ end
 if test -d "$HOME/bin"
     # fennel-ls
     # jattach
-    export PATH="$HOME/bin:$PATH"
+    fish_add_path --global --prepend "$HOME/bin"
 end
 
 if test -d "$HOME/go/bin"
-    export PATH="$HOME/go/bin:$PATH"
+    fish_add_path --global --prepend "$HOME/go/bin"
 end
 
 if test -d /usr/local/go/bin
     # from wget install (see fish/install/install.fish)
-    export PATH="/usr/local/go/bin:$PATH"
+    fish_add_path --global --prepend "/usr/local/go/bin"
 end
 
 # FYI for now just make install it
@@ -50,32 +50,32 @@ end
 # end
 
 if test -d "$HOME/.local/bin"
-    export PATH="$HOME/.local/bin:$PATH"
+    fish_add_path --global --prepend "$HOME/.local/bin"
 end
 if test -d "$HOME/.cargo/bin"
-    export PATH="$HOME/.cargo/bin:$PATH"
+    fish_add_path --global --prepend "$HOME/.cargo/bin"
 end
 
 if test -d "$HOME/repos/github/tree-sitter/tree-sitter/target/release"
-    export PATH="$HOME/repos/github/tree-sitter/tree-sitter/target/release:$PATH"
+    fish_add_path --global --prepend "$HOME/repos/github/tree-sitter/tree-sitter/target/release"
 end
 
 if test -d "$HOME/repos/github/ribru17/ts_query_ls/target/release"
-    export PATH="$HOME/repos/github/ribru17/ts_query_ls/target/release:$PATH"
+    fish_add_path --global --prepend "$HOME/repos/github/ribru17/ts_query_ls/target/release"
 end
 
 if test -d "$HOME/repos/github/zed-industries/zed/target/debug"
-    export PATH="$HOME/repos/github/zed-industries/zed/target/release:$PATH"
+    fish_add_path --global --prepend "$HOME/repos/github/zed-industries/zed/target/release"
 end
 
 if test -d "$HOME/repos/github/openai/codex/codex-rs/target/release"
     # apply_patch tool, among others
-    export PATH="$HOME/repos/github/openai/codex/codex-rs/target/release:$PATH"
+    fish_add_path --global --prepend "$HOME/repos/github/openai/codex/codex-rs/target/release"
 end
 
 # fish-lsp (i.e. on arch)... brew on macOS
 if test -d "$HOME/repos/github/ndonfris/fish-lsp/bin"
-    export PATH="$HOME/repos/github/ndonfris/fish-lsp/bin:$PATH"
+    fish_add_path --global --prepend "$HOME/repos/github/ndonfris/fish-lsp/bin"
 end
 
 # kubectl krew
@@ -84,18 +84,18 @@ if test -d "$HOME/.krew/bin"
     # set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
 
     # I will just go with ~/.krew/bin
-    export PATH="$HOME/.krew/bin:$PATH"
+    fish_add_path --global --prepend "$HOME/.krew/bin"
 end
 
 # /snap/bin (ubuntu/wsl)
 if test -d /snap/bin
-    export PATH="/snap/bin:$PATH"
+    fish_add_path --global --prepend "/snap/bin"
 end
 
 # ? fix ~/.dotnet/tools in path (replace with abs path) => see zshrc (ignore wrong path in /etc/paths.d/dotnet-cli-tools with ~/.dotnet/tools)
 if test -d "$HOME/.dotnet/tools"
     # make dotnet tool install --global work (add to PATH)
-    export PATH="$HOME/.dotnet/tools:$PATH"
+    fish_add_path --global --prepend "$HOME/.dotnet/tools"
 end
 
 ### COMPLETIONS path:
@@ -145,48 +145,48 @@ end
 
 # ghcup (haskell)
 if test -d "$HOME/.ghcup/bin"
-    export PATH="$HOME/.ghcup/bin:$PATH"
+    fish_add_path --global --prepend "$HOME/.ghcup/bin"
     # FYI
     # ghcup list --tool ghc
     # ghcup set ghc latest/recommended
 end
 
 function use_brew_llvm
-    export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+    fish_add_path --global --prepend "/opt/homebrew/opt/llvm/bin"
 end
 
 if test -d "/opt/homebrew/opt/postgresql@17/bin"
     # todo should I warn if newer or other version is installed instead?
     #  btw v14 is the "default" version in homebrew, why? why not v17?
     #  also if I use `brew link postgresql@17` it appends _17 on end of all commands which YUCK hence adding here:
-    export PATH="/opt/homebrew/opt/postgresql@17/bin:$PATH"
+    fish_add_path --global --prepend "/opt/homebrew/opt/postgresql@17/bin"
 end
 
 # google-cloud-sdk
 if test -d /opt/homebrew/share/google-cloud-sdk/bin
-    export PATH="/opt/homebrew/share/google-cloud-sdk/bin:$PATH"
+    fish_add_path --global --prepend "/opt/homebrew/share/google-cloud-sdk/bin"
 end
 
 if test -d "$HOME/repos/github/ggml-org/llama.cpp/build/bin"
     # TODO is this what I want?
-    export PATH="$HOME/repos/github/ggml-org/llama.cpp/build/bin:$PATH"
+    fish_add_path --global --prepend "$HOME/repos/github/ggml-org/llama.cpp/build/bin"
     set GGUF_MODELS "$HOME/repos/github/ggml-org/llama.cpp/models"
 end
 
 # /opt/cuda/bin/
 if test -d /opt/cuda/bin
-    export PATH="/opt/cuda/bin:$PATH"
+    fish_add_path --global --prepend "/opt/cuda/bin"
 end
 
 # ~/.npm-global/bin
 if test -d "$HOME/.npm-global/bin"
-    export PATH="$HOME/.npm-global/bin:$PATH"
+    fish_add_path --global --prepend "$HOME/.npm-global/bin"
 end
 
 # Rancher Desktop (IIGC created once user creates a cluster)
 # ~/.rd/bin"
 if test -d "$HOME/.rd/bin"
-    export PATH="$PATH:$HOME/.rd/bin"
+    fish_add_path --global --append "$HOME/.rd/bin"
 end
 
 # # * bash from source
@@ -248,5 +248,5 @@ end
 # if llvm-18 present, use it, in future detect which version I want or set it per env?
 #   FYI test -e takes 23us in fish on m1 mac so that is acceptable overhead for startup files
 if test -e /usr/lib/llvm-18/bin/
-    set -x PATH /usr/lib/llvm-18/bin $PATH
+    fish_add_path --global --prepend "/usr/lib/llvm-18/bin"
 end
