9370f60bb7504d03db49bba2af4788dc5a3bfd82
Wes Higbee <wes.mcclure@gmail.com>
2026-04-07 02:03:49 -0500

    move warning down so I can take advantage of is_interactive function

--- bash/shared.bashrc.sh
+++ bash/shared.bashrc.sh
@@ -2,17 +2,17 @@ UNAME_S=$(uname -s)
 is_macos() { [[ "$UNAME_S" = "Darwin" ]]; }
 is_linux() { [[ "$UNAME_S" = "Linux" ]]; }
 
-# ** warn if not using custom build:
-if is_interactive; then
-    [[ "$BASH" == ~/repos/github/g0t4/bash/bash ]] || echo "NOT USING CUSTOM BUILD OF BASH: $BASH"
-    # [[ "$BASH_VERSION" == 5.3.3* ]] || echo "unexpected bash version: $BASH_VERSION"
-fi
-
 # * RUN BEFORE any path mods...
 BASH_DOTFILES="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 source "$BASH_DOTFILES/startup/vetted-benign/early/funcs.bash"
 source "$BASH_DOTFILES/startup/vetted-benign/early/path-init.bash"
 export WES_DOTFILES="$(realpath "$BASH_DOTFILES"/..)"
+
+# ** warn if not using custom build:
+if is_interactive; then
+    [[ "$BASH" == ~/repos/github/g0t4/bash/bash ]] || echo "NOT USING CUSTOM BUILD OF BASH: $BASH"
+    # [[ "$BASH_VERSION" == 5.3.3* ]] || echo "unexpected bash version: $BASH_VERSION"
+fi
 
 if is_macos; then
 
