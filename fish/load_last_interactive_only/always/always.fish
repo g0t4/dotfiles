be23a74968d5b1a50c8e2d29a6399b032fbc5eb2
Wes Higbee <wes.mcclure@gmail.com>
2026-04-16 01:25:06 -0500

    shorten

--- fish/load_last_interactive_only/always/always.fish
+++ fish/load_last_interactive_only/always/always.fish
@@ -63,8 +63,8 @@ function ask_rewrite_diff_reviewer
     diff_two_commands "jq .request_body.messages[-1].content -r $trace_file" "jq .response_message.content -r $trace_file"
 end
 
+abbr t view_trace # go with tr if t is an issue
 abbr trace view_trace
-# abbr thread view_trace # nuke this if it becomes a crutch
 function view_trace
     set _python3 "$ASK_REPO/.venv/bin/python3"
     set _script_py "$ASK_REPO/tools/chat_viewer/__main__.py"
