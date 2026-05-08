31c88b023929e35269919737e74564a056bd782f
Wes Higbee <wes.mcclure@gmail.com>
2026-05-08 08:45:21 -0500

    easy starting point for nix learning/testing

--- nix/mac/flake.nix
+++ nix/mac/flake.nix
@@ -1,15 +1,44 @@
 {
-  description = "A very basic flake";
+  description = "Wes dev environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "aarch64-darwin"; # Apple Silicon
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+      packages.${system}.default = pkgs.buildEnv {
+        name = "wes-tools";
+        paths = [
+          pkgs.fd
+          pkgs.ripgrep
+          pkgs.fzf
+          pkgs.bat
+          pkgs.eza
+          pkgs.jq
+          pkgs.yq
+          pkgs.hyperfine
+          pkgs.difftastic
+          pkgs.delta
+          pkgs.httpie
+          pkgs.glow
+          pkgs.tmux
+          pkgs.zellij
+          pkgs.shellcheck
+          pkgs.shfmt
+          pkgs.lua-language-server
+          pkgs.tree-sitter
+          # infrastructure for editing flakes (etc):
+          pkgs.nixfmt # nix fmt fails for formatting currently with coc... doc is wiped (server says to erase entire doc on format so likely `nix fmt` is not outputing the updated document over STDOUT ... likely need to pass different args to fix?
+          pkgs.nix
+        ];
+      };
+    };
 }
