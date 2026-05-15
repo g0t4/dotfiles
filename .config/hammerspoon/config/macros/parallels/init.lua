8007ea0022ccb4923d284b196f08f8d5b0abcbae
Wes Higbee <wes.mcclure@gmail.com>
2026-05-15 17:51:44 -0500

    renames

--- .config/hammerspoon/config/macros/parallels/init.lua
+++ .config/hammerspoon/config/macros/parallels/init.lua
@@ -1,4 +1,27 @@
-function ParallelsX_control_center()
+function Parallels_CloseFocusedWindow()
+    -- because Parallels Desktop, in their fucking infinite wisdom, decided to disable Cmd+W for all windows in the app
+    --  and the app constantly pops up the fucking "make a new VM wysiwg fucking window every time you remove your last VM... so all the time when using packer to build new VMs..."
+    --  and you cannot close it with cmd fucking W
+    local win = hs.window.focusedWindow()
+    if not win then
+        return false
+    end
+
+    local ax = hs.axuielement.windowElement(win)
+    if not ax then
+        return false
+    end
+
+    local close = ax:attributeValue("AXCloseButton")
+    if close then
+        close:performAction("AXPress")
+        return true
+    end
+
+    return false
+end
+
+function Parallels_ControlCenter()
     local app = hs.application.get("Parallels Desktop")
     if not app then
         print("Parallels Desktop not running")
