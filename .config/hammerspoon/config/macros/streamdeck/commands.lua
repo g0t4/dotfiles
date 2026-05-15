4c7e699f4cb1790bf99a41d541103afba25e21d0
gptoss120b <wes.mcclure+gptoss120b@gmail.com>
2026-05-15 09:35:39 -0500

    Add global ParallelsX_control_center function for Streamdeck

--- .config/hammerspoon/config/macros/streamdeck/commands.lua
+++ .config/hammerspoon/config/macros/streamdeck/commands.lua
@@ -1,5 +1,21 @@
 local verbose = require("config.macros.streamdeck.helpers").verbose
 
+-- Activate Parallels Desktop Control Center menu item
+-- Uses hs.application to find Parallels Desktop and selects the "Control Center" menu item.
+function ParallelsX_control_center()
+    local app = hs.application.get("Parallels Desktop")
+    if not app then
+        verbose("Parallels Desktop not running")
+        return
+    end
+    -- Find the menu path; typical menu item is under the app's menu bar: "Window" -> "Control Center"
+    local menuPath = {"Window", "Control Center"}
+    local success = app:selectMenuItem(menuPath)
+    if not success then
+        verbose("Failed to select Control Center menu item")
+    end
+end
+
 function runCommand(cmd)
     -- AVOID hs.execute and os.execute... PITA, can't see STDERR or not easily
     local handle = io.popen(cmd .. " 2>&1") -- Redirects STDERR to STDOUT
