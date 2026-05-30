782ceb97c941e2871d6ba29df165e6634e8917b9
Wes Higbee <wes.mcclure@gmail.com>
2026-05-30 02:03:02 -0500

    move

--- fish/load_last_interactive_only/always/always.fish
+++ fish/load_last_interactive_only/always/always.fish
@@ -90,7 +90,6 @@ end
 abbr ba browse_traces agents
 abbr br browse_traces rewrite
 abbr bf browse_traces fim
-abbr ps pii_scanner
 function browse_traces
     set _python3 "$ASK_REPO/.venv/bin/python3"
     env PYTHONPATH="$ASK_REPO" $_python3 -m tools.chat_viewer.browser $argv
@@ -106,6 +105,7 @@ function view_trace
     env PYTHONPATH="$ASK_REPO" $_python3 -m tools.chat_viewer.__main__ $argv
 end
 
+abbr ps pii_scanner
 function pii_scanner
     # Run the PII scanner tool using the module namespace.
     # tools is now a proper Python package, so we invoke the module with
