2f59c99b90867a2b51ca3d86db972a532bd2ed99
Wes Higbee <wes.mcclure@gmail.com>
2026-06-07 09:28:59 -0500

    styling message types

--- .config/nvim/lua/non-plugins/treesitter/qwen_chatml.lua
+++ .config/nvim/lua/non-plugins/treesitter/qwen_chatml.lua
@@ -13,3 +13,11 @@ vim.api.nvim_set_hl(0, "@qwen_repo_name", { fg = "#ffe66d", bold = true })
 vim.api.nvim_set_hl(0, "@qwen_file_sep_token", { fg = "#89dceb" })
 vim.api.nvim_set_hl(0, "@qwen_file_path", { fg = "#89dceb", bold = true, underline = true })
 vim.api.nvim_set_hl(0, "@qwen_file_contents", { fg = "#89dceb" })
+
+-- message types
+vim.api.nvim_set_hl(0, "@qwen_system_message", { fg = "#ff6b6b", bold = true, })
+vim.api.nvim_set_hl(0, "@qwen_developer_message", { fg = "#f7b267", bold = true, })
+vim.api.nvim_set_hl(0, "@qwen_user_message", { fg = "#61afef", })
+vim.api.nvim_set_hl(0, "@qwen_assistant_message", { fg = "#98c379", })
+vim.api.nvim_set_hl(0, "@qwen_tool_response_message", { fg = "#c678dd", })
+vim.api.nvim_set_hl(0, "@qwen_all_other_roles_message", { fg = "#7f848e", italic = true, })
