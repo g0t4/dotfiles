f987dee1832d6bdaed2576428c08aa8c6b8b68fe
Wes Higbee <wes.mcclure@gmail.com>
2026-06-20 19:19:24 -0500

    I don't need these verbose messages anymore

--- .config/git/hooks/core.fish
+++ .config/git/hooks/core.fish
@@ -3,20 +3,20 @@
 set repo (git rev-parse --show-toplevel 2>/dev/null)
 
 if not string match --quiet --regex "/(wes[^/]+|g0t4)/[^/]+\$" $repo
-    echo not g0t4 repo, skipping...
+    # echo not g0t4 repo, skipping...
     return
 end
 
 if test -d "$repo/.ctags.d"; and test -f Makefile
     # assume Makefile target for tags
-    rich --print "[#FF2D55 bold] make tags"
+    # echo "make tags"
     make tags
 end
 
 if test -f "$repo/.rag.yaml"
     # convention => .rag.yaml config file
     #  that way I explicitly have to make the file and opt-in
-    rich --print "[#FF2D55 bold] rag_indexer"
+    # echo "rag_indexer"
 
     #  PRN I should have an option inside to toggle off the auto rag_indexer... that I think can go inside the python code
     #     heck even this check could go into the python code
