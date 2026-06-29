b5495cb41f09b9b123f1e97a4aef8468bf7b3c19
Wes Higbee <wes.mcclure@gmail.com>
2026-06-29 12:05:32 -0500

    tinker

--- .config/hammerspoon/config/macros/screenpal/windows/busted.hs.test.lua
+++ .config/hammerspoon/config/macros/screenpal/windows/busted.hs.test.lua
@@ -1,7 +1,12 @@
-local log = require("config.logs").hammerspoons()
+require("config.hs_harness.busted") -- PRN assume loaded by hs init?
 
 describe("FOO", function()
     it("dammit", function()
+        local log = require("config.logs").hammerspoons()
         log:info("TESTING WORKS")
+        assert.same("bar", true)
     end)
 end)
+
+-- failure to STDOUT => float window in nvim for <leader>hs
+assert.same("fuuu", true)
