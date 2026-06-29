5784b9e6df6db4a39bf0f704d4211931255299cc
Wes Higbee <wes.mcclure@gmail.com>
2026-06-29 13:09:56 -0500

    testing

--- .config/hammerspoon/config/macros/screenpal/windows/test.hs.lua
+++ .config/hammerspoon/config/macros/screenpal/windows/test.hs.lua
@@ -15,10 +15,10 @@ local AppWindows = require("config.macros.screenpal.app_windows")
 -- PRN setup facade to get at controls in spal app
 local app = get_app_element_or_throw("com.screenpal.app")
 local wins = AppWindows.new(app)
-wins:_refresh()
-log:info(wins)
-local tool_win = wins:get_tool_bar_window()
-log:info(tool_win)
+-- wins:_refresh()
+-- log:info(wins)
+-- local tool_win = wins:get_tool_bar_window()
+-- log:info(tool_win)
 
 -- switch to screenpal
 app:asHSApplication():activate()
