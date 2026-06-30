6bfb9509deb0c8b647f1b3921b14c93675d01536
Wes Higbee <wes.mcclure@gmail.com>
2026-06-30 07:07:35 -0500

    testing SPAL

--- .config/hammerspoon/config/macros/screenpal/windows/test.hs.lua
+++ .config/hammerspoon/config/macros/screenpal/windows/test.hs.lua
@@ -23,16 +23,16 @@ local wins = AppWindows.new(app)
 -- switch to screenpal
 app:asHSApplication():activate()
 
--- -- * test v + mute => working fast and fine
--- -- type v key
-hs.eventtap.keyStroke({}, "v") -- open volume tool in the silence under cursor... wow it works fast and good
--- --
-local volume_menu = require("config.macros.screenpal.windows.volume_menu").new(wins)
-log:info(volume_menu)
-volume_menu:wait_for_volume_to_be_muted() -- TODO anything in here that would not wait appropriately to pull off the volume menu clicking to mute? .. i.e. skip waiting for submenu to appear and just rely on first access to work?
+-- -- -- * test v + mute => working fast and fine
+-- -- -- type v key
+-- hs.eventtap.keyStroke({}, "v") -- open volume tool in the silence under cursor... wow it works fast and good
+-- -- --
+-- local volume_menu = require("config.macros.screenpal.windows.volume_menu").new(wins)
+-- log:info(volume_menu)
+-- volume_menu:wait_for_volume_to_be_muted() -- TODO anything in here that would not wait appropriately to pull off the volume menu clicking to mute? .. i.e. skip waiting for submenu to appear and just rely on first access to work?
 
 -- * test act_on_silence(MUTE_INWARD)
--- SPal_ActOnThisSilence('MUTE_INWARD') -- streamdeck button triggers this
+SPal_ActOnThisSilence('MUTE_INWARD') -- streamdeck button triggers this
 -- TODO wait for this to be slow again... right now mute is super fast even though I haven't accepted a mute edit so the mute button has to be changed every time... and all are curently fast ... will see if delays creep up later
 
 -- TODO! just use this script style for now is FINE!
