9f09e5f73a52f22337b26ab7b21744fb39b1f12d
Wes Higbee <wes.mcclure@gmail.com>
2026-07-19 01:19:55 -0500

    ansible via pipx

---
+++ install/pipx.installs.fish
@@ -0,0 +1,10 @@
+# ansible
+pipx install --include-deps ansible
+# --include-deps is for ansible-core to install ansible-* executables
+
+# further executables:
+pipx inject --include-apps ansible ansible-lint argcomplete
+
+# libraries only:
+pipx inject --include-apps ansible requests
+# tomli-w
