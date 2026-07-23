4744b5b69d96dd3a4f36c2f034f96a568605697c
Wes Higbee <wes.mcclure@gmail.com>
2026-07-23 00:19:04 -0500

    fuk seriously this entire MCP fucking SDK has no motherfucking introspection on a fucking failure... we're just going to SHIT IN YOUR FACE ABOUT FUCKING PYDANTIC NIPPLE TWISTING AND THEN TELL YOU NOTHING NOTHING NOTHING ABOUT WHAT WAS SENT OR CAUSED THIS.../ FUCk

--- fish/load_last_interactive_only/always/my_ai.fish
+++ fish/load_last_interactive_only/always/my_ai.fish
@@ -126,7 +126,8 @@ function mcp_server_semantic_grep
     # BTW I prefer this wrapper approach vs pyproject.toml + project.scripts... these work without any install other than creating the venv initially
     #  then as the end user, I don't have to even think about venv/paths
     set _python3 "$ASK_REPO/.venv/bin/python3"
-    env PYTHONPATH="$ASK_REPO/lua/ask-openai/rag" $_python3 -m mcp_server.__main__ $argv
+    tee /tmp/shit_not_working | env PYTHONPATH="$ASK_REPO/lua/ask-openai/rag" $_python3 -m mcp_server.__main__ $argv
+    # env PYTHONPATH="$ASK_REPO/lua/ask-openai/rag" $_python3 -m mcp_server.__main__ $argv
 end
 complete -c mcp_server_semantic_grep --no-files
 complete -c mcp_server_semantic_grep \
