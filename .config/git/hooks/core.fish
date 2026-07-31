4f2592075608190b365aa860b1b0884f799dd36c
Wes Higbee <wes.mcclure@gmail.com>
2026-07-31 14:26:48 -0500

    cleanup

--- .config/git/hooks/core.fish
+++ .config/git/hooks/core.fish
@@ -9,14 +9,12 @@ end
 
 if test -d "$repo/.ctags.d"; and test -f Makefile
     # assume Makefile target for tags
-    # echo "make tags"
     make tags
 end
 
 if test -f "$repo/.rag.yaml"
     # convention => .rag.yaml config file
     #  that way I explicitly have to make the file and opt-in
-    # echo "rag_indexer"
 
     #  PRN I should have an option inside to toggle off the auto rag_indexer... that I think can go inside the python code
     #     heck even this check could go into the python code
