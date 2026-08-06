a02858db77e34e2f589fc5807b2a665e33337d18
Wes Higbee <wes.mcclure@gmail.com>
2026-08-06 00:16:45 -0500

    move to fix keymap

--- .config/nvim/lua/non-plugins/quickfixs.lua
+++ .config/nvim/lua/non-plugins/quickfixs.lua
@@ -25,24 +25,6 @@ function open_hammerspoon_failure_in_quickfix()
     }):start()
 end
 
--- `q` as in [q]uickfix
--- TODO move this somewhere else too, was gonna use <leader>h and hence put here but I want <leader>q now
-vim.keymap.set('n', "<leader>qhs", open_hammerspoon_failure_in_quickfix)
-
--- FYI in treesitter you can open in quick fix with ctrl-q and then use these for navigating items:
--- quickfix item navigation:
-vim.keymap.set('n', "<leader>qn", function() vim.cmd('cnext') end) -- next item in quickfix
-vim.keymap.set('n', "<leader>qp", function() vim.cmd('cprev') end) -- prev item in quickfix
--- move by file:
-vim.keymap.set('n', "<leader>qnf", function() vim.cmd('cnfile') end)
-vim.keymap.set('n', "<leader>qpf", function() vim.cmd('cpfile') end)
-
-vim.keymap.set('n', "<leader>qp", function() vim.cmd('cprev') end) -- prev item in quickfix
-vim.keymap.set('n', "<leader>qc", set_quickfix_from_clipboard_AUTO_DETECT)
-
-
--- TODO last failure => AskAgent :)
-
 -- * WIP for quick fix / location-list
 function set_quickfix_from_clipboard_lua_error()
     local text = fix_clipboard_lua_error_paths()
@@ -106,3 +88,22 @@ function set_quickfix_from_clipboard_IIRC_HAMMERSPOON(reg)
     })
     vim.cmd("copen")
 end
+
+-- `q` as in [q]uickfix
+-- TODO move this somewhere else too, was gonna use <leader>h and hence put here but I want <leader>q now
+vim.keymap.set('n', "<leader>qhs", open_hammerspoon_failure_in_quickfix)
+
+-- FYI in treesitter you can open in quick fix with ctrl-q and then use these for navigating items:
+-- quickfix item navigation:
+vim.keymap.set('n', "<leader>qn", function() vim.cmd('cnext') end) -- next item in quickfix
+vim.keymap.set('n', "<leader>qp", function() vim.cmd('cprev') end) -- prev item in quickfix
+-- move by file:
+vim.keymap.set('n', "<leader>qnf", function() vim.cmd('cnfile') end)
+vim.keymap.set('n', "<leader>qpf", function() vim.cmd('cpfile') end)
+
+vim.keymap.set('n', "<leader>qp", function() vim.cmd('cprev') end) -- prev item in quickfix
+vim.keymap.set('n', "<leader>qc", set_quickfix_from_clipboard_AUTO_DETECT)
+
+
+-- TODO last failure => AskAgent :)
+
