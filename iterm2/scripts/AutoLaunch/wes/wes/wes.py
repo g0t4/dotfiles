8d3aa2c2ff688b4372748311d46121e33ac98344
Wes Higbee <wes.mcclure@gmail.com>
2026-08-23 22:32:19 -0500

    rename

--- iterm2/scripts/AutoLaunch/wes/wes/wes.py
+++ iterm2/scripts/AutoLaunch/wes/wes/wes.py
@@ -41,24 +41,24 @@ async def main(connection: iterm2.Connection):
         #     also Cmd+T => Cmd+Shift+Control+T
         # *** New Window helpers
         # FYI also had to remap Cmd+N => Cmd+Shift+Control+N in Keyboard Maestro
-        n = keystroke.keycode == iterm2.Keycode.ANSI_N
-        if n and control and command and shift:
+        key_n = keystroke.keycode == iterm2.Keycode.ANSI_N
+        if key_n and control and command and shift:
             await wes_new_window(connection, force_local=False)
             return
         #
-        if n and control and command:
+        if key_n and control and command:
             await wes_new_window(connection, force_local=True)
             return
 
         # *** New Tab helpers
         # FYI also had to remap Cmd+T => Cmd+Shift+Control+T in Keyboard Maestro
-        t = keystroke.keycode == iterm2.Keycode.ANSI_T
-        if t and control and command and shift:
+        key_t = keystroke.keycode == iterm2.Keycode.ANSI_T
+        if key_t and control and command and shift:
             await wes_new_tab(connection, force_local=False)
             return
         # FYI also had to remap Cmd+Shift+T in KM => Cmd+Ctrl+T
         #    cannot remap to same keys, wouldn't work :)
-        if t and control and command:
+        if key_t and control and command:
             await wes_new_tab(connection, force_local=True)
             return
 
