741793da1ce35f117a161f317657c8e573460035
Wes Higbee <wes.mcclure@gmail.com>
2026-08-28 06:41:01 -0500

    Add StreamDeckITerm2ScriptingInspector macro to open/refresh iTerm2 Scripting Inspector

--- .config/hammerspoon/config/macros/iterm/init.lua
+++ .config/hammerspoon/config/macros/iterm/init.lua
@@ -26,3 +26,36 @@ function NeovimAskToggleRag()
     --     F13/F16/17 (etc)
     NeovimExecCommand("lua require(\"ask-openai.config\").toggle_rag()")
 end
+
+function StreamDeckITerm2ScriptingInspector()
+    -- Open iTerm2's Scripting Inspector (or refresh it if already open)
+    -- PRN refactor to AXUIElement, ported as-is for commit history
+    local script = [[
+        tell application "System Events"
+
+            set proc to application process "iTerm2"
+            set inspectorWindow to a reference to window "Scripting Inspector" of proc
+            if not (exists inspectorWindow) then
+                set consoleWindow to a reference to window "Scripting Console" of proc
+                if not (exists consoleWindow) then
+                    -- app:menuBar(1):menuBarItem(7):menu(1):menuItem(1):menu(1):menuItem(13)
+                    set consoleMenuItem to a reference to menu item "Console" of menu ¬
+                        "Manage" of menu item "Manage" of menu "Scripts" of menu bar 1 of proc
+                    click consoleMenuItem
+                end if
+                set Inspector to button "Inspector" of window "Script Console" of ¬
+                    application process "iTerm2"
+                click Inspector
+            else
+                perform action "AXRaise" of inspectorWindow
+                click (first button of inspectorWindow whose description is "refresh")
+                -- REFRESH BUTTON
+                --   app:window(1):button(1)
+                --   AXDescription: refresh<string>
+                --   unique ref: app:window('Scripting Inspector'):button('')
+            end if
+
+        end tell
+    ]]
+    hs.osascript.applescript(script)
+end
