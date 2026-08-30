d41eb5b983be3950f076b415a2d113809f1436d4
Wes Higbee <wes.mcclure@gmail.com>
2026-08-30 17:46:30 -0500

    auto lazy load rich module on first use... no need to `import rich`

---
+++ .config/xonsh/rc.d/_repl_convenience.py
@@ -0,0 +1,32 @@
+# ~/.xonshrc
+from xonsh.lib.lazyasd import LazyObject
+import importlib
+
+# FYI all xonsh startup files ~/.config/xonsh/rc.d/* share one namespace
+#  thus you get the appearance of globals
+#  this is unlike python where each module (file) has its own namespace
+
+# * lazy loaded auto-imports
+# FYI if any issues with these, just eager load them if they're small (i.e. rich is small enough)
+
+rich = LazyObject(lambda: importlib.import_module("rich"), globals(), "rich")
+inspect = LazyObject(lambda: rich.inspect, globals(), "inspect")
+#   => `inspect(@)`
+
+
+# * maybes
+# httpx = LazyObject(lambda: importlib.import_module("httpx"), globals(), "httpx")
+# np = LazyObject(lambda: importlib.import_module("numpy"), globals(), "np")
+# plt = LazyObject(lambda: importlib.import_module("matplotlib.pyplot"), globals(), "plt")
+
+# * startup modules use the following so there's no need to auto import (unless I no longer import them in other startup files):
+# Path = LazyObject(lambda: importlib.import_module("pathlib").Path, globals(), "Path")
+# re = LazyObject(lambda: importlib.import_module("re"), globals(), "re")
+# subprocess = LazyObject(lambda: importlib.import_module("subprocess"), globals(), "subprocess")
+# os = LazyObject(lambda: importlib.import_module("os"), globals(), "os")
+# sys = LazyObject(lambda: importlib.import_module("sys"), globals(), "sys")
+# json = LazyObject(lambda: importlib.import_module("json"), globals(), "json")
+
+
+# def dottable(obj):
+#     if isinstance( obj, dict):
