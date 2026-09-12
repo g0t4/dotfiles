7a1a2fbd47e3356edb09e3da312dc38dbaf1a802
Wes Higbee <wes.mcclure@gmail.com>
2026-09-12 06:10:42 -0500

    fix logging and async timeout on semantic click handler on neovim quit

--- .config/nvim/lua/non-plugins/werkspaces/semantic-client.lua
+++ .config/nvim/lua/non-plugins/werkspaces/semantic-client.lua
@@ -1,12 +1,10 @@
-
-
-
-
 local os = require("os")
 local uv = require("luv")
 -- ? does it matter if I use luv or vim.uv?
+local log = require('devtools.logs.logger').universal()
 
 local socket_path = os.getenv("HOME") .. "/.config/wes-iterm2/run/semantic-click-handler.sock"
+local notify_timeout_ms = 500
 
 local M = {}
 
@@ -14,43 +12,40 @@ function M.NotifyDaemonOfSessionQuit()
     local session_id = os.getenv("ITERM_SESSION_ID")
 
     if session_id == nil then
-        print("No session id, aborting...")
+        log:warn("[semantic-client] No session id, aborting...")
         return
     end
 
+    local done_or_failed = false
     local client = uv.new_pipe(false)
 
-
-    local function log_file(msg)
-        -- disable for now, can re-enable if needed
-        if true then
+    local function finish(err)
+        if done_or_failed then
             return
         end
-
-        local path = os.getenv("HOME") .. "/.config/wes-iterm2/semantic-client.log"
-        local file = io.open(path, "a")
-        if not file then
-            print("Error opening log file: " .. path)
-            return
+        done_or_failed = true
+        if err ~= nil then
+            log:error("[semantic-client]", err)
         end
-        file:write(msg .. "\n")
-        file:close()
+        if client ~= nil then
+            client:read_stop()
+            client:close()
+        end
     end
 
-    log_file("Starting client...")
-
-    local done_or_failed = false
+    log:info("[semantic-client] notifying server of quit")
 
     client:connect(socket_path, function(err)
-        log_file("Connected to socket...")
+        if err then
+            finish("Error connecting to socket: " .. err)
+            return
+        end
+
+        log:info("Connected to socket")
         client:write(session_id, function(write_err)
-            log_file("Wrote session_id: " .. session_id)
+            log:info("[semantic-client] Wrote session_id", session_id)
             if write_err then
-                log_file("Error writing to socket: " .. write_err)
-                print("Error writing to socket: " .. write_err)
-                done_or_failed = true
-                client:close()
-                uv.stop()
+                finish("[semantic-client] Error writing to socket: " .. write_err)
                 return
             end
             print("Wrote session_id: " .. session_id)
@@ -58,33 +53,36 @@ function M.NotifyDaemonOfSessionQuit()
             -- TODO oh yeah derp... I need a response from the server else I start closing the window before it is done getting frame info (hence that error)
             -- FYI could have server send back messages... but let's just go with iterm2's console logs (for daemon)
             client:read_start(function(read_err, data)
-                log_file("Received data: " .. data)
+                log:info("[semantic-client] Received data: ", data)
                 if read_err then
-                    print("Error reading from socket: " .. read_err)
-                    done_or_failed = true
-                    client:close()
-                    uv.stop()
+                    finish("[semantic-client] Error reading from socket: " .. read_err)
+                    return
+                end
+
+                if data == nil then
+                    -- EOF without DONE should still unblock shutdown.
+                    finish()
                     return
                 end
 
                 if data == "DONE" then
-                    -- PRN any scenario where all of DONE isn't immediately received in one go? IOTW accumulate a buffer across read callbacks
-                    done_or_failed = true
-                    client:close()
-                    uv.stop()
+                    -- PRN any scenario where all of DONE isn't immediately received in one go?
+                    -- IOTW accumulate a buffer across read callbacks
+                    finish()
                 end
             end)
         end)
     end)
 
     -- PRN switch to a synchronous socket library (luarocks install luasocket?)... no reason for the client to be async
-    -- TODO how about add a timeout to bail if it takes too long?... i.e. if server never responds w/ message
-    while not done_or_failed do
-        -- FYI, first call to uv.run() returns after client:write and before client:read_start is called... so I have to call it again
-        -- so, use a boolean now to check if done and loop on uv.run()... works good
-        -- FYI also uv.sleep(10) fixes the race condition too...
-        -- basically once this exits my callbacks are doomed b/c the iterm window is nuked before the server is done gathering window state
-        uv.run("default")
+
+    local timed_out = not vim.wait(notify_timeout_ms, function()
+        -- check if complete (done or fail)
+        return done_or_failed
+    end, 10)
+
+    if timed_out then
+        finish("Timed out waiting for semantic daemon response")
     end
 end
 
