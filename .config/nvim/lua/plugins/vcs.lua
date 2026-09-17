6d55921df00c0c017aa283312acc1e1bb6a83f01
Wes Higbee <wes.mcclure@gmail.com>
2026-09-17 01:24:40 -0500

    wait, move to auto defined events to lazy load vs NONE ... that way I can always :Lazy load

--- .config/nvim/lua/plugins/vcs.lua
+++ .config/nvim/lua/plugins/vcs.lua
@@ -16,11 +16,11 @@ local plugins = {
         -- cons: adjusting the gutter can be annoying/jarring when you make a first change
 
         'lewis6991/gitsigns.nvim',
-        event = 'BufRead', -- * COMMENT OUT for ONLY LAZY LOADING
+        enabled = true,
         lazy = true, -- always true so it can be conditionally loaded (if not auto on event)
         -- can I lazy load depending on buffer read's file's path?
 
-        enabled = function()
+        event = function()
             local cwd = vim.fn.getcwd()
             -- if dir has "course-" in the path anywhere then assume it is a course recording
             local in_course_dir = cwd:find("/course-") ~= nil
@@ -28,7 +28,11 @@ local plugins = {
             -- local log = require("devtools.logs.logger").universal()
             -- log:info("cwd", cwd, "in_course_dir", in_course_dir)
 
-            return not in_course_dir
+            if not in_course_dir then
+                -- allow auto load when not in course repo
+                return {'BufRead'}
+            end
+            return {''}
         end,
 
 
