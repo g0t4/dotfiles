325910e8da8b098de5bca5467ee68fd7351cdc79
Wes Higbee <wes.mcclure@gmail.com>
2026-09-18 05:07:10 -0500

    re-gen w/o line numbers

--- .config/xonsh/lib/wes_files_abbreviations.py
+++ .config/xonsh/lib/wes_files_abbreviations.py
@@ -41,58 +41,58 @@ def _ask_status(_context, _match):
 
 
 def register_files_abbreviations():
-    abbr('lat', 'ls -alht')  # Fish line 2
-    abbr('las', 'ls -alhS')  # Fish line 3
-    abbr('la', 'ls -alh')  # Fish line 4
-    abbr(re.compile('^cd\\.\\.+$'), _expand_dots_command)  # Fish line 47
-    abbr(re.compile('^\\.\\.+$'), _expand_dots_only, position="anywhere")  # Fish line 49
-    abbr(re.compile('^\\.\\.+$'), _expand_dots_command)  # Fish line 53
-    abbr('cd-', 'cd -')  # Fish line 56
-    abbr('cpr', 'cp -r')  # Fish line 59
-    abbr('lns', 'ln -s')  # Fish line 60
-    abbr('ask_status', _ask_status)  # Fish line 72
-    abbr('touch', 'touchp')  # Fish line 136
-    abbr('mkfile', 'touchp')  # Fish line 137
-    abbr('mkdir', 'mkdir -p')  # Fish line 194
-    abbr(re.compile('=[^\\b]+'), _expand_zsh_equals, position="anywhere")  # Fish line 219
-    abbr('cdm', 'cd_dir_of_man_page')  # Fish line 240
-    abbr('cdbrew', 'cd_dir_of_brew_pkg')  # Fish line 256
-    abbr('cdc', 'cd_dir_of_command')  # Fish line 261
-    abbr('cdd', 'cd_dir_of_path')  # Fish line 289
-    abbr('cdl', 'cd_last_dir__in_current_dir')  # Fish line 291
-    abbr('bath', 'bat --style=header')  # Fish line 319
-    abbr('batf', 'bat --style=full')  # Fish line 320
-    abbr(re.compile('(du|dust)\\d+'), _fish_abbreviation('dustX'))  # Fish line 494
-    abbr('dust_HOME_2G', 'dust --number-of-lines 500 ~/ +2G')  # Fish line 500
-    abbr('dust_HOMES_2G', 'dust --number-of-lines 500 /Users +2G')  # Fish line 501
-    abbr('dust_ROOT_10G', 'dust --number-of-lines 500 / +10G')  # Fish line 502
-    abbr('dust_HOME_recent_100M', 'dust --number-of-lines 500 --mtime -3 ~/ +100M')  # Fish line 503
-    abbr('dust_HOME_old_100M', 'dust --number-of-lines 500 --mtime +90 ~/ +100M')  # Fish line 504
-    abbr('-n', '--number-of-lines 500', position="anywhere", commands=('dust',))  # Fish line 508
-    abbr('-M', '--mtime +7 # greater than 7 days ago', position="anywhere", commands=('dust',))  # Fish line 509
-    abbr('dust_past_week', 'dust --mtime -7')  # Fish line 510
-    abbr('dust_past_month', 'dust --mtime -30')  # Fish line 511
-    abbr('df', 'grc df -h')  # Fish line 515
-    abbr('dfm', 'grc df -h /System/Volumes/Data')  # Fish line 517
-    abbr(re.compile('forr\\d*'), _fish_abbreviation('forr_abbr'))  # Fish line 526
-    abbr('findd', 'find . -type d -iname "*%*"', cursor_marker="%")  # Fish line 540
-    abbr('finddr', 'find . -type d -iregex ".*%.*"', cursor_marker="%")  # Fish line 541
-    abbr(re.compile('tree\\d+'), _fish_abbreviation('treeX'))  # Fish line 616
-    abbr(re.compile('treed\\d+'), _fish_abbreviation('treedX'))  # Fish line 621
-    abbr(re.compile('treeh\\d+'), _fish_abbreviation('treehX'))  # Fish line 626
-    abbr(re.compile('treeu\\d+'), _fish_abbreviation('treeuX'))  # Fish line 631
-    abbr('nvim_start_server_attached', 'nvim --listen localhost:6666')  # Fish line 660
-    abbr('nvim_start_server_not_attached', 'nvim --listen localhost:6666 --embed')  # Fish line 661
-    abbr('nvim_client_attach_ui', 'nvim --server localhost:6666 --remote-ui')  # Fish line 663
-    abbr('nvim_client_send_command', 'nvim --server localhost:6666 --remote-send')  # Fish line 664
-    abbr('nvim_client_eval_expr', 'nvim --server localhost:6666 --remote-expr')  # Fish line 665
-    abbr('nvim_client_open_files_in_new_tabs', 'nvim --server localhost:6666 --remote-tab')  # Fish line 666
-    abbr('f', 'fish')  # Fish line 672
-    abbr('x', 'xonsh')  # Fish line 673
-    abbr('n', 'nvim')  # Fish line 676
-    abbr('nr', _fish_abbreviation('nr_expand'))  # Fish line 677
-    abbr('nd', _fish_abbreviation('nd_expand'))  # Fish line 685
-    abbr('nh', _fish_abbreviation('nh_expand'))  # Fish line 693
-    abbr('nn', _fish_abbreviation('nn_expand'))  # Fish line 701
-    abbr('chmx', 'chmod +x')  # Fish line 952
-    abbr('chmR', 'chmod -R')  # Fish line 953
+    abbr('lat', 'ls -alht')
+    abbr('las', 'ls -alhS')
+    abbr('la', 'ls -alh')
+    abbr(re.compile('^cd\\.\\.+$'), _expand_dots_command)
+    abbr(re.compile('^\\.\\.+$'), _expand_dots_only, position="anywhere")
+    abbr(re.compile('^\\.\\.+$'), _expand_dots_command)
+    abbr('cd-', 'cd -')
+    abbr('cpr', 'cp -r')
+    abbr('lns', 'ln -s')
+    abbr('ask_status', _ask_status)
+    abbr('touch', 'touchp')
+    abbr('mkfile', 'touchp')
+    abbr('mkdir', 'mkdir -p')
+    abbr(re.compile('=[^\\b]+'), _expand_zsh_equals, position="anywhere")
+    abbr('cdm', 'cd_dir_of_man_page')
+    abbr('cdbrew', 'cd_dir_of_brew_pkg')
+    abbr('cdc', 'cd_dir_of_command')
+    abbr('cdd', 'cd_dir_of_path')
+    abbr('cdl', 'cd_last_dir__in_current_dir')
+    abbr('bath', 'bat --style=header')
+    abbr('batf', 'bat --style=full')
+    abbr(re.compile('(du|dust)\\d+'), _fish_abbreviation('dustX'))
+    abbr('dust_HOME_2G', 'dust --number-of-lines 500 ~/ +2G')
+    abbr('dust_HOMES_2G', 'dust --number-of-lines 500 /Users +2G')
+    abbr('dust_ROOT_10G', 'dust --number-of-lines 500 / +10G')
+    abbr('dust_HOME_recent_100M', 'dust --number-of-lines 500 --mtime -3 ~/ +100M')
+    abbr('dust_HOME_old_100M', 'dust --number-of-lines 500 --mtime +90 ~/ +100M')
+    abbr('-n', '--number-of-lines 500', position="anywhere", commands=('dust',))
+    abbr('-M', '--mtime +7 # greater than 7 days ago', position="anywhere", commands=('dust',))
+    abbr('dust_past_week', 'dust --mtime -7')
+    abbr('dust_past_month', 'dust --mtime -30')
+    abbr('df', 'grc df -h')
+    abbr('dfm', 'grc df -h /System/Volumes/Data')
+    abbr(re.compile('forr\\d*'), _fish_abbreviation('forr_abbr'))
+    abbr('findd', 'find . -type d -iname "*%*"', cursor_marker="%")
+    abbr('finddr', 'find . -type d -iregex ".*%.*"', cursor_marker="%")
+    abbr(re.compile('tree\\d+'), _fish_abbreviation('treeX'))
+    abbr(re.compile('treed\\d+'), _fish_abbreviation('treedX'))
+    abbr(re.compile('treeh\\d+'), _fish_abbreviation('treehX'))
+    abbr(re.compile('treeu\\d+'), _fish_abbreviation('treeuX'))
+    abbr('nvim_start_server_attached', 'nvim --listen localhost:6666')
+    abbr('nvim_start_server_not_attached', 'nvim --listen localhost:6666 --embed')
+    abbr('nvim_client_attach_ui', 'nvim --server localhost:6666 --remote-ui')
+    abbr('nvim_client_send_command', 'nvim --server localhost:6666 --remote-send')
+    abbr('nvim_client_eval_expr', 'nvim --server localhost:6666 --remote-expr')
+    abbr('nvim_client_open_files_in_new_tabs', 'nvim --server localhost:6666 --remote-tab')
+    abbr('f', 'fish')
+    abbr('x', 'xonsh')
+    abbr('n', 'nvim')
+    abbr('nr', _fish_abbreviation('nr_expand'))
+    abbr('nd', _fish_abbreviation('nd_expand'))
+    abbr('nh', _fish_abbreviation('nh_expand'))
+    abbr('nn', _fish_abbreviation('nn_expand'))
+    abbr('chmx', 'chmod +x')
+    abbr('chmR', 'chmod -R')
