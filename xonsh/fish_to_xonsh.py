e9046754ed8398a7f0ebc676f5a894df537dccf7
Wes Higbee <wes.mcclure@gmail.com>
2026-09-25 03:42:52 -0500

    notes

--- xonsh/fish_to_xonsh.py
+++ xonsh/fish_to_xonsh.py
@@ -99,6 +99,9 @@ from wes_fish_migration import (
     unsupported_abbreviation,
 )
 
+# FYI defining this globally clobbers the value for all other modules that came before... b/c there's only one namespace...
+# SO, keep in mind, if you try to check this later it will have FISH_FUNCTIONS values from the last module loaded that defined it...
+# IOTW only call register immediately and use it right away... IDEALLY lets just inline the list into register function
 FISH_FUNCTIONS = (
 {"".join(f"    {name!r},\n" for name in functions)}\
 )
